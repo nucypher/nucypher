40345c340c7b7a655a49be3460c7dbd8b1a86638
Michael Egorov <michwill@gmail.com>
2017-09-12 23:54:15 -0700

    [KMS-ETH]- Token transfers work

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,5 +1,10 @@
+import pytest
+from ethereum.tester import TransactionFailed
+
+
 def test_create_token(web3, chain):
     creator = web3.eth.accounts[1]
+    account1 = web3.eth.accounts[0]
 
     # Create an ERC20 token
     token, txhash = chain.provider.get_or_deploy_contract(
@@ -9,13 +14,23 @@ def test_create_token(web3, chain):
                 'from': creator})
     assert txhash is not None
 
-    # Check balance of our (test) account which created the token
-    # We created the token using coinbase account
-    # Other accounts pre-loaded with ethers are at:
-    # web3.eth.accounts
+    # Account balances
     assert token.call().balanceOf(creator) == 10 ** 9
-    assert token.call().balanceOf(web3.eth.accounts[0]) == 0
+    assert token.call().balanceOf(account1) == 0
 
+    # Basic properties
     assert token.call().name() == 'NuCypher KMS'
     assert token.call().decimals() == 6
     assert token.call().symbol() == 'KMS'
+
+    # Cannot send ethers to the contract
+    with pytest.raises(TransactionFailed):
+        tx = web3.eth.sendTransaction({
+            'from': account1, 'to': token.address, 'value': 10 ** 9})
+        chain.wait.for_receipt(tx)
+
+    # Can transfer tokens
+    tx = token.transact({'from': creator}).transfer(account1, 10000)
+    chain.wait.for_receipt(tx)
+    assert token.call().balanceOf(account1) == 10000
+    assert token.call().balanceOf(creator) == 10 ** 9 - 10000
