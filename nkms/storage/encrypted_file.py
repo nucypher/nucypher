0bd05e13365573703ebc676ac53390312b0b8b88
tuxxy <me@johnpacific.com>
2017-09-14 10:14:29 -0700

    Add open_new and write functions
    Add comment and fix PEP8

--- nkms/storage/encrypted_file.py
+++ nkms/storage/encrypted_file.py
@@ -1,5 +1,6 @@
 import msgpack
 import os
+import io
 from nacl.utils import random
 from nkms.crypto import default_algorithm, symmetric_from_algorithm
 
@@ -9,6 +10,10 @@ class EncryptedFile(object):
         """
         Creates an EncryptedFile object that allows the user to encrypt or
         decrypt data into a file defined at `path`.
+        An EncryptedFile object actually is composed of two files:
+        1) The ciphertext -- This is the chunked and encrypted ciphertext
+        2) The header -- This contains the metadata of the ciphertext that
+            tells us how to decrypt it, or add more data.
 
         :param bytes key: Symmetric key to use for encryption/decryption
         :param string/bytes path: Path of file to open
@@ -118,12 +123,25 @@ class EncryptedFile(object):
         ciphertext = self.file_obj.read(chunk_size)
         return self.cipher.decrypt(ciphertext, nonce=nonce)
 
+    def open_new(self, keys, chunk_size=1000000, nonce=None):
+        """
+        Opens a new EncryptedFile and creates a header for it ready for
+        writing encrypted data.
+
+        :param list keys: Encrypted keys to put in the header.
+        :param int chunk_size: Size of encrypted chunks in bytes, default is 1MB
+        :param bytes nonce: 20 byte Nonce to use for encryption
+        """
+        self.file_obj = open(self.path, mode=self.mode)
+        self._build_header(nonce=nonce, keys=keys, chunk_size=chunk_size)
+
     def open(self, is_new=False):
         """
         Opens a file for Encryption/Decryption.
 
         :param bool is_new: Is the file new (and empty)?
         """
+        # TODO: Error if self.file_obj is already defined
         self.file_obj = open(self.path, mode=self.mode)
 
         # file_obj is now ready for reading/writing encrypted data
@@ -149,13 +167,29 @@ class EncryptedFile(object):
             chunks.append(self._read_chunk(self.header['chunk_size'], nonce))
         return chunks
 
-    #def write(self, data, chunk_size=1000000):
-    #    """
-    #    Writes encrypted data to self.file_obj.
+    def write(self, data):
+        """
+        Writes encrypted data to self.file_obj.
 
-    #    :param bytes data: Data to encrypt and write
-    #    
-    #    :return: Number of chunks written
+        :param bytes data: Data to encrypt and write
+        :param int chunk_num: Chunk number to start writing at
+
+        :return: Number of chunks written
+        :rtype: int
+        """
+        # Always start off at the last chunk_num
+        chunk_num = self.header['num_chunks']
+        buf_data = io.BytesIO(data)
+
+        plaintext = buf_data.read(self.header['chunk_size'])
+        while len(plaintext) > 0:
+            nonce = (self.header['nonce']
+                     + chunk_num.to_bytes(4, byteorder='big'))
+            enc_msg = self.cipher.encrypt(plaintext, nonce=nonce)
+            self.file_obj.write(enc_msg.ciphertext)
+            plaintext = buf_data.read(self.header['chunk_size'])
+            chunk_num += 1
+        self._update_header({'num_chunks': chunk_num})
 
     def close(self):
         """
