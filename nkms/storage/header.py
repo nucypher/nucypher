92401d739b9ec60c4c85b77ad571517ace88dbf5
tuxxy <me@johnpacific.com>
2017-09-14 11:21:04 -0700

    Add Header object in header.py

---
+++ nkms/storage/header.py
@@ -0,0 +1,9 @@
+class Header(object):
+    def __init__(self, header_path=None):
+        """
+        Initializes a header object that contains metadata about a storage
+        object (ie: EncryptedFile)
+
+        :param bytes/string header_path: Path to the file containing the header
+        """
+        pass
