4ee4a20a748c39d7f6b7a925935b8e4d159726a8
tuxxy <me@johnpacific.com>
2017-09-14 12:12:12 -0700

    Add _build_header method and build the header in __init__

--- nkms/storage/header.py
+++ nkms/storage/header.py
@@ -1,19 +1,20 @@
 import msgpack
+from nacl.utils import random
 
 
 class Header(object):
-    def __init__(self, header_path=None, header_dict=None):
+    def __init__(self, header_path=None, header={}):
         """
         Initializes a header object that contains metadata about a storage
         object (ie: EncryptedFile)
 
         :param bytes/string header_path: Path to the file containing the header
+        :param dict header: Header params to use when building the header
         """
         if header_path:
             self.header = self._read_header(header_path)
         else:
-            # TODO: Build a header
-            pass
+            self.header = self._build_header(**header)
 
     def _read_header(self, header_path):
         with open(header_path, mode='rb') as f:
@@ -23,3 +24,28 @@ class Header(object):
             except ValueError as e:
                 raise e
         return header
+
+    def _build_header(self, version=100, nonce=None, keys=None,
+                      chunk_size=1000000, num_chunks=0):
+        """
+        Builds a header and sets the header dict in the `Header` object.
+
+        :param int version: Version of the NuCypher header
+        :param bytes nonce: Nonce to write to header, default is random(20)
+        :param list keys: Keys to write to header
+        :param int chunk_size: Size of each chunk in bytes, default is 1MB
+        :param int num_chunks: Number of chunks in ciphertext
+
+        :return: dict of header
+        :rtype: Dict
+        """
+        if not nonce:
+            nonce = random(20)
+
+        return {
+            'version': version,
+            'nonce': nonce,
+            'keys': keys,
+            'chunk_size': chunk_size,
+            'num_chunks': num_chunks,
+        }
