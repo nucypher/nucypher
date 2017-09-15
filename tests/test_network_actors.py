14ba51425ecd829b88e3e217d621718c4ec98091
jMyles <justin@justinholmes.com>
2017-09-15 15:03:02 -0700

    Another rough sketch of a test for KeyChain.

--- tests/test_network_actors.py
+++ tests/test_network_actors.py
@@ -1,19 +1,32 @@
+from nkms.policy.models import PolicyGroup
+from nkms.crypto.keys import KeyChain
 
 
 def test_alice_has_ursulas_public_key_and_uses_it_to_encode_policy_payload():
 
+    keychain_alice = KeyChain()
+    keychain_bob = KeyChain()
+    keychain_ursula = KeyChain()
 
-    pk_alice =
-    sk_alice =
+    # For example, a hashed path.
+    resource_id = b"as098duasdlkj213098asf"
+    kfrag_id = 74
 
-    pk_ursula =
-    sk_ursula =
+    # Alice runs this to get a policy object.
+    policy_group = PolicyGroup.dictate(keychain_alice.pubkey_sig,
+                            keychain_bob.pubkey_sig,
+                            resource_id,
+                            kfrag_id
+                            )
 
-    # TODO: In the real world, this will be a fingerprint of alice's pk, followed by a hash of the path.
-    policy_id =
+    for policy in PolicyGroup:
+        policy.enact()
 
-    # TODO: In the real world, this will be a kFrag, a Challenge Pack, and a Treasure Map.
-    policy_payload =
 
-    encrypted_payload = pk_ursula.encrypt(policy_payload)
+    ### policy.enact(self):
+        ursula = find_ursula()
+        payload = keychain_ursula.pubkey_enc("In real life, this will be a kFrag, a ChallengePack, and a TreasureMap")
+        policy.offer(ursula, payload    )
+
+
 
