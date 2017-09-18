2a3b61f1ba4d99be00ea9b3d6dc32a847349db70
tuxxy <me@johnpacific.com>
2017-09-18 09:33:23 -0700

    Add boilerplate for KeyRing __init__ method

--- nkms/crypto/keyring/keyring.py
+++ nkms/crypto/keyring/keyring.py
@@ -2,4 +2,12 @@
 
 
 class KeyRing(object):
-    pass
+    def __init__(self, sig_keypair=None, enc_keypair=None):
+        if not sig_keypair:
+            # Generate signing keypair
+            pass
+        if not enc_keypair:
+            # Generate encryption keypair
+            pass
+        self.sig_keypair = sig_keypair
+        self.enc_keypair = enc_keypair
