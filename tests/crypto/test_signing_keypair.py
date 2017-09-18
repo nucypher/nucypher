79b6f178a851eb15899cf573bc01bc8e8dab7a77
tuxxy <me@johnpacific.com>
2017-09-18 10:55:19 -0700

    Add test_verification for test_signing_keypair

--- tests/crypto/test_signing_keypair.py
+++ tests/crypto/test_signing_keypair.py
@@ -14,6 +14,24 @@ class TestSigningKeypair(unittest.TestCase):
         signature = self.keypair.sign(msg_digest)
 
         try:
-            msgpack.loads(signature)
+            sig = msgpack.loads(signature)
+            self.assertTrue(1, len(sig[0]))     # Check v
+            self.assertTrue(32, len(sig[1]))    # Check r
+            self.assertTrue(32, len(sig[2]))    # Check s
         except Exception as e:
             self.fail("Failed to msgpack.load signature: {}".format(e))
+
+    def test_verification(self):
+        msg_digest = sha3.keccak_256(self.msg).digest()
+        signature = self.keypair.sign(msg_digest)
+
+        try:
+            sig = msgpack.loads(signature)
+            self.assertTrue(1, len(sig[0]))     # Check v
+            self.assertTrue(32, len(sig[1]))    # Check r
+            self.assertTrue(32, len(sig[2]))    # Check s
+        except Exception as e:
+            self.fail("Failed to msgpack.load signature: {}".format(e))
+
+        verify_sig = self.keypair.verify(msg_digest, signature)
+        self.assertTrue(verify_sig)
