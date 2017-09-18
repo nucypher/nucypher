0b714fff5e5517c39b5ad7acb8891a8c9264f97c
tuxxy <me@johnpacific.com>
2017-09-18 10:32:56 -0700

    Add sign/verify methods to KeyRing

--- nkms/crypto/keyring/keyring.py
+++ nkms/crypto/keyring/keyring.py
@@ -1,13 +1,36 @@
-
+import sha3
+from nkms.crypto.keyring.keys import SigningKeypair
 
 
 class KeyRing(object):
     def __init__(self, sig_keypair=None, enc_keypair=None):
-        if not sig_keypair:
-            # Generate signing keypair
-            pass
+        self.sig_keypair = SigningKeypair(sig_keypair)
         if not enc_keypair:
             # Generate encryption keypair
             pass
-        self.sig_keypair = sig_keypair
         self.enc_keypair = enc_keypair
+
+    def sign(self, message):
+        """
+        Signs a message and returns a signature with the keccak hash.
+
+        :param bytes message: Message to sign in bytes
+
+        :rtype: bytestring
+        :return: Signature of message
+        """
+        msg_digest = sha3.keccak_256(message)
+        return self.sig_keypair.sign(msg_digest)
+
+    def verify(self, message, signature):
+        """
+        Verifies a signature.
+
+        :param bytes message: Message to check signature for
+        :param bytes signature: Signature to validate
+
+        :rtype: Boolean
+        :return: Is the message signature valid or not?
+        """
+        msg_digest = sha3.keccak_256(message)
+        return self.sig_keypair.verify(msg_digest, signature)
---
+++ nkms/crypto/keyring/keys/__init__.py
@@ -0,0 +1,1 @@
+from .signing_keypair import SigningKeypair
