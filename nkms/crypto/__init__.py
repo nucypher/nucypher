28a7dda8bf8a10dcac9a211ec5a4d510ed2c1f3b
Michael Egorov <michwill@gmail.com>
2017-09-18 11:15:46 -0700

    Use default generator point (same as in Bitcoin and Ethereum)

--- nkms/crypto/__init__.py
+++ nkms/crypto/__init__.py
@@ -1,20 +1,13 @@
 import importlib
 from nacl.utils import random  # noqa
+from npre.curves import secp256k1
 
-# 'Random' parameter g here is derived from Bitcoin's hashMerkleRoot of
-# its genesis block:
-# bbs98.ec.serialize(
-#    bbs98.ec.hashEC(
-#       pre.ecgroup,
-#       base64.decodebytes(b'4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b')
-#       bbs98.ec.G)) = bytes.fromhex('0103bf16807874dc8dfc45c64efc541ef526e63d5cd083ea1e87347c42c19739eab3')
 default_algorithm = dict(
         symmetric=dict(
             cipher='nacl'),
         pre=dict(
             cipher='bbs98',     # BBS98 is only temporary here, for development
-            curve=714,          # secp256k1 in OpenSSL
-            g=bytes.fromhex('0103bf16807874dc8dfc45c64efc541ef526e63d5cd083ea1e87347c42c19739eab3'),
+            curve=secp256k1,
             m=None, n=None))
 
 
