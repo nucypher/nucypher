eb74e98c90abb10d432247980b00c94aa9e0ba49
tuxxy <me@johnpacific.com>
2017-10-05 16:58:14 -0600

    Add ECIES de/encapsulate methods and gen_priv methods and format code better

--- nkms/crypto/crypto.py
+++ nkms/crypto/crypto.py
@@ -1,13 +1,77 @@
+from npre import umbral
+from npre import elliptic_curve
 from nacl.secret import SecretBox
+from typing import Tuple
 
 
 class Crypto(object):
+    PRE = umbral.PRE()
+
     @staticmethod
-    def symm_encrypt(key: bytes, plaintext: bytes) -> bytes:
+    def symm_encrypt(
+        key: bytes,
+        plaintext: bytes
+    ) -> bytes:
+        """
+        Performs symmetric encryption using nacl.SecretBox.
+
+        :param key: Key to encrypt with
+        :param plaintext: Plaintext to encrypt
+
+        :return: Encrypted ciphertext
+        """
         cipher = SecretBox(key)
         return cipher.encrypt(plaintext)
 
     @staticmethod
-    def symm_decrypt(key: bytes, ciphertext: bytes) -> bytes:
+    def symm_decrypt(
+        key: bytes,
+        ciphertext: bytes
+    ) -> bytes:
+        """
+        Decrypts ciphertext performed with nacl.SecretBox.
+
+        :param key: Key to decrypt with
+        :param ciphertext: Nacl.SecretBox ciphertext to decrypt
+
+        :return: Decrypted Plaintext
+        """
         cipher = SecretBox(key)
         return cipher.decrypt(ciphertext)
+
+    @staticmethod
+    def ecies_gen_priv() -> elliptic_curve.Element:
+        """
+        Generates an ECIES private key.
+
+        :return: An ECIES private key
+        """
+        return Crypto.PRE.gen_priv()
+
+    @staticmethod
+    def ecies_encapsulate(
+        pubkey: elliptic_curve.Element
+    ) -> Tuple[bytes, umbral.EncryptedKey]:
+        """
+        Encapsulates an ECIES generated symmetric key for a public key.
+
+        :param pubkey: Pubkey to generate a key for
+
+        :return: Generated key in bytes, and EncryptedKey
+        """
+        return Crypto.PRE.encapsulate(pubkey)
+
+    @staticmethod
+    def ecies_decapsulate(
+        privkey: elliptic_curve.Element,
+        enc_key: umbral.EncryptedKey
+    ) -> bytes:
+        """
+        Decapsulates an ECIES generated encrypted key with a private key.
+
+        :param privkey: Private key to decrypt the key with
+        :param enc_key: Encrypted Key to decrypt
+
+        :return: Decrypted symmetric key
+        """
+        return Crypto.PRE.decapsulate(privkey, enc_key)
