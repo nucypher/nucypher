ccece81783ff0bccc25faa40b1d1d679e0fe6177
tuxxy <me@johnpacific.com>
2017-10-11 13:54:13 -0600

    Add test_ecdsa_keypair_generation to test_keypairs

--- tests/keystore/test_keypairs.py
+++ tests/keystore/test_keypairs.py
@@ -10,10 +10,21 @@ class TestKeypairs(unittest.TestCase):
     def test_ecies_keypair_generation(self):
         self.ecies_keypair.gen_privkey()
 
-        self.assertTrue(self.ecies_keypair.privkey != None)
+        self.assertTrue(self.ecies_keypair.privkey is not None)
         self.assertEqual(bytes, type(self.ecies_keypair.privkey))
         self.assertEqual(32, len(self.ecies_keypair.privkey))
 
-        self.assertTrue(self.ecies_keypair.pubkey != None)
+        self.assertTrue(self.ecies_keypair.pubkey is not None)
         self.assertEqual(bytes, type(self.ecies_keypair.pubkey))
         self.assertEqual(33, len(self.ecies_keypair.pubkey))
+
+    def test_ecdsa_keypair_generation(self):
+        self.ecdsa_keypair.gen_privkey()
+
+        self.assertTrue(self.ecdsa_keypair.privkey is not None)
+        self.assertEqual(bytes, type(self.ecdsa_keypair.privkey))
+        self.assertEqual(32, len(self.ecdsa_keypair.privkey))
+
+        self.assertTrue(self.ecdsa_keypair.pubkey is not None)
+        self.assertEqual(bytes, type(self.ecdsa_keypair.pubkey))
+        self.assertEqual(64, len(self.ecdsa_keypair.pubkey))
