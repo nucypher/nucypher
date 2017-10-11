8fb36b8b133559e47a15353a7907e7fe4930c17e
tuxxy <me@johnpacific.com>
2017-10-11 13:59:27 -0600

    Add boolean switch to generate private key in gen_ecies_keypair and gen_ecdsa_keypair

--- nkms/keystore/keystore.py
+++ nkms/keystore/keystore.py
@@ -15,22 +15,34 @@ class KeyStore(object):
         """
         pass
 
-    def gen_ecies_keypair(self) -> keypairs.EncryptingKeypair:
+    def gen_ecies_keypair(self, gen_priv=True) -> keypairs.EncryptingKeypair:
         """
         Generates an ECIES keypair.
 
+        TODO: Initalize keypair with provided data.
+
+        :param gen_priv: Generate private key or not?
+
         :return: ECIES encrypting keypair
         """
-        ecies_keypair = keypairs.EncryptingKeypair().gen_privkey()
+        ecies_keypair = keypairs.EncryptingKeypair()
+        if gen_priv:
+            ecies_keypair.gen_privkey()
         return ecies_keypair
 
-    def gen_ecdsa_keypair(self) -> keypairs.SigningKeypair:
+    def gen_ecdsa_keypair(self, gen_priv=True) -> keypairs.SigningKeypair:
         """
         Generates an ECDSA keypair.
 
+        TODO: Initalize keypair with provided data.
+
+        :param gen_priv: Generate private key or not?
+
         :return ECDSA signing keypair
         """
-        ecdsa_keypair = keypairs.SigningKeypair().gen_privkey()
+        ecdsa_keypair = keypairs.SigningKeypair()
+        if gen_priv:
+            ecdsa_keypair.gen_privkey()
         return ecdsa_keypair
 
     def get_key(self):
