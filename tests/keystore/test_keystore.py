d01e6e1bae2768e3aac03f9bb367f1e6bcaf4103
tuxxy <me@johnpacific.com>
2017-10-20 16:20:53 -0600

    Add tests tesT_lmdb_keystore

--- tests/keystore/test_keystore.py
+++ tests/keystore/test_keystore.py
@@ -1,10 +1,11 @@
 import unittest
+import sha3
 from nkms.keystore import keystore, keypairs
 
 
 class TestKeyStore(unittest.TestCase):
     def setUp(self):
-        self.ks = keystore.KeyStore()
+        self.ks = keystore.KeyStore('test')
 
     def test_ecies_keypair_generation(self):
         keypair = self.ks.gen_ecies_keypair()
@@ -18,14 +19,48 @@ class TestKeyStore(unittest.TestCase):
         self.assertEqual(bytes, type(keypair.privkey))
         self.assertEqual(bytes, type(keypair.pubkey))
 
-    def test_get_key(self):
-        # TODO: Implement this.
-        pass
+    def test_lmdb_keystore(self):
+        keypair = self.ks.gen_ecies_keypair()
+        self.assertEqual(keypairs.EncryptingKeypair, type(keypair))
+        self.assertEqual(bytes, type(keypair.privkey))
+        self.assertEqual(bytes, type(keypair.pubkey))
 
-    def test_add_key(self):
-        # TODO: Implement this.
-        pass
+        # Test add_key pubkey
+        fingerprint_pub = self.ks.add_key(keypair, store_pub=True)
+        self.assertEqual(bytes, type(fingerprint_pub))
+        self.assertEqual(64, len(fingerprint_pub))
 
-    def test_del_key(self):
-        # TODO: Implement this.
-        pass
+        key_hash = sha3.keccak_256(keypair.pubkey).hexdigest().encode()
+        self.assertEqual(key_hash, fingerprint_pub)
+
+        # Test add_key privkey
+        fingerprint_priv = self.ks.add_key(keypair, store_pub=False)
+        self.assertEqual(bytes, type(fingerprint_priv))
+        self.assertEqual(64, len(fingerprint_priv))
+
+        key_hash = sha3.keccak_256(keypair.privkey).hexdigest().encode()
+        self.assertEqual(key_hash, fingerprint_priv)
+
+        # Test get_key pubkey
+        keypair_pub = self.ks.get_key(fingerprint_pub)
+        self.assertEqual(keypairs.EncryptingKeypair, type(keypair_pub))
+        self.assertTrue(keypair_pub.public_only)
+        self.assertEqual(keypair.pubkey, keypair_pub.pubkey)
+
+        # Test get_key privkey
+        keypair_priv = self.ks.get_key(fingerprint_priv)
+        self.assertEqual(keypairs.EncryptingKeypair, type(keypair_priv))
+        self.assertFalse(keypair_priv.public_only)
+        self.assertEqual(keypair.privkey, keypair_priv.privkey)
+        self.assertIsNotNone(keypair_priv.pubkey)
+        self.assertEqual(keypair.pubkey, keypair_priv.pubkey)
+
+        # Test del_key pubkey
+        self.ks.del_key(fingerprint_pub)
+        key = self.ks.get_key(fingerprint_pub)
+        self.assertIsNone(key)
+
+        # Test del_key privkey
+        self.ks.del_key(fingerprint_priv)
+        key = self.ks.get_key(fingerprint_priv)
+        self.assertIsNone(key)
