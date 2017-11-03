f9de664c6d4b678b6dc1909a44dd456c2070b636
tuxxy <john@nucypher.com>
2017-11-02 18:19:26 -0700

    Set secure_delete pragma

--- nkms/keystore/db/__init__.py
+++ nkms/keystore/db/__init__.py
@@ -1,4 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 
 
 Base = declarative_base()
+
+
+@event.listens_for(Engine, "connect")
+def set_secure_delete_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA secure_delete=on")
+    cursor.close()
