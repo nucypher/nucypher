e2fd18dfc9539204b63b143c39a7530636b7ad85
tuxxy <john@nucypher.com>
2017-11-02 17:00:42 -0700

    Add __init__ and get_fingerprint methods to models.Key

--- nkms/keystore/db/models.py
+++ nkms/keystore/db/models.py
@@ -1,10 +1,28 @@
+import sha3
+
 from nkms.keystore.db import Base
-from sqlalchemy import Column, Integer, BigInteger, Binary
+from nkms.crypto import api as API
+from sqlalchemy import Column, Integer, BigInteger, String
 
 
 class Key(Base):
     __tablename__ = 'keys'
 
     id = Column(Integer, primary_key=True)
-    fingerprint = Column(Binary(32), unique=True)
+    fingerprint = Column(String(64), unique=True)
     key_data = Column(BigInteger, unique=True)
+
+    def __init__(self, key_data):
+        self.fingerprint = Key.get_fingerprint(key_data)
+        self.key_data = key_data
+
+    @classmethod
+    def get_fingerprint(cls, key_data: bytes) -> str:
+        """
+        Hashes the key with keccak_256 and returns the hexdigest as a String.
+
+        :param key_data: Actual key data to hash
+
+        :return: Fingerprint of key as a string
+        """
+        return sha3.keccak_256(key_data).hexdigest()
