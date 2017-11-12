72ffa361a4756a9e0b2163baa02b8b929f8d5d09
jMyles <justin@justinholmes.com>
2017-11-11 17:46:00 -0800

    BytestringSplitter is now allowed to give back the remainder.

--- nkms/crypto/utils.py
+++ nkms/crypto/utils.py
@@ -1,28 +1,43 @@
+import msgpack
 
 
 class BytestringSplitter(object):
-
-    def __init__(self, *message_types):
+    def __init__(self, *message_types, return_remainder=False, msgpack_remainder=False):
         """
         :param message_types:  A collection of types of messages to parse.
         """
         self.message_types = message_types
+        self.return_remainder = return_remainder
+        self.msgpack_remainder = msgpack_remainder
 
     def __call__(self, splittable):
-        if len(self) != len(splittable):
-            raise ValueError("Wrong number of bytes to constitute message types {} - need {}, got {}".format(self.message_types, len(self), len(splittable)))
-
+        if not any((self.return_remainder, self.msgpack_remainder)) and len(self) != len(splittable):
+            raise ValueError(
+                "Wrong number of bytes to constitute message types {} - need {}, got {} \n Did you mean to return the remainder?".format(
+                    self.message_types, len(self), len(splittable)))
+        if len(self) > len(splittable):
+            raise ValueError(
+                "Not enough bytes to constitute message types {} - need {}, got {}".format(self.message_types,
+                                                                                           len(self),
+                                                                                           len(splittable)))
         cursor = 0
         message_objects = []
 
         for message_type in self.message_types:
             message_class, message_length = self.get_message_meta(message_type)
             expected_end_of_object_bytes = cursor + message_length
-            bytes_for_this_object = splittable[cursor:cursor + expected_end_of_object_bytes]
+            bytes_for_this_object = splittable[cursor:expected_end_of_object_bytes]
             message = message_class(bytes_for_this_object)
 
             message_objects.append(message)
             cursor = expected_end_of_object_bytes
+
+        remainder = splittable[cursor:]
+
+        if self.msgpack_remainder:
+            message_objects.append(msgpack.loads(remainder))
+        elif self.return_remainder:
+            message_objects.append(remainder)
 
         return message_objects
 
@@ -32,4 +47,3 @@ class BytestringSplitter(object):
     @staticmethod
     def get_message_meta(message_type):
         return message_type if isinstance(message_type, tuple) else (message_type, message_type._EXPECTED_LENGTH)
-
