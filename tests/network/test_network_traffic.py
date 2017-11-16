370757f8ee67c77c88503d36ff8a4cea9898fb85
jMyles <justin@justinholmes.com>
2017-11-16 15:37:00 -0800

    Testing set_kfrag - both tests fail for the moment.

--- tests/network/test_network_traffic.py
+++ tests/network/test_network_traffic.py
@@ -12,18 +12,11 @@ app = App(routes=routes)
 
 
 def test_set_kfrag():
-    """
-    Testing a view directly.
-    """
-    response = URSULAS[0].set_kfrag("some_hrac")
+    response = URSULAS[0].set_kfrag(u"some_hrac")
     assert response == "something useful"
 
 
 def test_http_request():
-    """
-    Testing a view, using the test client.
-    """
     client = TestClient(app)
-    response = client.get('http://localhost/kFrag/some_hrac')
+    response = client.post('http://localhost/kFrag/some_hrac')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Welcome to API Star!'}
