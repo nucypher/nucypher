74dd1d67c3e4457c34b24fb3213d0bca72321c14
Michael Egorov <michwill@gmail.com>
2017-11-16 23:44:11 -0800

    [KMS-ETH]- Common functions to connect to local ethereum node or testrpc-py

---
+++ nkms_eth/blockchain.py
@@ -0,0 +1,30 @@
+import populus
+import threading
+import nkms_eth
+from os.path import dirname, join, abspath
+
+_project = threading.local()
+
+
+def project():
+    # Hardcoded the config for the project
+    # It will read user-specific configs also which may override it
+    if not hasattr(_project, 'project'):
+        project_dir = join(dirname(abspath(nkms_eth.__file__)), 'project')
+        _project.project = populus.Project(project_dir, create_config_file=False)
+    return _project.project
+
+
+def chain(name='mainnetrpc'):
+    if not hasattr(_project, 'chain'):
+        _project.chain = project().get_chain(name).__enter__()
+    return _project.chain
+
+
+def disconnect():
+    _project.chain.__exit__(None, None, None)
+    delattr(_project, 'chain')
+
+
+def web3():
+    return chain().web3
