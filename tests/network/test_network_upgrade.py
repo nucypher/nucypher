7a525b801e15afce8b72308f1b6ec7c4d81e93f0
jMyles <justin@justinholmes.com>
2017-12-01 12:54:33 -0800

    Move Alice REST test to proper module.

--- tests/network/test_network_upgrade.py
+++ tests/network/test_network_upgrade.py
@@ -1,4 +1,14 @@
-from tests.utilities import EVENT_LOOP
+from tests.utilities import EVENT_LOOP, MockNetworkyStuff
+
+
+def test_alice_enacts_policies_in_policy_group_via_rest(enacted_policy_group):
+    """
+    Now that Alice has made a PolicyGroup, she can enact its policies, using Ursula's Public Key to encrypt each offer
+    and transmitting them via REST.
+    """
+    ursula = enacted_policy_group.policies[0].ursula
+    kfrag_that_was_set = ursula.keystore.get_kfrag(enacted_policy_group.hrac())
+    assert bool(kfrag_that_was_set)  # TODO: This can be a more poignant assertion.
 
 
 def test_bob_can_follow_treasure_map(enacted_policy_group, ursulas, alice, bob):
