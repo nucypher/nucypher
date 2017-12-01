b1f9b72886572907718ff7e60f7493b23b875f76
jMyles <justin@justinholmes.com>
2017-11-30 16:54:50 -0800

    Session-wide fixture injection means we don't need to grab alice and bob from the PolicyGroup.

--- tests/network/test_network_upgrade.py
+++ tests/network/test_network_upgrade.py
@@ -1,12 +1,10 @@
 from tests.utilities import EVENT_LOOP
 
 
-def test_bob_can_follow_treasure_map(enacted_policy_group, ursulas):
+def test_bob_can_follow_treasure_map(enacted_policy_group, ursulas, alice, bob):
     """
     Upon receiving a TreasureMap, Bob populates his list of Ursulas with the correct number.
     """
-    alice = enacted_policy_group.alice
-    bob = enacted_policy_group.bob
     assert len(bob._ursulas) == 0
 
     setter, encrypted_treasure_map, packed_encrypted_treasure_map, signature_for_bob, signature_for_ursula = alice.publish_treasure_map(
