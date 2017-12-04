2fc80d34141e4e1eb55aa05c87a0dd23c2b63e62
jMyles <justin@justinholmes.com>
2017-12-04 13:58:11 -0800

    Providing KFrag compatibility.

--- nkms/crypto/fragments.py
+++ nkms/crypto/fragments.py
@@ -1,3 +1,4 @@
+from npre.constants import UNKNOWN_KFRAG
 from npre.umbral import RekeyFrag
 
 
@@ -5,19 +6,45 @@ class PFrag(object):
     pass
 
 
-class KFrag(bytes):
+class KFrag(object):
 
     _EXPECTED_LENGTH = 66
     _id_length = 33
     _key_length = 33
+    _is_unknown_kfrag = False
 
-    def __init__(self, id_plus_key):
-        kfrag_id = id_plus_key[self._id_length:]
-        kfrag_key = id_plus_key[:self._key_length]
-        self._umbral_kfrag = RekeyFrag.from_bytes(id_plus_key)
+    def __init__(self, id_plus_key_as_bytes=None, umbral_kfrag=None):
+        if all((id_plus_key_as_bytes, umbral_kfrag)):
+            raise ValueError("Pass either the id/key or an umbral_kfrag (or neither for UNKNOWN_KFRAG).  Not both.")
+        elif id_plus_key_as_bytes:
+            self._umbral_kfrag = RekeyFrag.from_bytes(id_plus_key_as_bytes)
+        elif umbral_kfrag:
+            self._umbral_kfrag = umbral_kfrag
+        else:
+            self._is_unknown_kfrag = True
 
     def __bytes__(self):
         return bytes(self._umbral_kfrag)
+
+    def __eq__(self, other_kfrag):
+        if other_kfrag is UNKNOWN_KFRAG:
+            return bool(self._is_unknown_kfrag)
+        else:
+            return bytes(self) == bytes(other_kfrag)
+
+    @property
+    def key(self):
+        return self._umbral_kfrag.key
+
+    @property
+    def id(self):
+        return self._umbral_kfrag.id
+
+    def __add__(self, other):
+        return bytes(self) + other
+
+    def __radd__(self, other):
+        return other + bytes(self)
 
 
 class CFrag(object):
