7ebe8eb56c20b24f14e0ed2dd17b510e3acce96f
jMyles <justin@justinholmes.com>
2017-12-04 13:08:21 -0800

    First sketch of KFrag class.

--- nkms/crypto/fragments.py
+++ nkms/crypto/fragments.py
@@ -1,9 +1,23 @@
+from npre.umbral import RekeyFrag
+
+
 class PFrag(object):
     pass
 
 
-class KFrag(object):
-    pass
+class KFrag(bytes):
+
+    _EXPECTED_LENGTH = 66
+    _id_length = 33
+    _key_length = 33
+
+    def __init__(self, id_plus_key):
+        kfrag_id = id_plus_key[self._id_length:]
+        kfrag_key = id_plus_key[:self._key_length]
+        self._umbral_kfrag = RekeyFrag.from_bytes(id_plus_key)
+
+    def __bytes__(self):
+        return bytes(self._umbral_kfrag)
 
 
 class CFrag(object):
