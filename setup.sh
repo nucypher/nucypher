3287d869937566aee08dac2ebddfc65f70c68e59
Kieran R Prasch <kieranprasch@gmail.com>
2018-02-01 12:53:13 -0800

    [KMS-ETH]- Ports deps to pipenv: system deps, pipenv, and solc install run from bash installer.

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-virtualenv -p python3 .venv
-source .venv/bin/activate
-pip3 install -r requirements.txt
-wget https://github.com/ethereum/solidity/releases/download/v0.4.19/solc-static-linux -O .venv/bin/solc
-chmod +x .venv/bin/solc
-pip3 install -e .
