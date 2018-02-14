19fd2c4343c243b656a28b2e6ae74d9cb3a5d7a6
jMyles <justin@justinholmes.com>
2018-02-13 17:34:16 -0800

    This test is no longer needed with KFrag class.

--- tests/crypto/test_bytestring_types.py
+++ tests/crypto/test_bytestring_types.py
@@ -2,6 +2,7 @@ import pytest
 
 from nkms.crypto import api
 from nkms.crypto.api import secure_random
+from nkms.crypto.constants import KFRAG_LENGTH
 from nkms.crypto.signature import Signature
 from nkms.crypto.utils import BytestringSplitter
 from umbral.bignum import BigNum
@@ -28,23 +29,6 @@ def test_split_signature_from_arbitrary_bytes():
     splitter = BytestringSplitter(Signature, (bytes, how_many_bytes))
 
     rebuilt_signature, rebuilt_bytes = splitter(signature + some_bytes)
-
-
-def test_split_kfrag_from_arbitrary_bytes():
-    rand_id = b'\x00' + api.secure_random(32)
-    rand_key = b'\x00' + api.secure_random(32)
-
-    KFrag(id_=BigNum.gen_rand(), key=BigNum.gen_rand(), x=Point.gen_rand(), u1=Point.gen_rand(), z1=BigNum.gen_rand(),
-          z2=BigNum.gen_rand())
-
-
-    # TODO: The rest of this test no longer makes sense with the new KFrag class.
-    how_many_bytes = 10
-    some_bytes = secure_random(how_many_bytes)
-
-    splitter = BytestringSplitter(KFrag, (bytes, how_many_bytes))
-    rebuilt_kfrag, rebuilt_bytes = splitter(kfrag + some_bytes)
-    assert kfrag == rebuilt_kfrag
 
 
 def test_trying_to_extract_too_many_bytes_raises_typeerror():
