b4e34c0981df3392fb4ba397dc4963b29f35b3bc
jMyles <justin@justinholmes.com>
2018-03-18 00:42:28 -0700

    Using new "to cryptography" APIs.

--- tests/characters/test_ursula_prepares_to_act_as_mining_node.py
+++ tests/characters/test_ursula_prepares_to_act_as_mining_node.py
@@ -5,5 +5,5 @@ from nkms.crypto.powers import SigningPower
 def test_ursula_generates_self_signed_cert():
     ursula = Ursula(attach_server=False)
     cert, cert_private_key = ursula.generate_self_signed_certificate()
-    public_key_numbers = ursula.public_key(SigningPower).point_key.to_cryptography_pub_key().public_numbers()
+    public_key_numbers = ursula.public_key(SigningPower).to_cryptography_pubkey().public_numbers()
     assert cert.public_key().public_numbers() == public_key_numbers
