6e24f0555678c595b354a3c1afafe228bf8531b0
tuxxy <john@nucypher.com>
2018-04-04 17:27:30 -0600

    Add key derivation functions for master and wrapping keys, and add functiosn to encrypt/decrypt key material

--- nkms/config/config.py
+++ nkms/config/config.py
@@ -4,6 +4,11 @@ from eth_account import Account
 
 from umbral.keys import UmbralPrivateKey
 from web3.auto import w3
+import web3
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from nacl.secret import SecretBox
 
 
 from nkms.config.utils import _derive_wrapping_key_from_master_key, _decrypt_key
@@ -178,4 +183,67 @@ class KMSConfig:
         """Reads the config file and creates a KMSConfig instance"""
 
 
+def _derive_master_key_from_passphrase(salt: bytes, passphrase: str):
+    """
+    Uses Scrypt derivation to derive a master key for encrypting key material.
+    See RFC 7914 for n, r, and p value selections.
+    """
+    master_key = Scrypt(
+        salt=salt,
+        length=32,
+        n=2**20,
+        r=8,
+        p=1,
+        backend=default_backend()
+    ).derive(passphrase.encode())
+
+    return master_key
+
+
+def _derive_wrapping_key_from_master_key(salt: bytes, master_key: bytes):
+    """
+    Uses HKDF to derive a 32 byte wrapping key to encrypt key material with.
+    """
+    wrapping_key = HKDF(
+        algorithm=hashes.SHA512(),
+        length=32,
+        salt=salt,
+        info=b'NuCypher-KMS-KeyWrap',
+        backend=default_backend()
+    ).derive(master_key)
+
+    return wrapping_key
+
+
+def _encrypt_key(wrapping_key: bytes, key_material: bytes):
+    """
+    Encrypts a key with nacl's XSalsa20-Poly1305 algorithm (SecretBox).
+    Returns an encrypted key as bytes with the nonce appended.
+    """
+    nonce = os.urandom(24)
+    enc_key = SecretBox(wrapping_key).encrypt(key_material, nonce)
+
+    return enc_key + nonce
+
+
+def _decrypt_key(wrapping_key: bytes, enc_key_material: bytes, nonce: bytes):
+    """
+    Decrypts an encrypted key with nacl's XSalsa20-Poly1305 algorithm (SecretBox).
+    Returns a decrypted key as bytes.
+    """
+    dec_key = SecretBox(wrapping_key).encrypt(enc_key_material, nonce)
+
+    return dec_key
+
+
+def _generate_encryption_keys():
+    privkey = UmbralPrivateKey.gen_key()
+    pubkey = priv_key.get_pubkey()
+
+
+# TODO: Do we really want to use Umbral keys for signing?
+# TODO: Perhaps we can use Curve25519/EdDSA for signatures?
+def _generate_signing_keys():
+    privkey = UmbralPrivateKey.gen_key()
+    pubkey = priv_key.get_pubkey()
 
