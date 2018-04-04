16c6062b4d9d79a0d5d73968a87bbbc5b780ff93
jMyles <justin@justinholmes.com>
2018-04-03 23:34:49 -0700

    Putting cryptoKits back into the main repo for now.

--- nkms/crypto/kits.py
+++ nkms/crypto/kits.py
@@ -1,5 +1,52 @@
-from crypto_kits.kits import MessageKit
 from nkms.crypto.splitters import key_splitter, capsule_splitter
+from constant_sorrow import constants
+
+
+class CryptoKit:
+    return_remainder_when_splitting = True
+    splitter = None
+
+    @classmethod
+    def split_bytes(cls, some_bytes):
+        if not cls.splitter:
+            raise TypeError("This kit doesn't have a splitter defined.")
+
+        return cls.splitter(some_bytes,
+                            return_remainder=cls.return_remainder_when_splitting)
+
+    @classmethod
+    def from_bytes(cls, some_bytes):
+        constituents = cls.split_bytes(some_bytes)
+        return cls(*constituents)
+
+
+class MessageKit(CryptoKit):
+
+    _signature = constants.NOT_SIGNED
+
+    def __init__(self, capsule, alice_pubkey=None, ciphertext=None):
+        self.ciphertext = ciphertext
+        self.capsule = capsule
+        self.alice_pubkey = alice_pubkey
+
+    def to_bytes(self, include_alice_pubkey=True):
+        # We include the capsule first.
+        as_bytes = bytes(self.capsule)
+
+        # Then, before the ciphertext, we see if we're including alice's public key.
+        # We want to put that first because it's typically of known length.
+        if include_alice_pubkey and self.alice_pubkey:
+            as_bytes += bytes(self.alice_pubkey)
+
+        as_bytes += self.ciphertext
+        return as_bytes
+
+    @property
+    def signature(self):
+        return self._signature
+
+    def __bytes__(self):
+        return bytes(self.capsule) + self.ciphertext
 
 
 class UmbralMessageKit(MessageKit):
