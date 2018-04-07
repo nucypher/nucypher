667d22b2f23e0d3212b97ef652a477e938ba22b2
Kieran Prasch <kieranprasch@gmail.com>
2018-04-06 17:16:47 -0700

    [KMS-ETH]- Embelish actor tests

--- tests/entities/test_agents.py
+++ tests/entities/test_agents.py
@@ -1,6 +1,6 @@
 import pytest
 
-from nkms_eth.utilities import spawn_miners, MockNuCypherMinerConfig
+from nkms_eth.utilities import MockNuCypherMinerConfig
 
 M = 10 ** 6
 
@@ -10,7 +10,7 @@ def test_get_swarm(testerchain, mock_token_deployer, mock_miner_agent):
     mock_token_deployer._global_airdrop(amount=10000)
 
     creator, *addresses = testerchain._chain.web3.eth.accounts
-    spawn_miners(addresses=addresses, miner_agent=mock_miner_agent, locktime=1, m=M)
+    testerchain.spawn_miners(addresses=addresses, miner_agent=mock_miner_agent, locktime=1, m=M)
 
     default_period_duration = MockNuCypherMinerConfig._hours_per_period
     testerchain.wait_time(default_period_duration)
