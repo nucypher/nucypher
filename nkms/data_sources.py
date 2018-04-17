d32f290d6b8e090936f31cd60aa6fec3d63a5886
jMyles <justin@justinholmes.com>
2018-04-17 01:44:26 -0700

    DataSource now automatically generates a signer - not sure how we want to generate this keypair.  See #241.

--- nkms/data_sources.py
+++ nkms/data_sources.py
@@ -1,10 +1,14 @@
 from nkms.crypto.api import encrypt_and_sign
-
+from nkms.crypto.signature import SignatureStamp
+from nkms.keystore.keypairs import SigningKeypair
+from constant_sorrow.constants import NO_SIGNING_POWER
 
 class DataSource:
 
-    def __init__(self, policy_pubkey_enc, signer):
+    def __init__(self, policy_pubkey_enc, signer=NO_SIGNING_POWER):
         self._policy_pubkey_enc = policy_pubkey_enc
+        if signer is NO_SIGNING_POWER:
+            signer = SignatureStamp(SigningKeypair())  # TODO: Generate signing key properly.  #241
         self.stamp = signer
 
     def encapsulate_single_message(self, message):
