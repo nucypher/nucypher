df6fef44e89074cdc88444ae082bcd6e9a90de35
Kieran Prasch <kieranprasch@gmail.com>
2018-06-11 10:59:56 -0700

    Keep tests up-to-date; Create convention for test accounts, adds miners fixture, use properties and injected deployer_addresses.

--- tests/blockchain/eth/client/test_setup.py
+++ tests/blockchain/eth/client/test_setup.py
@@ -11,5 +11,7 @@ def test_chain_creation(testerchain):
 
 def test_nucypher_contract_compiled(testerchain):
     # Ensure that solidity smart contacts are available, post-compile.
-    token_contract_identifier = NucypherTokenDeployer(blockchain=testerchain)._contract_name
+    origin, *everybody_else = testerchain.interface.w3.eth.accounts
+
+    token_contract_identifier = NucypherTokenDeployer(blockchain=testerchain, deployer_address=origin)._contract_name
     assert token_contract_identifier in testerchain.interface._ControlCircumflex__raw_contract_cache
