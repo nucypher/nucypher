2a466d52041c52feb04dd79cc89d4739291a9ee1
Kieran Prasch <kieranprasch@gmail.com>
2018-06-11 10:58:08 -0700

    Fixes miner entity test failure, multi-path fixture resolution issues

--- tests/blockchain/eth/entities/test_actors.py
+++ tests/blockchain/eth/entities/test_actors.py
@@ -12,20 +12,28 @@ class TestMiner:
 
     @pytest.fixture(scope='class')
     def miner(self, testerchain, mock_token_agent, mock_miner_agent):
-        mock_token_agent.token_airdrop(amount=100000 * constants.M)
-        _origin, ursula, *everybody_else = testerchain.interface.w3.eth.accounts
-        miner = Miner(miner_agent=mock_miner_agent, ether_address=ursula)
+        origin, *everybody_else = testerchain.interface.w3.eth.accounts
+        mock_token_agent.token_airdrop(origin=origin, addresses=everybody_else, amount=100000*constants.M)
+        miner = Miner(miner_agent=mock_miner_agent, ether_address=everybody_else[0])
         return miner
 
+    @pytest.mark.usefixtures("mock_policy_agent")
     def test_miner_locking_tokens(self, testerchain, miner, mock_miner_agent):
 
-        assert constants.MIN_ALLOWED_LOCKED < miner.token_balance(), "Insufficient miner balance"
+        testerchain.ether_airdrop(amount=10000)
+
+        assert constants.MIN_ALLOWED_LOCKED < miner.token_balance, "Insufficient miner balance"
+
+        now = maya.now()
+        expiration = now.add(days=constants.MIN_LOCKED_PERIODS)
 
         miner.stake(amount=constants.MIN_ALLOWED_LOCKED,         # Lock the minimum amount of tokens
-                    lock_periods=constants.MIN_LOCKED_PERIODS)   # ... for the fewest number of periods
+                    expiration=expiration)
+                    # lock_periods=constants.MIN_LOCKED_PERIODS)   # ... for the fewest number of periods
 
         # Verify that the escrow is "approved" to receive tokens
-        assert mock_miner_agent.token_agent.contract.functions.allowance(miner.ether_address, mock_miner_agent.contract_address).call() == 0
+        assert mock_miner_agent.token_agent.contract.functions.allowance(miner.ether_address,
+                                                                         mock_miner_agent.contract_address).call() == 0
 
         # Staking starts after one period
         assert mock_miner_agent.contract.functions.getLockedTokens(miner.ether_address).call() == 0
@@ -37,26 +45,26 @@ class TestMiner:
 
     @pytest.mark.slow()
     @pytest.mark.usefixtures("mock_policy_agent")
-    def test_miner_collects_staking_reward_tokens(self, testerchain, miner, mock_token_agent, mock_miner_agent):
+    def test_miner_collects_staking_reward(self, deployed_testerchain, miner, mock_token_agent, mock_miner_agent):
 
         # Capture the current token balance of the miner
-        initial_balance = miner.token_balance()
-        assert mock_token_agent.get_balance(miner.ether_address) == miner.token_balance()
+        initial_balance = miner.token_balance
+        assert mock_token_agent.get_balance(miner.ether_address) == miner.token_balance
 
         miner.stake(amount=constants.MIN_ALLOWED_LOCKED,         # Lock the minimum amount of tokens
                     lock_periods=constants.MIN_LOCKED_PERIODS)   # ... for the fewest number of periods
 
         # Have other address lock tokens
-        _origin, ursula, *everybody_else = testerchain.interface.w3.eth.accounts
+        _origin, ursula, *everybody_else = deployed_testerchain.interface.w3.eth.accounts
         mock_miner_agent.spawn_random_miners(addresses=everybody_else)
 
         # ...wait out the lock period...
         for _ in range(28):
-            testerchain.time_travel(periods=1)
+            deployed_testerchain.time_travel(periods=1)
             miner.confirm_activity()
 
         # ...wait more...
-        testerchain.time_travel(periods=2)
+        deployed_testerchain.time_travel(periods=2)
         miner.mint()
         miner.collect_staking_reward()
 
