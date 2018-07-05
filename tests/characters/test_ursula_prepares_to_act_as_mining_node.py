eddcf0f5f035a1c75427e46ecb1e581a25b4d443
jMyles <justin@justinholmes.com>
2018-07-04 18:59:54 -0700

    Test for stamp evidence.

--- tests/characters/test_ursula_prepares_to_act_as_mining_node.py
+++ tests/characters/test_ursula_prepares_to_act_as_mining_node.py
@@ -10,3 +10,16 @@ def test_ursula_generates_self_signed_cert():
     cert, cert_private_key = ursula.generate_self_signed_certificate()
     public_key_numbers = ursula.public_key(SigningPower).to_cryptography_pubkey().public_numbers()
     assert cert.public_key().public_numbers() == public_key_numbers
+
+
+@pytest.mark.skip
+def test_federated_ursula_substantiates_stamp():
+    assert False
+
+
+def test_blockchain_ursula_substantiates_stamp(mining_ursulas):
+    first_ursula = list(mining_ursulas)[0]
+    signature = first_ursula.evidence_of_decentralized_identity
+    proper_public_key_for_first_ursula = signature.recover_public_key_from_msg(bytes(first_ursula.stamp))
+    proper_address_for_first_ursula = proper_public_key_for_first_ursula.to_checksum_address()
+    assert proper_address_for_first_ursula == first_ursula.checksum_public_address
