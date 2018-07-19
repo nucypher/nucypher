18b93e791f97da2abc70b47441c5d00dadc3a61d
Kieran Prasch <kieranprasch@gmail.com>
2018-07-19 16:01:42 -0700

    very thin ursula process handler

--- nucypher_cli/run_ursula.py
+++ nucypher_cli/run_ursula.py
@@ -5,38 +5,27 @@
 """
 
 import asyncio
-import os
 
-from cli.metadata import DEFAULT_SEED_NODE_DIR, collect_stored_nodes, write_node_metadata
 from nucypher.characters import Ursula
+from nucypher_cli.metadata import DEFAULT_SEED_NODE_DIR, collect_stored_nodes, write_node_metadata
 
 
-def spin_up_ursula(seed_node_dir=DEFAULT_SEED_NODE_DIR, cleanup=True):
+def spin_up_ursula(seed_node_dir=DEFAULT_SEED_NODE_DIR):
 
-    # Collect nodes from the filesystem
-    seed_nodes, other_nodes = collect_stored_nodes()
+    # Initialize #
 
-    # Start DHT loop
-    asyncio.set_event_loop(asyncio.new_event_loop())
+    seed_nodes, other_nodes = collect_stored_nodes()     # Collect known nodes
+    asyncio.set_event_loop(asyncio.new_event_loop())     # Init DHT async loop
+    ursula = Ursula.from_config(known_nodes=seed_nodes)  # Init Ursula
+    ursula.dht_listen()                                  # Start DHT
 
-    # Initialize Ursula
-    URSULA = Ursula.from_config(known_nodes=seed_nodes)
-    URSULA.dht_listen()
+    # Execute #
 
-    try:
+    write_node_metadata(node=ursula,      # Save node
+                        node_metadata_dir=seed_node_dir)
 
-        # Save node
-        metadata_filepath = write_node_metadata(node=URSULA, node_metadata_dir=seed_node_dir)
-
-        # Enter learning loop
-        URSULA.start_learning_loop()
-        URSULA.get_deployer().run()
-
-    finally:
-
-        if cleanup is True:
-            os.remove(URSULA.db_name)
-            os.remove(metadata_filepath)
+    ursula.start_learning_loop()          # Enter learning loop
+    ursula.get_deployer().run()           # Run TLS Deployer
 
 
 if __name__ == "__main__":
