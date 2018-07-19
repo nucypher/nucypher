60cf1d70d0a1f4ef504214ddc8951d956af1953f
Kieran Prasch <kieranprasch@gmail.com>
2018-07-19 11:40:28 -0700

    Create nucypher-cli entry point in setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 VERSION = '0.1'
 
-INSTALL_REQUIRES = [
-]
 
-TESTS_REQUIRE = [
-]
+# NOTE: Use Pipfile & Pipfile.lock to manage dependencies\
+INSTALL_REQUIRES = []
+TESTS_REQUIRE = []
 
 setup(name='nucypher',
       version=VERSION,
       description='A proxy re-encryption network to empower privacy in decentralized systems.',
       install_requires=INSTALL_REQUIRES,
       extras_require={'testing': TESTS_REQUIRE},
-      packages=find_packages(),
+      packages=['nucypher'],
       package_data={'nucypher': [
           'blockchain/eth/*', 'project/contracts/*',
           'blockchain/eth/sol_source/contracts/lib/*',
           'blockchain/eth/sol_source/contracts/zepellin/math/*',
           'blockchain/eth/sol_source/contracts/zepellin/ownership/*',
           'blockchain/eth/sol_source/contracts/zepellin/token/*']},
-      include_package_data=True)
+      include_package_data=True,
+      entry_points='''
+          [console_scripts]
+          nucypher-cli=cli.main:cli
+      '''
+      )
