f7877624f784c5cf37473b5bf053cff549bcf736
Kieran Prasch <kieranprasch@gmail.com>
2018-07-19 13:44:03 -0700

    Start logging with cli

---
+++ cli/__init__.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
