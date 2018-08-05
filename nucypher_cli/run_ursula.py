77308e2d8dbc1f0044a03d0eba4b2af8ae700374
Kieran Prasch <kieranprasch@gmail.com>
2018-08-04 22:06:37 -0700

    Twisted staking process handler

--- nucypher_cli/run_ursula.py
+++ nucypher_cli/run_ursula.py
@@ -1,39 +1,69 @@
-"""
-!! WARNING !!
-!! This is not an actual mining script: Don't use this to mine. !!
-
-"""
-
 import asyncio
-import sys
+
+import click
 
 from nucypher.characters import Ursula
-from nucypher_cli.metadata import DEFAULT_SEED_NODE_DIR, collect_stored_nodes, write_node_metadata
+from nucypher_cli.metadata import collect_stored_nodes, write_node_metadata
+
+RUNTIME_DIR = './.nucypher'
 
 
-def spin_up_ursula(provider_uri=None, rest_uri=None, seed_node_dir=None):
+@click.option('--rest-port', type=int)
+@click.option('--dht-port', type=int)
+@click.option('--db-name')
+@click.option('--checksum-address', type=hex)
+@click.option('--stake-index', type=int)
+@click.option('--metadata-dir')
+@click.command()
+def run_ursula(rest_port, dht_port, db_name, checksum_address, stake_index, metadata_dir) -> None:
+    """
+    ======================================
 
-    # Prep #
-    if seed_node_dir is None:
-        seed_node_dir = DEFAULT_SEED_NODE_DIR
+    ~ WARNING: DO NOT USE THIS TO MINE ~
 
-    # Initialize #
+    This is not an actual mining script -
+    it is a twisted process handler
 
-    seed_nodes, other_nodes = collect_stored_nodes()     # Collect known nodes
-    asyncio.set_event_loop(asyncio.new_event_loop())     # Init DHT async loop
-    ursula = Ursula.from_config(known_nodes=seed_nodes)  # Init Ursula
-    ursula.dht_listen()                                  # Start DHT
+    ======================================
 
-    # Execute #
 
-    write_node_metadata(node=ursula,      # Save node
-                        node_metadata_dir=seed_node_dir)
+    This process handler is executed in the main thread using the nucypher-cli,
+    implemented with twisted ProcessProtocol and spawnProcess.
 
-    ursula.start_learning_loop()          # Enter learning loop
-    ursula.get_deployer().run()           # Run TLS Deployer
+    The following procedure is required to "spin-up" an Ursula node.
+
+        1. Collect all known known from storages
+        2. Start the asyncio event loop
+        3. Initialize Ursula object
+        4. Start DHT listener
+        5. Enter the learning loop
+        6. Run TLS deployment
+        7. Start the staking daemon
+
+    Configurable values are first read from the .ini configuration file,
+    but can be overridden (mostly for testing purposes) with inline cli options.
+
+    """
+
+    known_nodes, other_nodes = collect_stored_nodes()  # 1. Collect known nodes
+    asyncio.set_event_loop(asyncio.new_event_loop())   # 2. Init DHT async loop
+
+    # 3. Initialize Ursula (includes overrides)
+    ursula = Ursula.from_config(known_nodes=known_nodes,
+                                rest_port=rest_port,
+                                dht_port=dht_port,
+                                db_name=db_name,
+                                checksum_address=checksum_address,
+                                stake_index=stake_index)
+
+    ursula.dht_listen()           # 4. Start DHT
+    write_node_metadata(node=ursula, node_metadata_dir=metadata_dir)
+
+    ursula.start_learning_loop()  # 5. Enter learning loop
+    ursula.get_deployer().run()   # 6. Run TLS Deployer
+
+    # ursula.stake()              # TODO: 7. start staking daemon
 
 
 if __name__ == "__main__":
-    _, *args = sys.argv
-
-    spin_up_ursula()
+    run_ursula()
