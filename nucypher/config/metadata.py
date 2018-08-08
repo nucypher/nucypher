7ffc76837a8963f546065889a4bd2067074a2e13
Kieran Prasch <kieranprasch@gmail.com>
2018-08-07 18:14:28 -0700

    glob node metadata inside users datadir, split into 2 buckets.

--- nucypher/config/metadata.py
+++ nucypher/config/metadata.py
@@ -1,24 +1,30 @@
 import binascii
 import os
+from glob import glob
+from os.path import abspath
 from typing import Set
 
-from nucypher.characters import Ursula
-from nucypher.config.constants import DEFAULT_SEED_NODE_DIR
+from nucypher.characters import Ursula, Character
+from nucypher.config.constants import DEFAULT_SEED_NODE_DIR, DEFAULT_KNOWN_NODE_DIR, DEFAULT_CONFIG_ROOT
 
 
-def read_node_metadata(filepath) -> Ursula:
+def read_node_metadata(filepath: str, federated_only=False) -> Ursula:
+
+    """Init one ursula from node storage file"""
 
     with open(filepath, "r") as seed_file:
         seed_file.seek(0)
-        seed_node_bytes = binascii.unhexlify(seed_file.read())
-        node = Ursula.from_bytes(seed_node_bytes, federated_only=True)
+        node_bytes = binascii.unhexlify(seed_file.read())
+
+        node = Ursula.from_bytes(node_bytes, federated_only=federated_only)
         return node
 
 
-def write_node_metadata(node, node_metadata_dir: str) -> str:
+def write_node_metadata(seed_node: bool, node: Ursula, node_metadata_dir: str) -> str:
 
     filename = "node-metadata-{}".format(node.rest_interface.port)
-    metadata_filepath = os.path.join(node_metadata_dir, filename)
+    node_type = 'known' if not seed_node else 'seed'
+    metadata_filepath = os.path.join(node_metadata_dir, '{}_nodes'.format(node_type), filename)
 
     with open(metadata_filepath, "w") as f:
         f.write(bytes(node).hex())
@@ -26,24 +32,27 @@ def write_node_metadata(node, node_metadata_dir: str) -> str:
     return metadata_filepath
 
 
-def read_metadata_dir(node_metadata_dir: str) -> Set[Ursula]:
-
-    try:
-        seed_node_files = os.listdir(node_metadata_dir)
-    except FileNotFoundError:
-        raise RuntimeError("No seed node metadata found at {}".format(node_metadata_dir))
-
-    nodes = set()
-    for seed_node_file in seed_node_files:
-        node = read_node_metadata(filepath=seed_node_file)
-        nodes.add(node)
-
-    return nodes
-
-
-def collect_stored_nodes(seed_node_dir=DEFAULT_SEED_NODE_DIR) -> tuple:
+def collect_stored_nodes(config_root=DEFAULT_CONFIG_ROOT,
+                         seed_nodes_dirs=None,
+                         known_nodes_dirs: list=None,
+                         federated_only=False) -> tuple:
     """Collect stored node data from multiple sources and aggregate them into known node sets"""
 
-    seed_nodes = read_metadata_dir(node_metadata_dir=seed_node_dir)
-    other_nodes = set()
-    return seed_nodes, other_nodes
+    if known_nodes_dirs is None:
+        known_nodes_dirs = [DEFAULT_KNOWN_NODE_DIR]
+
+    if seed_nodes_dirs is None:
+        seed_nodes_dirs = [DEFAULT_SEED_NODE_DIR]
+
+    # TODO: use seed bucket?
+    _all_node_dirs = known_nodes_dirs + seed_nodes_dirs
+
+    glob_pattern = os.path.join(config_root, '*', 'node-metadata-*',)
+    metadata_paths = sorted(glob(glob_pattern), key=os.path.getctime)
+
+    nodes = list()
+    for metadata_path in metadata_paths:
+        node = read_node_metadata(filepath=abspath(metadata_path), federated_only=federated_only)
+        nodes.append(node)
+
+    return tuple(nodes)
