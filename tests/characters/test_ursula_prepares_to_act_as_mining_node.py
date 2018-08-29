b40707eeeb8936314cda2d90978ce1a9f5a19384
jMyles <justin@justinholmes.com>
2018-08-29 14:19:37 +0200

    Uncorked stake (ie, Ursula doesn't stake really for tests) and removed skips.

--- tests/characters/test_ursula_prepares_to_act_as_mining_node.py
+++ tests/characters/test_ursula_prepares_to_act_as_mining_node.py
@@ -7,7 +7,7 @@ from nucypher.utilities.blockchain import make_ursulas
 from nucypher.utilities.network import MockRestMiddleware
 
 
-@pytest.mark.skip
+@pytest.mark.skip("To be implemented...?")
 def test_federated_ursula_substantiates_stamp():
     assert False
 
@@ -34,7 +34,6 @@ def test_new_ursula_announces_herself(testerchain):
     assert ursula_in_a_house in ursula_with_a_mouse._known_nodes.values()
 
 
-@pytest.mark.skip
 def test_blockchain_ursula_substantiates_stamp(mining_ursulas):
     first_ursula = list(mining_ursulas)[0]
     signature_as_bytes = first_ursula._evidence_of_decentralized_identity
@@ -47,7 +46,6 @@ def test_blockchain_ursula_substantiates_stamp(mining_ursulas):
     assert first_ursula._stamp_has_valid_wallet_signature
 
 
-@pytest.mark.skip
 def test_blockchain_ursula_verifies_stamp(mining_ursulas):
     first_ursula = list(mining_ursulas)[0]
 
@@ -59,7 +57,6 @@ def test_blockchain_ursula_verifies_stamp(mining_ursulas):
     assert first_ursula.verified_stamp
 
 
-@pytest.mark.skip
 def test_vladimir_cannot_verify_interface_with_ursulas_signing_key(mining_ursulas):
     his_target = list(mining_ursulas)[4]
 
@@ -88,7 +85,6 @@ def test_vladimir_cannot_verify_interface_with_ursulas_signing_key(mining_ursula
         vladimir.validate_metadata()
 
 
-@pytest.mark.skip
 def test_vladimir_uses_his_own_signing_key(alice, mining_ursulas):
     """
     Similar to the attack above, but this time Vladimir makes his own interface signature
