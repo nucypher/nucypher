dea99a9847d4bd3dc2808991ddaad5ecf64235f7
jMyles <justin@justinholmes.com>
2018-09-12 14:02:44 +0200

    Making some middleware methods less secret so that they can be used in tests.

--- nucypher/utilities/sandbox.py
+++ nucypher/utilities/sandbox.py
@@ -182,15 +182,15 @@ class MockRestMiddleware(RestMiddleware):
     class NotEnoughMockUrsulas(MinerAgent.NotEnoughMiners):
         pass
 
-    def __get_mock_client_by_ursula(self, ursula):
+    def _get_mock_client_by_ursula(self, ursula):
         port = ursula.rest_information()[0].port
-        return self.__get_mock_client_by_port(port)
+        return self._get_mock_client_by_port(port)
 
-    def __get_mock_client_by_url(self, url):
+    def _get_mock_client_by_url(self, url):
         port = int(url.split(":")[1])
-        return self.__get_mock_client_by_port(port)
+        return self._get_mock_client_by_port(port)
 
-    def __get_mock_client_by_port(self, port):  # TODO
+    def _get_mock_client_by_port(self, port):  # TODO
         try:
             ursula = _TEST_KNOWN_URSULAS_CACHE[port]
             rest_app = ursula.rest_app
@@ -201,34 +201,34 @@ class MockRestMiddleware(RestMiddleware):
         return mock_client
 
     def consider_arrangement(self, arrangement=None):
-        mock_client = self.__get_mock_client_by_ursula(arrangement.ursula)
+        mock_client = self._get_mock_client_by_ursula(arrangement.ursula)
         response = mock_client.post("http://localhost/consider_arrangement", bytes(arrangement))
         assert response.status_code == 200
         return response
 
     def enact_policy(self, ursula, id, payload):
-        mock_client = self.__get_mock_client_by_ursula(ursula)
+        mock_client = self._get_mock_client_by_ursula(ursula)
         response = mock_client.post('http://localhost/kFrag/{}'.format(id.hex()), payload)
         assert response.status_code == 200
         return True, ursula.stamp.as_umbral_pubkey()
 
     def send_work_order_payload_to_ursula(self, work_order):
-        mock_client = self.__get_mock_client_by_ursula(work_order.ursula)
+        mock_client = self._get_mock_client_by_ursula(work_order.ursula)
         payload = work_order.payload()
         id_as_hex = work_order.arrangement_id.hex()
         return mock_client.post('http://localhost/kFrag/{}/reencrypt'.format(id_as_hex), payload)
 
     def get_treasure_map_from_node(self, node, map_id):
-        mock_client = self.__get_mock_client_by_ursula(node)
+        mock_client = self._get_mock_client_by_ursula(node)
         return mock_client.get("http://localhost/treasure_map/{}".format(map_id))
 
     def node_information(self, host, port):
-        mock_client = self.__get_mock_client_by_port(port)
+        mock_client = self._get_mock_client_by_port(port)
         response = mock_client.get("http://localhost/public_information")
         return response
 
     def get_nodes_via_rest(self, url, announce_nodes=None, nodes_i_need=None):
-        mock_client = self.__get_mock_client_by_url(url)
+        mock_client = self._get_mock_client_by_url(url)
 
         if nodes_i_need:
             # TODO: This needs to actually do something.
