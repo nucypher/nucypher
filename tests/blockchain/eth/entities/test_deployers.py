754eae6e3d32ebf591b4f24eed16ed753eea9760
Kieran Prasch <kieranprasch@gmail.com>
2018-09-27 19:52:46 -0700

    Inreaces test coverage for deployer and agent.

--- tests/blockchain/eth/entities/test_deployers.py
+++ tests/blockchain/eth/entities/test_deployers.py
@@ -41,40 +41,98 @@ def test_token_deployer_and_agent(testerchain):
     assert token_agent.contract_address == same_token_agent.contract_address
     assert token_agent == same_token_agent  # __eq__
 
-    testerchain.interface._registry.clear()
+    testerchain.interface.registry.clear()
 
 
 @pytest.mark.slow()
 def test_deploy_ethereum_contracts(testerchain):
-    """
-    A bare minimum nucypher deployment fixture.
-    """
+
     origin, *everybody_else = testerchain.interface.w3.eth.accounts
 
+    #
+    # Nucypher Token
+    #
     token_deployer = NucypherTokenDeployer(blockchain=testerchain, deployer_address=origin)
+    assert token_deployer.deployer_address == origin
+
+    with pytest.raises(ContractDeployer.ContractDeploymentError):
+        assert token_deployer.contract_address is constants.CONTRACT_NOT_DEPLOYED
+    assert not token_deployer.is_armed
+    assert not token_deployer.is_deployed
+
     token_deployer.arm()
+    assert token_deployer.is_armed
+
     token_deployer.deploy()
+    assert token_deployer.is_deployed
+    assert len(token_deployer.contract_address) == 42
 
     token_agent = NucypherTokenAgent(blockchain=testerchain)
+    assert len(token_agent.contract_address) == 42
+    assert token_agent.contract_address == token_deployer.contract_address
+
+    another_token_agent = token_deployer.make_agent()
+    assert len(another_token_agent.contract_address) == 42
+    assert another_token_agent.contract_address == token_deployer.contract_address == token_agent.contract_address
+
+    #
+    # Miner Escrow
+    #
 
     miners_escrow_secret = os.urandom(DISPATCHER_SECRET_LENGTH)
     miner_escrow_deployer = MinerEscrowDeployer(
         token_agent=token_agent,
         deployer_address=origin,
         secret_hash=testerchain.interface.w3.sha3(miners_escrow_secret))
+    assert miner_escrow_deployer.deployer_address == origin
+
+    with pytest.raises(ContractDeployer.ContractDeploymentError):
+        assert miner_escrow_deployer.contract_address is constants.CONTRACT_NOT_DEPLOYED
+    assert not miner_escrow_deployer.is_armed
+    assert not miner_escrow_deployer.is_deployed
+
     miner_escrow_deployer.arm()
+    assert miner_escrow_deployer.is_armed
+
     miner_escrow_deployer.deploy()
+    assert miner_escrow_deployer.is_deployed
+    assert len(miner_escrow_deployer.contract_address) == 42
 
     miner_agent = MinerAgent(token_agent=token_agent)
+    assert len(miner_agent.contract_address) == 42
+    assert miner_agent.contract_address == miner_escrow_deployer.contract_address
 
+    another_miner_agent = miner_escrow_deployer.make_agent()
+    assert len(another_miner_agent.contract_address) == 42
+    assert another_miner_agent.contract_address == miner_escrow_deployer.contract_address == miner_agent.contract_address
+
+
+    #
+    # Policy Manager
+    #
     policy_manager_secret = os.urandom(DISPATCHER_SECRET_LENGTH)
     policy_manager_deployer = PolicyManagerDeployer(
         miner_agent=miner_agent,
         deployer_address=origin,
         secret_hash=testerchain.interface.w3.sha3(policy_manager_secret))
+    assert policy_manager_deployer.deployer_address == origin
+
+    with pytest.raises(ContractDeployer.ContractDeploymentError):
+        assert policy_manager_deployer.contract_address is constants.CONTRACT_NOT_DEPLOYED
+    assert not policy_manager_deployer.is_armed
+    assert not policy_manager_deployer.is_deployed
+
     policy_manager_deployer.arm()
+    assert policy_manager_deployer.is_armed
+
     policy_manager_deployer.deploy()
+    assert policy_manager_deployer.is_deployed
+    assert len(policy_manager_deployer.contract_address) == 42
 
     policy_agent = policy_manager_deployer.make_agent()
+    assert len(policy_agent.contract_address) == 42
+    assert policy_agent.contract_address == policy_manager_deployer.contract_address
 
-    # TODO: assert
+    another_policy_agent = policy_manager_deployer.make_agent()
+    assert len(another_policy_agent.contract_address) == 42
+    assert another_policy_agent.contract_address == policy_manager_deployer.contract_address == policy_agent.contract_address
