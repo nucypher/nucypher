014a1094ab16d928401fc5496532e101b17e625e
szotov <zotov89@mail.ru>
2018-10-09 18:36:39 +0300

    Added methods to verify signature on-chain

--- tests/blockchain/eth/contracts/contracts/SignatureVerifierMock.sol
+++ tests/blockchain/eth/contracts/contracts/SignatureVerifierMock.sol
@@ -17,4 +17,16 @@ contract SignatureVerifierMock {
         return SignatureVerifier.recover(_hash, _signature);
     }
 
+    function toAddress(bytes _publicKey) public pure returns (address) {
+        return SignatureVerifier.toAddress(_publicKey);
+    }
+
+    function verify(bytes _message, bytes _signature, bytes _publicKey, SignatureVerifier.HashAlgorithm _algorithm)
+        public
+        pure
+        returns (bool)
+    {
+        return SignatureVerifier.verify(_message, _signature, _publicKey, _algorithm);
+    }
+
 }
