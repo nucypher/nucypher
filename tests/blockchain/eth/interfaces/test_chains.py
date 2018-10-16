b0ed140eaa8600f172637338050d3e9ba4d335b6
Kieran Prasch <kieranprasch@gmail.com>
2018-10-16 12:51:38 -0700

    Agency transaction usage bug fixes

--- tests/blockchain/eth/interfaces/test_chains.py
+++ tests/blockchain/eth/interfaces/test_chains.py
@@ -1,6 +1,3 @@
-from nucypher.blockchain.eth.deployers import NucypherTokenDeployer
-
-
 def test_testerchain_creation(testerchain):
     # Ensure we are testing on the correct network...
     assert 'tester' in testerchain.interface.provider_uri
@@ -9,9 +6,3 @@ def test_testerchain_creation(testerchain):
     assert testerchain.interface.w3.eth.blockNumber >= 0
 
 
-def test_nucypher_contract_compiled(testerchain):
-    # Ensure that solidity smart contacts are available, post-compile.
-    origin, *everybody_else = testerchain.interface.w3.eth.accounts
-
-    token_contract_identifier = NucypherTokenDeployer(blockchain=testerchain, deployer_address=origin)._contract_name
-    assert token_contract_identifier in testerchain.interface._BlockchainInterface__raw_contract_cache
