bff460e4b626459093d28268ca41a284985e8633
szotov <zotov89@mail.ru>
2018-10-25 16:45:28 +0300

    Small refactoring and renaming

--- tests/blockchain/eth/contracts/main/challenge/test_challenge_overseer.py
+++ tests/blockchain/eth/contracts/main/challenge/test_challenge_overseer.py
@@ -2,16 +2,14 @@ import os
 
 import coincurve
 import pytest
-from eth_tester.exceptions import TransactionFailed
-from eth_utils import to_canonical_address
-from umbral import pre, keys
-
-from umbral.keys import UmbralPrivateKey
 from cryptography.hazmat.backends.openssl import backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
-from umbral.signing import Signature, Signer
-from itertools import chain
+from eth_tester.exceptions import TransactionFailed
+from eth_utils import to_canonical_address
+
+from umbral.keys import UmbralPrivateKey
+from umbral.signing import Signature
 
 
 def sign_data(data, umbral_privkey):
@@ -119,74 +117,3 @@ def test_challenge_cfrag(testerchain, escrow, challenge_contract):
         tx = challenge_contract.functions.challengeCFrag(
             capsule, cfrag, recoverable_signature, umbral_pubkey_bytes, signed_umbral_pubkey).transact()
         testerchain.wait_for_receipt(tx)
-
-
-@pytest.mark.slow
-def test_serialization(testerchain):
-    contract, _ = testerchain.interface.deploy_contract('SerializationLibraryMock')
-
-    capsule_bytes = os.urandom(98)
-    result = contract.functions.toOriginalCapsule(capsule_bytes).call()
-    assert capsule_bytes == bytes().join(result)
-
-    delegating_privkey = keys.UmbralPrivateKey.gen_key()
-    # params = delegating_privkey.params
-    _symmetric_key, capsule = pre._encapsulate(delegating_privkey.get_pubkey())
-    capsule_bytes = capsule.to_bytes()
-    result = contract.functions.toOriginalCapsule(capsule_bytes).call()
-    assert bytes(capsule._point_e) == result[0]
-    assert bytes(capsule._point_v) == result[1]
-    assert capsule._bn_sig.to_bytes() == result[2]
-
-    proof_bytes = os.urandom(228)
-    result = contract.functions.toCorrectnessProof(proof_bytes).call()
-    assert proof_bytes == bytes().join(result)
-
-    proof_bytes = os.urandom(270)
-    result = contract.functions.toCorrectnessProof(proof_bytes).call()
-    assert proof_bytes == bytes().join(result)
-
-    signing_privkey = keys.UmbralPrivateKey.gen_key()
-    signer = Signer(signing_privkey)
-
-    priv_key_bob = keys.UmbralPrivateKey.gen_key()
-    pub_key_bob = priv_key_bob.get_pubkey()
-
-    kfrags = pre.split_rekey(delegating_privkey, signer, pub_key_bob, 1, 2)
-    metadata = b"This is an example of metadata for re-encryption request"
-
-    cfrag = pre.reencrypt(kfrags[0], capsule, metadata=metadata)
-    proof = cfrag.proof
-    proof_bytes = proof.to_bytes()
-
-    result = contract.functions.toCorrectnessProof(proof_bytes).call()
-    assert bytes(proof._point_e2) == result[0]
-    assert bytes(proof._point_v2) == result[1]
-    assert bytes(proof._point_kfrag_commitment) == result[2]
-    assert bytes(proof._point_kfrag_pok) == result[3]
-    assert proof.bn_sig.to_bytes() == result[4]
-    assert bytes(proof.kfrag_signature) == result[5]
-    assert bytes(proof.metadata) == result[6]
-
-    cfrag_bytes = os.urandom(164)
-    full_cfrag_bytes = cfrag_bytes + proof_bytes
-    result = contract.functions.toCapsuleFrag(full_cfrag_bytes).call()
-    assert cfrag_bytes == bytes().join(result)
-    result = contract.functions.toCorrectnessProofFromCapsuleFrag(full_cfrag_bytes).call()
-    assert proof_bytes == bytes().join(result)
-
-    cfrag_bytes = cfrag.to_bytes()
-    result = contract.functions.toCapsuleFrag(cfrag_bytes).call()
-    assert bytes(cfrag._point_e1) == result[0]
-    assert bytes(cfrag._point_v1) == result[1]
-    assert bytes(cfrag._kfrag_id) == result[2]
-    assert bytes(cfrag._point_noninteractive) == result[3]
-    assert bytes(cfrag._point_xcoord) == result[4]
-    result = contract.functions.toCorrectnessProofFromCapsuleFrag(cfrag_bytes).call()
-    assert bytes(proof._point_e2) == result[0]
-    assert bytes(proof._point_v2) == result[1]
-    assert bytes(proof._point_kfrag_commitment) == result[2]
-    assert bytes(proof._point_kfrag_pok) == result[3]
-    assert proof.bn_sig.to_bytes() == result[4]
-    assert bytes(proof.kfrag_signature) == result[5]
-    assert bytes(proof.metadata) == result[6]
