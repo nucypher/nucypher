b1cb9cfbe48f57bf3c70cb954130551715137af2
Kieran Prasch <kieranprasch@gmail.com>
2018-11-04 14:08:31 +0100

    Optional sentry reporting via simple twisted log observer

---
+++ nucypher/utilities/logging.py
@@ -0,0 +1,25 @@
+from twisted.logger import ILogObserver
+from zope.interface import provider
+from nucypher.config.constants import REPORT_TO_SENTRY
+
+
+@provider(ILogObserver)
+def simpleObserver(event):
+    print(event)
+
+
+if REPORT_TO_SENTRY:
+    from twisted.python import log
+    from sentry_sdk import Client, capture_exception, add_breadcrumb
+    from nucypher.config.constants import NUCYPHER_SENTRY_ENDPOINT
+
+    client = Client(dsn=NUCYPHER_SENTRY_ENDPOINT)
+
+    def logToSentry(event):
+        if not event.get('isError') or 'failure' not in event:
+            add_breadcrumb(event)
+            return
+        f = event['failure']
+        capture_exception((f.type, f.value, f.getTracebackObject()))
+
+    log.addObserver(logToSentry)
