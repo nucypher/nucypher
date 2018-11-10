01dda169d292c02aa54495e8ebb10fa20c50d968
Kieran Prasch <kieran@nucypher.com>
2018-11-10 13:41:10 -0800

    Pin eth-utils version

--- examples/finnegans_wake_demo/download_finnegans_wake.sh
+++ examples/finnegans_wake_demo/download_finnegans_wake.sh
@@ -2,4 +2,4 @@
 
 echo "Downloading Finnegan's Wake Text..."
 wget "https://github.com/nucypher/nucypher-kms/files/1765576/finnegans-wake.txt" -O ./finnegans-wake.txt
-echo "Successfully downloaded. To run the demo execute 'python finnegans-wake-federated.py'"
+echo "Successfully downloaded. To run the demo execute 'python finnegans-wake-concise-demo.py'"
