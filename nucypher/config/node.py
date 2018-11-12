04159208c317840ba500d930f4aba970e7aaadfe
Kieran Prasch <kieran@nucypher.com>
2018-11-12 08:06:49 -0800

    Adds missing web status template; Fixes #552

--- nucypher/config/node.py
+++ nucypher/config/node.py
@@ -501,7 +501,7 @@ class NodeConfiguration(ABC):
                     message = "There are existing files located at {}".format(self.config_root)
                     raise self.ConfigurationError(message)
             except FileNotFoundError:
-                message = "Cannot write configuration files because the directory {} does not exist."
+                message = "Cannot write configuration files because the directory {} does not exist.".format(self.config_root)
                 raise self.ConfigurationError(message)
 
         #
