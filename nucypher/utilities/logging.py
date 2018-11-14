fd084de0e61166e33d7fe8cd98e42e82506a79b1
Kieran Prasch <kieranprasch@gmail.com>
2018-11-14 17:37:38 +0100

    Twisted log observer utilities; TextFile, Json, Sentry.

--- nucypher/utilities/logging.py
+++ nucypher/utilities/logging.py
@@ -16,20 +16,63 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 
-from sentry_sdk import Client, capture_exception, add_breadcrumb
-from twisted.logger import ILogObserver
+import datetime
+import pathlib
+
+from sentry_sdk import capture_exception, add_breadcrumb
+from twisted.logger import FileLogObserver, jsonFileLogObserver
+from twisted.python.log import ILogObserver
+from twisted.python.logfile import DailyLogFile
 from zope.interface import provider
+
+from nucypher.config.constants import USER_LOG_DIR
+
+
+def formatUrsulaLogEvent(event):
+    """
+    Format log lines for file logging.
+    """
+    human_time = datetime.datetime.fromtimestamp(event.get('log_time')).strftime('%c')
+
+    log_line = '{} [{}] ({}): {}\n'.format(event.get('log_level').name.upper(),
+                                           human_time,
+                                           event.get('log_namespace'),
+                                           event.get('log_format'))
+    return log_line
+
+
+def _get_or_create_user_log_dir():
+    return pathlib.Path(USER_LOG_DIR).mkdir(parents=True, exist_ok=True)
+
+
+def getJsonFileObserver():
+    _get_or_create_user_log_dir()
+    logfile = DailyLogFile("ursula.log.json", USER_LOG_DIR)
+    observer = jsonFileLogObserver(outFile=logfile)
+    return observer
+
+
+def getTextFileObserver():
+    _get_or_create_user_log_dir()
+    logfile = DailyLogFile("ursula.log", USER_LOG_DIR)
+    observer = FileLogObserver(formatEvent=formatUrsulaLogEvent, outFile=logfile)
+    return observer
 
 
 @provider(ILogObserver)
 def simpleObserver(event):
-    message = '{} ({}): {}'.format(event.get('log_level').name.upper(),
-                                   event.get('log_namespace'),
-                                   event.get('log_format'))
+    message = '{level} ({source}): {message}'.format(level=event.get('log_level').name.upper(),
+                                                     source=event.get('log_namespace'),
+                                                     message=event.get('log_format'))
     print(message)
 
 
+@provider(ILogObserver)
 def logToSentry(event):
+    """
+    Twisted observer for Sentry...
+    Capture tracebacks and leave a trail of breadcrumbs.
+    """
 
     # Handle Logs
     if not event.get('isError') or 'failure' not in event:
