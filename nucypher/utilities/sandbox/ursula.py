efdfcf7218599b7ea2032439ac723f955f433c11
Kieran Prasch <kieran@nucypher.com>
2018-11-14 20:23:00 -0800

    Rip Ursula Protocol out of utilities

--- nucypher/utilities/sandbox/ursula.py
+++ nucypher/utilities/sandbox/ursula.py
@@ -14,28 +14,20 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
-import os
+
 import random
-import sys
 
-import maya
-import time
-from os import linesep
+from typing import Union, Set
 
-import click
-from eth_utils import to_checksum_address
-from twisted.internet import reactor
-from twisted.protocols.basic import LineReceiver
-from typing import Set, Union
-
-from nucypher.blockchain.eth import constants
+from nucypher.blockchain.eth.constants import MIN_ALLOWED_LOCKED, MIN_LOCKED_PERIODS, MAX_MINTING_PERIODS
 from nucypher.characters.lawful import Ursula
 from nucypher.config.characters import UrsulaConfiguration
-from nucypher.config.constants import SEEDNODES
 from nucypher.crypto.api import secure_random
-from nucypher.utilities.sandbox.constants import (DEFAULT_NUMBER_OF_URSULAS_IN_DEVELOPMENT_NETWORK,
-                                                  TEST_URSULA_STARTING_PORT,
-                                                  TEST_KNOWN_URSULAS_CACHE)
+from nucypher.utilities.sandbox.constants import (
+    TEST_KNOWN_URSULAS_CACHE,
+    TEST_URSULA_STARTING_PORT,
+    DEFAULT_NUMBER_OF_URSULAS_IN_DEVELOPMENT_NETWORK
+)
 
 
 def make_federated_ursulas(ursula_config: UrsulaConfiguration,
@@ -96,11 +88,11 @@ def make_decentralized_ursulas(ursula_config: UrsulaConfiguration,
                                        **ursula_overrides)
         if stake is True:
 
-            min_stake, balance = int(constants.MIN_ALLOWED_LOCKED), ursula.token_balance
+            min_stake, balance = MIN_ALLOWED_LOCKED, ursula.token_balance
             amount = random.randint(min_stake, balance)
 
             # for a random lock duration
-            min_locktime, max_locktime = int(constants.MIN_LOCKED_PERIODS), int(constants.MAX_MINTING_PERIODS)
+            min_locktime, max_locktime = MIN_LOCKED_PERIODS, MAX_MINTING_PERIODS
             periods = random.randint(min_locktime, max_locktime)
 
             ursula.initialize_stake(amount=amount, lock_periods=periods)
@@ -118,117 +110,4 @@ def make_decentralized_ursulas(ursula_config: UrsulaConfiguration,
                 ursula_to_teach.remember_node(ursula_to_learn_about)
 
     return ursulas
-
-
-class UrsulaCommandProtocol(LineReceiver):
-
-    delimiter = linesep.encode("ascii")
-    encoding = 'utf-8'
-
-    width = 80
-    height = 24
-
-    commands = (
-        'status',
-        'stop',
-
-    )
-
-    def __init__(self, ursula):
-        self.ursula = ursula
-        self.start_time = maya.now()
-        super().__init__()
-
-    def _paint_known_nodes(self):
-        # Gather Data
-        known_nodes = self.ursula.known_nodes
-        known_certificate_files = os.listdir(self.ursula.known_certificates_dir)
-        number_of_known_nodes = len(known_nodes)
-        seen_nodes = len(known_certificate_files)
-
-        # Operating Mode
-        federated_only = self.ursula.federated_only
-        if federated_only:
-            click.secho("Configured in Federated Only mode", fg='green')
-
-        # Heading
-        label = "Known Nodes (connected {} / seen {})".format(number_of_known_nodes, seen_nodes)
-        heading = '\n' + label + " " * (45 - len(label)) + "Last Seen    "
-        click.secho(heading, bold=True, nl=False)
-
-        # Legend
-        color_index = {
-            'self': 'yellow',
-            'known': 'white',
-            'seednode': 'blue'
-        }
-        for node_type, color in color_index.items():
-            click.secho('{0:<6} | '.format(node_type), fg=color, nl=False)
-        click.echo('\n')
-
-        seednode_addresses = list(bn.checksum_address for bn in SEEDNODES)
-        for address, node in known_nodes.items():
-            row_template = "{} | {} | {} | {} | {}"
-            node_type = 'known'
-            if node.checksum_public_address == self.ursula.checksum_public_address:
-                node_type = 'self'
-                row_template += ' ({})'.format(node_type)
-            elif node.checksum_public_address in seednode_addresses:
-                node_type = 'seednode'
-                row_template += ' ({})'.format(node_type)
-            click.secho(row_template.format(node.checksum_public_address,
-                                            node.rest_url().ljust(20),  # TODO: Maybe put this 20 somewhere
-                                            node.nickname.ljust(50),
-                                            node.timestamp,
-                                            node.last_seen,
-                                            ), fg=color_index[node_type])
-
-    def paintStatus(self):
-        stats = ['Ursula {}'.format(self.ursula.checksum_public_address),
-                 '-'*50,
-                 'Uptime: {}'.format(maya.now() - self.start_time), # TODO
-                 'Learning Round: {}'.format(self.ursula._learning_round),
-                 'Operating Mode: {}'.format('Federated' if self.ursula.federated_only else 'Decentralized'),  # TODO
-                 'Rest Interface {}'.format(self.ursula.rest_url()),
-                 'Node Storage Type {}:'.format(self.ursula.node_storage._name.capitalize()),
-                 'Known Nodes: {}'.format(len(self.ursula.known_nodes)),
-                 'Work Orders: {}'.format(len(self.ursula._work_orders))]
-
-        if self.ursula._current_teacher_node:
-            teacher = 'Current Teacher: {}: ({})'.format(self.ursula._current_teacher_node,
-                                                      self.ursula._current_teacher_node.rest_url())
-            stats.append(teacher)
-
-        click.echo('\n'+'\n'.join(stats))
-
-    def connectionMade(self):
-        message = '\nConnected to node console {}@{}'.format(self.ursula.checksum_public_address,
-                                                             self.ursula.rest_url())
-        click.secho(message, fg='yellow')
-        click.secho("Type 'help' or '?' for help")
-        self.transport.write(b'Ursula >>> ')
-
-    def lineReceived(self, line):
-        line = line.decode(encoding=self.encoding).strip().lower()
-
-        commands = {
-            'known_nodes': self._paint_known_nodes,
-            'status': self.paintStatus,
-            'stop': reactor.stop,
-            'cycle_teacher': self.ursula.cycle_teacher_node
-        }
-
-        try:
-            commands[line]()
-        except KeyError:
-            click.secho("Invalid input. Options are {}".format(', '.join(commands)))
-
-        self.transport.write(b'Ursula >>> ')
-
-    def terminalSize(self, width, height):
-        self.width = width
-        self.height = height
-        self.terminal.eraseDisplay()
-        self._redraw()
-
 
