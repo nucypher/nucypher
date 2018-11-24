3806fe68f7ba44c24892bdc871208ae4df097206
Kieran Prasch <kieran@nucypher.com>
2018-11-23 21:12:06 -0800

    Integrated test entry point configuration and executable

---
+++ tests/run_tests.py
@@ -0,0 +1,41 @@
+"""
+This file is part of nucypher.
+
+nucypher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+nucypher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+import os
+from os.path import abspath, dirname
+
+import maya
+import pytest
+
+
+class NucypherPytestRunner:
+    TEST_PATH = os.path.join(abspath(dirname(__file__)), 'cli')
+    PYTEST_ARGS = ['--verbose', '--runslow', TEST_PATH]
+
+    def pytest_sessionstart(self):
+        print("*** Running Nucypher CLI Tests ***")
+        self.start_time = maya.now()
+
+    def pytest_sessionfinish(self):
+        duration = maya.now() - self.start_time
+        print("*** Nucypher Test Run Report ***")
+        print("""Run Duration ... {}""".format(duration))
+
+
+def run():
+    pytest.main(NucypherPytestRunner.PYTEST_ARGS, plugins=[NucypherPytestRunner()])
