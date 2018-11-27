156f4717e9eb3e8f82ef379023046df3c64e48bc
David Núñez <david@nucypher.com>
2018-11-27 13:47:21 +0100

    Comments and code reorganization

--- examples/heartbeat_demo/alicia.py
+++ examples/heartbeat_demo/alicia.py
@@ -4,20 +4,17 @@ from nucypher.config.storages import LocalFileBasedNodeStorage
 from nucypher.crypto.powers import EncryptingPower, SigningPower
 from nucypher.network.middleware import RestMiddleware
 from nucypher.utilities.logging import simpleObserver
-from umbral.keys import UmbralPublicKey
-
-from doctor_keys import get_doctor_pubkeys
 
 import datetime
 import os
 import shutil
 import maya
 import json
+import sys
 from twisted.logger import globalLogPublisher
 
 
-POLICY_FILENAME = "policy.json"
-
+POLICY_FILENAME = "policy-metadata.json"
 
 ######################
 # Boring setup stuff #
@@ -30,29 +27,22 @@ globalLogPublisher.addObserver(simpleObserver)
 TEMP_ALICE_DIR = "{}/alicia-files".format(os.path.dirname(os.path.abspath(__file__)))
 TEMP_URSULA_CERTIFICATE_DIR = "{}/ursula-certs".format(TEMP_ALICE_DIR)
 
-
-SEEDNODE_URL = "SEEDNODE URL ..."
-
-
-# Here are our Policy details.
-policy_end_datetime = maya.now() + datetime.timedelta(days=5)
-m, n = 3, 5
-label = b"label-"+os.urandom(4).hex().encode()
-
-######################################
-# Alice, the Authority of the Policy #
-######################################
+# We expect the url of the seednode as the first argument.
+SEEDNODE_URL = sys.argv[1]
 
 
+#######################################
+# Alicia, the Authority of the Policy #
+#######################################
+
+
+# We get a persistent Alice.
 passphrase = "TEST_ALICIA_INSECURE_DEVELOPMENT_PASSWORD"
-
-
 try:  # If we had an existing Alicia in disk, let's get it from there
     alice_config_file = os.path.join(TEMP_ALICE_DIR, "config_root", "alice.config")
     new_alice_config = AliceConfiguration.from_configuration_file(
             filepath=alice_config_file,
             network_middleware=RestMiddleware(),
-            #known_nodes={ursula},
             start_learning_now=False,
             save_metadata=False,
         )
@@ -67,7 +57,6 @@ except:  # If anything fails, let's create Alicia from scratch
                                              certificates_directory=TEMP_URSULA_CERTIFICATE_DIR,
                                              federated_only=True,
                                              minimum_stake=0)
-
 
     # Let's create an Alice from a Configuration.
     # This requires creating a local storage for her first.
@@ -87,49 +76,76 @@ except:  # If anything fails, let's create Alicia from scratch
         known_nodes={ursula},
         start_learning_now=False,
         federated_only=True,
-        #save_metadata=False,
-        #load_metadata=False,
         learn_on_same_thread=True,
     )
     alicia = alice_config(passphrase=passphrase,
                           known_certificates_dir=TEMP_URSULA_CERTIFICATE_DIR,
                           )
 
-    # We will save Alice's config to a file for later use
+    # We will save Alicia's config to a file for later use
     alice_config_file = alice_config.to_configuration_file()
-    #print(alice_config_file)
 
+# Let's get to learn about the NuCypher network
+alicia.start_learning_loop(now=True)
 
-# Alicia can create the public key associated to the label,
-# even before creating any policy.
+# At this point, Alicia is fully operational and can create policies.
+# The Policy Label is a bytestring that categorizes the data that Alicia wants to share.
+# Note: we add some random chars to create different policies, only for demonstration purposes
+label = "heart-data-❤️-"+os.urandom(4).hex()
+label = label.encode()
+
+# Alicia can create the public key associated to the policy label,
+# even before creating any associated policy.
 policy_pubkey = alicia.get_policy_pubkey_from_label(label)
 
-# This illustrates that Data Sources can produce encrypted data for policies
+print("The policy public key for "
+      "label '{}' is {}".format(label.decode("utf-8"), policy_pubkey.to_bytes().hex()))
+
+# Data Sources can produce encrypted data for access policies
 # that **don't exist yet**.
+# In this example, we create a local file with encrypted data, containing
+# heart rate measurements from a heart monitor
 import heart_monitor
 heart_monitor.generate_heart_rate_samples(policy_pubkey,
                                           label=label,
                                           save_as_file=True)
 
 
+# Alicia now wants to share data associated with this label.
+# To do so, she needs the public key of the recipient.
+# In this example, we generate it on the fly (for demonstration purposes)
+from doctor_keys import get_doctor_pubkeys
 doctor_pubkeys = get_doctor_pubkeys()
 
-print("doctor_pubkeys", doctor_pubkeys)
+powers_and_material = {
+    EncryptingPower: doctor_pubkeys['enc'],
+    SigningPower: doctor_pubkeys['sig']
+}
 
-powers_and_material = {EncryptingPower: doctor_pubkeys['enc'],
-                       SigningPower: doctor_pubkeys['sig']}
-
+# We create a view of the Bob who's going to be granted access.
 doctor_strange = Bob.from_public_keys(powers_and_material=powers_and_material,
                                       federated_only=True)
 
-alicia.start_learning_loop(now=True)
+# Here are our remaining Policy details, such as:
+# - Policy duration
+policy_end_datetime = maya.now() + datetime.timedelta(days=5)
+# - m-out-of-n: This means Alicia splits the re-encryption key in 5 pieces and
+#               she requires Bob to seek collaboration of at least 3 Ursulas
+m, n = 3, 5
 
+
+# With this information, Alicia creates a policy granting access to Bob.
+# The policy is sent to the NuCypher network.
+print("Creating access policy for the Doctor...")
 policy = alicia.grant(bob=doctor_strange,
                       label=label,
                       m=m,
                       n=n,
                       expiration=policy_end_datetime)
+print("Done!")
 
+# For the demo, we need a way to share with Bob some additional info
+# about the policy, so we store it in a JSON file
 policy_info = {
     "policy_pubkey": policy.public_key.to_bytes().hex(),
     "alice_sig_pubkey": bytes(alicia.stamp).hex(),
