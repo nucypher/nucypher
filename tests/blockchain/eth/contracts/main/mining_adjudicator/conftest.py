4b825955004c65183ec0c51403deb955ec20599d
szotov <zotov89@mail.ru>
2018-12-05 20:03:18 +0300

    Upgradability for the MiningAdjudicator contract

--- tests/blockchain/eth/contracts/main/mining_adjudicator/conftest.py
+++ tests/blockchain/eth/contracts/main/mining_adjudicator/conftest.py
@@ -30,21 +30,18 @@ def escrow(testerchain):
     return escrow
 
 
-# @pytest.fixture(params=[False, True])
-@pytest.fixture()
+@pytest.fixture(params=[False, True])
 def adjudicator_contract(testerchain, escrow, request):
-    # creator, client, bad_node, node1, node2, node3, *everyone_else = testerchain.interface.w3.eth.accounts
-
     contract, _ = testerchain.interface.deploy_contract('MiningAdjudicator', escrow.address, ALGORITHM_SHA256)
 
-    # if request.param:
-    #     secret_hash = testerchain.interface.w3.sha3(secret)
-    #     dispatcher, _ = testerchain.interface.deploy_contract('Dispatcher', contract.address, secret_hash)
-    #
-    #     # Deploy second version of the government contract
-    #     contract = testerchain.interface.w3.eth.contract(
-    #         abi=contract.abi,
-    #         address=dispatcher.address,
-    #         ContractFactoryClass=Contract)
+    if request.param:
+        secret_hash = testerchain.interface.w3.sha3(secret)
+        dispatcher, _ = testerchain.interface.deploy_contract('Dispatcher', contract.address, secret_hash)
+
+        # Deploy second version of the government contract
+        contract = testerchain.interface.w3.eth.contract(
+            abi=contract.abi,
+            address=dispatcher.address,
+            ContractFactoryClass=Contract)
 
     return contract
