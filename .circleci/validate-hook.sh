dccb5afd4c3d218091cb23d0ee51fe883553811f
Kieran Prasch <kieranprasch@gmail.com>
2018-12-14 13:32:51 -0800

    Add commit hook for circleCI config file validation

---
+++ .circleci/validate-hook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# The following line is needed by the CircleCI Local Build Tool (due to Docker interactivity)
+exec < /dev/tty
+
+# If validation fails, tell Git to stop and provide error message. Otherwise, continue.
+if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
+	echo "CircleCI Configuration Failed Validation."
+	echo $eMSG
+	exit 1
+fi
