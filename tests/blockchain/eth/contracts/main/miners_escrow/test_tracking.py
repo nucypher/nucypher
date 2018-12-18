1dc19d2895fcb4dea6ed87b7f50462b0d8a31402
szotov <zotov89@mail.ru>
2018-12-18 14:06:36 +0300

    Added blank lines after license in blockchain tests

--- tests/blockchain/eth/contracts/main/miners_escrow/test_tracking.py
+++ tests/blockchain/eth/contracts/main/miners_escrow/test_tracking.py
@@ -14,6 +14,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
+
+
 import pytest
 from eth_tester.exceptions import TransactionFailed
 from web3.contract import Contract
