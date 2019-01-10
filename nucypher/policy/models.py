3bfd7bf97733ebb9e17e5b75fed70329ec65b30f
jMyles <justin@justinholmes.com>
2019-01-10 14:33:47 -0500

    No need to try to dig the cert up form a non-existent storage; it's already on the node.

--- nucypher/policy/models.py
+++ nucypher/policy/models.py
@@ -255,11 +255,9 @@ class Policy:
                 return self.publish(network_middleware)
 
     def consider_arrangement(self, network_middleware, ursula, arrangement):
-        certificate_filepath = ursula.node_storage.generate_certificate_filepath(checksum_address=arrangement.ursula.checksum_public_address)
         try:
             ursula.verify_node(network_middleware,
-                               accept_federated_only=arrangement.federated,
-                               certificate_filepath=certificate_filepath)
+                               accept_federated_only=arrangement.federated)
         except ursula.InvalidNode:
             # TODO: What do we actually do here?  Report this at least (355)?
             # Maybe also have another bucket for invalid nodes?
