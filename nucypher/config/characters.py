d5f044350e3fc8e0e211e3caaa7364775b3f6857
Kieran Prasch <kieran@nucypher.com>
2019-01-21 11:45:04 -0800

    Keyring generation method for BobConfiguration

--- nucypher/config/characters.py
+++ nucypher/config/characters.py
@@ -120,3 +120,10 @@ class BobConfiguration(NodeConfiguration):
     CONFIG_FILENAME = '{}.config'.format(_NAME)
     DEFAULT_CONFIG_FILE_LOCATION = os.path.join(DEFAULT_CONFIG_ROOT, CONFIG_FILENAME)
     DEFAULT_REST_PORT = 7151
+
+    def write_keyring(self, password: str, **generation_kwargs) -> NucypherKeyring:
+
+        return super().write_keyring(password=password,
+                                     encrypting=True,
+                                     rest=False,
+                                     **generation_kwargs)
