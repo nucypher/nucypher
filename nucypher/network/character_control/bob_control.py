4513000b32eaa8ae052303f4cf16fb761d432ab1
tuxxy <me@johnpacific.com>
2019-02-10 13:46:18 -0700

    Add bob_control skeleton routes for join_policy and retrieve

--- nucypher/network/character_control/bob_control.py
+++ nucypher/network/character_control/bob_control.py
@@ -3,4 +3,19 @@ from flask import Flask, request, Response
 def make_bob_control(drone_bob: 'Bob'):
     bob_control = Flask('bob-control')
 
+    @bob_control.route('/join_policy', methods=['POST'])
+    def join_policy():
+        try:
+            pass
+        except KeyError as e:
+            return Response(e, status=500)
+
+
+    @bob_control.route('/retrieve', methods=['POST'])
+    def retrieve():
+        try:
+            pass
+        except KeyError as e:
+            return Response(e, status=500)
+
     return bob_control
