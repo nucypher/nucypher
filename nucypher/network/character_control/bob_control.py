282441557bd924e611282b689a613b89d6e5eb11
tuxxy <me@johnpacific.com>
2019-02-10 20:45:54 -0700

    Fill out make_bob_control and build passing tests

--- nucypher/network/character_control/bob_control.py
+++ nucypher/network/character_control/bob_control.py
@@ -1,21 +1,53 @@
+import json
 from flask import Flask, request, Response
 
-def make_bob_control(drone_bob: 'Bob'):
+from umbral.keys import UmbralPublicKey
+
+from nucypher.characters.lawful import Bob
+from nucypher.crypto.kits import UmbralMessageKit
+from nucypher.data_sources import DataSource
+
+def make_bob_control(drone_bob: Bob):
     bob_control = Flask('bob-control')
 
     @bob_control.route('/join_policy', methods=['POST'])
     def join_policy():
         try:
-            pass
+            label = bytes.fromhex(request.args['label'])
+            alice_pubkey_sig = bytes.fromhex(request.args['alice_signing_pubkey'])
         except KeyError as e:
             return Response(e, status=500)
+
+        drone_bob.join_policy(label=label, alice_pubkey_sig=alice_pubkey_sig)
+
+        return Response('Policy joined!', status=200)
 
 
     @bob_control.route('/retrieve', methods=['POST'])
     def retrieve():
         try:
-            pass
+            label = bytes.fromhex(request.args['label'])
+            policy_pubkey_enc = bytes.fromhex(request.args['policy_encrypting_pubkey'])
+            alice_pubkey_sig = bytes.fromhex(request.args['alice_signing_pubkey'])
+            datasource_pubkey_sig = bytes.fromhex(request.args['datasource_signing_pubkey'])
+            message_kit = bytes.fromhex(request.args['message_kit'])
         except KeyError as e:
             return Response(e, status=500)
+
+        policy_pubkey_enc = UmbralPublicKey.from_bytes(policy_pubkey_enc)
+        alice_pubkey_sig = UmbralPublicKey.from_bytes(alice_pubkey_sig)
+        message_kit = UmbralMessageKit.from_bytes(message_kit)
+
+        data_source = DataSource.from_public_keys(policy_pubkey_enc,
+                                                  datasource_pubkey_sig,
+                                                  label=label)
+        drone_bob.join_policy(label=label, alice_pubkey_sig=alice_pubkey_sig)
+        plaintexts = drone_bob.retrieve(message_kit=message_kit,
+                                        data_source=data_source,
+                                        alice_verifying_key=alice_pubkey_sig)
+
+        plaintexts = [plaintext.hex() for plaintext in plaintexts]
+
+        return Response(json.dumps(plaintexts), status=200)
 
     return bob_control
--- tests/network/test_character_control.py
+++ tests/network/test_character_control.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import maya
 
 from nucypher.crypto.powers import DecryptingPower
@@ -37,3 +38,31 @@ def test_alice_character_control_grant(alice_control, federated_bob):
 
     encrypted_map = TreasureMap.from_bytes(response.data)
     assert encrypted_map._hrac != None
+
+
+def test_bob_character_control_join_policy(bob_control, enacted_federated_policy):
+    request_data = {
+        'label': enacted_federated_policy.label.hex(),
+        'alice_signing_pubkey': bytes(enacted_federated_policy.alice.stamp).hex(),
+    }
+
+    response = bob_control.post('/join_policy', query_string=request_data)
+    assert response.data == b'Policy joined!'
+    assert response.status_code == 200
+
+
+def test_bob_character_control_retrieve(bob_control, enacted_federated_policy, capsule_side_channel):
+    message_kit, data_source = capsule_side_channel
+    request_data = {
+        'label': enacted_federated_policy.label.hex(),
+        'policy_encrypting_pubkey': bytes(enacted_federated_policy.public_key).hex(),
+        'alice_signing_pubkey': bytes(enacted_federated_policy.alice.stamp).hex(),
+        'message_kit': message_kit.to_bytes().hex(),
+        'datasource_signing_pubkey': bytes(data_source.stamp).hex(),
+    }
+
+    response = bob_control.post('/retrieve', query_string=request_data)
+
+    plaintext = json.loads(response.data)
+    assert response.status_code == 200
+    assert bytes.fromhex(plaintext[0]) == b'Welcome to the flippering.'
