3d6f2a4feedea5d07449a5fd5197112999b878cc
tuxxy <me@johnpacific.com>
2019-02-12 18:28:25 -0700

    Bump version: 0.1.0-alpha.10 → 0.1.0-alpha.11

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ author = 'NuCypher'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.0-alpha.10'
+release = '0.1.0-alpha.11'
 
 
 # -- General configuration ---------------------------------------------------
