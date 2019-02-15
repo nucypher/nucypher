00ec1addb62422c3b8c4ca4a8b43c0fafa51fe8d
Kieran R. Prasch <kieranprasch@gmail.com>
2019-02-14 20:46:19 -0700

    Nucypher CLI character entrypoints for enrico, alico, bob, moe, and ursula.

--- nucypher/cli/main.py
+++ nucypher/cli/main.py
@@ -18,18 +18,10 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import click
-from twisted.logger import Logger
-from twisted.logger import globalLogPublisher
 
-from constant_sorrow import constants
-from constant_sorrow.constants import TEMPORARY_DOMAIN
-from nucypher.characters.lawful import Ursula
-from nucypher.cli import moe, ursula, status, alice
+from nucypher.cli import moe, ursula, status, alice, bob, enrico
 from nucypher.cli.config import echo_version, nucypher_click_config
-from nucypher.cli.painting import BANNER, paint_configuration, paint_known_nodes
-from nucypher.cli.processes import UrsulaCommandProtocol
-from nucypher.config.characters import UrsulaConfiguration
-from nucypher.config.constants import DEFAULT_CONFIG_ROOT
+from nucypher.cli.painting import BANNER
 from nucypher.utilities.logging import GlobalConsoleLogger
 
 GlobalConsoleLogger.start_if_not_started()
@@ -48,5 +40,7 @@ def nucypher_cli(click_config, verbose):
 
 nucypher_cli.add_command(status.status)
 nucypher_cli.add_command(alice.alice)
+nucypher_cli.add_command(bob.bob)
+nucypher_cli.add_command(enrico.enrico)
 nucypher_cli.add_command(moe.moe)
 nucypher_cli.add_command(ursula.ursula)
