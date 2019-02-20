12334e9a596b10584ea938af2f74b0acbca9c539
Kieran R. Prasch <kieranprasch@gmail.com>
2019-02-19 19:03:50 -0700

    Steps towards native character CLI implementation and unicode domains

--- nucypher/characters/lawful.py
+++ nucypher/characters/lawful.py
@@ -247,7 +247,7 @@ class Alice(Character, PolicyAuthor):
                                                    federated=federated_only)
 
             response_data = {'result': {'label': new_policy.label.decode(),
-                                         'policy_encrypting_key': new_policy.public_key.to_bytes().hex()},
+                                        'policy_encrypting_key': new_policy.public_key.to_bytes().hex()},
                              'version': str(nucypher.__version__)}
 
             return Response(json.dumps(response_data), status=200)
@@ -1086,6 +1086,7 @@ class Ursula(Teacher, Character, Miner):
                      federated_only: bool = False) -> 'Ursula':
         return node_storage.get(checksum_address=checksum_adress,
                                 federated_only=federated_only)
+
 
     #
     # Properties
