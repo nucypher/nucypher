b820a9d9c4735d7facd8c8607b11d0606e002fe4
tuxxy <me@johnpacific.com>
2019-03-08 18:37:17 -0700

    Bump version: 0.1.0-alpha.17 → 0.1.0-alpha.18

--- nucypher/__about__.py
+++ nucypher/__about__.py
@@ -28,7 +28,7 @@ __url__ = "https://github.com/nucypher/nucypher"
 
 __summary__ = 'A proxy re-encryption network to empower privacy in decentralized systems.'
 
-__version__ = "0.1.0-alpha.17"
+__version__ = "0.1.0-alpha.18"
 
 __author__ = "NuCypher"
 
