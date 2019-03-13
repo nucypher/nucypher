19a11d0ab15c83b08a20ecd88db98664ed74efff
szotov <zotov89@mail.ru>
2019-03-13 15:13:18 +0300

    Added one more slashing scenario and some ascii diagrams

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+	'aafigure.sphinxext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
