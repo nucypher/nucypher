4fda514d7e9c9a172a3edbdfecd946a3e950a072
Kieran Prasch <kieran@nucypher.com>
2019-04-23 07:32:14 +0300

    Building a geth shortcut CLI entrypoint and process management for future decentralized private blockchains

--- nucypher/blockchain/eth/clients.py
+++ nucypher/blockchain/eth/clients.py
@@ -1,14 +1,20 @@
+import json
 import os
-import shutil
 
+import click
 from geth import LoggingMixin
 from geth.accounts import ensure_account_exists
-from geth.chain import get_live_data_dir, get_chain_data_dir, initialize_chain, get_ropsten_data_dir
+from geth.chain import get_chain_data_dir, initialize_chain, is_live_chain, \
+    is_ropsten_chain
 from geth.process import BaseGethProcess
-from geth.wrapper import construct_test_chain_kwargs, get_max_socket_path_length, get_geth_binary_path
-from web3 import Web3
+from geth.wrapper import construct_test_chain_kwargs
 
 from nucypher.config.constants import DEFAULT_CONFIG_ROOT
+
+
+NUCYPHER_CHAIN_IDS = {
+    'devnet': 112358,
+}
 
 
 class NuCypherGethDevProcess(BaseGethProcess, LoggingMixin):
@@ -29,71 +35,60 @@ class NuCypherGethDevProcess(BaseGethProcess, LoggingMixin):
         self.command = [*self.command, '--dev']
 
 
-class NuCypherGethTestnetProcess(LoggingMixin, BaseGethProcess):  # FIXME
+class NuCypherGethDevnetProcess(LoggingMixin, BaseGethProcess):
 
-    ETH_ALLOCATION = str(Web3.fromWei(1000000000000000000000000000000, 'wei'))
-
-    __CHAIN_NAME = 'testnet'
-    __CHAIN_ID = 112358
+    GENESIS_FILEPATH = os.path.join('deploy', 'geth', 'genesis.json')
+    __CHAIN_NAME = 'devnet'
+    __CHAIN_ID = NUCYPHER_CHAIN_IDS[__CHAIN_NAME]
 
     def __init__(self,
+                 password: str = None,
                  config_root: str = None,
                  overrides: dict = None):
 
+        # if password is None:
+        #     password = click.prompt("Enter Geth node password: ", hide_input=True)
+
         if overrides is None:
             overrides = dict()
-        overrides.update({'network_id': str(self.__CHAIN_ID)})
 
+        # Validate
+        invalid_override = f"You cannot specify `network_id` for a {self.__class__.__name__}"
         if 'data_dir' in overrides:
-            raise ValueError(f"You cannot specify `data_dir` for a {self.__class__.__name__}")
+            raise ValueError(invalid_override)
+        if 'network_id' in overrides:
+            raise ValueError(invalid_override)
 
         # Set the data dir
         if config_root is None:
             base_dir = os.path.join(DEFAULT_CONFIG_ROOT, '.ethereum')
         else:
             base_dir = os.path.join(config_root, '.ethereum')
-
         self.data_dir = get_chain_data_dir(base_dir=base_dir, name=self.__CHAIN_NAME)
 
-        # Generate Geth CLI args
-        geth_kwargs = construct_test_chain_kwargs(data_dir=self.data_dir, **overrides)
+        # Hardcoded Geth CLI args for devnet child process
+        ipc_path = os.path.join(self.data_dir, 'geth.ipc')
+        geth_kwargs = {'unlock': '0',
+                       'network_id': str(self.__CHAIN_ID),
+                       'port': '30303',
+                       'verbosity': '5',
+                       'rpc_enabled': 'true',
+                       'data_dir': self.data_dir,
+                       'ipc_path': ipc_path}
 
-        # ensure that an account is present or crash
-        coinbase = ensure_account_exists(**geth_kwargs)
+        _coinbase = ensure_account_exists(**geth_kwargs)
 
-        # Sanity Check
-        assert get_live_data_dir() != self.data_dir   # not mainnet
-        assert get_ropsten_data_dir != self.data_dir  # not ropsten
+        # Genesis & Blockchain Init
+        genesis_filepath = self.GENESIS_FILEPATH
+        needs_init = all((
+            not os.path.exists(genesis_filepath),
+            not is_live_chain(self.data_dir),
+            not is_ropsten_chain(self.data_dir),
+        ))
 
-        #
-        # Genesis
-        #
+        if needs_init:
+            with open(genesis_filepath) as file:
+                genesis_data = json.loads(file.read())
+            initialize_chain(genesis_data, **geth_kwargs)
 
-        allocations = {"balance": str(self.ETH_ALLOCATION)}
-
-        genesis_data = {
-            "overwrite": True,
-
-            "nonce": "0x0000000000000042",
-            "timestamp": "0x0",
-            "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "extraData": "0x",
-            "gasLimit": "8000000",
-            "difficulty": "2000",
-            "mixhash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "coinbase": coinbase,
-            "alloc": {
-                coinbase: allocations,
-            },
-            "config": {
-                "chainId": self.__CHAIN_ID,
-                "homesteadBlock": 0,
-                "eip155Block": 0,
-                "eip158Block": 0,
-                'daoForkBlock': 0,
-                'daoForkSupport': True,
-            }
-        }
-
-        initialize_chain(genesis_data, **geth_kwargs)
         super().__init__(geth_kwargs)
