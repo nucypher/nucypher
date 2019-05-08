c82e64dca48c11b51a6460fa32aa727211465cf4
derekpierre <derek.pierre@gmail.com>
2019-05-08 16:29:44 -0400

    Moe/Ursula Status Page: Change 'app' to 'page'

--- nucypher/characters/lawful.py
+++ nucypher/characters/lawful.py
@@ -34,7 +34,6 @@ from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.x509 import load_pem_x509_certificate, Certificate, NameOID
 from eth_utils import to_checksum_address
 from flask import request, Response
-from nucypher.status.status_app import UrsulaStatusApp
 from twisted.internet import threads
 from twisted.logger import Logger
 from umbral.keys import UmbralPublicKey
@@ -69,7 +68,7 @@ from nucypher.network.nicknames import nickname_from_seed
 from nucypher.network.nodes import Teacher
 from nucypher.network.protocols import InterfaceInfo, parse_node_uri
 from nucypher.network.server import ProxyRESTServer, TLSHostingPower, make_rest_app
-from nucypher.network.status.status_app import UrsulaStatusApp
+from nucypher.network.status.status_page import UrsulaStatusPage
 
 
 class Alice(Character, BlockchainPolicyAuthor):
@@ -878,7 +877,7 @@ class Ursula(Teacher, Character, Worker):
                     serving_domains=domains,
                 )
                 # attach status app to rest_app
-                UrsulaStatusApp(self, self.nickname, rest_app, '/status/')
+                UrsulaStatusPage(self, self.nickname, rest_app, '/status/')
 
                 #
                 # TLSHostingPower (Ephemeral Self-Ursula)
