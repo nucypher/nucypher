e9583686520a549d19be255ca2ce838abeba4e9e
Kieran R. Prasch <kieranprasch@gmail.com>
2019-05-16 08:34:15 -0600

    Goerli

--- nucypher/cli/characters/moe.py
+++ nucypher/cli/characters/moe.py
@@ -12,7 +12,7 @@ from nucypher.network.middleware import RestMiddleware
 @click.command()
 @click.option('--teacher-uri', help="An Ursula URI to start learning from (seednode)", type=click.STRING)
 @click.option('--min-stake', help="The minimum stake the teacher must have to be a teacher", type=click.INT, default=0)
-@click.option('--network', help="Network Domain Name", type=click.STRING, default='goerli-testnet')
+@click.option('--network', help="Network Domain Name", type=click.STRING)
 @click.option('--http-port', help="The host port to run Moe HTTP services on", type=NETWORK_PORT, default=12500)
 @click.option('--ws-port', help="The host port to run websocket network services on", type=NETWORK_PORT, default=9000)
 @click.option('--dry-run', '-x', help="Execute normally without actually starting the node", is_flag=True)
