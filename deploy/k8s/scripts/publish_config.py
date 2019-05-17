4b809a4041d432970be26855e97d53af51b5eef1
Damon C <gdamon@gmail.com>
2019-05-17 17:12:03 -0400

    deploy progress

--- deploy/k8s/scripts/publish_config.py
+++ deploy/k8s/scripts/publish_config.py
@@ -1,11 +1,13 @@
 import os
 import boto3
 
+ROOT_DIR = os.getenv('ROOT_DIR')
+
 tar_filename = '{}@{}.tar.gz'.format(
     os.getenv('ACCOUNT_ADDRESS'),
     os.getenv('MY_IP')
 )
-tarfile = os.path.join('/mnt/data/', tar_filename)
+tarfile = os.path.join(ROOT_DIR, tar_filename)
 
 s3_client = boto3.client('s3')
 s3_client.upload_file(tarfile, os.getenv('NUCYPHER_CONFIG_UPLOAD_BUCKET'), tar_filename)
--- deploy/k8s/scripts/run_geth.sh
+++ deploy/k8s/scripts/run_geth.sh
@@ -3,7 +3,7 @@
 args="$@"
 echo $GETH_PW > file.txt
 echo "running geth account new with $args"
-geth $args --password file.txt account new | grep "Public address of the key:" | awk 'NF>1{print $NF}' > /mnt/data/address.txt
+geth $args --password file.txt account new | grep "Public address of the key:" | awk 'NF>1{print $NF}' > $ROOT_DIR/address.txt
 rm file.txt
 echo "running geth with $args"
 geth $args
