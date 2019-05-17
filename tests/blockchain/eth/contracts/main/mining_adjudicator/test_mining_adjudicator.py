0e29b148f7053e6b57cf13355fbe0d3f04047492
szotov <zotov89@mail.ru>
2019-05-17 14:38:51 +0300

    Added way to slash intermediary contracts

--- tests/blockchain/eth/contracts/main/mining_adjudicator/test_mining_adjudicator.py
+++ tests/blockchain/eth/contracts/main/mining_adjudicator/test_mining_adjudicator.py
@@ -26,15 +26,11 @@ from eth_utils import to_canonical_address
 from typing import Tuple
 from web3.contract import Contract
 
-from umbral import pre
-from umbral.curvebn import CurveBN
+from nucypher.blockchain.eth.chains import Blockchain
 from umbral.keys import UmbralPrivateKey
 from umbral.point import Point
-from umbral.signing import Signer
 
-from nucypher.characters.lawful import Bob
-from nucypher.crypto.utils import canonical_address_from_umbral_key, get_coordinates_as_bytes
-from nucypher.policy.models import IndisputableEvidence, WorkOrder
+from nucypher.crypto.utils import get_coordinates_as_bytes
 
 
 ALGORITHM_KECCAK256 = 0
@@ -61,6 +57,7 @@ def test_evaluate_cfrag(testerchain,
     ursula = list(federated_ursulas)[0]
     creator, miner, wrong_miner, investigator, *everyone_else = testerchain.interface.w3.eth.accounts
     evaluation_log = adjudicator_contract.events.CFragEvaluated.createFilter(fromBlock='latest')
+    verdict_log = adjudicator_contract.events.IncorrectCFragVerdict.createFilter(fromBlock='latest')
 
     worker_stake = 1000
     worker_penalty_history = 0
@@ -75,7 +72,7 @@ def test_evaluate_cfrag(testerchain,
         return penalty_, reward_
 
     # Prepare one miner
-    tx = escrow.functions.setMinerInfo(miner, worker_stake).transact()
+    tx = escrow.functions.setMinerInfo(miner, worker_stake, Blockchain.NULL_ADDRESS).transact()
     testerchain.wait_for_receipt(tx)
     miner_umbral_public_key_bytes = get_coordinates_as_bytes(ursula.stamp)
 
@@ -119,9 +116,9 @@ def test_evaluate_cfrag(testerchain,
     assert number_of_evaluations == len(events)
     event_args = events[-1]['args']
     assert data_hash == event_args['evaluationHash']
-    assert miner == event_args['miner']
     assert investigator == event_args['investigator']
     assert event_args['correctness']
+    assert 0 == len(verdict_log.get_all_entries())
 
     ###############################
     # Test: Don't evaluate miner with data that already was checked
@@ -163,9 +160,14 @@ def test_evaluate_cfrag(testerchain,
     assert number_of_evaluations == len(events)
     event_args = events[-1]['args']
     assert data_hash == event_args['evaluationHash']
-    assert miner == event_args['miner']
     assert investigator == event_args['investigator']
     assert not event_args['correctness']
+    events = verdict_log.get_all_entries()
+    assert number_of_evaluations - 1 == len(events)
+    event_args = events[-1]['args']
+    assert data_hash == event_args['evaluationHash']
+    assert miner == event_args['violator']
+    assert miner == event_args['miner']
 
     ###############################
     # Test: Bob produces wrong precomputed data
@@ -235,9 +237,14 @@ def test_evaluate_cfrag(testerchain,
     assert number_of_evaluations == len(events)
     event_args = events[-1]['args']
     assert data_hash == event_args['evaluationHash']
-    assert miner == event_args['miner']
     assert investigator == event_args['investigator']
     assert not event_args['correctness']
+    events = verdict_log.get_all_entries()
+    assert number_of_evaluations - 1 == len(events)
+    event_args = events[-1]['args']
+    assert data_hash == event_args['evaluationHash']
+    assert miner == event_args['violator']
+    assert miner == event_args['miner']
 
     ###############################
     # Test: Third violation. Penalty reaches the maximum allowed
@@ -277,9 +284,14 @@ def test_evaluate_cfrag(testerchain,
     assert number_of_evaluations == len(events)
     event_args = events[-1]['args']
     assert data_hash == event_args['evaluationHash']
-    assert miner == event_args['miner']
     assert investigator == event_args['investigator']
     assert not event_args['correctness']
+    events = verdict_log.get_all_entries()
+    assert number_of_evaluations - 1 == len(events)
+    event_args = events[-1]['args']
+    assert data_hash == event_args['evaluationHash']
+    assert miner == event_args['violator']
+    assert miner == event_args['miner']
 
     #################
     # Test: Invalid evaluations
@@ -334,6 +346,7 @@ def test_evaluate_cfrag(testerchain,
     with pytest.raises(TransactionFailed):
         tx = adjudicator_contract.functions.evaluateCFrag(*wrong_args).transact()
         testerchain.wait_for_receipt(tx)
+
 
 @pytest.mark.slow
 def test_upgrading(testerchain):
