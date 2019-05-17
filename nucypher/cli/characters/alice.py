54826e3d74d119120ff46725151e42e34070c508
jMyles <justin@justinholmes.com>
2019-05-17 13:44:53 -0400

    Addressed passed via CLI no longer needs to be a checksum address; can just be a regular hex address.  Bob can now pass it.

--- nucypher/cli/characters/alice.py
+++ nucypher/cli/characters/alice.py
@@ -14,7 +14,7 @@ from nucypher.config.characters import AliceConfiguration
 
 @click.command()
 @click.argument('action')
-@click.option('--checksum-address', help="Run with a specified account", type=EIP55_CHECKSUM_ADDRESS)
+@click.option('--pay-with', help="Run with a specified account", type=EIP55_CHECKSUM_ADDRESS)
 @click.option('--teacher-uri', help="An Ursula URI to start learning from (seednode)", type=click.STRING)
 @click.option('--min-stake', help="The minimum stake the teacher must have to be a teacher", type=click.INT, default=0)
 @click.option('--discovery-port', help="The host port to run node discovery services on", type=NETWORK_PORT, default=9151)  # TODO
@@ -38,7 +38,7 @@ from nucypher.config.characters import AliceConfiguration
 @nucypher_click_config
 def alice(click_config,
           action,
-          checksum_address,
+          pay_with,
           teacher_uri,
           min_stake,
           http_port,
@@ -78,7 +78,7 @@ def alice(click_config,
 
         new_alice_config = AliceConfiguration.generate(password=click_config.get_password(confirm=True),
                                                        config_root=config_root,
-                                                       checksum_public_address=checksum_address,
+                                                       checksum_public_address=pay_with,
                                                        rest_host="localhost",
                                                        domains={network} if network else None,
                                                        federated_only=federated_only,
@@ -107,7 +107,7 @@ def alice(click_config,
                 domains={network} if network else None,
                 network_middleware=click_config.middleware,
                 rest_port=discovery_port,
-                checksum_public_address=checksum_address,
+                checksum_public_address=pay_with,
                 provider_uri=provider_uri)
         except FileNotFoundError:
             return actions.handle_missing_configuration_file(character_config_class=AliceConfiguration,
