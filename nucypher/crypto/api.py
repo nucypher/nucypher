29a53c43921d49074deb07344bd3547cb0055759
David Núñez <david@nucypher.com>
2019-05-20 17:13:35 +0200

    Added nucypher.crypto.api.sha256_digest().

--- nucypher/crypto/api.py
+++ nucypher/crypto/api.py
@@ -24,6 +24,7 @@ from constant_sorrow import constants
 from cryptography import x509
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.backends.openssl import backend
 from cryptography.hazmat.backends.openssl.ec import _EllipticCurvePrivateKey
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
@@ -86,8 +87,26 @@ def keccak_digest(*messages: bytes) -> bytes:
     """
     _hash = sha3.keccak_256()
     for message in messages:
-        _hash.update(message)
-    return _hash.digest()
+        _hash.update(bytes(message))
+    digest = _hash.digest()
+    return digest
+
+
+def sha256_digest(*messages: bytes) -> bytes:
+    """
+    Accepts an iterable containing bytes and digests it returning a
+    SHA256 digest of 32 bytes
+
+    :param bytes: Data to hash
+
+    :rtype: bytes
+    :return: bytestring of digested data
+    """
+    _hash_ctx = hashes.Hash(hashes.SHA256(), backend=backend)
+    for message in messages:
+        _hash_ctx.update(bytes(message))
+    digest = _hash_ctx.finalize()
+    return digest
 
 
 def ecdsa_sign(message: bytes,
