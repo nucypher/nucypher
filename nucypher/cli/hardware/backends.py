1219834958690a39dd62d3efa3e9b284cbf90f54
tuxxy <me@johnpacific.com>
2019-05-27 08:05:47 -0600

    Implement signing and verifying methods for the Trezor backend
    Fix abstractmethod verify_message signature

--- nucypher/cli/hardware/backends.py
+++ nucypher/cli/hardware/backends.py
@@ -17,6 +17,13 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 
 
 from abc import ABC, abstractmethod
+from collections import namedtuple
+from trezorlib import ethereum as trezor_eth
+from trezorlib.client import get_default_client
+from trezorlib.tools import parse_path
+from trezorlib.transport import TransportException
+
+from nucypher.crypto.signing import InvalidSignature
 
 
 class TrustedDevice(ABC):
@@ -25,15 +32,34 @@ class TrustedDevice(ABC):
     utilizing trusted hardware devices (e.g. trezor).
     This class specifies a few basic functions required for staking on the
     NuCypher network.
+
+    TODO: Define an abstractmethod for BIP44 derivation?
     """
 
-    @abstractmethod
-    def sign_message(self):
+    # We intentionally keep the address index off the path so that the
+    # subclass interfaces can handle which address index to use.
+    DEFAULT_BIP44_PATH = "m/44'/60'/0'/0/"
+
+    Signature = namedtuple('Signature', ['signature', 'address'])
+
+    class DeviceError(Exception):
         pass
 
     @abstractmethod
-    def verify_message(self):
-        pass
+    def sign_message(self, message: bytes, address_index: int = 0):
+        """
+        Abstract method for signing any arbitrary message via a device's API.
+
+        TODO: What format should signatures output?
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def verify_message(self, signature: bytes, messsage: bytes, address: str):
+        """
+        Abstract method for verifying a signature via a device's API.
+        """
+        raise NotImplementedError
 
 
 class Trezor(TrustedDevice):
@@ -42,10 +68,42 @@ class Trezor(TrustedDevice):
     """
 
     def __init__(self):
-        pass
+        # TODO: Should we import the trezor specific libs here or at the top?
+        try:
+            self.client = get_default_client()
+            self.__bip44_path = parse_path(self.DEFAULT_BIP44_PATH)
+        except TransportException:
+            raise RuntimeError("Could not find a TREZOR device to connect to. Have you unlocked it?")
 
-    def sign_message(self):
-        raise NotImplementedError
+    def sign_message(self, message: bytes, address_index: int = 0):
+        """
+        Signs a message via the TREZOR ethereum sign_message API and returns
+        the signature and the address used to sign it. This method requires
+        interaction between the TREZOR and the user.
 
-    def verify_message(self):
-        raise NotImplementedError
+        If an address_index is provided, it will use the address at that
+        index to sign the message. If no index is provided, the address at
+        the 0th index is used by default.
+        """
+        bip44_path = self.__bip44_path + [address_index]
+
+        sig = trezor_eth.sign_message(self.client, bip44_path, message)
+        return self.Signature(sig.signature, sig.address)
+
+    def verify_message(self, signature: bytes, message: bytes, address: str):
+        """
+        Verifies that a signature and message pair are from a specified
+        address via the TREZOR ethereum verify_message API. This method
+        requires interaction between the TREZOR and the user.
+
+        If the signature or message is not valid, it will raise a
+        nucypher.crypto.signing.InvalidSignature exception. Otherwise, it
+        will return True.
+
+        TODO: Should we provide some input validation for the ETH address?
+        """
+        is_valid = trezor_eth.verify_message(self.client, address, signature,
+                                             message)
+        if not is_valid:
+            raise InvalidSignature("Signature verification failed.")
+        return True
