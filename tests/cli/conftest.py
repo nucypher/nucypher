7250bce5995483f9115afe85f317d1db6098553b
David Núñez <david@nucypher.com>
2019-06-04 13:44:52 +0200

    Removing some re-emerging garbage, probably from a rebase

--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -30,11 +30,7 @@ from nucypher.utilities.sandbox.constants import (
     MOCK_CUSTOM_INSTALLATION_PATH,
     MOCK_ALLOCATION_INFILE,
     MOCK_REGISTRY_FILEPATH,
-    ONE_YEAR_IN_SECONDS,
-    MINERS_ESCROW_DEPLOYMENT_SECRET,
-    POLICY_MANAGER_DEPLOYMENT_SECRET,
-    MINING_ADJUDICATOR_DEPLOYMENT_SECRET,
-    USER_ESCROW_PROXY_DEPLOYMENT_SECRET)
+    ONE_YEAR_IN_SECONDS)
 from nucypher.utilities.sandbox.constants import MOCK_CUSTOM_INSTALLATION_PATH_2, INSECURE_DEVELOPMENT_PASSWORD
 
 
@@ -95,53 +91,6 @@ def custom_filepath():
     finally:
         with contextlib.suppress(FileNotFoundError):
             shutil.rmtree(_custom_filepath, ignore_errors=True)
-
-
-@pytest.fixture(scope='module')
-def custom_filepath_2():
-    _custom_filepath = MOCK_CUSTOM_INSTALLATION_PATH_2
-    with contextlib.suppress(FileNotFoundError):
-        shutil.rmtree(_custom_filepath, ignore_errors=True)
-    try:
-        yield _custom_filepath
-    finally:
-        with contextlib.suppress(FileNotFoundError):
-            shutil.rmtree(_custom_filepath, ignore_errors=True)
-
-
-@pytest.fixture(scope='session')
-def deployed_blockchain(token_economics):
-
-    # Interface
-    compiler = SolidityCompiler()
-    registry = InMemoryEthereumContractRegistry()
-    allocation_registry = InMemoryAllocationRegistry()
-    interface = BlockchainDeployerInterface(compiler=compiler,
-                                            registry=registry,
-                                            provider_uri=TEST_PROVIDER_URI)
-
-    # Blockchain
-    blockchain = TesterBlockchain(interface=interface, eth_airdrop=True, test_accounts=5, poa=True)
-    deployer_address = blockchain.etherbase_account
-
-    # Deployer
-    deployer = Deployer(blockchain=blockchain, deployer_address=deployer_address)
-
-    # The Big Three (+ Dispatchers)
-    deployer.deploy_network_contracts(miner_secret=MINERS_ESCROW_DEPLOYMENT_SECRET,
-                                      policy_secret=POLICY_MANAGER_DEPLOYMENT_SECRET,
-                                      adjudicator_secret=MINING_ADJUDICATOR_DEPLOYMENT_SECRET,
-                                      user_escrow_proxy_secret=USER_ESCROW_PROXY_DEPLOYMENT_SECRET)
-
-    # Start with some hard-coded cases...
-    all_yall = blockchain.unassigned_accounts
-    allocation_data = [{'address': all_yall[1],
-                        'amount': token_economics.maximum_allowed_locked,
-                        'duration': ONE_YEAR_IN_SECONDS}]
-
-    deployer.deploy_beneficiary_contracts(allocations=allocation_data, allocation_registry=allocation_registry)
-
-    yield blockchain, deployer_address, registry
 
 
 @pytest.fixture(scope='module')
