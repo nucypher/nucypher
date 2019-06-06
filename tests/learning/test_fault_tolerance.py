12d9d4731cf04eedd8ec19ba321633a1d868b433
David Núñez <david@nucypher.com>
2019-06-06 12:32:04 +0200

    Fix test for emission of unknown LL version warnings. Closes #1039

--- tests/learning/test_fault_tolerance.py
+++ tests/learning/test_fault_tolerance.py
@@ -1,10 +1,14 @@
+import os
 from collections import namedtuple
-from functools import partial
 
+from eth_utils.address import to_checksum_address
 from twisted.logger import globalLogPublisher, LogLevel
 
+from bytestring_splitter import VariableLengthBytestring
 from constant_sorrow.constants import NOT_SIGNED
 
+from nucypher.characters.base import Character
+from nucypher.network.nicknames import nickname_from_seed
 from nucypher.network.nodes import FleetStateTracker
 from nucypher.utilities.sandbox.middleware import MockRestMiddleware
 from nucypher.utilities.sandbox.ursula import make_federated_ursulas
@@ -69,16 +73,17 @@ def test_blockchain_ursula_stamp_verification_tolerance(blockchain_ursulas, capl
 
 
 def test_emit_warning_upon_new_version(ursula_federated_test_config, caplog):
-    lonely_ursula_maker = partial(make_federated_ursulas,
-                                  ursula_config=ursula_federated_test_config,
-                                  quantity=2,
-                                  know_each_other=True)
 
-    learner = lonely_ursula_maker().pop()
-    teacher, new_node = lonely_ursula_maker()
+    nodes = make_federated_ursulas(ursula_config=ursula_federated_test_config,
+                                   quantity=3,
+                                   know_each_other=False)
+    teacher, learner, new_node = nodes
+
+    learner.remember_node(teacher)
+    teacher.remember_node(learner)
+    teacher.remember_node(new_node)
 
     new_node.TEACHER_VERSION = learner.LEARNER_VERSION + 1
-    learner._current_teacher_node = teacher
 
     warnings = []
 
@@ -95,19 +100,46 @@ def test_emit_warning_upon_new_version(ursula_federated_test_config, caplog):
                                                                                learner.LEARNER_VERSION)
 
     # Now let's go a little further: make the version totally unrecognizable.
-    crazy_bytes_representation = int(learner.LEARNER_VERSION + 1).to_bytes(2, byteorder="big") \
-                                 + b"totally unintelligible nonsense"
+
+    # First, there's enough garbage to at least scrape a potential checksum address
+    fleet_snapshot = os.urandom(32 + 4)
+    random_bytes = os.urandom(50)  # lots of garbage in here
+    future_version = learner.LEARNER_VERSION + 42
+    version_bytes = future_version.to_bytes(2, byteorder="big")
+    crazy_bytes = fleet_snapshot + VariableLengthBytestring(version_bytes + random_bytes)
+    signed_crazy_bytes = bytes(teacher.stamp(crazy_bytes))
 
     Response = namedtuple("MockResponse", ("content", "status_code"))
-    response = Response(content=crazy_bytes_representation, status_code=200)
+    response = Response(content=signed_crazy_bytes + crazy_bytes, status_code=200)
+
+    learner._current_teacher_node = teacher
     learner.network_middleware.get_nodes_via_rest = lambda *args, **kwargs: response
     learner.learn_from_teacher_node()
 
-    # TODO: #1039 - Fails because the above mocked Response is unsigned, and the API now enforces interface signatures
-    # assert len(warnings) == 2
-    # assert warnings[1]['log_format'] == learner.unknown_version_message.format(new_node,
-    #                                                                            new_node.TEACHER_VERSION,
-    #                                                                            learner.LEARNER_VERSION)
+    # If you really try, you can read a node representation from the garbage
+    accidental_checksum = to_checksum_address(random_bytes[:20])
+    accidental_nickname = nickname_from_seed(accidental_checksum)[0]
+    accidental_node_repr = Character._display_name_template.format("Ursula", accidental_nickname, accidental_checksum)
+
+    assert len(warnings) == 2
+    assert warnings[1]['log_format'] == learner.unknown_version_message.format(accidental_node_repr,
+                                                                               future_version,
+                                                                               learner.LEARNER_VERSION)
+
+    # This time, however, there's not enough garbage to assume there's a checksum address...
+    random_bytes = os.urandom(2)
+    crazy_bytes = fleet_snapshot + VariableLengthBytestring(version_bytes + random_bytes)
+    signed_crazy_bytes = bytes(teacher.stamp(crazy_bytes))
+
+    response = Response(content=signed_crazy_bytes + crazy_bytes, status_code=200)
+
+    learner._current_teacher_node = teacher
+    learner.learn_from_teacher_node()
+
+    assert len(warnings) == 3
+    # ...so this time we get a "really unknown version message"
+    assert warnings[2]['log_format'] == learner.really_unknown_version_message.format(future_version,
+                                                                                      learner.LEARNER_VERSION)
 
     globalLogPublisher.removeObserver(warning_trapper)
 
