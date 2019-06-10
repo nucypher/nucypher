b600d713f60b4183b90b52226739671a74cf29ed
David Núñez <david@nucypher.com>
2019-06-10 15:17:32 +0200

    StakingEscrowAgent API for checking staker-worker relationship

--- tests/blockchain/eth/entities/agents/test_staking_escrow_agent.py
+++ tests/blockchain/eth/entities/agents/test_staking_escrow_agent.py
@@ -14,45 +14,99 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
+
+import os
+
 import pytest
+from eth_utils.address import to_checksum_address
 
 from nucypher.blockchain.eth.agents import StakingEscrowAgent
+from nucypher.blockchain.eth.chains import Blockchain
 
 
 @pytest.mark.slow()
 def test_deposit_tokens(testerchain, agency, token_economics):
-    origin, someone, *everybody_else = testerchain.interface.w3.eth.accounts
-    token_agent, staking_agent, policy_agent = agency
+    token_agent, staking_agent, _policy_agent = agency
 
-    agent = staking_agent
     locked_tokens = token_economics.minimum_allowed_locked * 5
 
+    staker_account = testerchain.unassigned_accounts[0]
+
+    balance = token_agent.get_balance(address=staker_account)
+    assert balance == 0
+
+    # The staker receives an initial amount of tokens
+    _txhash = token_agent.transfer(amount=token_economics.minimum_allowed_locked * 10,
+                                   target_address=staker_account,
+                                   sender_address=testerchain.etherbase_account)
+
+    #
+    # Deposit: The staker deposits tokens in the StakingEscrow contract.
+    # Previously, she needs to approve this transfer on the token contract.
+    #
+
     _txhash = token_agent.approve_transfer(amount=token_economics.minimum_allowed_locked * 10,  # Approve
-                                           target_address=agent.contract_address,
-                                           sender_address=someone)
+                                           target_address=staking_agent.contract_address,
+                                           sender_address=staker_account)
 
-    _txhash = token_agent.transfer(amount=token_economics.minimum_allowed_locked * 10,      # Transfer
-                                   target_address=someone,
-                                   sender_address=origin)
-
-
-    #
-    # Deposit
-    #
-
-    txhash = agent.deposit_tokens(amount=locked_tokens,
-                                  lock_periods=token_economics.minimum_locked_periods,
-                                  sender_address=someone)
+    txhash = staking_agent.deposit_tokens(amount=locked_tokens,
+                                          lock_periods=token_economics.minimum_locked_periods,
+                                          sender_address=staker_account)
 
     # Check the receipt for the contract address success code
+    # TODO: What are we checking here? Why transaction rejected?
     receipt = testerchain.wait_for_receipt(txhash)
     assert receipt['status'] == 1, "Transaction Rejected"
-    assert receipt['logs'][2]['address'] == agent.contract_address
+    assert receipt['logs'][2]['address'] == staking_agent.contract_address
 
     testerchain.time_travel(periods=1)
-    balance = token_agent.get_balance(address=someone)
+    balance = token_agent.get_balance(address=staker_account)
     assert balance == locked_tokens
-    assert agent.get_locked_tokens(staker_address=someone) == locked_tokens
+    assert staking_agent.get_locked_tokens(staker_address=staker_account) == locked_tokens
+
+
+@pytest.mark.slow()
+def test_locked_tokens(testerchain, agency, token_economics):
+    _token_agent, staking_agent, _policy_agent = agency
+    staker_account = testerchain.unassigned_accounts[0]
+    locked_amount = staking_agent.get_locked_tokens(staker_address=staker_account)
+    assert token_economics.maximum_allowed_locked >= locked_amount >= token_economics.minimum_allowed_locked
+
+
+@pytest.mark.slow()
+def test_get_all_stakes(testerchain, agency, token_economics):
+    _token_agent, staking_agent, _policy_agent = agency
+    staker_account = testerchain.unassigned_accounts[0]
+
+    all_stakes = list(staking_agent.get_all_stakes(staker_address=staker_account))
+    assert len(all_stakes) == 1
+    stake_info = all_stakes[0]
+    assert len(stake_info) == 3
+    start_period, end_period, value = stake_info
+    assert end_period > start_period
+    assert token_economics.maximum_allowed_locked > value > token_economics.minimum_allowed_locked
+
+
+@pytest.mark.slow()
+def test_stakers_and_workers_relationships(testerchain, agency):
+    _token_agent, staking_agent, _policy_agent = agency
+
+    staker_account, worker_account, *other = testerchain.unassigned_accounts
+
+    # The staker hasn't set a worker yet
+    assert Blockchain.NULL_ADDRESS == staking_agent.get_worker_from_staker(staker_address=staker_account)
+
+    _txhash = staking_agent.set_worker(staker_address=staker_account,
+                                       worker_address=worker_account)
+
+    # We can check the staker-worker relation from both sides
+    assert worker_account == staking_agent.get_worker_from_staker(staker_address=staker_account)
+    assert staker_account == staking_agent.get_staker_from_worker(worker_address=worker_account)
+
+    # No staker-worker relationship
+    random_address = to_checksum_address(os.urandom(20))
+    assert Blockchain.NULL_ADDRESS == staking_agent.get_worker_from_staker(staker_address=random_address)
+    assert Blockchain.NULL_ADDRESS == staking_agent.get_staker_from_worker(worker_address=random_address)
 
 
 @pytest.mark.slow()
@@ -78,29 +132,6 @@ def test_get_swarm(agency, blockchain_ursulas):
         int(staker_addr, 16)  # Verify the address is hex
     except ValueError:
         pytest.fail()
-
-
-@pytest.mark.slow()
-def test_locked_tokens(agency, blockchain_ursulas, token_economics):
-    token_agent, staking_agent, policy_agent = agency
-    agent = staking_agent
-    ursula = blockchain_ursulas[2]
-    locked_amount = agent.get_locked_tokens(staker_address=ursula.checksum_address)
-    assert token_economics.maximum_allowed_locked >= locked_amount >= token_economics.minimum_allowed_locked
-
-
-@pytest.mark.slow()
-def test_get_all_stakes(agency, blockchain_ursulas, token_economics):
-    token_agent, staking_agent, policy_agent = agency
-    agent = staking_agent
-    ursula = blockchain_ursulas[2]
-    all_stakes = list(agent.get_all_stakes(staker_address=ursula.checksum_address))
-    assert len(all_stakes) == 1
-    stake_info = all_stakes[0]
-    assert len(stake_info) == 3
-    start_period, end_period, value = stake_info
-    assert end_period > start_period
-    assert token_economics.maximum_allowed_locked > value > token_economics.minimum_allowed_locked
 
 
 @pytest.mark.slow()
