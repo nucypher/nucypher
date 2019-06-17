ef07719e4303d7d310cdd7054142dbdf0c3f1171
Kieran Prasch <kieran@nucypher.com>
2019-06-17 14:06:50 -0700

    Use condensed Blockchain API in config

--- nucypher/config/node.py
+++ nucypher/config/node.py
@@ -32,11 +32,9 @@ from twisted.logger import Logger
 from umbral.signing import Signature
 
 from nucypher.blockchain.eth.agents import PolicyAgent, StakingEscrowAgent, NucypherTokenAgent
-from nucypher.blockchain.eth.chains import Blockchain
+from nucypher.blockchain.eth.interfaces import Blockchain
 from nucypher.blockchain.eth.registry import EthereumContractRegistry
-from nucypher.blockchain.eth.token import StakeTracker
 from nucypher.config.base import BaseConfiguration
-from nucypher.config.constants import DEFAULT_CONFIG_ROOT, BASE_DIR
 from nucypher.config.keyring import NucypherKeyring
 from nucypher.config.storages import NodeStorage, ForgetfulNodeStorage, LocalFileBasedNodeStorage
 from nucypher.crypto.powers import CryptoPowerUp, CryptoPower
@@ -180,15 +178,22 @@ class CharacterConfiguration(BaseConfiguration):
     def dev_mode(self) -> bool:
         return self.__dev_mode
 
-    def connect_to_blockchain(self, recompile_contracts: bool = False, full_sync: bool = False) -> None:
+    @property
+    def known_nodes(self):
+        return self.__fleet_state
+
+    def connect_to_blockchain(self, sync_now: bool = False) -> None:
         if self.federated_only:
             raise CharacterConfiguration.ConfigurationError("Cannot connect to blockchain in federated mode")
-        self.blockchain = Blockchain.connect(provider_uri=self.provider_uri,
-                                             compile=recompile_contracts,
-                                             poa=self.poa,
-                                             fetch_registry=True,
-                                             provider_process=self.provider_process,
-                                             sync=full_sync)
+
+        self.blockchain = Blockchain(provider_uri=self.provider_uri,
+                                     poa=self.poa,
+                                     fetch_registry=True,
+                                     provider_process=self.provider_process,
+                                     sync_now=sync_now)
+
+        # Read Ethereum Node Keyring
+        self.accounts = self.blockchain.w3.eth.accounts
 
     def connect_to_contracts(self) -> None:
         self.token_agent = NucypherTokenAgent(blockchain=self.blockchain)
@@ -317,12 +322,13 @@ class CharacterConfiguration(BaseConfiguration):
     def dynamic_payload(self) -> dict:
         """Exported dynamic configuration values for initializing Ursula"""
         self.read_known_nodes()
-        payload = dict(network_middleware=self.network_middleware or self.DEFAULT_NETWORK_MIDDLEWARE(),
+        payload = dict(blockchain=self.blockchain,
+                       network_middleware=self.network_middleware or self.DEFAULT_NETWORK_MIDDLEWARE(),
                        known_nodes=self.known_nodes,
                        node_storage=self.node_storage,
-                       crypto_power_ups=self.derive_node_power_ups() or None)
+                       crypto_power_ups=self.derive_node_power_ups())
         if not self.federated_only:
-            self.connect_to_blockchain(recompile_contracts=False)
+            self.connect_to_blockchain()
             payload.update(blockchain=self.blockchain)
         return payload
 
