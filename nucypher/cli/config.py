9a9767fa0c5a7fda41b03554952aea58206d95dd
Bogdan Opanchuk <bogdan@opanchuk.net>
2019-06-18 18:23:49 -0700

    Make boolean envvar parsing more strict, and `log_to_sentry` and `log_to_file` type-stable

--- nucypher/cli/config.py
+++ nucypher/cli/config.py
@@ -18,6 +18,7 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import collections
+from distutils.util import strtobool
 import functools
 import os
 
@@ -44,6 +45,15 @@ from nucypher.utilities.logging import (
 from nucypher.utilities.sandbox.middleware import MockRestMiddleware
 
 
+def get_env_bool(var_name: str, default: bool) -> bool:
+    if var_name in os.environ:
+        # TODO: which is better: to fail on an incorrect envvar, or to use the default?
+        # Currently doing the former.
+        return strtoobool(os.environ[var_name])
+    else:
+        return default
+
+
 class NucypherClickConfig:
 
     # Output Sinks
@@ -54,8 +64,8 @@ class NucypherClickConfig:
     # Environment Variables
     config_file = os.environ.get('NUCYPHER_CONFIG_FILE')
     sentry_endpoint = os.environ.get("NUCYPHER_SENTRY_DSN", __sentry_endpoint)
-    log_to_sentry = os.environ.get("NUCYPHER_SENTRY_LOGS", True)
-    log_to_file = os.environ.get("NUCYPHER_FILE_LOGS", True)
+    log_to_sentry = get_env_bool("NUCYPHER_SENTRY_LOGS", True)
+    log_to_file = get_env_bool("NUCYPHER_FILE_LOGS", True)
 
     # Sentry Logging
     if log_to_sentry is True:
