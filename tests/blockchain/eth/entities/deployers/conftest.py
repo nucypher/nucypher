0e50796ff08b3c3f2a953d3c0b65480f1dab0728
David Núñez <david@nucypher.com>
2019-07-10 11:41:14 +0200

    Extract common fixtures for deployers tests

---
+++ tests/blockchain/eth/entities/deployers/conftest.py
@@ -0,0 +1,37 @@
+"""
+This file is part of nucypher.
+
+nucypher is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+nucypher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import pytest
+from nucypher.blockchain.eth.deployers import (NucypherTokenDeployer,
+                                               StakingEscrowDeployer)
+
+
+@pytest.fixture(scope="module")
+def token_deployer(session_testerchain):
+
+    token_deployer = NucypherTokenDeployer(blockchain=session_testerchain,
+                                           deployer_address=session_testerchain.etherbase_account)
+    return token_deployer
+
+
+@pytest.fixture(scope="module")
+def staking_escrow_deployer(session_testerchain, token_deployer):
+    token_deployer.deploy()
+
+    staking_escrow_deployer = StakingEscrowDeployer(blockchain=session_testerchain,
+                                                    deployer_address=session_testerchain.etherbase_account)
+    return staking_escrow_deployer
