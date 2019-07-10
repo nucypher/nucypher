76ef19d2ae12d59fa1009ef2538a9f6add12b8f0
tuxxy <me@johnpacific.com>
2019-07-10 16:05:04 -0600

    Remove deprecated code

--- nucypher/cli/hardware/backends.py
+++ nucypher/cli/hardware/backends.py
@@ -18,10 +18,6 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 
 from abc import ABC, abstractmethod
 from collections import namedtuple
-from functools import wraps
-from importlib import import_module
-
-from nucypher.crypto.signing import InvalidSignature
 
 
 class TrustedDevice(ABC):
@@ -91,93 +87,4 @@ class TrustedDevice(ABC):
         Abstract method for signing an Ethereum transaction via a device's
         API.
         """
-        raise NotImplementedError
-
-
-class Trezor(TrustedDevice):
-    """
-    An implementation of a Trezor device for staking on the NuCypher network.
-    """
-
-    def __init__(self):
-        try:
-            # Handle imports specific for trezor devices
-            from trezorlib.transport import TransportException
-            self.usb1 = import_module('usb1')
-            self.trezor_client = import_module('trezorlib.client')
-            self.trezor_device = import_module('trezorlib.device')
-            self.trezor_eth = import_module('trezorlib.ethereum')
-            self.trezor_tools = import_module('trezorlib.tools')
-
-            self.client = self.trezor_client.get_default_client()
-            self.__bip44_path = self.trezor_tools.parse_path(self.DEFAULT_BIP44_PATH)
-        except TransportException:
-            raise RuntimeError("Could not find a TREZOR device to connect to. Have you unlocked it?")
-        except ModuleNotFoundError:
-            raise RuntimeError("The nucypher package wasn't installed with the 'trezor' extra.")
-
-    def _handle_device_call(device_func):
-        @wraps(device_func)
-        def wrapped_call(inst, *args, **kwargs):
-            try:
-                result = device_func(inst, *args, **kwargs)
-            except inst.usb1.USBErrorNoDevice:
-                raise inst.DeviceError("The client cannot communicate to the TREZOR USB device. Was it disconnected?")
-            except inst.usb1.USBErrorBusy:
-                raise inst.DeviceError("The TREZOR USB device is busy.")
-            else:
-                return result
-        return wrapped_call
-
-    @_handle_device_call
-    def _reset(self):
-        """
-        Erases the TREZOR device by calling the wipe device function in
-        preparation to configure it for staking.
-
-        WARNING: This will delete ALL data on the TREZOR.
-        """
-        return self.trezor_device.wipe(self.client)
-
-    @_handle_device_call
-    def configure(self):
-        raise NotImplementedError
-
-    @_handle_device_call
-    def sign_message(self, message: bytes, address_index: int = 0):
-        """
-        Signs a message via the TREZOR ethereum sign_message API and returns
-        the signature and the address used to sign it. This method requires
-        interaction between the TREZOR and the user.
-
-        If an address_index is provided, it will use the address at that
-        index to sign the message. If no index is provided, the address at
-        the 0th index is used by default.
-        """
-        bip44_path = self.__bip44_path + [address_index]
-
-        sig = self.trezor_eth.sign_message(self.client, bip44_path, message)
-        return self.Signature(sig.signature, sig.address)
-
-    @_handle_device_call
-    def verify_message(self, signature: bytes, message: bytes, address: str):
-        """
-        Verifies that a signature and message pair are from a specified
-        address via the TREZOR ethereum verify_message API. This method
-        requires interaction between the TREZOR and the user.
-
-        If the signature or message is not valid, it will raise a
-        nucypher.crypto.signing.InvalidSignature exception. Otherwise, it
-        will return True.
-
-        TODO: Should we provide some input validation for the ETH address?
-        """
-        is_valid = self.trezor_eth.verify_message(self.client, address,
-                                                  signature, message)
-        if not is_valid:
-            raise InvalidSignature("Signature verification failed.")
-        return True
-
-    @_handle_device_call
-    def sign_eth_transaction(self):
         raise NotImplementedError
