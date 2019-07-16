ca0715fec81ada2df0daf7edec41b87133fc87b9
Kieran Prasch <kieran@nucypher.com>
2019-07-16 16:45:26 -0700

    Adds integration test for creating new geth accounts over RPC clients.

--- tests/blockchain/eth/clients/test_geth_integration.py
+++ tests/blockchain/eth/clients/test_geth_integration.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+from eth_utils import is_checksum_address
 
 from nucypher.blockchain.eth.interfaces import BlockchainInterface
 from nucypher.crypto.api import verify_eip_191
@@ -9,6 +10,8 @@ from nucypher.crypto.api import verify_eip_191
 #
 # NOTE: This module is skipped on CI
 #
+from nucypher.utilities.sandbox.constants import INSECURE_DEVELOPMENT_PASSWORD
+
 
 def test_geth_EIP_191_client_signature_integration(geth_dev_node):
     if 'CIRCLECI' in os.environ:
@@ -26,3 +29,14 @@ def test_geth_EIP_191_client_signature_integration(geth_dev_node):
                               signature=signature,
                               message=stamp)
     assert is_valid
+
+
+def test_geth_create_new_account(geth_dev_node):
+    if 'CIRCLECI' in os.environ:
+        pytest.skip("Do not run Geth nodes in CI")
+
+    # Start a geth process
+    blockchain = BlockchainInterface(provider_process=geth_dev_node)
+    blockchain.connect(fetch_registry=False, sync_now=False)
+    new_account = blockchain.client.new_account(password=INSECURE_DEVELOPMENT_PASSWORD)
+    assert is_checksum_address(new_account)
