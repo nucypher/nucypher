3061ebe7f82169cbf7d2e9241ed6442346360223
Kieran R. Prasch <kieranprasch@gmail.com>
2019-08-21 14:04:28 -0700

    Bump version: 0.1.0-alpha.24 → 0.1.0-alpha.25

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ author = 'NuCypher'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.0-alpha.24'
+release = '0.1.0-alpha.25'
 
 
 # -- General configuration ---------------------------------------------------
