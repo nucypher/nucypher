c6c823780307306bfa6b01516b79a2ca6bc2c863
jMyles <justin@justinholmes.com>
2019-08-22 10:33:21 +0200

    New Bob CLI test to show retrieve().

--- tests/cli/test_bob.py
+++ tests/cli/test_bob.py
@@ -1,4 +1,14 @@
+import json
+import os
+from base64 import b64decode, b64encode
+
+from nucypher.characters.control.emitters import JSONRPCStdoutEmitter
+from nucypher.characters.lawful import Ursula
 from nucypher.cli.main import nucypher_cli
+from nucypher.config.characters import BobConfiguration
+from nucypher.crypto.kits import UmbralMessageKit
+from nucypher.crypto.powers import SigningPower
+from nucypher.utilities.sandbox.constants import INSECURE_DEVELOPMENT_PASSWORD, TEMPORARY_DOMAIN, TEST_PROVIDER_URI
 
 
 def test_bob_cannot_init_with_dev_flag(click_runner):
@@ -8,3 +18,68 @@ def test_bob_cannot_init_with_dev_flag(click_runner):
     result = click_runner.invoke(nucypher_cli, init_args, catch_exceptions=False)
     assert result.exit_code == 2
     assert 'Cannot create a persistent development character' in result.output, 'Missing or invalid error message was produced.'
+
+
+def test_bob_retrieve_args(click_runner,
+                           capsule_side_channel,
+                           enacted_federated_policy,
+                           federated_ursulas,
+                           custom_filepath_2,
+                           federated_alice
+                           ):
+    teacher = list(federated_ursulas)[0]
+
+    first_message = capsule_side_channel.reset(plaintext_passthrough=True)
+    three_message_kits = [capsule_side_channel(), capsule_side_channel(), capsule_side_channel()]
+
+    bob_config_root = custom_filepath_2
+    bob_configuration_file_location = os.path.join(bob_config_root, BobConfiguration.generate_filename())
+    label = enacted_federated_policy.label
+
+    # I already have a Bob.
+
+    # Need to init so that the config file is made, even though we won't use this Bob.
+    bob_init_args = ('bob', 'init',
+                     '--network', TEMPORARY_DOMAIN,
+                     '--config-root', bob_config_root,
+                     '--federated-only')
+
+    envvars = {'NUCYPHER_KEYRING_PASSWORD': INSECURE_DEVELOPMENT_PASSWORD}
+
+    bob_init_response = click_runner.invoke(nucypher_cli, bob_init_args, catch_exceptions=False, env=envvars)
+
+    message_kit_bytes = bytes(three_message_kits[0])
+    message_kit_b64_bytes = b64encode(message_kit_bytes)
+    UmbralMessageKit.from_bytes(message_kit_bytes)
+
+    retrieve_args = ('bob', 'retrieve',
+                     '--mock-networking',
+                     '--json-ipc',
+                     '--teacher', teacher.seed_node_metadata(as_teacher_uri=True),
+                     '--config-file', bob_configuration_file_location,
+                     '--message-kit', message_kit_b64_bytes,
+                     '--label', label,
+                     '--policy-encrypting-key', federated_alice.get_policy_encrypting_key_from_label(label).hex(),
+                     '--alice-verifying-key', federated_alice.public_keys(SigningPower).hex()
+                     )
+
+    from nucypher.cli import actions
+
+    def substitue_bob(*args, **kwargs):
+        this_fuckin_guy = enacted_federated_policy.bob
+        somebody_else = Ursula.from_teacher_uri(teacher_uri=kwargs['teacher_uri'],
+                                               min_stake=0,
+                                               federated_only=True,
+                                               network_middleware=this_fuckin_guy.network_middleware)
+        this_fuckin_guy.remember_node(somebody_else)
+        this_fuckin_guy.controller.emitter = JSONRPCStdoutEmitter()
+        return this_fuckin_guy
+
+    actions.make_cli_character = substitue_bob
+
+    retrieve_response = click_runner.invoke(nucypher_cli, retrieve_args, catch_exceptions=False, env=envvars)
+    assert retrieve_response.exit_code == 0
+
+    retrieve_response = json.loads(retrieve_response.output)
+    for cleartext in retrieve_response['result']['cleartexts']:
+        assert cleartext.encode() == capsule_side_channel.plaintexts[1]
