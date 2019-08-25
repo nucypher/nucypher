99d44c098146f6fd637bb60c198a7fd68c883314
Damon C <gdamon@gmail.com>
2019-08-25 17:30:04 +0200

    handle images, output text, errors

--- apps/connectnative/panel/panel.js
+++ apps/connectnative/panel/panel.js
@@ -4,39 +4,73 @@ const prevdata = {};
 
 //callbacks
 
+function onGenericNucypherReturn(data){
+    try {
+        clearResults();
+        displayResults(JSON.parse(data.result).result);
+    } catch {
+        displayError(data.result);
+    }
+}
+
+function onDecrypt(data){
+    try {
+        results = JSON.parse(data.result).result;
+        const cleartexts = results.cleartexts;
+
+        console.log()
+        var lg = $('<ul class="list-group"></ul>')
+        $('#output').append(lg);
+        $.each(cleartexts, function(i, d){
+            lg.append(
+                `<li class="list-group-item maybeimage"><div class="text">${d}</div></div>`
+            );
+        });
+        const img_lookup = {
+            '/' : 'jpg',
+            'i' : 'png',
+            'r' : 'gif',
+        }
+        $('.maybeimage').each(function(t){
+            $(this).parent().find('.convertimg').remove();
+            if (img_lookup[$(this).text().charAt(0)]){
+                type=img_lookup[$(this).text().charAt(0)];
+                $(this).parent().append(`<button imgtype="${type}"class="btn btn-success convertimg">is this an image?</button>`);
+            }
+        });
+
+        $('.convertimg').on("click", function(e){
+            var el = $(this).prev('.maybeimage')
+            var text = el.find('.text').text();
+            var img = $('<img src="#"></img');
+            el.find('.text').hide();
+            img.attr('src', `data:image/${$(this).attr('imgtype')};base64,`+text);
+            el.append(img);
+            $(this).html("nope not an image.").off("click").on("click", function(e){
+                $(this).prev('.maybeimage').find(".text").show();
+                $(this).prev('.maybeimage').find("img").remove();
+                $(this).remove();
+            });
+        });
+    } catch {
+        displayError(data.result);
+    }
+}
+
 function onGetPassword(){
-    $('#commandform').empty();
-    $('#submitbutton').off("click");
+    clearResults()
     $('#commandform').append('<h4 style="color:salmon"> please enter your password</h4>');
 }
 
-function onRetrieved(data){
-    displayResults(data)
-}
-
-function onStatus(data){
-    displayResults(data)
-}
-
-function onGranted(data) {
-    const result = JSON.parse(data.result).result;
-    $('#commandform').empty();
-    $('#submitbutton').off("click");
-    $('#commandform').append('<div class="alert alert-success" role="alert">Success</div>');
-    displayResults(result);
-}
 
 function onOptions(data){
-
-    console.log(prevdata);
+    clearResults()
     if (data.error && data.error === "keyring password is required"){
         return onGetPassword();
     }
 
-    $('#commandform').empty();
-    $('#submitbutton').off("click");
-    $('#commandform').append(`<input type="hidden" name="character" value="${data.input.character}"></input>`)
-    $('#commandform').append(`<input type="hidden" name="action" value="${data.input.action}"></input>`)
+    $('#commandform').append(`<div class="form-group"><input type="hidden" name="character" value="${data.input.character}"></input></div>`)
+    $('#commandform').append(`<div class="form-group"><input type="hidden" name="action" value="${data.input.action}"></input></div>`)
 
     const ui = {
         text: '<div class="form-group"><input class="form-control" type="text"></input></div>',
@@ -49,7 +83,6 @@ function onOptions(data){
         $.each(Object.keys(options), function(i, o){
             var type = options[o];
             var name = o.replace("_", " ");
-
 
             $('#commandform').append(`<label for="${name}input">${name}</label>`);
             var el = $(ui[type])
@@ -69,24 +102,18 @@ function onOptions(data){
                 keyring_password: $('#passwordinput').val(),
             }
             submitdata = Object.assign(submitdata, $('#commandform').serializeObject())
-            prevdata[`${submitdata.character}.${submitdata.action}`] = submitdata;
-
-            console.log(prevdata);
             bgPort.postMessage({route: "execute", data: submitdata});
         });
     } catch {
         // json can't be parsed?
-        var data = {result: data.result || "NuCypher returned an empty result."};
-        displayResults(data);
+        var data = data.result || "NuCypher returned an empty result.";
+        displayError(data);
     }
-
-
 }
 
 // button events
 $('.btn.action').on("click", function(){
-    $('#commandform').empty();
-    $('#submitbutton').off("click");
+    clearResults()
 
     bgPort.postMessage({
         route: "options",
@@ -107,10 +134,12 @@ $('#passwordbutton').on("click", function(){
 // internal workings
 function fDispatcher(message){
     const callbacks = {
-        'bob.retrieve': onRetrieved,
-        'alice.grant': onGranted,
+        'bob.retrieve': onGenericNucypherReturn,
+        'alice.grant': onGenericNucypherReturn,
+        'alice.decrypt': onDecrypt,
+        'enrico.encrypt': onGenericNucypherReturn,
         'need-password': onGetPassword,
-        'status': onStatus,
+        'status': onGenericNucypherReturn,
         'options': onOptions,
     }
     if (callbacks[message.route] !== undefined){
@@ -119,11 +148,25 @@ function fDispatcher(message){
 }
 
 function displayResults(result){
+    $('#output').append('<div class="alert alert-success" role="alert">Success</div>');
     var lg = $('<ul class="list-group"></ul>')
-    $('#commandform').append(lg);
+    $('#output').append(lg);
     $.each(Object.keys(result), function(i, a){
         lg.append(`<li class="list-group-item"><strong>${a}:</strong> ${result[a]}</li>`)
     })
+}
+
+function clearResults(){
+    $('#commandform').empty();
+    $('#submitbutton').off("click");
+    $('#output').empty();
+}
+
+function displayError(result){
+    $('#output').append('<div class="alert alert-danger" role="alert">Error</div>');
+    var lg = $('<ul class="list-group"></ul>')
+    $('#output').append(lg);
+    lg.append(`<li class="list-group-item"><strong>result:</strong> ${result}</li>`)
 }
 
 var bgPort = browser.runtime.connect({name: "panel-messages"});
