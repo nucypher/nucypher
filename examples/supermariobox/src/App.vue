827df4eceed675798208654544868dc0bec0f547
Damon C <gdamon@gmail.com>
2019-08-29 05:03:54 -0700

    connect native app and example

---
+++ examples/supermariobox/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Logo style="width:300px" :hide-logo="false" :hide-text="false"/>
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue';
+import Logo from './components/NuCypherLogo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Main,
+    Logo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
