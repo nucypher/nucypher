4bb83cf1803fec243d5c1911f93adb3980f642dd
David Núñez <david@nucypher.com>
2019-09-02 18:29:35 +0200

    Adapt sampling tests to new changes

--- tests/blockchain/eth/contracts/main/staking_escrow/test_sampling.py
+++ tests/blockchain/eth/contracts/main/staking_escrow/test_sampling.py
@@ -18,14 +18,13 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 
 import pytest
 from eth_tester.exceptions import TransactionFailed
-from web3.contract import Contract
 
+from nucypher.blockchain.eth.interfaces import BlockchainInterface
 
 @pytest.mark.slow
 def test_sampling(testerchain, token, escrow_contract):
     escrow = escrow_contract(5 * 10 ** 8)
-    NULL_ADDR = '0x' + '0' * 40
-    creator = testerchain.client.accounts[0]
+    creator = testerchain.etherbase_account
 
     # Give Escrow tokens for reward and initialize contract
     tx = token.functions.transfer(escrow.address, 10 ** 9).transact({'from': creator})
@@ -43,16 +42,18 @@ def test_sampling(testerchain, token, escrow_contract):
         testerchain.wait_for_receipt(tx)
         amount = amount // 2
 
-    # Cant't use sample without points or with zero periods value
-    with pytest.raises((TransactionFailed, ValueError)):
+    # Can't use sample without points, with zero periods value, or not sorted in ascending order.
+    with pytest.raises(TransactionFailed):
         escrow.functions.sample([], 1).call()
-    with pytest.raises((TransactionFailed, ValueError)):
+    with pytest.raises(TransactionFailed):
         escrow.functions.sample([1], 0).call()
+    with pytest.raises(TransactionFailed):
+        escrow.functions.sample([3, 2, 1], 0).call()
 
     # No stakers yet
     addresses = escrow.functions.sample([1], 1).call()
     assert 1 == len(addresses)
-    assert NULL_ADDR == addresses[0]
+    assert BlockchainInterface.NULL_ADDRESS == addresses[0]
 
     all_locked_tokens = 0
     # All stakers lock tokens for different lock periods
@@ -74,7 +75,7 @@ def test_sampling(testerchain, token, escrow_contract):
     # So sampling in current period is useless
     addresses = escrow.functions.sample([1], 1).call()
     assert 1 == len(addresses)
-    assert NULL_ADDR == addresses[0]
+    assert BlockchainInterface.NULL_ADDRESS == addresses[0]
 
     # Wait next period and check all locked tokens
     testerchain.time_travel(hours=1)
@@ -89,35 +90,36 @@ def test_sampling(testerchain, token, escrow_contract):
         testerchain.wait_for_receipt(tx)
 
     # Sample one staker by value less than first staker's stake
-    addresses = escrow.functions.sample([all_locked_tokens // 3], 1).call()
+    addresses = escrow.functions.sample([largest_locked - 1], 1).call()
     assert 1 == len(addresses)
     assert stakers[0] == addresses[0]
 
     # Sample two stakers by values that are equal to first and second stakes
     # In the result must be second and third stakers because of strict condition in the sampling
-    # sumOfLockedTokens > point
-    addresses = escrow.functions.sample([largest_locked, largest_locked // 2], 1).call()
+    addresses = escrow.functions.sample([largest_locked, largest_locked + largest_locked // 2], 1).call()
     assert 2 == len(addresses)
     assert stakers[1] == addresses[0]
     assert stakers[2] == addresses[1]
+
+    # Sample two stakers by values that within first and second stakes
+    # In the result must be second and third stakers because of strict condition in the sampling
+    # sumOfLockedTokens > point
+    addresses = escrow.functions.sample([largest_locked - 1, largest_locked + 1], 1).call()
+    assert 2 == len(addresses)
+    assert stakers[0] == addresses[0]
+    assert stakers[1] == addresses[1]
 
     # Sample staker by the max duration of the longest stake
     # The result is the staker who has the longest stake
     addresses = escrow.functions.sample([1], len(stakers)).call()
     assert 1 == len(addresses)
     assert stakers[-1] == addresses[0]
+
     # Sample staker using the duration more than the longest stake
     # The result is empty
     addresses = escrow.functions.sample([1], len(stakers) + 1).call()
     assert 1 == len(addresses)
-    assert NULL_ADDR == addresses[0]
-
-    # Sample by values that more than all locked tokens
-    # Only one staker will be in the result
-    addresses = escrow.functions.sample([largest_locked, largest_locked], 1).call()
-    assert 2 == len(addresses)
-    assert stakers[1] == addresses[0]
-    assert NULL_ADDR == addresses[1]
+    assert BlockchainInterface.NULL_ADDRESS == addresses[0]
 
     # Sample stakers by different durations and minimum value
     # Each result is the first appropriate stake by length
@@ -127,9 +129,11 @@ def test_sampling(testerchain, token, escrow_contract):
         assert stakers[index] == addresses[0]
 
     # Sample all stakers by values as stake minus one
-    # The result must contain all stakers because of condition sumOfLockedTokens > point
+    # The result must contain all stakers
     points = [escrow.functions.getLockedTokens(staker).call() for staker in stakers]
     points[0] = points[0] - 1
+    for i in range(1, len(points)):
+        points[i] += points[i-1]
     addresses = escrow.functions.sample(points, 1).call()
     assert stakers == addresses
 
