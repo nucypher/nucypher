9cdddc504b6ae5fdba5d15b9fce77bf0adf6091f
damon <gdamon@gmail.com>
2019-09-04 16:55:38 -0700

    adds connectnative demo web extension

---
+++ apps/connectnative/background.js
@@ -0,0 +1,95 @@
+
+// connect native
+const nucypher = browser.runtime.connectNative("nucypher");
+var popupWindow;
+var password;
+
+nucypher.onMessage.addListener((response) => {
+  // response is a key which can be used to decrypt the original image.
+  let route = response.route;
+  if (response.input.options){
+    route = "options";
+  }
+  if (ports['panel-messages']){
+    ports['panel-messages'].postMessage({
+      route: route,
+      data: response,
+    });
+  }
+  if(ports['content-messages']){
+    ports['content-messages'].postMessage({
+      route: route,
+      data: response,
+    });
+  }
+});
+// encd connect native
+
+//callbacks
+
+function NucypherExecute(request) {
+  if (request.character === "undefined") {
+    delete request.character;
+  }
+  nucypher.postMessage(request);
+};
+
+function NucypherOptions(request) {
+  request.options = true;
+  request.keyring_password = password;
+  request.args = {
+    options: true,
+  }
+  nucypher.postMessage(request);
+}
+
+function getPassword(request){
+  ports['panel-messages'].postMessage({
+    route: 'need-password',
+  });
+}
+
+function setPassword(data){
+  password = data;
+  ports['content-messages'].postMessage({
+    route: 'setPassword',
+    data: data,
+  });
+}
+
+// browser interaction
+var ports = {
+  "panel-messages": null,
+  "content-messages": null,
+}
+
+function Dispatcher(message){
+  const callbacks = {
+    execute: NucypherExecute,
+    options: NucypherOptions,
+    'need-password': getPassword,
+    setPassword: setPassword,
+  }
+  if (callbacks[message.route] !== undefined){
+    return callbacks[message.route](message.data);
+  }
+}
+
+function connected(p) {
+  ports[p.name] = p;
+  ports[p.name].onMessage.addListener(Dispatcher);
+  console.log(ports)
+}
+browser.runtime.onConnect.addListener(connected);
+
+//panel interaction
+browser.browserAction.onClicked.addListener(() => {
+  popupWindow = browser.windows.create(
+    {
+      type: "detached_panel",
+      url: "popup.html",
+      width: 600,
+      height: 600,
+    }
+  );
+})
---
+++ apps/connectnative/content.js
@@ -0,0 +1,70 @@
+
+$("body").find("nucypher").append(
+    '<div><div style="height:300px"><img style="height:300px" class="imgcontainer" src="'+browser.runtime.getURL("images/box.png")+'"><img class="coin" style="width:100px;margin-top:50px;" src="'+browser.runtime.getURL("images/coin.gif")+'"></div><span class="label">click to retrieve</span></div>'
+);
+$("body").find(".coin").hide();
+
+var bgPort = browser.runtime.connect({name: "content-messages"});
+var password = null;
+
+function formatId(messageKit){
+    return messageKit.slice(0, 10).replace('+', 'x').replace('/', '1');
+}
+
+function onRetrieved(data){
+    const element_id = formatId(data.input.args['message-kit']);
+    var element = $('#' + element_id);
+    if (data.result.length){
+        let imagedata = JSON.parse(data.result).result.cleartexts[0];
+        element.find(".imgcontainer").attr('src', 'data:image/png;base64,' + imagedata).show();
+    } else{
+        element.find(".imgcontainer").attr('src', browser.runtime.getURL("images/denied.png")).show()
+    }
+    element.find('.coin').hide();
+}
+
+function setPassword(data){
+    password = data;
+}
+
+function fDispatcher(message){
+    const callbacks = {
+        'bob.retrieve': onRetrieved,
+        setPassword: setPassword,
+    }
+    if (callbacks[message.route] !== undefined){
+        return callbacks[message.route](message.data);
+    }
+}
+
+bgPort.onMessage.addListener(fDispatcher);
+
+$("body").find("nucypher").on("click", function(){
+
+    if (password) {
+        $(this).find('.prompt').remove();
+        var data = JSON.parse($(this).attr("data-data"));
+        $(this).attr("id", formatId(data['message-kit']));
+        $(this).find('.coin').show();
+        $(this).find('.imgcontainer').hide().attr(
+            'src', browser.runtime.getURL("images/denied.png"));
+
+        const message = {
+            route: 'execute',
+            data: {
+                character: 'bob',
+                action: 'retrieve',
+                keyring_password: password,
+                args: data,
+            }
+        };
+
+        bgPort.postMessage(message);
+    } else {
+        alert("Please Open the NuCypher Control Panel and enter a password");
+        bgPort.postMessage({
+            route: "need-password",
+        });
+    }
+});
+
