f505012b10576ca9116de1065e43d38d2a9d5af3
Kieran R. Prasch <kieranprasch@gmail.com>
2019-09-04 19:51:32 -0700

    Remove configuration dependency from status CLI. renames CLI 'view' -> 'network'. Support status via registry filepath.

--- nucypher/cli/status.py
+++ nucypher/cli/status.py
@@ -16,18 +16,16 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 
-import binascii
 import click
 
 from nucypher.blockchain.eth.agents import StakingEscrowAgent, ContractAgency
 from nucypher.blockchain.eth.interfaces import BlockchainInterfaceFactory
-from nucypher.blockchain.eth.registry import InMemoryContractRegistry
+from nucypher.blockchain.eth.registry import InMemoryContractRegistry, LocalContractRegistry
 from nucypher.characters.banners import NU_BANNER
 from nucypher.cli.actions import get_provider_process
 from nucypher.cli.config import nucypher_click_config
-from nucypher.cli.painting import paint_contract_status, paint_stakers, paint_locked_tokens_status, paint_known_nodes
-from nucypher.cli.types import EIP55_CHECKSUM_ADDRESS
-from nucypher.config.characters import UrsulaConfiguration
+from nucypher.cli.painting import paint_contract_status, paint_stakers, paint_locked_tokens_status
+from nucypher.cli.types import EIP55_CHECKSUM_ADDRESS, EXISTING_READABLE_FILE
 
 
 @click.command()
@@ -36,17 +34,18 @@ from nucypher.config.characters import UrsulaConfiguration
 @click.option('--sync/--no-sync', default=False)
 @click.option('--geth', '-G', help="Run using the built-in geth node", is_flag=True)
 @click.option('--provider', 'provider_uri', help="Blockchain provider's URI", type=click.STRING, default="auto://")
+@click.option('--registry-filepath', help="Custom contract registry filepath", type=EXISTING_READABLE_FILE)
 @click.option('--periods', help="Number of periods", type=click.INT, default=90)
 @click.option('--staking-address', help="Address of a NuCypher staker", type=EIP55_CHECKSUM_ADDRESS)
 @nucypher_click_config
-def status(click_config, action, provider_uri, sync, geth, poa, periods, staking_address):
+def status(click_config, action, provider_uri, sync, geth, poa, periods, staking_address, registry_filepath):
     """
     Echo a snapshot of live NuCypher Network metadata.
 
     \b
     Actions
     -------------------------------------------------
-    view           Overall information of the NuCypher Network
+    network        Overall information of the NuCypher Network
     stakers        Show relevant information about stakers
     locked-tokens  Display a graph of evolution of locked tokens
 
@@ -73,20 +72,17 @@ def status(click_config, action, provider_uri, sync, geth, poa, periods, staking
         click.secho(str(e), bold=True, fg='red')
         return  # Exit
 
-    # TODO: Allow --registry
-    registry = InMemoryContractRegistry.from_latest_publication()
+    if registry_filepath:
+        registry = LocalContractRegistry(filepath=registry_filepath)
+    else:
+        registry = InMemoryContractRegistry.from_latest_publication()
     staking_agent = ContractAgency.get_agent(StakingEscrowAgent, registry=registry)
 
-    if action == 'view':
-        ursula_config = UrsulaConfiguration.from_configuration_file(filepath=click_config.config_file)
-        if not ursula_config.federated_only:
-            paint_contract_status(registry, emitter=emitter)
-
-        # TODO: Handle failure to paint known nodes when there's no known nodes metadata stored
-        paint_known_nodes(emitter=click_config.emitter, ursula=ursula_config)
+    if action == 'network':
+        paint_contract_status(registry, emitter=emitter)
         return  # Exit
 
-    if action == 'stakers':
+    elif action == 'stakers':
         stakers = [staking_address] if staking_address else staking_agent.get_stakers()
         paint_stakers(emitter=emitter, stakers=stakers, agent=staking_agent)
         return  # Exit
