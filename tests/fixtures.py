812349ffd5eb469dba9c6bcdd146965e85ae0006
David Núñez <david@nucypher.com>
2019-09-10 11:11:17 +0200

    Reduce probability of skewed stake distribution in stakers fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -61,6 +61,8 @@ from nucypher.utilities.sandbox.blockchain import token_airdrop, TesterBlockchai
 from nucypher.utilities.sandbox.constants import (
     DEVELOPMENT_ETH_AIRDROP_AMOUNT,
     DEVELOPMENT_TOKEN_AIRDROP_AMOUNT,
+    MIN_STAKE_FOR_TESTS,
+    BONUS_TOKENS_FOR_TESTS,
     MOCK_POLICY_DEFAULT_M,
     MOCK_URSULA_STARTING_PORT,
     NUMBER_OF_URSULAS_IN_DEVELOPMENT_NETWORK,
@@ -69,7 +71,7 @@ from nucypher.utilities.sandbox.constants import (
     INSECURE_DEVELOPMENT_PASSWORD,
     MOCK_REGISTRY_FILEPATH,
     TEST_GAS_LIMIT,
-    INSECURE_DEPLOYMENT_SECRET_HASH
+    INSECURE_DEPLOYMENT_SECRET_HASH,
 )
 from nucypher.utilities.sandbox.middleware import MockRestMiddleware
 from nucypher.utilities.sandbox.policy import generate_random_label
@@ -481,8 +483,7 @@ def stakers(testerchain, agency, token_economics, test_registry):
         staker.transacting_power = TransactingPower(password=INSECURE_DEVELOPMENT_PASSWORD, account=account)
         staker.transacting_power.activate()
 
-        min_stake, balance = token_economics.minimum_allowed_locked, staker.token_balance
-        amount = random.randint(min_stake, balance)
+        amount = MIN_STAKE_FOR_TESTS + random.randrange(BONUS_TOKENS_FOR_TESTS)
 
         # for a random lock duration
         min_locktime, max_locktime = token_economics.minimum_locked_periods, token_economics.maximum_rewarded_periods
