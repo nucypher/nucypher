4b5e74788fcce0120252944aede4fb876bf5d922
jMyles <justin@justinholmes.com>
2019-09-21 16:54:26 -0700

    Adding logging around consecutive retrieval test.

--- tests/cli/test_bob.py
+++ tests/cli/test_bob.py
@@ -88,6 +88,7 @@ def test_bob_retrieves_twice_via_cli(click_runner,
 
         # Once...
         retrieve_response = click_runner.invoke(nucypher_cli, retrieve_args, catch_exceptions=False, env=envvars)
+        log.info(f"First retrieval response: {retrieve_response.output}")
         assert retrieve_response.exit_code == 0
 
         retrieve_response = json.loads(retrieve_response.output)
@@ -96,6 +97,7 @@ def test_bob_retrieves_twice_via_cli(click_runner,
 
         # and again!
         retrieve_response = click_runner.invoke(nucypher_cli, retrieve_args, catch_exceptions=False, env=envvars)
+        log.info(f"Second retrieval response: {retrieve_response.output}")
         assert retrieve_response.exit_code == 0
 
         retrieve_response = json.loads(retrieve_response.output)
