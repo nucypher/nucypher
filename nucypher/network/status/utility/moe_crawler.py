bfd9f1ab36d20b1b3e3cc20f4607dbb823043a4e
derekpierre <derek.pierre@gmail.com>
2019-10-02 15:20:30 -0400

    Add historical graph for number of stakers - I'm pretty sure the Influx queries can be improved but suffices for now. Improve graph layouts (titles, zerolines, number of ticks on x-axis)

--- nucypher/network/status/utility/moe_crawler.py
+++ nucypher/network/status/utility/moe_crawler.py
@@ -177,7 +177,7 @@ class MoeCrawlerDBClient:
     def __init__(self, host, port, database):
         self._client = InfluxDBClient(host=host, port=port, database=database)
 
-    def get_previously_locked_tokens_over_range(self, days: int):
+    def get_historical_locked_tokens_over_range(self, days: int):
         today = datetime.utcnow()
         range_end = datetime(year=today.year, month=today.month, day=today.day,
                              hour=0, minute=0, second=0, microsecond=0)
@@ -194,7 +194,6 @@ class MoeCrawlerDBClient:
 
         # Note: all days may not have values eg. days before DB started getting populated
         # As time progresses this should be less of an issue
-        end_time = MayaDT.from_datetime(range_end)
         locked_tokens_dict = OrderedDict()
         for r in results:
             locked_stake = r['sum']
@@ -203,6 +202,32 @@ class MoeCrawlerDBClient:
                 locked_tokens_dict[MayaDT.from_rfc3339(r['time']).datetime()] = locked_stake
 
         return locked_tokens_dict
+
+    def get_historical_num_stakers_over_range(self, days: int):
+        today = datetime.utcnow()
+        range_end = datetime(year=today.year, month=today.month, day=today.day,
+                             hour=0, minute=0, second=0, microsecond=0)
+        range_begin = range_end - timedelta(days=days - 1)
+        results = list(self._client.query(f"SELECT COUNT(staker_address) FROM "
+                                          f"("
+                                            f"SELECT staker_address, LAST(locked_stake)"
+                                            f"FROM moe_network_info WHERE "
+                                            f"time >= '{MayaDT.from_datetime(range_begin).rfc3339()}' AND "
+                                            f"time < '{MayaDT.from_datetime(range_end + timedelta(days=1)).rfc3339()}' "
+                                            f"GROUP BY staker_address, time(1d)"
+                                          f") "
+                                          "GROUP BY time(1d)").get_points())   # 1 day measurements
+
+        # Note: all days may not have values eg. days before DB started getting populated
+        # As time progresses this should be less of an issue
+        num_stakers_dict = OrderedDict()
+        for r in results:
+            locked_stake = r['count']
+            if locked_stake:
+                # Dash accepts datetime objects for graphs
+                num_stakers_dict[MayaDT.from_rfc3339(r['time']).datetime()] = locked_stake
+
+        return num_stakers_dict
 
     def close(self):
         self._client.close()
