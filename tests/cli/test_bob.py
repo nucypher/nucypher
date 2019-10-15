068f02e2afedd402025791fb4fdf323695ad732f
derekpierre <derek.pierre@gmail.com>
2019-10-15 14:56:40 -0400

    Changed CLI format for "bob" command - improve help and detection of invalid commands Update unit tests

--- tests/cli/test_bob.py
+++ tests/cli/test_bob.py
@@ -1,10 +1,11 @@
 from nucypher.cli.main import nucypher_cli
 
 
+# TODO: test can probably be removed (--dev is not a valid option for `init`)
 def test_bob_cannot_init_with_dev_flag(click_runner):
     init_args = ('bob', 'init',
                  '--federated-only',
                  '--dev')
     result = click_runner.invoke(nucypher_cli, init_args, catch_exceptions=False)
     assert result.exit_code == 2
-    assert 'Cannot create a persistent development character' in result.output, 'Missing or invalid error message was produced.'
+    assert 'no such option: --dev' in result.output, 'Missing or invalid error message was produced.'
