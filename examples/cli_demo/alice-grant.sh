3cc1147670a4875f4468407449abe61b2a8fbf30
David Núñez <david@nucypher.com>
2019-11-04 13:12:28 +0100

    First pass at a full CLI demo in CI: grant, encrypt and retrieve

---
+++ examples/cli_demo/alice-grant.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $# != 1 ]; then
+    echo "This script requires exactly one argument for policy label"
+    exit 1
+fi
+
+# Setup Alice prefabricated keyring and ETH key
+tar -xzf ~/nucypher/examples/cli_demo/alice.tar.gz -C ~
+mkdir --parents ~/alice/known_nodes/metadata
+mkdir ~/alice/known_nodes/certificates
+mkdir --parents /tmp/cli-demo
+cp ~/alice/alice-0xADA.eth ~/.ethereum/goerli/keystore
+
+# Setup runtime environment
+export NUCYPHER_KEYRING_PASSWORD=$TEST_ALICE_KEYRING_PASSWORD
+export NUCYPHER_ALICE_ETH_PASSWORD=$TEST_ALICE_ETH_PASSWORD
+ALICE_ETH_ADDRESS='0xADA16B764e8aa666120c7Ea6E339E66730C96674'
+BOB_ENCRYPTING_KEY='02be9e3d9209194e2d9506ef45062e39a92bde3db8f53b417d89754236e8bf7e78'
+BOB_VERIFYING_KEY='0350240f404ccf073509a6cf679f0582416f342cf796660bac963262052d8c8677'
+TEACHER='discover.nucypher.network:9151'
+LABEL=$1
+EXPIRATION=$(python3 -c "import maya, datetime; print((maya.now() + datetime.timedelta(days=1)).iso8601())")
+
+# Alice grants
+nucypher alice grant \
+    --teacher $TEACHER \
+    --bob-verifying-key $BOB_VERIFYING_KEY \
+    --bob-encrypting-key $BOB_ENCRYPTING_KEY \
+    --label $LABEL \
+    --expiration $EXPIRATION \
+    --m 1 --n 1 --rate 1 \
+    --config-file ~/alice/alice.json \
+    --light \
+    --json-ipc \
+    | tee /tmp/grant.json
+
+cp ~/.cache/nucypher/log/nucypher.log /tmp/cli-demo
+cp /tmp/grant.json /tmp/cli-demo
+
+# Print the policy key to test correct format of grant output
+cat /tmp/grant.json | python3 -c "import sys, json; print(json.load(sys.stdin)['result']['policy_encrypting_key'])"
--- examples/cli_demo/bob-retrieve.sh
+++ examples/cli_demo/bob-retrieve.sh
@@ -4,26 +4,40 @@
 tar -xzf ~/nucypher/examples/cli_demo/bob.tar.gz -C ~
 mkdir --parents ~/bob/known_nodes/metadata
 mkdir ~/bob/known_nodes/certificates
-mkdir /tmp/cli-demo
+mkdir --parents /tmp/cli-demo
 
 # Setup runtime environment
 export NUCYPHER_KEYRING_PASSWORD=$TEST_BOB_KEYRING_PASSWORD
 ALICE_KEY='03b9d29b7679dda13138b27d3f532d79369662f02b9a30246a0c839de7ae0d9d5e'
-POLICY_KEY_1_1='0357ab01c0fec21fee5245a2388fbdefd0770a08ff4cfee6251588bea178549f0f'
+
 TEACHER='discover.nucypher.network:9151'
 RANDOM=$$
 MESSAGE=$RANDOM
 
+if [ -f /tmp/grant.json ]; then
+    PARSE_POLICY_KEY=$(cat /tmp/grant.json \
+        | python3 -c "import sys, json; print(json.load(sys.stdin)['result']['policy_encrypting_key'])")
+    POLICY_KEY="${PARSE_POLICY_KEY}"
+    LABEL=$1
+else
+    # Use a predefined label and policy encrypting key
+    LABEL=aceituno
+    POLICY_KEY='0357ab01c0fec21fee5245a2388fbdefd0770a08ff4cfee6251588bea178549f0f'
+fi
+
 # Enrico does what he does best
-nucypher enrico encrypt --policy-encrypting-key $POLICY_KEY_1_1 --message $MESSAGE --json-ipc | tee /tmp/enrico.json
+nucypher enrico encrypt --policy-encrypting-key $POLICY_KEY --message $MESSAGE --json-ipc | tee /tmp/enrico.json
+
+cp /tmp/enrico.json /tmp/cli-demo
+cp ~/.cache/nucypher/log/nucypher.log /tmp/cli-demo
 
 MESSAGE_KIT=$(cat /tmp/enrico.json | python3 -c "import sys, json; print(json.load(sys.stdin)['result']['message_kit'])")
 
 # Bob retrieves cleartext for message kit
 nucypher bob retrieve \
-    --label aceituno \
+    --label $LABEL \
     --message-kit $MESSAGE_KIT \
-    --policy-encrypting-key $POLICY_KEY_1_1 \
+    --policy-encrypting-key $POLICY_KEY \
     --alice-verifying-key $ALICE_KEY \
     --teacher $TEACHER \
     --provider $INFURA_GOERLI_ENDPOINT \
@@ -31,11 +45,9 @@ nucypher bob retrieve \
     --json-ipc \
     | tee /tmp/retrieve.json
 
+cp /tmp/retrieve.json /tmp/cli-demo
+cp ~/.cache/nucypher/log/nucypher.log /tmp/cli-demo
+
 # Test that the retrieved cleartext is correct
 cat /tmp/retrieve.json \
 | python3 -c "import sys, json, base64; assert b'$MESSAGE' == base64.b64decode(json.load(sys.stdin)['result']['cleartexts'][0])"
-
-# Save artifacts for later inspection
-cp .cache/nucypher/log/nucypher.log /tmp/cli-demo
-cp /tmp/retrieve.json /tmp/cli-demo
-cp /tmp/enrico.json /tmp/cli-demo
