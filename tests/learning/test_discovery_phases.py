367195d5e714a0a497accee166ba0d89514e2921
jMyles <justin@justinholmes.com>
2019-11-13 18:48:17 -0800

    Moving performance mocks to separate module.

--- tests/learning/test_discovery_phases.py
+++ tests/learning/test_discovery_phases.py
@@ -15,17 +15,16 @@ You should have received a copy of the GNU Affero General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from unittest.mock import patch
-
 import time
-from umbral.config import default_params
-from umbral.signing import Signature
 
 from nucypher.characters.lawful import Ursula
 from nucypher.config.characters import AliceConfiguration
 from nucypher.utilities.logging import GlobalLoggerSettings
 from nucypher.utilities.sandbox.middleware import MockRestMiddlewareForLargeFleetTests
 from nucypher.utilities.sandbox.ursula import make_federated_ursulas
+from tests.performance_mocks import mock_cert_storage, mock_cert_loading, mock_rest_app_creation, mock_cert_generation, \
+    mock_secret_source, mock_remember_node, mock_record_fleet_state, mock_verify_node, mock_message_verification, \
+    mock_metadata_validation, mock_signature_bytes, mock_stamp_call, mock_pubkey_from_bytes, VerificationTracker
 
 """
 Node Discovery happens in phases.  The first step is for a network actor to learn about the mere existence of a Node.
@@ -45,102 +44,11 @@ performance bottlenecks.
 
 
 def test_alice_can_learn_about_a_whole_bunch_of_ursulas(ursula_federated_test_config):
-    # First, we need to do some optimizing of this test in order
-    # to be able to create a whole bunch of Ursulas without it freezing.
-    # BEGIN CRAZY MONKEY PATCHING BLOCK
-    class NotAPublicKey:
-        _serial = 10000
-
-        @classmethod
-        def tick(cls):
-            cls._serial += 1
-
-        def __init__(self, serial=None):
-            if serial is None:
-                self.tick()
-                self.serial = str(self._serial).encode()
-            else:
-                self.serial = serial
-
-        def __bytes__(self):
-            return b"not a compressed public key:" + self.serial
-
-        @classmethod
-        def from_bytes(cls, some_bytes):
-            return cls(serial=some_bytes[-5:])
-
-        def to_bytes(self, *args, **kwargs):
-            return b"this is not a public key... but it is 64 bytes.. so, ya know" + self.serial
-
-    class NotAPrivateKey:
-        params = default_params()
-
-        fake_signature = Signature.from_bytes(
-            b'@\xbfS&\x97\xb3\x9e\x9e\xd3\\j\x9f\x0e\x8fY\x0c\xbeS\x08d\x0b%s\xf6\x17\xe2\xb6\xcd\x95u\xaapON\xd9E\xb3\x10M\xe1\xf4u\x0bL\x99q\xd6\r\x8e_\xe5I\x1e\xe5\xa2\xcf\xe5\x8be_\x077Gz'
-        )
-
-        def public_key(self):
-            return NotAPublicKey()
-
-        def get_pubkey(self, *args, **kwargs):
-            return self.public_key()
-
-        def to_cryptography_privkey(self, *args, **kwargs):
-            return self
-
-        def sign(self, *args, **kwargs):
-            return b'0D\x02 @\xbfS&\x97\xb3\x9e\x9e\xd3\\j\x9f\x0e\x8fY\x0c\xbeS\x08d\x0b%s\xf6\x17\xe2\xb6\xcd\x95u\xaap\x02 ON\xd9E\xb3\x10M\xe1\xf4u\x0bL\x99q\xd6\r\x8e_\xe5I\x1e\xe5\xa2\xcf\xe5\x8be_\x077Gz'
-
-        @classmethod
-        def stamp(cls, *args, **kwargs):
-            return cls.fake_signature
-
-        @classmethod
-        def signature_bytes(cls, *args, **kwargs):
-            return b'@\xbfS&\x97\xb3\x9e\x9e\xd3\\j\x9f\x0e\x8fY\x0c\xbeS\x08d\x0b%s\xf6\x17\xe2\xb6\xcd\x95u\xaapON\xd9E\xb3\x10M\xe1\xf4u\x0bL\x99q\xd6\r\x8e_\xe5I\x1e\xe5\xa2\xcf\xe5\x8be_\x077Gz'
-
-    class NotACert:
-        class Subject:
-            def get_attributes_for_oid(self, *args, **kwargs):
-                class Pseudonym:
-                    value = "0x51347fF6eb8F1D39B83B5e9c244Dc2E1E9EB14B4"
-
-                return Pseudonym(), "Or whatever?"
-
-        subject = Subject()
-
-        def public_bytes(self, does_not_matter):
-            return b"this is not a cert."
-
-        def public_key(self):
-            return NotAPublicKey()
-
-    def do_not_create_cert(*args, **kwargs):
-        return NotACert(), NotAPrivateKey()
-
-    def simple_remember(ursula, node, *args, **kwargs):
-        address = node.checksum_address
-        ursula.known_nodes[address] = node
-
-    class NotARestApp:
-        testing = True
-
-    mock_cert_storage = patch("nucypher.config.storages.ForgetfulNodeStorage.store_node_certificate",
-                              new=lambda *args, **kwargs: "do not store cert.")
-    mock_cert_loading = patch("nucypher.characters.lawful.load_pem_x509_certificate",
-                              new=lambda *args, **kwargs: NotACert())
-    mock_cert_generation = patch("nucypher.keystore.keypairs.generate_self_signed_certificate", new=do_not_create_cert)
-    mock_rest_app_creation = patch("nucypher.characters.lawful.make_rest_app",
-                                   new=lambda *args, **kwargs: (NotARestApp(), "this is not a datastore"))
-    mock_secret_source = patch("nucypher.keystore.keypairs.Keypair._private_key_source",
-                               new=lambda *args, **kwargs: NotAPrivateKey())
-    mock_remember_node = patch("nucypher.characters.lawful.Ursula.remember_node", new=simple_remember)
 
     with GlobalLoggerSettings.pause_all_logging_while():
         with mock_cert_storage, mock_cert_loading, mock_rest_app_creation, mock_cert_generation, mock_secret_source, mock_remember_node:
             _ursulas = make_federated_ursulas(ursula_config=ursula_federated_test_config,
                                               quantity=5000, know_each_other=False)
-            # END FIRST CRAZY MONKEY PATCHING BLOCK
             all_ursulas = {u.checksum_address: u for u in _ursulas}
             for ursula in _ursulas:
                 ursula.known_nodes._nodes = all_ursulas
@@ -153,42 +61,19 @@ def test_alice_can_learn_about_a_whole_bunch_of_ursulas(ursula_federated_test_co
                                 save_metadata=False,
                                 reload_metadata=False)
 
-    class VerificationTracker:
-        node_verifications = 0
-        metadata_verifications = 0
+    with mock_cert_storage, mock_verify_node, mock_record_fleet_state:
+        alice = config.produce(known_nodes=list(_ursulas)[:1])
 
-        @classmethod
-        def fake_verify_node(cls, *args, **kwargs):
-            cls.node_verifications += 1
-
-        @classmethod
-        def fake_verify_metadata(cls, *args, **kwargs):
-            cls.metadata_verifications += 1
-
-    with mock_cert_storage:
-        with patch("nucypher.characters.lawful.Ursula.verify_node", new=VerificationTracker.fake_verify_node):
-            with patch("nucypher.network.nodes.FleetStateTracker.record_fleet_state", new=lambda *args, **kwargs: None):
-                alice = config.produce(known_nodes=list(_ursulas)[:1],
-                                       )
     # We started with one known_node and verified it.
     # TODO: Consider changing this - #1449
     assert VerificationTracker.node_verifications == 1
 
-    with patch("nucypher.config.storages.ForgetfulNodeStorage.store_node_certificate",
-               new=lambda *args, **kwargs: "do not store cert."):
-        with patch("nucypher.characters.lawful.Ursula.verify_node", new=VerificationTracker.fake_verify_node):
-            with patch("nucypher.network.nodes.Teacher.validate_metadata",
-                       new=VerificationTracker.fake_verify_metadata):
-                with patch('nucypher.characters.lawful.Alice.verify_from', new=lambda *args, **kwargs: None):
-                    with patch('umbral.keys.UmbralPublicKey.from_bytes', NotAPublicKey.from_bytes):
-                        with patch('nucypher.characters.lawful.load_pem_x509_certificate',
-                                   new=lambda *args, **kwargs: NotACert()):
-                            with patch('nucypher.crypto.signing.SignatureStamp.__call__', new=NotAPrivateKey.stamp):
-                                with patch('umbral.signing.Signature.__bytes__', new=NotAPrivateKey.signature_bytes):
-                                    started = time.time()
-                                    alice.block_until_number_of_known_nodes_is(8, learn_on_this_thread=True, timeout=60)
-                                    ended = time.time()
-                                    elapsed = ended - started
+    with mock_cert_storage, mock_cert_loading, mock_verify_node, mock_message_verification, mock_metadata_validation:
+        with mock_pubkey_from_bytes, mock_stamp_call, mock_signature_bytes:
+            started = time.time()
+            alice.block_until_number_of_known_nodes_is(8, learn_on_this_thread=True, timeout=60)
+            ended = time.time()
+            elapsed = ended - started
 
     assert VerificationTracker.node_verifications == 1  # We have only verified the first Ursula.
     assert sum(isinstance(u, Ursula) for u in alice.known_nodes) < 20  # We haven't instantiated many Ursulas.
---
+++ tests/performance_mocks.py
@@ -0,0 +1,128 @@
+from unittest.mock import patch
+
+from umbral.config import default_params
+from umbral.signing import Signature
+
+mock_cert_storage = patch("nucypher.config.storages.ForgetfulNodeStorage.store_node_certificate",
+                          new=lambda *args, **kwargs: "do not store cert.")
+mock_message_verification = patch('nucypher.characters.lawful.Alice.verify_from', new=lambda *args, **kwargs: None)
+
+mock_record_fleet_state = patch("nucypher.network.nodes.FleetStateTracker.record_fleet_state",
+                                new=lambda *args, **kwargs: None)
+
+
+class NotAPublicKey:
+    _serial = 10000
+
+    @classmethod
+    def tick(cls):
+        cls._serial += 1
+
+    def __init__(self, serial=None):
+        if serial is None:
+            self.tick()
+            self.serial = str(self._serial).encode()
+        else:
+            self.serial = serial
+
+    def __bytes__(self):
+        return b"not a compressed public key:" + self.serial
+
+    @classmethod
+    def from_bytes(cls, some_bytes):
+        return cls(serial=some_bytes[-5:])
+
+    def to_bytes(self, *args, **kwargs):
+        return b"this is not a public key... but it is 64 bytes.. so, ya know" + self.serial
+
+
+class NotAPrivateKey:
+    params = default_params()
+
+    fake_signature = Signature.from_bytes(
+        b'@\xbfS&\x97\xb3\x9e\x9e\xd3\\j\x9f\x0e\x8fY\x0c\xbeS\x08d\x0b%s\xf6\x17\xe2\xb6\xcd\x95u\xaapON\xd9E\xb3\x10M\xe1\xf4u\x0bL\x99q\xd6\r\x8e_\xe5I\x1e\xe5\xa2\xcf\xe5\x8be_\x077Gz'
+    )
+
+    def public_key(self):
+        return NotAPublicKey()
+
+    def get_pubkey(self, *args, **kwargs):
+        return self.public_key()
+
+    def to_cryptography_privkey(self, *args, **kwargs):
+        return self
+
+    def sign(self, *args, **kwargs):
+        return b'0D\x02 @\xbfS&\x97\xb3\x9e\x9e\xd3\\j\x9f\x0e\x8fY\x0c\xbeS\x08d\x0b%s\xf6\x17\xe2\xb6\xcd\x95u\xaap\x02 ON\xd9E\xb3\x10M\xe1\xf4u\x0bL\x99q\xd6\r\x8e_\xe5I\x1e\xe5\xa2\xcf\xe5\x8be_\x077Gz'
+
+    @classmethod
+    def stamp(cls, *args, **kwargs):
+        return cls.fake_signature
+
+    @classmethod
+    def signature_bytes(cls, *args, **kwargs):
+        return b'@\xbfS&\x97\xb3\x9e\x9e\xd3\\j\x9f\x0e\x8fY\x0c\xbeS\x08d\x0b%s\xf6\x17\xe2\xb6\xcd\x95u\xaapON\xd9E\xb3\x10M\xe1\xf4u\x0bL\x99q\xd6\r\x8e_\xe5I\x1e\xe5\xa2\xcf\xe5\x8be_\x077Gz'
+
+
+class NotACert:
+    class Subject:
+        def get_attributes_for_oid(self, *args, **kwargs):
+            class Pseudonym:
+                value = "0x51347fF6eb8F1D39B83B5e9c244Dc2E1E9EB14B4"
+
+            return Pseudonym(), "Or whatever?"
+
+    subject = Subject()
+
+    def public_bytes(self, does_not_matter):
+        return b"this is not a cert."
+
+    def public_key(self):
+        return NotAPublicKey()
+
+
+mock_cert_loading = patch("nucypher.characters.lawful.load_pem_x509_certificate",
+                          new=lambda *args, **kwargs: NotACert())
+
+
+def do_not_create_cert(*args, **kwargs):
+    return NotACert(), NotAPrivateKey()
+
+
+def simple_remember(ursula, node, *args, **kwargs):
+    address = node.checksum_address
+    ursula.known_nodes[address] = node
+
+
+class NotARestApp:
+    testing = True
+
+
+class VerificationTracker:
+    node_verifications = 0
+    metadata_verifications = 0
+
+    @classmethod
+    def fake_verify_node(cls, *args, **kwargs):
+        cls.node_verifications += 1
+
+    @classmethod
+    def fake_verify_metadata(cls, *args, **kwargs):
+        cls.metadata_verifications += 1
+
+
+mock_cert_generation = patch("nucypher.keystore.keypairs.generate_self_signed_certificate", new=do_not_create_cert)
+mock_rest_app_creation = patch("nucypher.characters.lawful.make_rest_app",
+                               new=lambda *args, **kwargs: (NotARestApp(), "this is not a datastore"))
+mock_secret_source = patch("nucypher.keystore.keypairs.Keypair._private_key_source",
+                           new=lambda *args, **kwargs: NotAPrivateKey())
+
+mock_remember_node = patch("nucypher.characters.lawful.Ursula.remember_node", new=simple_remember)
+mock_verify_node = patch("nucypher.characters.lawful.Ursula.verify_node", new=VerificationTracker.fake_verify_node)
+
+mock_metadata_validation = patch("nucypher.network.nodes.Teacher.validate_metadata",
+                                 new=VerificationTracker.fake_verify_metadata)
+
+mock_pubkey_from_bytes = patch('umbral.keys.UmbralPublicKey.from_bytes', NotAPublicKey.from_bytes)
+mock_stamp_call = patch('nucypher.crypto.signing.SignatureStamp.__call__', new=NotAPrivateKey.stamp)
+mock_signature_bytes = patch('umbral.signing.Signature.__bytes__', new=NotAPrivateKey.signature_bytes)
