38ef6ecb8cc3d613ad5b50ed0deb1e1458964c90
jMyles <justin@justinholmes.com>
2019-11-19 20:52:42 -0800

    For the moment, let's imagine we're not doing this specialized verification.

--- nucypher/policy/policies.py
+++ nucypher/policy/policies.py
@@ -324,15 +324,15 @@ class Policy(ABC):
                 return self.publish(network_middleware=network_middleware)
 
     def consider_arrangement(self, network_middleware, ursula, arrangement) -> bool:
-        try:
-            ursula.verify_node(network_middleware, registry=self.alice.registry)  # From the perspective of alice.
-        except ursula.InvalidNode:
-            # TODO: What do we actually do here?  Report this at least (355)?
-            # Maybe also have another bucket for invalid nodes?
-            # It's possible that nothing sordid is happening here;
-            # this node may be updating its interface info or rotating a signing key
-            # and we learned about a previous one.
-            raise
+        # try:
+        #     ursula.verify_node(network_middleware, registry=self.alice.registry)  # From the perspective of alice.
+        # except ursula.InvalidNode:
+        #     # TODO: What do we actually do here?  Report this at least (355)?
+        #     # Maybe also have another bucket for invalid nodes?
+        #     # It's possible that nothing sordid is happening here;
+        #     # this node may be updating its interface info or rotating a signing key
+        #     # and we learned about a previous one.
+        #     raise
 
         negotiation_response = network_middleware.consider_arrangement(arrangement=arrangement)
 
