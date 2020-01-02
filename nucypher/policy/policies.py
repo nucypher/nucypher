2d2b279555bbc838cf84040eae2efaf75d1d54cb
jMyles <justin@justinholmes.com>
2020-01-02 04:01:40 -0800

    This block is no longer needed.

--- nucypher/policy/policies.py
+++ nucypher/policy/policies.py
@@ -324,16 +324,6 @@ class Policy(ABC):
                 return self.publish(network_middleware=network_middleware)
 
     def consider_arrangement(self, network_middleware, ursula, arrangement) -> bool:
-        # try:
-        #     ursula.verify_node(network_middleware, registry=self.alice.registry)  # From the perspective of alice.
-        # except ursula.InvalidNode:
-        #     # TODO: What do we actually do here?  Report this at least (355)?
-        #     # Maybe also have another bucket for invalid nodes?
-        #     # It's possible that nothing sordid is happening here;
-        #     # this node may be updating its interface info or rotating a signing key
-        #     # and we learned about a previous one.
-        #     raise
-
         negotiation_response = network_middleware.consider_arrangement(arrangement=arrangement)
 
         # TODO: check out the response: need to assess the result and see if we're actually good to go.
