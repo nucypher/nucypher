e5688bdb42a394a2d5c10b7a112c57ec1e168c00
damon <gdamon@gmail.com>
2020-01-05 09:16:11 -0800

    update specification validation test with new exception

--- tests/characters/test_specifications.py
+++ tests/characters/test_specifications.py
@@ -1,6 +1,6 @@
 import pytest
-from marshmallow.exceptions import ValidationError
 from nucypher.characters.control.specifications import AliceSpecification
+from nucypher.characters.control.specifications.exceptions import SpecificationError
 
 
 def test_alice_spec_validate(federated_alice, federated_bob):
@@ -8,6 +8,6 @@ def test_alice_spec_validate(federated_alice, federated_bob):
     createPolicy = AliceSpecification.get_serializer('create_policy')
     specs = AliceSpecification.get_specifications('create_policy')
 
-    with pytest.raises(ValidationError) as e:
+    with pytest.raises(SpecificationError) as e:
         result = createPolicy.load({})
 
