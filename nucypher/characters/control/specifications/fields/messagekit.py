a439ad83f5119cea7d9cf1a4f90337be94cb25a9
damon <gdamon@gmail.com>
2020-01-07 14:15:45 -0800

    fix messagekit serializer

--- nucypher/characters/control/specifications/fields/messagekit.py
+++ nucypher/characters/control/specifications/fields/messagekit.py
@@ -1,15 +1,24 @@
-from marshmallow import fields
+from typing import Union
 from base64 import b64decode, b64encode
+from marshmallow import fields
+from nucypher.crypto.kits import UmbralMessageKit
 
 
 class MessageKitField(fields.Field):
 
-    def _serialize(self, value, attr, obj, **kwargs):
-        return b64encode(bytes(value)).decode()
+    def _serialize(self, value: UmbralMessageKit, attr, obj, **kwargs):
+        return b64encode(value.to_bytes()).decode()
 
     def _deserialize(self, value, attr, data, **kwargs):
         if isinstance(value, bytes):
             return value
         return b64decode(value)
+
+    def _validate(self, value):
+        try:
+            umbral_key = UmbralMessageKit.from_bytes(value)
+            return True
+        except Exception as e:
+            return False
 
 fields.MessageKit = MessageKitField
