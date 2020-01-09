498587dc9cceeae6b96b4092cab6cd4d715122d4
jMyles <justin@justinholmes.com>
2020-01-09 11:20:35 -0800

    Doing JIT verification now.

--- nucypher/characters/lawful.py
+++ nucypher/characters/lawful.py
@@ -1213,15 +1213,6 @@ class Ursula(Teacher, Character, Worker):
                 raise Learner.NotATeacher(
                     f"{checksum_address} is staking less than the specified minimum stake value ({minimum_stake}).")
 
-        # Verify the node's TLS certificate
-        try:
-            potential_seed_node.verify_node(network_middleware=network_middleware,
-                                            registry=registry,
-                                            certificate_filepath=temp_certificate_filepath)
-        except potential_seed_node.InvalidNode:
-            # TODO: What if our seed node fails verification?
-            raise
-
         # OK - everyone get out
         temp_node_storage.forget()
         return potential_seed_node
