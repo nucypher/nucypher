szotov/nucypher-kms

Tests for wind-down parameter, wind-down parameter in PreallocationEscrow

tests/blockchain/eth/contracts/integration/test_intercontract_integration.py
@@ -33,6 +33,7 @@ from nucypher.crypto.signing import SignatureStamp
 
 
 DISABLE_RE_STAKE_FIELD = 3
+WIND_DOWN_FIELD = 10
 
 DISABLED_FIELD = 5
 
@@ -422,6 +423,9 @@ def test_all(testerchain,
     assert 1000 == escrow.functions.getLockedTokens(preallocation_escrow_2.address, 6).call()
     assert 0 == escrow.functions.getLockedTokens(preallocation_escrow_2.address, 7).call()
     assert 0 == escrow.functions.getCompletedWork(preallocation_escrow_2.address).call()
+    tx = preallocation_escrow_interface_2.functions.setWindDown(True).transact({'from': ursula2})
+    testerchain.wait_for_receipt(tx)
+    assert escrow.functions.stakerInfo(preallocation_escrow_interface_2.address).call()[WIND_DOWN_FIELD]
 
     # Another Ursula claims tokens
     tx = worklock.functions.claim().transact({'from': ursula4, 'gas_price': 0})
@@ -530,6 +534,9 @@ def test_all(testerchain,
     testerchain.wait_for_receipt(tx)
     tx = escrow.functions.setReStake(False).transact({'from': ursula1})
     testerchain.wait_for_receipt(tx)
+    tx = escrow.functions.setWindDown(True).transact({'from': ursula1})
+    testerchain.wait_for_receipt(tx)
+    assert escrow.functions.stakerInfo(ursula1).call()[WIND_DOWN_FIELD]
     tx = escrow.functions.confirmActivity().transact({'from': ursula1})
     testerchain.wait_for_receipt(tx)
     escrow_balance += 1000
@@ -546,6 +553,9 @@ def test_all(testerchain,
     testerchain.wait_for_receipt(tx)
     tx = preallocation_escrow_interface_1.functions.setWorker(ursula3).transact({'from': ursula3})
     testerchain.wait_for_receipt(tx)
+    tx = preallocation_escrow_interface_1.functions.setWindDown(True).transact({'from': ursula3})
+    testerchain.wait_for_receipt(tx)
+    assert escrow.functions.stakerInfo(preallocation_escrow_interface_1.address).call()[WIND_DOWN_FIELD]
     tx = escrow.functions.confirmActivity().transact({'from': ursula3})
     testerchain.wait_for_receipt(tx)
     escrow_balance += 1000

nucypher/nucypher

tests/blockchain/eth/contracts/integration/test_intercontract_integration.py
@@ -33,6 +33,7 @@ from nucypher.crypto.signing import SignatureStamp
 
 
 DISABLE_RE_STAKE_FIELD = 3
+WIND_DOWN_FIELD = 10
 
 DISABLED_FIELD = 5
 
@@ -400,6 +401,9 @@ def test_all(testerchain,
     escrow_balance = token_economics.erc20_reward_supply + staker2_tokens
     assert escrow.functions.getAllTokens(staker2).call() == staker2_tokens
     assert escrow.functions.getCompletedWork(staker2).call() == 0
+    tx = escrow.functions.setWindDown(True).transact({'from': staker2})
+    testerchain.wait_for_receipt(tx)
+    assert escrow.functions.stakerInfo(staker2).call()[WIND_DOWN_FIELD]
 
     # Burn remaining tokens in WorkLock
     tx = worklock.functions.burnUnclaimed().transact({'from': creator})
@@ -517,6 +521,9 @@ def test_all(testerchain,
     testerchain.wait_for_receipt(tx)
     tx = escrow.functions.setReStake(False).transact({'from': staker1})
     testerchain.wait_for_receipt(tx)
+    tx = escrow.functions.setWindDown(True).transact({'from': staker1})
+    testerchain.wait_for_receipt(tx)
+    assert escrow.functions.stakerInfo(staker1).call()[WIND_DOWN_FIELD]
     tx = escrow.functions.confirmActivity().transact({'from': staker1})
     testerchain.wait_for_receipt(tx)
     escrow_balance += 1000
@@ -533,6 +540,9 @@ def test_all(testerchain,
     testerchain.wait_for_receipt(tx)
     tx = preallocation_escrow_interface_1.functions.setWorker(staker3).transact({'from': staker3})
     testerchain.wait_for_receipt(tx)
+    tx = preallocation_escrow_interface_1.functions.setWindDown(True).transact({'from': staker3})
+    testerchain.wait_for_receipt(tx)
+    assert escrow.functions.stakerInfo(preallocation_escrow_interface_1.address).call()[WIND_DOWN_FIELD]
     tx = escrow.functions.confirmActivity().transact({'from': staker3})
     testerchain.wait_for_receipt(tx)
     escrow_balance += 1000