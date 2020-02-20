d0bbec12a9afae2ca71471b5088a66cd5a6be194
David Núñez <david@nucypher.com>
2020-02-20 16:48:39 +0100

    Allow to create transacting WorkLock bidders with HW wallets

--- nucypher/blockchain/eth/actors.py
+++ nucypher/blockchain/eth/actors.py
@@ -1520,13 +1520,17 @@ class Bidder(NucypherTokenActor):
         pass
 
     @validate_checksum_address
-    def __init__(self, checksum_address: str, client_password: str = None, *args, **kwargs):
+    def __init__(self,
+                 checksum_address: str,
+                 is_transacting: bool = True,
+                 client_password: str = None,
+                 *args, **kwargs):
         super().__init__(checksum_address=checksum_address, *args, **kwargs)
         self.worklock_agent = ContractAgency.get_agent(WorkLockAgent, registry=self.registry)
         self.staking_agent = ContractAgency.get_agent(StakingEscrowAgent, registry=self.registry)
         self.economics = EconomicsFactory.get_economics(registry=self.registry)
 
-        if client_password:
+        if is_transacting:
             self.transacting_power = TransactingPower(password=client_password, account=checksum_address)
             self.transacting_power.activate()
 
