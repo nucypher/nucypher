8d57aa9525e0c84a18d6e01813dde9c7c2629087
jMyles <justin@justinholmes.com>
2020-03-04 16:04:50 -0800

    Vlad's APIs are consistent with the real deal again.

--- tests/acceptance/characters/test_ursula_prepares_to_act_as_worker.py
+++ tests/acceptance/characters/test_ursula_prepares_to_act_as_worker.py
@@ -64,7 +64,6 @@ def test_blockchain_ursula_verifies_stamp(blockchain_ursulas):
     assert first_ursula.verified_stamp
 
 
-@pytest.mark.skip("See Issue #1075")  # TODO: Issue #1075
 def test_vladimir_cannot_verify_interface_with_ursulas_signing_key(blockchain_ursulas):
     his_target = list(blockchain_ursulas)[4]
 
@@ -78,7 +77,7 @@ def test_vladimir_cannot_verify_interface_with_ursulas_signing_key(blockchain_ur
     vladimir = Vladimir.from_target_ursula(his_target, claim_signing_key=True)
 
     # Vladimir can substantiate the stamp using his own ether address...
-    vladimir.substantiate_stamp(client_password=INSECURE_DEVELOPMENT_PASSWORD)
+    vladimir.substantiate_stamp()
     vladimir.validate_worker = lambda: True
     vladimir.validate_worker()  # lol
 
@@ -95,7 +94,6 @@ def test_vladimir_cannot_verify_interface_with_ursulas_signing_key(blockchain_ur
         vladimir.validate_metadata()
 
 
-@pytest.mark.skip("See Issue #1075")  # TODO: Issue #1075
 def test_vladimir_invalidity_without_stake(testerchain, blockchain_ursulas, blockchain_alice):
     his_target = list(blockchain_ursulas)[4]
     vladimir = Vladimir.from_target_ursula(target_ursula=his_target)
@@ -103,14 +101,13 @@ def test_vladimir_invalidity_without_stake(testerchain, blockchain_ursulas, bloc
     message = vladimir._signable_interface_info_message()
     signature = vladimir._crypto_power.power_ups(SigningPower).sign(vladimir.timestamp_bytes() + message)
     vladimir._Teacher__interface_signature = signature
-    vladimir.substantiate_stamp(client_password=INSECURE_DEVELOPMENT_PASSWORD)
+    vladimir.substantiate_stamp()
 
     # However, the actual handshake proves him wrong.
     with pytest.raises(vladimir.InvalidNode):
-        vladimir.verify_node(blockchain_alice.network_middleware, certificate_filepath="doesn't matter")
+        vladimir.verify_node(blockchain_alice.network_middleware.client, certificate_filepath="doesn't matter")
 
 
-@pytest.mark.skip("See Issue #1075")  # TODO: Issue #1075
 def test_vladimir_uses_his_own_signing_key(blockchain_alice, blockchain_ursulas):
     """
     Similar to the attack above, but this time Vladimir makes his own interface signature
@@ -122,7 +119,7 @@ def test_vladimir_uses_his_own_signing_key(blockchain_alice, blockchain_ursulas)
     message = vladimir._signable_interface_info_message()
     signature = vladimir._crypto_power.power_ups(SigningPower).sign(vladimir.timestamp_bytes() + message)
     vladimir._Teacher__interface_signature = signature
-    vladimir.substantiate_stamp(client_password=INSECURE_DEVELOPMENT_PASSWORD)
+    vladimir.substantiate_stamp()
 
     vladimir._worker_is_bonded_to_staker = lambda: True
     vladimir._staker_is_really_staking = lambda: True
@@ -133,7 +130,7 @@ def test_vladimir_uses_his_own_signing_key(blockchain_alice, blockchain_ursulas)
 
     # However, the actual handshake proves him wrong.
     with pytest.raises(vladimir.InvalidNode):
-        vladimir.verify_node(blockchain_alice.network_middleware, certificate_filepath="doesn't matter")
+        vladimir.verify_node(blockchain_alice.network_middleware.client, certificate_filepath="doesn't matter")
 
 
 # TODO: Change name of this file, extract this test
