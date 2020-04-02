86b26c4140ede234349583f5dbdd882c516b942b
Kieran Prasch <kieran@nucypher.com>
2020-04-01 20:47:46 -0700

    Bump version: 2.1.0-beta.0 → 2.1.0-beta.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ author = 'NuCypher'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.1.0-beta.0'
+release = '2.1.0-beta.1'
 
 
 # -- General configuration ---------------------------------------------------
