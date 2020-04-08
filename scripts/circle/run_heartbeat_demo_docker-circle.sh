93353635ce44f58ebfc71bb53df2347654a9a311
K Prasch <kieranprasch@gmail.com>
2020-04-08 08:11:36 -0700

    Update scripts/circle/run_heartbeat_demo_docker-circle.sh
    Co-Authored-By: Derek Pierre <derek.pierre@gmail.com>

--- scripts/circle/run_heartbeat_demo_docker-circle.sh
+++ scripts/circle/run_heartbeat_demo_docker-circle.sh
@@ -9,7 +9,7 @@ echo "Starting Up Heartbeat Demo Test..."
 # run some ursulas
 docker-compose up -d
 
-echo "wait a few seconds for initting and learning"
+echo "running heartbeat demo"
 
 # run alicia and bob all in one running of docker since we lack persistent disks in circle
 docker-compose run nucypher-circle-dev bash /code/scripts/circle/run_heartbeat_alicia_and_bob.sh
