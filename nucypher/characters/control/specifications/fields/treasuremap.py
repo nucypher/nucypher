f92cdc00175cf32dc96141851e673b9a5cb7e22e
damon <gdamon@gmail.com>
2020-04-13 18:16:10 -0700

    remove janky TreasureMap Validation

--- nucypher/characters/control/specifications/fields/treasuremap.py
+++ nucypher/characters/control/specifications/fields/treasuremap.py
@@ -1,11 +1,7 @@
 from marshmallow import fields
 from base64 import b64decode, b64encode
-from bytestring_splitter import BytestringSplitter, VariableLengthBytestring
 from nucypher.characters.control.specifications.fields.base import BaseField
 from nucypher.characters.control.specifications.exceptions import InvalidInputData, InvalidNativeDataTypes
-from nucypher.crypto.signing import Signature
-from nucypher.crypto.constants import KECCAK_DIGEST_LENGTH
-from nucypher.crypto.kits import UmbralMessageKit
 
 
 class TreasureMap(BaseField, fields.Field):
@@ -20,13 +16,9 @@ class TreasureMap(BaseField, fields.Field):
             raise InvalidInputData(f"Could not parse {self.name}: {e}")
 
     def _validate(self, value):
-
-        splitter = BytestringSplitter(Signature,
-                                  (bytes, KECCAK_DIGEST_LENGTH),  # hrac
-                                  (UmbralMessageKit, VariableLengthBytestring)
-                                  )  # TODO: USe the one from TMap
+        from nucypher.policy.collections import TreasureMap
         try:
-            signature, hrac, tmap_message_kit = splitter(value)
+            TreasureMap.from_bytes(value)
             return True
         except InvalidNativeDataTypes as e:
             raise InvalidInputData(f"Could not parse {self.name}: {e}")
