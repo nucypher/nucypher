0ced8681ddf829b9c1769edea065caa945343fef
David Núñez <david@nucypher.com>
2020-04-18 01:55:49 +0200

    Adapt Checkpointing library to solc 0.6.5

--- nucypher/blockchain/eth/sol/source/aragon/staking/Checkpointing.sol
+++ nucypher/blockchain/eth/sol/source/aragon/staking/Checkpointing.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.6.5;
 
 
 library Checkpointing {
@@ -14,62 +14,58 @@ library Checkpointing {
         uint192 value;
     }
 
-    struct History {
-        Checkpoint[] history;
-    }
-
-    function add(History storage self, uint256 time, uint256 value) internal {
+    function add(Checkpoint[] storage self, uint256 time, uint256 value) internal {
         require(time <= MAX_UINT64, ERROR_TIME_TOO_BIG);
         require(value <= MAX_UINT192, ERROR_VALUE_TOO_BIG);
 
         add192(self, uint64(time), uint192(value));
     }
 
-    function add64(History storage self, uint64 time, uint256 value) internal {
+    function add64(Checkpoint[] storage self, uint64 time, uint256 value) internal {
         require(value <= MAX_UINT192, ERROR_VALUE_TOO_BIG);
 
         add192(self, time, uint192(value));
     }
 
-    function get(History storage self, uint256 time) internal view returns (uint256) {
+    function get(Checkpoint[] storage self, uint256 time) internal view returns (uint256) {
         require(time <= MAX_UINT64, ERROR_TIME_TOO_BIG);
 
         return uint256(get192(self, uint64(time)));
     }
 
-    function get64(History storage self, uint64 time) internal view returns (uint256) {
+    function get64(Checkpoint[] storage self, uint64 time) internal view returns (uint256) {
         return uint256(get192(self, time));
     }
 
-    function lastUpdated(History storage self) internal view returns (uint256) {
-        uint256 length = self.history.length;
+    function lastUpdated(Checkpoint[] storage self) internal view returns (uint256) {
+        uint256 length = self.length;
 
         if (length > 0) {
-            return uint256(self.history[length - 1].time);
+            return uint256(self[length - 1].time);
         }
 
         return 0;
     }
 
-    function getLatestValue(History storage self) internal view returns (uint256) {
-        uint256 length = self.history.length;
+    function getLatestValue(Checkpoint[] storage self) internal view returns (uint256) {
+        uint256 length = self.length;
         if (length > 0) {
-            return uint256(self.history[length - 1].value);
+            return uint256(self[length - 1].value);
         }
 
         return 0;
     }
 
-    function add192(History storage self, uint64 time, uint192 value) internal {
-        uint256 length = self.history.length;
+    function add192(Checkpoint[] storage self, uint64 time, uint192 value) internal {
+        uint256 length = self.length;
 
         if (length == 0) {
-            self.history.push(Checkpoint(time, value));
+            self.push(Checkpoint(time, value));
         } else {
-            Checkpoint storage currentCheckpoint = self.history[length - 1];
+            Checkpoint storage currentCheckpoint = self[length - 1];
             uint64 currentCheckpointTime = currentCheckpoint.time;
             if (time > currentCheckpointTime) {
-                self.history.push(Checkpoint(time, value));
+                self.push(Checkpoint(time, value));
             } else if (time == currentCheckpointTime) {
                 currentCheckpoint.value = value;
             } else { // ensure list ordering
@@ -78,8 +74,8 @@ library Checkpointing {
         }
     }
 
-    function get192(History storage self, uint64 time) internal view returns (uint192) {
-        uint256 length = self.history.length;
+    function get192(Checkpoint[] storage self, uint64 time) internal view returns (uint192) {
+        uint256 length = self.length;
 
         if (length == 0) {
             return 0;
@@ -88,12 +84,12 @@ library Checkpointing {
         uint256 lastIndex = length - 1;
 
         // short-circuit
-        Checkpoint storage lastCheckpoint = self.history[lastIndex];
+        Checkpoint storage lastCheckpoint = self[lastIndex];
         if (time >= lastCheckpoint.time) {
             return lastCheckpoint.value;
         }
 
-        if (time < self.history[0].time) {
+        if (time < self[0].time) {
             return 0;
         }
 
@@ -102,7 +98,7 @@ library Checkpointing {
 
         while (high > low) {
             uint256 mid = (high + low + 1) / 2; // average, ceil round
-            Checkpoint storage checkpoint = self.history[mid];
+            Checkpoint storage checkpoint = self[mid];
             uint64 midTime = checkpoint.time;
 
             if (time > midTime) {
@@ -114,6 +110,6 @@ library Checkpointing {
             }
         }
 
-        return self.history[low].value;
+        return self[low].value;
     }
 }
