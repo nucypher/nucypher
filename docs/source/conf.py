69fd86f965122b9608fd4cc1971fd2a4d0478adb
Kieran Prasch <kieran@nucypher.com>
2020-05-04 08:55:04 -0700

    remove pre-commit hook for pushing to master to allow for bumpversion to do its thing.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'NuCypher'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.1.0-beta.3'
+release = '2.1.0-beta.4'
 
 
 # -- General configuration ---------------------------------------------------
