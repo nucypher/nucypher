83bfefc9a191c7271a0ea8d813565430ff1f3823
Kieran R. Prasch <kieranprasch@gmail.com>
2020-05-08 14:38:00 -0700

    Use pathlib in test mixed configuration cli test

--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -17,15 +17,13 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import contextlib
-from pathlib import Path
 
-from datetime import datetime
 import json
 import os
-import shutil
-
 import pytest
+import shutil
 from click.testing import CliRunner
+from datetime import datetime
 
 from nucypher.blockchain.eth.registry import InMemoryContractRegistry, LocalContractRegistry
 from nucypher.config.characters import UrsulaConfiguration, StakeHolderConfiguration
@@ -85,7 +83,7 @@ def new_local_registry():
 
 @pytest.fixture(scope='module')
 def custom_filepath():
-    _custom_filepath = Path(MOCK_CUSTOM_INSTALLATION_PATH)
+    _custom_filepath = MOCK_CUSTOM_INSTALLATION_PATH
     with contextlib.suppress(FileNotFoundError):
         shutil.rmtree(_custom_filepath, ignore_errors=True)
     yield _custom_filepath
@@ -95,14 +93,14 @@ def custom_filepath():
 
 @pytest.fixture(scope='module')
 def custom_filepath_2():
-    _custom_filepath = Path(MOCK_CUSTOM_INSTALLATION_PATH_2)
+    _custom_filepath = MOCK_CUSTOM_INSTALLATION_PATH_2
     with contextlib.suppress(FileNotFoundError):
         shutil.rmtree(_custom_filepath, ignore_errors=True)
     try:
         yield _custom_filepath
     finally:
         with contextlib.suppress(FileNotFoundError):
-            shutil.rmtree(_custom_filepath / '*', ignore_errors=True)
+            shutil.rmtree(_custom_filepath, ignore_errors=True)
 
 
 @pytest.fixture(scope='module')
