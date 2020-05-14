af86ff6dbe25ead0e47ba1d3d7811d0e2a15af87
Kieran Prasch <kieran@nucypher.com>
2020-05-13 17:22:51 -0700

    Mock click.confirm fixture; Basis for confirm enable restaking test

--- tests/cli/functional/actions/test_confirm_actions.py
+++ tests/cli/functional/actions/test_confirm_actions.py
@@ -4,13 +4,18 @@ import click
 import pytest
 
 from nucypher.characters.control.emitters import StdoutEmitter
-from nucypher.cli.actions.confirm import confirm_deployment
+from nucypher.cli.actions.confirm import confirm_deployment, confirm_enable_restaking_lock
 from nucypher.cli.literature import ABORT_DEPLOYMENT
 
 
 @pytest.fixture()
 def mock_click_prompt(mocker):
     return mocker.patch.object(click, 'prompt')
+
+
+@pytest.fixture()
+def mock_click_confirm(mocker):
+    return mocker.patch.object(click, 'confirm')
 
 
 @pytest.fixture()
@@ -41,3 +46,10 @@ def test_confirm_deployment(mock_click_prompt, test_emitter, stdout_trap, mock_t
 
     output = stdout_trap.getvalue()
     assert not output
+
+
+def test_confirm_enable_restaking_lock(mock_click_confirm, test_emitter, stdout_trap):
+    mock_click_confirm.return_value = True
+    result = confirm_enable_restaking_lock(emitter=test_emitter, release_period=1, staking_address='0xdeadbeef')
+
+    assert result
