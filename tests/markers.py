1193573fb9b991dd8563b27280a2bc4e0aad3ce1
Kieran Prasch <kieran@nucypher.com>
2020-05-31 20:25:13 -0700

    Fixes #1909 - Use a decorator to skip tests on CI.

--- tests/markers.py
+++ tests/markers.py
@@ -16,8 +16,8 @@
 """
 
 import os
-import pytest
 
+import pytest
 
 circleci_only = pytest.mark.skipif(condition=('CIRCLECI' not in os.environ), reason='Only run on CircleCI')
 skip_on_circleci = pytest.mark.skipif(condition=('CIRCLECI' in os.environ), reason='Do not run on CircleCI')
