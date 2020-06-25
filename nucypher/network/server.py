52564b446fd33a266f8fe7c10fe388eea888f7e7
jMyles <justin@justinholmes.com>
2020-06-24 19:23:42 -0700

    No need to verify announced nodes anymore, since we do this lazily now.  Fixes #555.

--- nucypher/network/server.py
+++ nucypher/network/server.py
@@ -186,50 +186,10 @@ def make_rest_app(
         sprouts = _node_class.batch_from_bytes(request.data,
                                              registry=this_node.registry)
 
-        # TODO: This logic is basically repeated in learn_from_teacher_node and remember_node.
-        # Let's find a better way.  #555
         for node in sprouts:
-            @crosstown_traffic()
-            def learn_about_announced_nodes():
-                if node in this_node.known_nodes:
-                    if node.timestamp <= this_node.known_nodes[node.checksum_address].timestamp:
-                        return
+            this_node.remember_node(node)
 
-                node.mature()
-
-                try:
-                    node.verify_node(this_node.network_middleware.client,
-                                     registry=this_node.registry)
-
-                # Suspicion
-                except node.SuspiciousActivity as e:
-                    # 355
-                    # TODO: Include data about caller?
-                    # TODO: Account for possibility that stamp, rather than interface, was bad.
-                    # TODO: Maybe also record the bytes representation separately to disk?
-                    message = f"Suspicious Activity about {node}: {str(e)}.  Announced via REST."
-                    log.warn(message)
-                    this_node.suspicious_activities_witnessed['vladimirs'].append(node)
-                except NodeSeemsToBeDown as e:
-                    # This is a rather odd situation - this node *just* contacted us and asked to be verified.  Where'd it go?  Maybe a NAT problem?
-                    log.info(f"Node announced itself to us just now, but seems to be down: {node}.  Response was {e}.")
-                    log.debug(f"Phantom node certificate: {node.certificate}")
-                # Async Sentinel
-                except Exception as e:
-                    log.critical(f"This exception really needs to be handled differently: {e}")
-                    raise
-
-                # Believable
-                else:
-                    log.info("Learned about previously unknown node: {}".format(node))
-                    this_node.remember_node(node)
-                    # TODO: Record new fleet state
-
-                # Cleanup
-                finally:
-                    forgetful_node_storage.forget()
-
-        # TODO: What's the right status code here?  202?  Different if we already knew about the node?
+        # TODO: What's the right status code here?  202?  Different if we already knew about the node(s)?
         return all_known_nodes()
 
     @rest_app.route('/consider_arrangement', methods=['POST'])
