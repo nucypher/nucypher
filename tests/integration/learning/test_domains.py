7d66740c7e4e067ccc7960db85265e9d74df4d3e
jMyles <justin@justinholmes.com>
2020-06-25 19:29:03 -0700

    Using lonely_ursula_maker in all the right places.

--- tests/integration/learning/test_domains.py
+++ tests/integration/learning/test_domains.py
@@ -20,15 +20,12 @@ from functools import partial
 from tests.utils.ursula import make_federated_ursulas
 
 
-def test_learner_learns_about_domains_separately(ursula_federated_test_config, caplog):
-        lonely_ursula_maker = partial(make_federated_ursulas,
-                                      ursula_config=ursula_federated_test_config,
-                                      quantity=3,
-                                      know_each_other=True)
+def test_learner_learns_about_domains_separately(lonely_ursula_maker, caplog):
+        _lonely_ursula_maker = partial(lonely_ursula_maker, know_each_other=True, quantity=3)
 
-        global_learners = lonely_ursula_maker(domains={"nucypher1.test_suite"})
-        first_domain_learners = lonely_ursula_maker(domains={"nucypher1.test_suite"})
-        second_domain_learners = lonely_ursula_maker(domains={"nucypher2.test_suite"})
+        global_learners = _lonely_ursula_maker(domains={"nucypher1.test_suite"})
+        first_domain_learners = _lonely_ursula_maker(domains={"nucypher1.test_suite"})
+        second_domain_learners = _lonely_ursula_maker(domains={"nucypher2.test_suite"})
 
         big_learner = global_learners.pop()
 
@@ -45,8 +42,8 @@ def test_learner_learns_about_domains_separately(ursula_federated_test_config, c
         # All domain 1 nodes
         assert len(big_learner.known_nodes) == 5
 
-        new_first_domain_learner = lonely_ursula_maker(domains={"nucypher1.test_suite"}).pop()
-        _new_second_domain_learner = lonely_ursula_maker(domains={"nucypher2.test_suite"})
+        new_first_domain_learner = _lonely_ursula_maker(domains={"nucypher1.test_suite"}).pop()
+        _new_second_domain_learner = _lonely_ursula_maker(domains={"nucypher2.test_suite"})
 
         new_first_domain_learner._current_teacher_node = big_learner
         new_first_domain_learner.learn_from_teacher_node()
