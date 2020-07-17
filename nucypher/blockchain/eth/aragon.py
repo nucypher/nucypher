40f866d3e119ce100358eab7ccb6da9838476c1e
David Núñez <david@nucypher.com>
2020-07-17 17:55:28 +0200

    Introducing Translators, wrappers around specific Web3 Contracts, like Agents, but without any stance on what to do next.

--- nucypher/blockchain/eth/aragon.py
+++ nucypher/blockchain/eth/aragon.py
@@ -16,8 +16,44 @@
 """
 from typing import Iterable, Tuple
 
+from eth_typing import ChecksumAddress
 from eth_utils import to_canonical_address
 from web3 import Web3
+from web3.contract import Contract, ContractFunction
+
+from nucypher.blockchain.eth.constants import TOKEN_MANAGER_CONTRACT_NAME
+from nucypher.blockchain.eth.interfaces import BlockchainInterfaceFactory
+
+
+class Translator:  # TODO: Not sure about this name, tbh
+    """Base class for Translators"""
+    contract_name: str = NotImplemented
+
+    def __init__(self, address: str, provider_uri: str = None):
+        self.blockchain = BlockchainInterfaceFactory.get_or_create_interface(provider_uri=provider_uri)
+        self.contract: Contract = self.blockchain.get_contract_instance(contract_name=self.contract_name,
+                                                                        address=address)
+
+
+class TokenManagerTranslator(Translator):
+
+    contract_name = TOKEN_MANAGER_CONTRACT_NAME
+
+    def mint(self, receiver_address: ChecksumAddress, amount: int) -> ContractFunction:
+        function_call = self.contract.functions.mint(receiver_address, amount)
+        return function_call
+
+    def issue(self, amount: int) -> ContractFunction:
+        function_call = self.contract.functions.issue(amount)
+        return function_call
+
+    def assign(self, receiver_address: ChecksumAddress, amount: int) -> ContractFunction:
+        function_call = self.contract.functions.assign(receiver_address, amount)
+        return function_call
+
+    def burn(self, holder_address: ChecksumAddress, amount: int) -> ContractFunction:
+        function_call = self.contract.functions.burn(holder_address, amount)
+        return function_call
 
 
 class CallScriptCodec:
