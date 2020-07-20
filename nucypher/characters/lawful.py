b6160eb041fc20a95ea15ffee3bb200ef3297874
jMyles <justin@justinholmes.com>
2020-07-20 12:37:05 -0700

    No need for the if here - this function checks if it's running.

--- nucypher/characters/lawful.py
+++ nucypher/characters/lawful.py
@@ -1246,8 +1246,7 @@ class Ursula(Teacher, Character, Worker):
     def stop(self, halt_reactor: bool = False) -> None:
         """Stop services"""
         self._availability_tracker.stop()
-        if self._learning_task.running:
-            self.stop_learning_loop()
+        self.stop_learning_loop()
         if not self.federated_only:
             self.work_tracker.stop()
         if self._arrangement_pruning_task.running:
