f38923a191319248b63bd849545f9018fe1d3504
Kieran R. Prasch <kieranprasch@gmail.com>
2020-07-30 16:18:55 -0700

    Fix minor ImportErrors.

--- tests/acceptance/blockchain/actors/test_staker.py
+++ tests/acceptance/blockchain/actors/test_staker.py
@@ -21,6 +21,7 @@ from eth_tester.exceptions import TransactionFailed
 from nucypher.blockchain.eth.agents import ContractAgency, StakingEscrowAgent
 from nucypher.blockchain.eth.token import NU, Stake
 from nucypher.blockchain.eth.utils import datetime_at_period
+from nucypher.crypto.powers import TransactingPower
 from tests.constants import FEE_RATE_RANGE, INSECURE_DEVELOPMENT_PASSWORD, DEVELOPMENT_TOKEN_AIRDROP_AMOUNT
 from tests.utils.blockchain import token_airdrop
 from tests.utils.ursula import make_decentralized_ursulas
