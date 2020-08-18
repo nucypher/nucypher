58c8f07dce0e959fb42f75dc16f0b8422a66f7a3
jMyles <justin@justinholmes.com>
2020-08-18 04:54:14 -0700

    Heartbeat also using block_until_complete().

--- nucypher/policy/policies.py
+++ nucypher/policy/policies.py
@@ -14,7 +14,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
-
+import math
 import time
 import random
 from abc import ABC, abstractmethod
