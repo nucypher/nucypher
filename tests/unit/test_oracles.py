342ac40e3e84980f47ad9b85fdab1cee6477b1c3
David Núñez <david@nucypher.com>
2020-08-19 21:22:51 +0200

    Testing Etherchain and Upvest oracle gas strategies

--- tests/unit/test_oracles.py
+++ tests/unit/test_oracles.py
@@ -21,7 +21,30 @@ import pytest
 from requests.exceptions import ConnectionError
 from web3 import Web3
 
-from nucypher.utilities.oracles import Oracle, EtherchainGasPriceOracle, UpvestGasPriceOracle
+from nucypher.utilities.oracles import (
+    EtherchainGasPriceOracle,
+    Oracle,
+    oracle_fallback_gas_price_strategy,
+    UpvestGasPriceOracle
+)
+
+etherchain_json = {
+    "safeLow": "99.0",
+    "standard": "105.0",
+    "fast": "108.0",
+    "fastest": "119.9"
+}
+
+upvest_json = {
+    "success": True,
+    "updated": "2020-08-19T02:38:00.172Z",
+    "estimates": {
+        "fastest": 105.2745,
+        "fast": 97.158,
+        "medium": 91.424,
+        "slow": 87.19
+    }
+}
 
 
 def test_probe_oracle(mocker):
@@ -54,12 +77,6 @@ def test_probe_oracle(mocker):
 
 
 def test_etherchain():
-    etherchain_json = {
-        "safeLow": "99.0",
-        "standard": "105.0",
-        "fast": "108.0",
-        "fastest": "119.9"
-    }
     oracle = EtherchainGasPriceOracle()
     with patch.object(oracle, '_probe_oracle'):
         oracle._raw_data = etherchain_json
@@ -68,19 +85,16 @@ def test_etherchain():
         assert oracle.get_gas_price('fast') == Web3.toWei(108.0, 'gwei')
         assert oracle.get_gas_price('fastest') == Web3.toWei(119.9, 'gwei')
         assert oracle.get_gas_price() == oracle.get_gas_price('fast')  # Default
+        parsed_gas_prices = oracle.gas_prices
+
+    with patch('nucypher.utilities.oracles.EtherchainGasPriceOracle._parse_gas_prices', autospec=True):
+        EtherchainGasPriceOracle.gas_prices = dict()
+        with patch.dict(EtherchainGasPriceOracle.gas_prices, values=parsed_gas_prices):
+            gas_strategy = oracle.construct_gas_strategy()
+            assert gas_strategy("web3", "tx") == Web3.toWei(108.0, 'gwei')
 
 
 def test_upvest():
-    upvest_json = {
-        "success": True,
-        "updated": "2020-08-19T02:38:00.172Z",
-        "estimates": {
-            "fastest": 105.2745,
-            "fast": 97.158,
-            "medium": 91.424,
-            "slow": 87.19
-        }
-    }
     oracle = UpvestGasPriceOracle()
     with patch.object(oracle, '_probe_oracle'):
         oracle._raw_data = upvest_json
@@ -89,3 +103,35 @@ def test_upvest():
         assert oracle.get_gas_price('fast') == Web3.toWei(97.158, 'gwei')
         assert oracle.get_gas_price('fastest') == Web3.toWei(105.2745, 'gwei')
         assert oracle.get_gas_price() == oracle.get_gas_price('fastest')  # Default
+        parsed_gas_prices = oracle.gas_prices
+
+    with patch('nucypher.utilities.oracles.UpvestGasPriceOracle._parse_gas_prices', autospec=True):
+        UpvestGasPriceOracle.gas_prices = dict()
+        with patch.dict(UpvestGasPriceOracle.gas_prices, values=parsed_gas_prices):
+            gas_strategy = oracle.construct_gas_strategy()
+            assert gas_strategy("web3", "tx") == Web3.toWei(105.2745, 'gwei')
+
+
+def test_oracle_fallback_gas_price_strategy():
+
+    mocked_gas_price = 0xFABADA
+
+    def mock_gas_strategy(web3, tx=None):
+        return mocked_gas_price
+
+    # In normal circumstances, the first oracle (Etherchain) will return the gas price
+    with patch('nucypher.utilities.oracles.EtherchainGasPriceOracle.construct_gas_strategy',
+               return_value=mock_gas_strategy):
+        assert oracle_fallback_gas_price_strategy("web3", "tx") == mocked_gas_price
+
+    # If the first oracle in the chain fails, we resort to the second one
+    with patch('nucypher.utilities.oracles.EtherchainGasPriceOracle._probe_oracle', side_effect=Oracle.OracleError):
+        with patch('nucypher.utilities.oracles.UpvestGasPriceOracle.construct_gas_strategy',
+                   return_value=mock_gas_strategy):
+            assert oracle_fallback_gas_price_strategy("web3", "tx") == mocked_gas_price
+
+    # If both oracles fail, we fallback to the rpc_gas_price_strategy
+    with patch('nucypher.utilities.oracles.EtherchainGasPriceOracle._probe_oracle', side_effect=Oracle.OracleError):
+        with patch('nucypher.utilities.oracles.UpvestGasPriceOracle._probe_oracle', side_effect=Oracle.OracleError):
+            with patch('nucypher.utilities.oracles.rpc_gas_price_strategy', side_effect=mock_gas_strategy):
+                assert oracle_fallback_gas_price_strategy("web3", "tx") == mocked_gas_price
