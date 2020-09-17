d4ac7ce35130af7c0a2a6f07d1cd6727a15f153c
David Núñez <david@nucypher.com>
2020-09-18 01:19:27 +0200

    Failing test showing that currently we learn about nodes from a different domain
    Demonstrates #1843

--- tests/integration/learning/test_domains.py
+++ tests/integration/learning/test_domains.py
@@ -17,7 +17,8 @@
 
 from functools import partial
 
-from tests.utils.ursula import make_federated_ursulas
+from nucypher.acumen.perception import FleetSensor
+from nucypher.config.storages import TemporaryFileBasedNodeStorage, LocalFileBasedNodeStorage
 
 
 def test_learner_learns_about_domains_separately(lonely_ursula_maker, caplog):
@@ -54,3 +55,40 @@ def test_learner_learns_about_domains_separately(lonely_ursula_maker, caplog):
     # However, it learned about *all* of the nodes in its own domain.
     assert set(first_domain_learners).intersection(
             n.mature() for n in new_first_domain_learner.known_nodes) == first_domain_learners
+
+
+def test_learner_restores_metadata_from_storage(lonely_ursula_maker, tmpdir):
+
+    # Create a local file-based node storage
+    root = tmpdir.mkdir("known_nodes")
+    metadata = root.mkdir("metadata")
+    certs = root.mkdir("certs")
+    old_storage = LocalFileBasedNodeStorage(federated_only=True,
+                                            metadata_dir=metadata,
+                                            certificates_dir=certs,
+                                            storage_root=root)
+
+    # Use the ursula maker with this storage so it's populated with nodes from one domain
+    _some_ursulas = lonely_ursula_maker(domain="fistro",
+                                        node_storage=old_storage,
+                                        know_each_other=True,
+                                        quantity=3,
+                                        save_metadata=True)
+
+    # Create a pair of new learners in a different domain, using the previous storage, and learn from it
+    new_learners = lonely_ursula_maker(domain="duodenal",
+                                       node_storage=old_storage,
+                                       quantity=2,
+                                       know_each_other=True,
+                                       save_metadata=False)
+    learner, buddy = new_learners
+    buddy._Learner__known_nodes = FleetSensor()
+
+    # The learner shouldn't learn about any node from the first domain, since it's different.
+    learner.learn_from_teacher_node()
+    for restored_node in learner.known_nodes:
+        assert restored_node.mature().serving_domain == learner.learning_domain
+
+    # In fact, since the storage only contains nodes from a different domain,
+    # the learner should only know its buddy from the second domain.
+    assert set(learner.known_nodes) == {buddy}
