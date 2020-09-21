c897a20d530ee7c4e2ac301e3e541ff935db03f5
David Núñez <david@nucypher.com>
2020-09-21 13:07:52 +0200

    When restoring nodes from disk, let's just complain once if there are nodes from other domains

--- nucypher/network/nodes.py
+++ nucypher/network/nodes.py
@@ -348,14 +348,18 @@ class Learner:
         stored_nodes = self.node_storage.all(federated_only=self.federated_only)  # TODO: #466
 
         restored_from_disk = []
+        invalid_nodes = defaultdict(list)
         for node in stored_nodes:
             node_domain = node.domain.decode('utf-8')
-            if node_domain != self.learning_domain.encode():
-                self.log.warn(f"Restored {node} metadata from storage, but appears to be for domain '{node_domain}'; "
-                              f"we're learning about '{self.learning_domain}'. Let's ignore it.")
+            if node_domain != self.learning_domain:
+                invalid_nodes[node_domain].append(node)
                 continue
             restored_node = self.remember_node(node, record_fleet_state=False)  # TODO: Validity status 1866
             restored_from_disk.append(restored_node)
+
+        if invalid_nodes:
+            self.log.warn(f"We're learning about domain '{self.learning_domain}', but found nodes from other domains; "
+                          f"let's ignore them. These domains and nodes are: {dict(invalid_nodes)}")
 
         return restored_from_disk
 
