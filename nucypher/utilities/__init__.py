64bc77d2d1286a4489f06d3e991e17d35dc95773
David Núñez <david@nucypher.com>
2020-09-29 16:26:20 +0200

    Dynamic mapping of Datafeed classes, à la Signers

--- nucypher/utilities/__init__.py
+++ nucypher/utilities/__init__.py
@@ -14,3 +14,10 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
+
+from nucypher.utilities.datafeeds import EthereumGasPriceDatafeed, EtherchainGasPriceDatafeed, UpvestGasPriceDatafeed
+
+EthereumGasPriceDatafeed._DATAFEEDS = {
+    EtherchainGasPriceDatafeed.simple_name: EtherchainGasPriceDatafeed,
+    UpvestGasPriceDatafeed.simple_name: UpvestGasPriceDatafeed
+}
