74bf6e6e9bd1c0d3e8fd1e732ceaf882d7ab14b7
derekpierre <derek.pierre@gmail.com>
2020-09-30 20:34:31 -0400

    Respond to RFCs for #2322.

--- nucypher/blockchain/eth/events.py
+++ nucypher/blockchain/eth/events.py
@@ -93,19 +93,13 @@ class ContractEventsThrottler:
                  to_block: int = None,  # defaults to latest block
                  max_blocks_per_call: int = DEFAULT_MAX_BLOCKS_PER_CALL,
                  **argument_filters):
-        if not agent:
-            raise ValueError(f"Contract agent must be provided")
-        if not event_name:
-            raise ValueError(f"Event name must be provided")
-        if not from_block:
-            raise ValueError(f"From block must be provided")
-
         self.event_filter = agent.events[event_name]
         self.from_block = from_block
         self.to_block = to_block if to_block else agent.blockchain.client.block_number
         # validity check of block range
-        if self.to_block <= self.from_block:
-            raise ValueError(f"Invalid block range provided ({self.from_block} - {self.to_block})")
+        if self.to_block < self.from_block:
+            raise ValueError(f"Invalid events block range: to_block {self.to_block} must be greater than or equal "
+                             f"to from_block {self.from_block}")
 
         self.max_blocks_per_call = max_blocks_per_call
         self.argument_filters = argument_filters
