23dfc1355c971044897cd28092cfcf7ab00f5aad
Kieran Prasch <kieran@nucypher.com>
2020-10-14 18:39:23 -0700

    Bump version: 4.0.0 → 4.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ author = 'NuCypher'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '4.0.0'
+release = '4.0.1'
 
 
 # -- General configuration ---------------------------------------------------
