168b897181dadbadbba7c25aad4c4050883bab26
damon <gdamon@gmail.com>
2020-10-28 05:16:18 -0700

    adds test for abort on error

--- tests/acceptance/blockchain/actors/test_worker.py
+++ tests/acceptance/blockchain/actors/test_worker.py
@@ -151,21 +151,20 @@ class WorkTrackerThatFailsHalfTheTime(WorkTracker):
 
         return MockStakingAgent()
 
-
     def _do_work(self) -> None:
-        self.period += 1
-        if self.period % 2:
+        self.attempts += 1
+        if self.attempts % 2:
             raise BaseException("zomg something went wrong")
         self.workdone += 1
 
-    def __init__(self, clock, *args, **kwargs):
+    def __init__(self, clock, abort_on_error, *args, **kwargs):
         self.workdone = 0
-        self.period = 0
+        self.attempts = 0
         self.CLOCK = clock
         self.log = Logger('stake-tracker')
         self._tracking_task = task.LoopingCall(self._do_work)
         self._tracking_task.clock = self.CLOCK
-        self._abort_on_error = False
+        self._abort_on_error = abort_on_error
 
 
 @pytest_twisted.inlineCallbacks
@@ -173,13 +172,42 @@ def test_worker_failure_resilience():
 
     # Control time
     clock = Clock()
-    worktracker = WorkTrackerThatFailsHalfTheTime(clock)
+    worktracker = WorkTrackerThatFailsHalfTheTime(clock, False)
 
     def advance_one_cycle(_):
         clock.advance(WorkTrackerThatFailsHalfTheTime.INTERVAL_CEIL)
 
     def checkworkstate(_):
-        assert worktracker.period / 2 == worktracker.workdone
+        assert worktracker.attempts / 2 == worktracker.workdone
+
+    def start():
+        worktracker.start()
+
+    d = threads.deferToThread(start)
+
+    for i in range(10):
+
+        d.addCallback(advance_one_cycle)
+        d.addCallback(checkworkstate)
+
+    yield d
+
+
+@pytest_twisted.inlineCallbacks
+def test_worker_failure_non_resilience():
+    """
+    abort on error is True for this one
+    """
+
+    # Control time
+    clock = Clock()
+    worktracker = WorkTrackerThatFailsHalfTheTime(clock, True)
+
+    def advance_one_cycle(_):
+        clock.advance(WorkTrackerThatFailsHalfTheTime.INTERVAL_CEIL)
+
+    def checkworkstate(_):
+        assert worktracker.workdone == 0
 
     def start():
         worktracker.start()
