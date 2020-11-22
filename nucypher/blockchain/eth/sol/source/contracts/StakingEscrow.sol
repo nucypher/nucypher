197cd09258e24f3c9fd90302be95aa8b5a3586e8
vzotova <vikki.zta@gmail.com>
2020-11-22 16:58:40 +0300

    Updates title tags in contracts

--- nucypher/blockchain/eth/sol/source/contracts/StakingEscrow.sol
+++ nucypher/blockchain/eth/sol/source/contracts/StakingEscrow.sol
@@ -43,6 +43,7 @@ interface WorkLockInterface {
 
 
 /**
+* @title StakingEscrow
 * @notice Contract holds and locks stakers tokens.
 * Each staker that locks their tokens will receive some compensation
 * @dev |v5.5.1|
