af5bb16fab702011922d5abcbf83786f2fce80c2
vzotova <vikki.zta@gmail.com>
2020-11-26 14:49:50 +0300

    Solidity 0.7.3->0.7.5

--- nucypher/blockchain/eth/sol/__conf__.py
+++ nucypher/blockchain/eth/sol/__conf__.py
@@ -15,4 +15,4 @@ You should have received a copy of the GNU Affero General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-SOLIDITY_COMPILER_VERSION = 'v0.7.3'
+SOLIDITY_COMPILER_VERSION = 'v0.7.5'
