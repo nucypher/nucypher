9cba73bd5343c298e8900d3dee155b124c936b92
David Núñez <david@nucypher.com>
2020-12-22 11:42:08 +0100

    Deprecate datafeed_fallback_gas_strategy

--- nucypher/utilities/gas_strategies.py
+++ nucypher/utilities/gas_strategies.py
@@ -99,30 +99,6 @@ def construct_capped_datafeed_median_strategy(speed: Optional[str] = None) -> Ca
     return max_price_gas_strategy_wrapper(construct_datafeed_median_strategy(speed=speed))
 
 
-# Fallback
-
-def construct_datafeed_fallback_strategy(speed: Optional[str] = None) -> Callable:
-    def datafeed_fallback_gas_price_strategy(web3: Web3, transaction_params: TxParams = None) -> Wei:
-        feeds = (UpvestGasPriceDatafeed, EtherchainGasPriceDatafeed, ZoltuGasPriceDatafeed)
-
-        for gas_price_feed_class in feeds:
-            try:
-                gas_strategy = gas_price_feed_class.construct_gas_strategy(speed=speed)
-                gas_price = gas_strategy(web3, transaction_params)
-            except Datafeed.DatafeedError:
-                continue
-            else:
-                return gas_price
-        else:
-            # Worst-case scenario, we get the price from the ETH node itself
-            return rpc_gas_price_strategy(web3, transaction_params)
-    return datafeed_fallback_gas_price_strategy
-
-
-def construct_capped_datafeed_fallback_strategy(speed: Optional[str] = None) -> Callable:
-    return max_price_gas_strategy_wrapper(construct_datafeed_fallback_strategy(speed=speed))
-
-
 #
 # Web3 gas strategies
 #
