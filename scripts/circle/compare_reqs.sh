a63a8bf83d67b542167ec76fcabcce5d6efd6dd3
David Núñez <david@nucypher.com>
2021-01-06 03:27:18 +0100

    CI: Store evidence of inconsistent requirement files

--- scripts/circle/compare_reqs.sh
+++ scripts/circle/compare_reqs.sh
@@ -3,11 +3,11 @@
 set -e
 
 # update lock and build requirements files
-yes | ./scripts/installation/relock_dependencies.sh circlereqs
+yes | ./scripts/installation/relock_dependencies.sh circle-requirements
 
 echo "---- validating requirements.txt ----"
 REQSHASH=$(md5sum requirements.txt | cut -d ' ' -f1)
-TESTHASH=$(md5sum circlereqs.txt | cut -d ' ' -f1)
+TESTHASH=$(md5sum circle-requirements.txt | cut -d ' ' -f1)
 
 echo "- $REQSHASH"
 echo "- $TESTHASH"
@@ -18,13 +18,13 @@ else
     echo "- requirements.txt contains inconsistencies ...."
     echo "- you may want to run `pipenv sync --dev` and then ./scripts/installation/relock_dependencies.sh ...."
     echo "- which will rebuild your *requirements.txt files ...."
-    diff requirements.txt circlereqs.txt
+    diff requirements.txt circle-requirements.txt
     exit 2
 fi
 
 echo "---- validating dev-requirements.txt ----"
 REQSHASH=$(md5sum dev-requirements.txt | cut -d ' ' -f1)
-TESTHASH=$(md5sum dev-circlereqs.txt | cut -d ' ' -f1)
+TESTHASH=$(md5sum dev-circle-requirements.txt | cut -d ' ' -f1)
 
 echo "- $REQSHASH"
 echo "- $TESTHASH"
@@ -36,6 +36,6 @@ else
     echo "- dev-requirements.txt contains inconsistencies ...."
     echo "- you may want to run `pipenv sync --dev` and then ./scripts/installation/relock_dependencies.sh ...."
     echo "- which will rebuild your *requirements.txt files ...."
-    diff dev-requirements.txt dev-circlereqs.txt
+    diff dev-requirements.txt dev-circle-requirements.txt
     exit 2
 fi
