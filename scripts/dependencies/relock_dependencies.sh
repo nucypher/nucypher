0fc8d1ea2cdb03a3e766d6b1ea46b7f83e9ca7eb
derekpierre <derek.pierre@gmail.com>
2021-01-07 11:10:57 -0500

    Include relock of docs requirements in the relock_dependencies script.

--- scripts/dependencies/relock_dependencies.sh
+++ scripts/dependencies/relock_dependencies.sh
@@ -9,12 +9,20 @@ pipenv --rm
 rm -f Pipfile.lock
 rm -f $PREFIX.txt
 rm -f dev-$PREFIX.txt
+rm -f docs-$PREFIX.txt
 
 echo "Removing pip and pipenv system cache..."
 rm -r ~/.cache/pip ~/.cache/pipenv
 
 # start enforcing failures
 set -e
+
+echo "Building Documentation Requirements"
+pushd ./scripts/dependencies/docs
+pipenv lock --clear --pre --requirements --no-header > ../../../docs-$PREFIX.txt
+rm -f Pipfile.lock
+pipenv --rm
+popd
 
 echo "Building Development Requirements"
 pipenv lock --clear --pre --requirements --dev-only --no-header > dev-$PREFIX.txt
