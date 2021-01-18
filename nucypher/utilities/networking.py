186d38ad3344379d8248686c7411a657d9efbc93
Kieran Prasch <kieran@nucypher.com>
2021-01-18 10:47:44 -0800

    Respond to RFCs in PR #2527

--- nucypher/utilities/networking.py
+++ nucypher/utilities/networking.py
@@ -149,7 +149,10 @@ def get_external_ip_from_default_teacher(network: str,
     return result
 
 
-def get_external_ip_from_known_nodes(known_nodes: FleetSensor, sample_size: int = 3) -> Union[str, None]:
+def get_external_ip_from_known_nodes(known_nodes: FleetSensor,
+                                     sample_size: int = 3,
+                                     log: Logger = IP_DETECTION_LOGGER
+                                     ) -> Union[str, None]:
     """
     Randomly select a sample of peers to determine the external IP address
     of this host. The first node to reply successfully will be used.
@@ -162,6 +165,7 @@ def get_external_ip_from_known_nodes(known_nodes: FleetSensor, sample_size: int 
     for node in sample:
         ip = _request_from_node(teacher=node, client=client)
         if ip:
+            log.info(f'Fetched external IP address ({ip}) from randomly selected known nodes.')
             return ip
 
 
