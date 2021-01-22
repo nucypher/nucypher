bda4db5ef6df9f45db030299e3a167cfc5fa11d4
Kieran Prasch <kieran@nucypher.com>
2021-01-22 11:18:10 -0800

    Simplify Ursula construction: breakout functionality to methods, reduce parameters, localize evidence.

--- nucypher/config/characters.py
+++ nucypher/config/characters.py
@@ -14,7 +14,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
-
+from collections import Iterable
 
 import os
 from constant_sorrow.constants import (
