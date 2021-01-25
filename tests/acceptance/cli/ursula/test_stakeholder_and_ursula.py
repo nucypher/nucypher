8a6ec6163e05569b1faeb54c9af265121d996120
derekpierre <derek.pierre@gmail.com>
2021-01-25 11:54:05 -0500

    Fix failing unit tests.

--- tests/acceptance/cli/ursula/test_stakeholder_and_ursula.py
+++ tests/acceptance/cli/ursula/test_stakeholder_and_ursula.py
@@ -574,6 +574,7 @@ def test_collect_rewards_integration(click_runner,
                     registry=agency_local_registry,
                     rest_host='127.0.0.1',
                     rest_port=ursula_port,
+                    provider_uri=TEST_PROVIDER_URI,
                     network_middleware=MockRestMiddleware(),
                     db_filepath=tempfile.mkdtemp(),
                     domain=TEMPORARY_DOMAIN)
