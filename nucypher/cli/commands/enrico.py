d5df9e72e62f10902ca62918cb768e117f718842
Kieran Prasch <kieran@nucypher.com>
2021-02-09 13:52:51 -0800

    Handle enrico cli defaults

--- nucypher/cli/commands/enrico.py
+++ nucypher/cli/commands/enrico.py
@@ -83,7 +83,7 @@ def encrypt(general_config, policy_encrypting_key, message, file, ipfs):
         emitter.error(f'Pass either --message or --file, not both.')
         raise click.Abort
 
-    if not message or file:
+    if not message and not file:
         message = click.prompt('Enter plaintext to encrypt', type=click.STRING)
 
     # Encryption Request
