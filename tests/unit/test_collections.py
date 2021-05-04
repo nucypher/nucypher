de17e60839431637402fc5c65d254d5625f30067
Kieran Prasch <kieran@nucypher.com>
2021-05-03 19:23:30 -0700

    TMap authorship alternate constuctor

--- tests/unit/test_collections.py
+++ tests/unit/test_collections.py
@@ -17,6 +17,7 @@
 
 
 import os
+import pytest
 
 from nucypher.crypto.powers import DecryptingPower, SigningPower
 from nucypher.policy.maps import TreasureMap
@@ -32,12 +33,12 @@ def test_complete_treasure_map_journey(federated_alice, federated_bob, federated
 
     mock_kfrag = os.urandom(KFrag.expected_bytes_length())
     make_kfrag_payload_spy = mocker.spy(TreasureMap, '_make_kfrag_payload')
-    encrypted_kfrags = dict()
 
     treasure_map.derive_hrac(alice_stamp=federated_alice.stamp,
                              bob_verifying_key=bob_verifying_key,
                              label="chili con carne 🔥".encode('utf-8'))
 
+    encrypted_kfrags = dict()
     for ursula in federated_ursulas:
         treasure_map.add_kfrag(ursula, mock_kfrag, federated_alice.stamp)
         encrypted_kfrags[ursula.checksum_address] = make_kfrag_payload_spy.spy_return
@@ -53,13 +54,30 @@ def test_complete_treasure_map_journey(federated_alice, federated_bob, federated
         assert mock_kfrag_payload == ursula.verify_from(federated_alice, encrypted_kfrag, decrypt=True)  # FIXME: 2203
 
     serialized_map = bytes(treasure_map)
-
+    # ...
     deserialized_map = TreasureMap.from_bytes(serialized_map)
-
-    assert treasure_map.version == deserialized_map.version == TreasureMap.version
 
     compass = federated_bob.make_compass_for_alice(federated_alice)
     deserialized_map.orient(compass)
 
     assert treasure_map.m == deserialized_map.m == 1
     assert set(treasure_map.destinations) == set(deserialized_map.destinations)
+    assert treasure_map == deserialized_map
+
+
+def test_treasure_map_versioning(mocker, federated_alice, federated_bob, federated_ursulas):
+    kfrags = [os.urandom(32) for _ in range(3)]
+    treasure_map = TreasureMap.author(alice=federated_alice,
+                                      bob=federated_bob,
+                                      label=b'still Bill',
+                                      ursulas=list(federated_ursulas)[:len(kfrags)],
+                                      kfrags=kfrags,
+                                      m=2)
+
+    # Good version (baseline)
+    serialized_map = bytes(treasure_map)
+    deserialized_map = TreasureMap.from_bytes(serialized_map)
+    assert treasure_map == deserialized_map
+
+    # b''.serialized_map.split(b'')[len(TreasureMap._PREFIX)+1] = int(TreasureMap.VERSION_NUMBER+1).to_bytes(1, 'big')
+    assert False
