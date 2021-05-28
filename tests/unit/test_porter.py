8de665a95595159a0bf78f310774cb331c67768d
derekpierre <derek.pierre@gmail.com>
2021-05-28 15:13:37 -0400

    Initial unit tests for fields.

---
+++ tests/unit/test_porter.py
@@ -0,0 +1,118 @@
+"""
+This file is part of nucypher.
+
+nucypher is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+nucypher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import os
+from base64 import b64encode
+
+import pytest
+from bytestring_splitter import VariableLengthBytestring
+
+from nucypher.control.specifications.exceptions import InvalidInputData
+from nucypher.policy.collections import WorkOrder as WorkOrderClass
+from nucypher.policy.policies import Arrangement
+from nucypher.utilities.porter.control.specifications.fields import (
+    TreasureMapID,
+    UrsulaChecksumAddress,
+    WorkOrder,
+    WorkOrderResult
+)
+
+
+def test_treasure_map_id_field(enacted_federated_policy):
+    treasure_map_id_hex = enacted_federated_policy.treasure_map.public_id()
+    other_hex = b"some date".hex()  # length is not 32-bytes
+
+    field = TreasureMapID()
+    serialized = field._serialize(value=treasure_map_id_hex, attr=None, obj=None)
+    assert serialized == treasure_map_id_hex
+    assert serialized != other_hex
+
+    deserialized = field._deserialize(value=serialized, attr=None, data=None)
+    assert deserialized == treasure_map_id_hex
+    assert deserialized != other_hex
+
+    field._validate(value=treasure_map_id_hex)
+    with pytest.raises(InvalidInputData):
+        field._validate(value=other_hex)
+
+
+def test_ursula_checksum_address_field(get_random_checksum_address):
+    ursula_checksum = get_random_checksum_address()
+    other_address = get_random_checksum_address()
+
+    assert ursula_checksum != other_address
+
+    field = UrsulaChecksumAddress()
+    serialized = field._serialize(value=ursula_checksum, attr=None, obj=None)
+    assert serialized == ursula_checksum
+    assert serialized != other_address
+
+    deserialized = field._deserialize(value=serialized, attr=None, data=None)
+    assert deserialized == ursula_checksum
+    assert deserialized != other_address
+
+    field._validate(value=ursula_checksum)
+    field._validate(value=ursula_checksum.lower())
+    field._validate(value=ursula_checksum.upper())
+    field._validate(value=other_address)
+    field._validate(value=other_address.lower())
+    field._validate(value=other_address.upper())
+
+    with pytest.raises(InvalidInputData):
+        field._validate(value="0xdeadbeef")
+
+
+def test_work_order_field(mock_ursula_reencrypts,
+                          federated_ursulas,
+                          get_random_checksum_address,
+                          federated_bob,
+                          federated_alice):
+    # Setup
+    ursula = list(federated_ursulas)[0]
+    tasks = [mock_ursula_reencrypts(ursula) for _ in range(3)]
+    material = [(task.capsule, task.signature, task.cfrag, task.cfrag_signature) for task in tasks]
+    capsules, signatures, cfrags, cfrag_signatures = zip(*material)
+
+    arrangement_id = os.urandom(Arrangement.ID_LENGTH)
+    work_order = WorkOrderClass.construct_by_bob(arrangement_id=arrangement_id,
+                                                 bob=federated_bob,
+                                                 alice_verifying=federated_alice.stamp.as_umbral_pubkey(),
+                                                 ursula=ursula,
+                                                 capsules=capsules)
+
+    # Test Work Order
+    work_order_bytes = work_order.payload()
+
+    field = WorkOrder()
+    serialized = field._serialize(value=work_order, attr=None, obj=None)
+    assert serialized == b64encode(work_order_bytes).decode()
+
+    deserialized = field._deserialize(value=serialized, attr=None, data=None)
+    assert deserialized == work_order_bytes
+
+    # Test Work Order Result
+    # TODO is this the correct way of doing this?
+    cfrag_byte_stream = bytes()
+    for cfrag in cfrags:
+        reencryption_signature = ursula.stamp(bytes(cfrag))
+        cfrag_byte_stream += VariableLengthBytestring(cfrag) + reencryption_signature
+
+    field = WorkOrderResult()
+    serialized = field._serialize(value=cfrag_byte_stream, attr=None, obj=None)
+    assert serialized == b64encode(cfrag_byte_stream).decode()
+
+    deserialized = field.deserialize(value=serialized, attr=None, data=None)
+    assert deserialized == cfrag_byte_stream
