35034dba7f446493af0a463868a40241b6c9946b
Piotr Roslaniec <p.roslaniec@gmail.com>
2021-08-10 10:12:02 +0200

    Add a missing docstring

--- nucypher/config/util.py
+++ nucypher/config/util.py
@@ -20,6 +20,12 @@ from typing import Optional, get_type_hints
 
 
 def cast_paths_from(cls, payload):
+    """
+    A serialization helper.
+    Iterates over constructor arguments of `cls` and `cls` parents. Finds arguments
+    of type `pathlib.Path` or `Optional[pathlib.Path]`. Based on this, it casts
+    corresponding values in `payload` from `str` to `pathlib.Path` or None.
+    """
     constructor_args = get_type_hints(cls.__init__)
     for ancestor in cls.__mro__:
         constructor_args.update(get_type_hints(ancestor.__init__))
