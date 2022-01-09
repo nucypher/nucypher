01179c5df5b1912588919c6828e0798cba02a471
Bogdan Opanchuk <bogdan@opanchuk.net>
2022-01-09 15:25:04 -0800

    [temporary] Check to see how long do performance tests take now

--- tests/integration/learning/test_discovery_phases.py
+++ tests/integration/learning/test_discovery_phases.py
@@ -65,7 +65,7 @@ performance bottlenecks.
 """
 
 
-@skip_on_circleci  # TODO: #2552 Taking 6-10 seconds on CircleCI, passing locally.
+#@skip_on_circleci  # TODO: #2552 Taking 6-10 seconds on CircleCI, passing locally.
 def test_alice_can_learn_about_a_whole_bunch_of_ursulas(highperf_mocked_alice):
     # During the fixture execution, Alice verified one node.
     # TODO: Consider changing this - #1449
@@ -102,7 +102,7 @@ def test_alice_can_learn_about_a_whole_bunch_of_ursulas(highperf_mocked_alice):
 _POLICY_PRESERVER = []
 
 
-@skip_on_circleci  # TODO: #2552 Taking 6-10 seconds on CircleCI, passing locally.
+#@skip_on_circleci  # TODO: #2552 Taking 6-10 seconds on CircleCI, passing locally.
 def test_alice_verifies_ursula_just_in_time(fleet_of_highperf_mocked_ursulas,
                                             highperf_mocked_alice,
                                             highperf_mocked_bob):
