ebb4cfa856d8b0e9e990a310f14f1ffbf2f79eb9
Damon Ciarelli <damon@nucypher.com>
2022-01-22 11:54:59 -0800

    add live ursula test

--- tests/contracts/main/application/test_worker.py
+++ tests/contracts/main/application/test_worker.py
@@ -16,6 +16,10 @@ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import pytest
+import pytest_twisted
+from twisted.internet import threads
+from twisted.internet.task import Clock
+
 from eth_tester.exceptions import TransactionFailed
 
 from nucypher.blockchain.eth.constants import NULL_ADDRESS
@@ -419,3 +423,144 @@ def test_ursula_contract_interactions(ursula_decentralized_test_config, testerch
     testerchain.wait_for_receipt(tx.transactionHash)
 
     assert blockchain_ursula.is_confirmed is True
+
+
+@pytest_twisted.inlineCallbacks
+def test_worker_auto_confirm(mocker,
+                                 testerchain,
+                                 test_registry,
+                                 staker,
+                                 agency,
+                                 token_economics,
+                                 ursula_decentralized_test_config):
+
+    staker.initialize_stake(amount=NU(token_economics.minimum_allowed_locked, 'NuNit'),
+                            lock_periods=int(token_economics.minimum_locked_periods))
+
+    # Get an unused address and create a new worker
+    worker_address = testerchain.unassigned_accounts[-1]
+
+    # Control time
+    clock = Clock()
+    WorkTracker.CLOCK = clock
+
+    # Bond the Worker and Staker
+    staker.bond_worker(worker_address=worker_address)
+
+    commit_spy = mocker.spy(Worker, 'commit_to_next_period')
+    replacement_spy = mocker.spy(WorkTracker, '_WorkTracker__fire_replacement_commitment')
+
+    # Make the Worker
+    ursula = make_decentralized_ursulas(ursula_config=ursula_decentralized_test_config,
+                                        stakers_addresses=[staker.checksum_address],
+                                        workers_addresses=[worker_address],
+                                        registry=test_registry).pop()
+
+    ursula.run(preflight=False,
+               discovery=False,
+               start_reactor=False,
+               worker=True,
+               eager=True,
+               block_until_ready=False)  # "start" services
+
+    initial_period = staker.staking_agent.get_current_period()
+
+    def start():
+        log("Starting Worker for auto-commitment simulation")
+        start_pytest_ursula_services(ursula=ursula)
+
+    def advance_one_period(_):
+        log('Advancing one period')
+        testerchain.time_travel(periods=1)
+        clock.advance(WorkTracker.INTERVAL_CEIL + 1)
+
+    def check_pending_commitments(number_of_commitments):
+        def _check_pending_commitments(_):
+            log(f'Checking we have {number_of_commitments} pending commitments')
+            assert number_of_commitments == len(ursula.work_tracker.pending)
+        return _check_pending_commitments
+
+    def pending_commitments(_):
+        log('Starting unmined transaction simulation')
+        testerchain.client.add_middleware(unmined_receipt_simulator_middleware)
+
+    def advance_one_cycle(_):
+        log('Advancing one tracking iteration')
+        clock.advance(ursula.work_tracker._tracking_task.interval + 1)
+
+    def advance_until_replacement_indicated(_):
+        last_committed_period = staker.staking_agent.get_last_committed_period(staker_address=staker.checksum_address)
+        log("Advancing until replacement is indicated")
+        testerchain.time_travel(periods=1)
+        clock.advance(WorkTracker.INTERVAL_CEIL + 1)
+        mocker.patch.object(WorkTracker, 'max_confirmation_time', return_value=1.0)
+        mock_last_committed_period = mocker.PropertyMock(return_value=last_committed_period)
+        mocker.patch.object(Worker, 'last_committed_period', new_callable=mock_last_committed_period)
+        clock.advance(ursula.work_tracker.max_confirmation_time() + 1)
+
+    def verify_unmined_commitment(_):
+        log('Verifying worker has unmined commitment transaction')
+
+        # FIXME: The test doesn't model accurately an unmined TX, but an unconfirmed receipt,
+        # so the tracker does not have pending TXs. If we want to model pending TXs we need to actually
+        # prevent them from being mined.
+        #
+        # assert len(ursula.work_tracker.pending) == 1
+        current_period = staker.staking_agent.get_current_period()
+        assert commit_spy.call_count == current_period - initial_period + 1
+
+    def verify_replacement_commitment(_):
+        log('Verifying worker has replaced commitment transaction')
+        assert replacement_spy.call_count > 0
+
+    def verify_confirmed(_):
+        # Verify that periods were committed on-chain automatically
+        last_committed_period = staker.staking_agent.get_last_committed_period(staker_address=staker.checksum_address)
+        current_period = staker.staking_agent.get_current_period()
+
+        expected_commitments = current_period - initial_period + 1
+        log(f'Verifying worker made {expected_commitments} commitments so far')
+        assert (last_committed_period - current_period) == 1
+        assert commit_spy.call_count == expected_commitments
+        assert replacement_spy.call_count == 0
+
+    # Behavioural Test, like a screenplay made of legos
+
+    # Ursula commits on startup
+    d = threads.deferToThread(start)
+    d.addCallback(verify_confirmed)
+    d.addCallback(advance_one_period)
+
+    d.addCallback(check_pending_commitments(1))
+    d.addCallback(advance_one_cycle)
+    d.addCallback(check_pending_commitments(0))
+
+    # Ursula commits for 3 periods with no problem
+    for i in range(3):
+        d.addCallback(advance_one_period)
+        d.addCallback(verify_confirmed)
+        d.addCallback(check_pending_commitments(1))
+
+    # Introduce unmined transactions
+    d.addCallback(advance_one_period)
+    d.addCallback(pending_commitments)
+
+    # Ursula's commitment transaction gets stuck
+    for i in range(4):
+        d.addCallback(advance_one_cycle)
+        d.addCallback(verify_unmined_commitment)
+
+    # Ursula recovers from this situation
+    d.addCallback(advance_one_cycle)
+    d.addCallback(verify_confirmed)
+    d.addCallback(advance_one_cycle)
+    d.addCallback(check_pending_commitments(0))
+
+    # but it happens again, resulting in a replacement transaction
+    d.addCallback(advance_until_replacement_indicated)
+    d.addCallback(advance_one_cycle)
+    d.addCallback(check_pending_commitments(1))
+    d.addCallback(advance_one_cycle)
+    d.addCallback(verify_replacement_commitment)
+
+    yield d
