912902b4f86d48e1a13822d1e630889f32363030
Bogdan Opanchuk <bogdan@opanchuk.net>
2022-02-20 14:16:23 -0800

    Updates for nucypher-core API

--- nucypher/characters/lawful.py
+++ nucypher/characters/lawful.py
@@ -1012,7 +1012,7 @@ class Ursula(Teacher, Character, Operator):
             decentralized_identity_evidence = None
         else:
             decentralized_identity_evidence = self.decentralized_identity_evidence
-        payload = NodeMetadataPayload(canonical_address=self.canonical_address,
+        payload = NodeMetadataPayload(staker_address=self.canonical_address,
                                       domain=self.domain,
                                       timestamp_epoch=timestamp.epoch,
                                       decentralized_identity_evidence=decentralized_identity_evidence,
