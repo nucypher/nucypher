aa2baaf2c9edb08341c845a07afd21326df7cc22
Kieran Prasch <kieranprasch@gmail.com>
2022-03-25 12:33:40 -0700

    Bump version: 5.3.3 → 6.0.0-rc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ author = 'NuCypher'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '5.3.3'
+release = '6.0.0'
 
 
 # -- General configuration ---------------------------------------------------
