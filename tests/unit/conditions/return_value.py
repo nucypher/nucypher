9609d5e8c95f04afdfe3067803a60b43b8f9b8dd
Kieran Prasch <kieranprasch@gmail.com>
2022-06-29 20:15:49 +0200

    Begin sanitization of return test comparators

---
+++ tests/unit/conditions/return_value.py
@@ -0,0 +1,15 @@
+import pytest
+
+from nucypher.policy.conditions.payment import ReturnValueTest
+
+
+def test_return_result_test_simple():
+    test = ReturnValueTest(comparator='>', value=0)
+    assert test.eval(1)
+    assert not test.eval(-1)
+
+
+def test_return_value_sanitization():
+    with pytest.raises(ValueError):
+        _test = ReturnValueTest('DROP', 'TABLE')
+
