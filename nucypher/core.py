9ac2c06ec9cefc8faf54a5a983aa66a811b23d51
Kieran Prasch <kieranprasch@gmail.com>
2022-08-10 10:36:45 -0700

    Additional core shims for alternate constructors

--- nucypher/core.py
+++ nucypher/core.py
@@ -2,9 +2,9 @@ from nucypher_core import *
 from nucypher_core import MessageKit as CoreMessageKit
 from nucypher_core import ReencryptionRequest as CoreReencryptionRequest
 from nucypher_core import RetrievalKit as CoreRetrievalKit
-from typing import Optional
+from typing import Optional, Tuple, Dict, Union, List
 
-from nucypher.policy.conditions.evm import EVMCondition
+from nucypher.policy.conditions.evm import ContractCondition
 
 
 class BoltOnConditions:
@@ -13,13 +13,13 @@ class BoltOnConditions:
 
     def __init__(self,
                  *args,
-                 condition: Optional[EVMCondition] = None,
+                 conditions: Optional['ConditionLingo'] = None,
                  core_instance: Optional = None,
                  **kwargs):
         if not core_instance:
             core_instance = self._CORE_CLASS(*args, **kwargs)
         self._core_instance = core_instance
-        self.condition = condition
+        self.conditions = conditions
 
     def __getattr__(self, attr):
         if attr in self.__dict__:
@@ -28,17 +28,24 @@ class BoltOnConditions:
 
     def __bytes__(self):
         payload = bytes(self._core_instance)
-        if self.condition:
+        if self.conditions:
             payload += self._DELIMITER
-            payload += bytes(self.condition)
+            payload += bytes(self.conditions)
         return payload
+
+    @classmethod
+    def _parse(cls, data) -> Tuple[bytes, bytes]:
+        if cls._DELIMITER in data:
+            data, condition_bytes = data.split(cls._DELIMITER)
+            return data, condition_bytes
+        return data, b''  # TODO: Handle empty conditions better
 
     @classmethod
     def from_bytes(cls, data: bytes):
         condition = None
         if cls._DELIMITER in data:
-            data, condition_bytes = data.split(cls._DELIMITER)
-            condition = EVMCondition.from_bytes(condition_bytes)
+            data, condition_bytes = cls._parse(data)
+            condition = ContractCondition.from_bytes(condition_bytes)
         core_instance = cls._CORE_CLASS.from_bytes(data)
         instance = cls(core_instance=core_instance, decryption_condition=condition)
         return instance
@@ -47,6 +54,15 @@ class BoltOnConditions:
 class RetrievalKit(BoltOnConditions):
     _CORE_CLASS = CoreRetrievalKit
 
+    @classmethod
+    def from_message_kit(cls, message_kit: MessageKit, *args, **kwargs):
+        # TODO: strip away the conditions for the lower layer
+        data, condition_bytes = cls._parse(bytes(message_kit))
+        core_mk_instance = MessageKit._CORE_CLASS.from_bytes(data)
+        core_instance = cls._CORE_CLASS.from_message_kit(message_kit=core_mk_instance, *args, **kwargs)
+        instance = cls(core_instance=core_instance)
+        return instance
+
 
 class MessageKit(BoltOnConditions):
     _CORE_CLASS = CoreMessageKit
@@ -54,3 +70,7 @@ class MessageKit(BoltOnConditions):
 
 class ReencryptionRequest(BoltOnConditions):
     _CORE_CLASS = CoreReencryptionRequest
+
+    def __init__(self, context: Optional[Dict[str, Union[str, int]]] = None, *args, **kwargs):
+        self.context = context
+        super().__init__(*args, **kwargs)
