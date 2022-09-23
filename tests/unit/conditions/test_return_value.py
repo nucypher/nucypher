8b34178f93dba1c34c6aa7c3d7c503e4d1fe6759
David Núñez <david@nucypher.com>
2022-09-23 10:05:36 +0200

    Add more basic unit tests for ReturnValueTest

--- tests/unit/conditions/test_return_value.py
+++ tests/unit/conditions/test_return_value.py
@@ -4,10 +4,20 @@ import pytest
 from nucypher.policy.conditions.lingo import ReturnValueTest
 
 
-def test_return_result_test_simple():
+def test_return_value_test_integer():
     test = ReturnValueTest(comparator='>', value='0')
     assert test.eval('1')
     assert not test.eval('-1')
+
+    test = ReturnValueTest(comparator='>', value=0)
+    assert test.eval(1)
+    assert not test.eval(-1)
+
+
+def test_return_value_test_string():
+    test = ReturnValueTest(comparator='==', value='"foo"')
+    assert test.eval('"foo"')
+    assert not test.eval('"bar"')
 
 
 def test_return_value_sanitization():
