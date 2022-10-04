0d99fc3c06d702a7d1fdddde801600c6581179cf
derekpierre <derek.pierre@gmail.com>
2022-10-04 13:46:43 -0400

    More RFCs from #2980.
    Co-authored-by: David Núñez <david@nucypher.com>.
    Co-authored-by: Kieran Prasch <kieranprasch@gmail.com>

--- nucypher/policy/conditions/__init__.py
+++ nucypher/policy/conditions/__init__.py
@@ -1,7 +1,25 @@
-import json
+"""
+ This file is part of nucypher.
 
+ nucypher is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ nucypher is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import json
 from pathlib import Path
 
 STANDARD_ABIS_FILEPATH = Path(__file__).parent / 'abis.json'
 with open(STANDARD_ABIS_FILEPATH, 'r') as file:
     STANDARD_ABIS = json.loads(file.read())
+
+STANDARD_ABI_CONTRACT_TYPES = set(STANDARD_ABIS)
