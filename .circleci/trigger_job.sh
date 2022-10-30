8d8e022e09f7862f3b1eddf52fa65f3afa2d68d2
Kieran Prasch <kieranprasch@gmail.com>
2022-10-30 16:43:24 +0000

    Initial port of circleCI to GH actions

--- .circleci/trigger_job.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-curl --user ${CIRCLE_API_TOKEN}: \
-     --request POST \
-     --form build_parameters[CIRCLE_JOB]=${JOB_NAME} \
-     --form build_parameters[CIRCLE_REPOSITORY_URL]=${CIRCLE_GIT_URL} \
-     --form config=@config.yml \
-     --form notify=false \
-     https://circleci.com/api/v1.1/project/github/nucypher/nucypher/
