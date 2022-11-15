bdaaafd9c37c08a3aba110150eeec3967497d61a
James Campbell <james.campbell@tanti.org.uk>
2022-11-15 14:58:56 +0000

    Fix newsfragment and ensure conditions test has a valid method

--- tests/unit/conditions/test_condition_validity.py
+++ tests/unit/conditions/test_condition_validity.py
@@ -116,7 +116,7 @@ def test_invalid_contract_condition():
     with pytest.raises(InvalidCondition):
         _ = ContractCondition(
             contract_address="0xaDD9D957170dF6F33982001E4c22eCCdd5539118",
-            method="getPolicy",
+            method="balanceOf",
             chain=TESTERCHAIN_CHAIN_ID,
             standard_contract_type="ERC20",
             function_abi={"rando": "ABI"},
