592c300461d9d0ff20fae324a07013e48f7be3cf
Kieran Prasch <kieranprasch@gmail.com>
2022-11-17 13:37:54 +0000

    update static files tests for twisted 22.10.0

--- nucypher/network/resources.py
+++ nucypher/network/resources.py
@@ -3,8 +3,9 @@
 import os
 from hendrix.facilities.resources import MediaResource
 
+
 def get_static_resources():
     resources = []
     if os.getenv('NUCYPHER_STATIC_FILES_ROOT'):
-        resources.append(MediaResource(os.getenv('NUCYPHER_STATIC_FILES_ROOT'), namespace='statics'))
+        resources.append(MediaResource(os.getenv('NUCYPHER_STATIC_FILES_ROOT').encode(), namespace=b'statics'))
     return resources
