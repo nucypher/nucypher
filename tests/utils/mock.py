a66adf373731e84e48d4b82e9c694f8e422f4435
Kieran Prasch <kieranprasch@gmail.com>
2022-12-13 11:42:00 +0000

    Removes unused test utility

--- tests/utils/mock.py
+++
@@ -1,9 +0,0 @@
-from nucypher.blockchain.economics import EconomicsFactory
-from nucypher.blockchain.eth.agents import ContractAgency
-from tests.mock.agents import MockContractAgency
-
-# def _mock_contract_agency(mocker, application_economics):
-#     mocker.patch.object(EconomicsFactory, 'get_economics', return_value=application_economics)
-#     ContractAgency.get_agent = MockContractAgency.get_agent
-#     ContractAgency.get_agent_by_contract_name = MockContractAgency.get_agent_by_contract_name
-#     yield MockContractAgency()
