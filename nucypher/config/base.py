0d53e62dd21d8dac215490f62b7ecb2080b599cb
Kieran Prasch <kieranprasch@gmail.com>
2022-12-23 09:57:08 -0800

    bump config version 4 -> 5

--- nucypher/config/base.py
+++ nucypher/config/base.py
@@ -1,6 +1,3 @@
-
-
-
 import json
 import re
 from abc import ABC, abstractmethod
@@ -307,7 +304,7 @@ class CharacterConfiguration(BaseConfiguration):
     'Sideways Engagement' of Character classes; a reflection of input parameters.
     """
 
-    VERSION = 4  # bump when static payload scheme changes
+    VERSION = 5  # bump when static payload scheme changes
 
     CHARACTER_CLASS = NotImplemented
     MNEMONIC_KEYSTORE = False
