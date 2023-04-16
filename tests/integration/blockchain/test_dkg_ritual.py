3e23a0b10000e8414fa5fc0ec0142b22e5bbe15b
Kieran Prasch <kieranprasch@gmail.com>
2023-04-16 12:59:28 -0700

    public exposure of per-request ferveo variant, ferveo==0.1.7 and nucypher-core integration of ThresholdDecryptionRequest and Response

--- tests/integration/blockchain/test_dkg_ritual.py
+++ tests/integration/blockchain/test_dkg_ritual.py
@@ -1,102 +1,121 @@
 import pytest
 import pytest_twisted
+from time import time
 from twisted.internet.threads import deferToThread
+from typing import List
 from web3.datastructures import AttributeDict
 
-from nucypher.characters.lawful import Enrico
-
-# constants
-DKG_SIZE = 1
-RITUAL_ID = 0
+from nucypher.blockchain.eth.agents import CoordinatorAgent
+from nucypher.characters.lawful import Enrico, Ursula
+from tests.mock.coordinator import MockCoordinatorAgent
+from tests.mock.interfaces import MockBlockchain
 
 # The message to encrypt and its conditions
 PLAINTEXT = "peace at dawn"
 CONDITIONS = [{'returnValueTest': {'value': '0', 'comparator': '>'}, 'method': 'timelock'}]
 
+# TODO: GEt these from the contract
+ROUND_1_EVENT_NAME = 'StartTranscriptRound'
+ROUND_2_EVENT_NAME = 'StartAggregationRound'
+
+PARAMS = [  # dkg_size, ritual_id, variant
+
+    (1, 0, 'simple'),
+    (4, 1, 'simple'),
+    (8, 2, 'simple'),
+    # TODO: enable these tests
+    # (32, 3, 'simple'),
+
+    # TODO: enable these tests
+    (1, 3, 'precomputed'),
+    # (4, 5, 'precomputed'),
+    # (8, 6, 'precomputed'),
+    # (32, 7, 'precomputed'),
+
+]
+
+BLOCKS = list(reversed(range(1, 100)))
+COORDINATOR = MockCoordinatorAgent(MockBlockchain())
+
+
+@pytest.fixture(scope="function", autouse=True)
+def mock_coordinator_agent(testerchain, application_economics, mock_contract_agency):
+    mock_contract_agency._MockContractAgency__agents[CoordinatorAgent] = COORDINATOR
+    yield COORDINATOR
+
 
 @pytest.fixture(scope='function')
 def cohort(ursulas, mock_coordinator_agent):
     """Creates a cohort of Ursulas"""
-    nodes = list(sorted(ursulas[:DKG_SIZE], key=lambda x: int(x.checksum_address, 16)))
-    assert len(nodes) == DKG_SIZE  # sanity check
     for u in ursulas:
         u.coordinator_agent = mock_coordinator_agent
         u.ritual_tracker.coordinator_agent = mock_coordinator_agent
-    return nodes
+    return ursulas
 
 
+def execute_round(cohort: List[Ursula], ritual_id: int, event_name: str):
+
+    # check that the ritual is being tracked locally upon initialization for each node
+    for ursula in cohort:
+        # this is a testing hack to make the event scanner work
+        # normally it's called by the reactor clock in a loop
+        event = AttributeDict(dict(
+            timestamp=lambda: int(time()),
+            event=event_name,
+            blockNumber=BLOCKS.pop(),
+            args=AttributeDict({'ritualId': ritual_id})
+        ))
+
+        ursula.ritual_tracker._handle_ritual_event(event, get_block_when=lambda x: event)
+
+
+@pytest.mark.parametrize('dkg_size, ritual_id, variant', PARAMS)
 @pytest_twisted.inlineCallbacks()
-def test_ursula_ritualist(testerchain, mock_coordinator_agent, cohort, alice, bob):
+def test_ursula_ritualist(testerchain, mock_coordinator_agent, cohort, alice, bob, dkg_size, ritual_id, variant):
     """Tests the DKG and the encryption/decryption of a message"""
 
-    # Round 0 - Initiate the ritual
+    cohort = cohort[:dkg_size]
+
     def initialize():
         """Initiates the ritual"""
         print("==================== INITIALIZING ====================")
         cohort_staking_provider_addresses = list(u.checksum_address for u in cohort)
-        receipt = mock_coordinator_agent.initiate_ritual(
+        mock_coordinator_agent.initiate_ritual(
             nodes=cohort_staking_provider_addresses,
             transacting_power=alice.transacting_power
         )
-        return receipt
+        assert mock_coordinator_agent.number_of_rituals() == ritual_id + 1
 
-    # Round 0 - Initiate the ritual
-    def test_initialize(receipt):
+    def round_1(_):
         """Checks the initialization of the ritual"""
         print("==================== CHECKING INITIALIZATION ====================")
+        # verify that the ritual is in the correct state
+        assert mock_coordinator_agent.get_ritual_status(ritual_id=ritual_id) == \
+               mock_coordinator_agent.Ritual.Status.AWAITING_TRANSCRIPTS
+        execute_round(cohort, ritual_id, ROUND_1_EVENT_NAME)
 
-        # check that the ritual was created on-chain
-        assert mock_coordinator_agent.number_of_rituals() == RITUAL_ID + 1
-        assert mock_coordinator_agent.get_ritual_status(RITUAL_ID) == mock_coordinator_agent.Ritual.Status.AWAITING_TRANSCRIPTS
-
-        # check that the ritual is being tracked locally upon initialization for each node
-        for ursula in cohort:
-            # this is a testing hack to make the event scanner work
-            # normally it's called by the reactor clock in a loop
-            event = AttributeDict(dict(
-                timestamp=lambda: 123456789,
-                event='StartTranscriptRound',
-                blockNumber=0,
-                args=AttributeDict({'ritualId': RITUAL_ID})
-            ))
-
-            d = ursula.ritual_tracker._handle_ritual_event(event, get_block_when=lambda x: event)
-            # check that the ritual was created locally for this node
-            assert len(ursula.ritual_tracker.rituals) == RITUAL_ID + 1
-
-    def block_until_dkg_finalized(_):
+    def round_2(_):
         """simulates the passage of time and the execution of the event scanner"""
         print("==================== BLOCKING UNTIL DKG FINALIZED ====================")
-        for ursula in cohort:
-            # this is a testing hack to make the event scanner work
-            # normally it's called by the reactor clock in a loop
-            event = AttributeDict(dict(
-                timestamp=lambda: 123456789,
-                event='StartAggregationRound',
-                blockNumber=0,
-                args=AttributeDict({'ritualId': RITUAL_ID})
-            ))
-            ursula.ritual_tracker._handle_ritual_event(event, get_block_when=lambda x: event)
-            # check that the ritual was created locally for this node
-            assert len(ursula.ritual_tracker.rituals) == RITUAL_ID + 1
+        execute_round(cohort, ritual_id, ROUND_2_EVENT_NAME)
 
-    def test_finality(_):
+    def finality(_):
         """Checks the finality of the DKG"""
         print("==================== CHECKING DKG FINALITY ====================")
 
-        status = mock_coordinator_agent.get_ritual_status(RITUAL_ID)
+        status = mock_coordinator_agent.get_ritual_status(ritual_id)
         assert status == mock_coordinator_agent.Ritual.Status.FINALIZED
         for ursula in cohort:
-            assert ursula.dkg_storage.get_transcript(RITUAL_ID) is not None
+            assert ursula.dkg_storage.get_transcript(ritual_id) is not None
 
-    def test_encrypt(_):
+    def encrypt(_):
         """Encrypts a message and returns the ciphertext and conditions"""
         print("==================== DKG ENCRYPTION ====================")
 
         # side channel fake-out by using the datastore from the last node in the cohort
         # alternatively, we could use the coordinator datastore
         last_node = cohort[-1]
-        encrypting_key = last_node.dkg_storage.get_public_key(RITUAL_ID)
+        encrypting_key = last_node.dkg_storage.get_public_key(ritual_id)
 
         # prepare message and conditions
         plaintext = PLAINTEXT.encode()
@@ -107,16 +126,31 @@ def test_ursula_ritualist(testerchain, mock_coordinator_agent, cohort, alice, bo
         ciphertext = enrico.encrypt_for_dkg(plaintext=plaintext, conditions=CONDITIONS)
         return ciphertext
 
-    def test_decrypt(ciphertext):
+    def decrypt(ciphertext):
         """Decrypts a message and checks that it matches the original plaintext"""
         print("==================== DKG DECRYPTION ====================")
-        # ritual_id, ciphertext, conditions are obtained from the side channel
         bob.start_learning_loop(now=True)
+
+        # ritual_id, ciphertext, conditions, and params are obtained from the side channel
+        params = cohort[0].dkg_storage.get_dkg_params(ritual_id)
+
         cleartext = bob.threshold_decrypt(
-            ritual_id=RITUAL_ID,
+            ritual_id=ritual_id,
             ciphertext=ciphertext,
             conditions=CONDITIONS,
-            # params=cohort[0].dkg_storage.get_dkg_params(RITUAL_ID),
+            params=params,
+            timeout=0,
+            variant=variant
+        )
+        assert bytes(cleartext) == PLAINTEXT.encode()
+
+        # again, but without `params`
+        cleartext = bob.threshold_decrypt(
+            ritual_id=ritual_id,
+            ciphertext=ciphertext,
+            conditions=CONDITIONS,
+            timeout=0,
+            variant=variant
         )
         assert bytes(cleartext) == PLAINTEXT.encode()
         print("==================== DECRYPTION SUCCESSFUL ====================")
@@ -128,15 +162,14 @@ def test_ursula_ritualist(testerchain, mock_coordinator_agent, cohort, alice, bo
         raise e
 
     # order matters
-    callbacks = [
-        test_initialize,
-        block_until_dkg_finalized,
-        test_finality,
-        test_encrypt,
-        test_decrypt,
-    ]
-
     d = deferToThread(initialize)
+    callbacks = [
+        round_1,
+        round_2,
+        finality,
+        encrypt,
+        decrypt,
+    ]
     for callback in callbacks:
         d.addCallback(callback)
         d.addErrback(error_handler)
--- tests/mock/coordinator.py
+++ tests/mock/coordinator.py
@@ -4,40 +4,49 @@ from eth_typing import ChecksumAddress
 from eth_utils import keccak
 from ferveo_py import PublicKey
 from typing import List
+from web3.types import TxReceipt
 
 from nucypher.blockchain.eth.agents import CoordinatorAgent
 from nucypher.crypto.powers import TransactingPower
+from tests.mock.agents import MockContractAgent
 from tests.mock.interfaces import MockBlockchain
 
 
-class MockCoordinatorV1:
-
-    SIGNALS = {}
+class MockCoordinatorAgent(MockContractAgent):
 
     Participant = CoordinatorAgent.Ritual.Participant
     Ritual = CoordinatorAgent.Ritual
     RitualStatus = CoordinatorAgent.Ritual.Status
 
     PUBLIC_KEY_SIZE = 104
+    EVENTS = {}
 
-    class Signal(Enum):
+    class Events(Enum):
         START_TRANSCRIPT_ROUND = 0
         START_AGGREGATION_ROUND = 1
 
-    def __init__(self, testerchain: MockBlockchain):
-        self.blockchain = testerchain
+    def __init__(self, blockchain: MockBlockchain, max_dkg_size: int = 64, timeout: int = 600):
+        self.blockchain = blockchain
         self.rituals = {}
-        self.timeout = 600
+        self.timeout = timeout
+        self.max_dkg_size = max_dkg_size
+        # Note that the call to super() is not necessary here
 
-    def emit_signal(self, ritual_id: int, signal: Signal, **kwargs) -> None:
-        self.SIGNALS[(int(time.time_ns()), ritual_id)] = (signal, {**kwargs, 'ritual_id': ritual_id})
+    def emit_event(self, ritual_id: int, signal: Events, **kwargs) -> None:
+        self.EVENTS[(int(time.time_ns()), ritual_id)] = (signal, {**kwargs, 'ritual_id': ritual_id})
 
-    def number_of_rituals(self) -> int:
-        return len(self.rituals)
+    def reset(self, **kwargs) -> None:
+        # self.rituals = {}
+        # self.EVENTS = {}
+        pass
+
+    #
+    # Transactions
+    #
 
     def initiate_ritual(
         self, nodes: List[ChecksumAddress], transacting_power: TransactingPower
-    ) -> None:
+    ) -> TxReceipt:
         ritual = self.Ritual(
             id=len(self.rituals),
             init_timestamp=int(time.time_ns()),
@@ -46,29 +55,59 @@ class MockCoordinatorV1:
             initiator=transacting_power.account,
         )
         self.rituals[ritual.id] = ritual
-        self.emit_signal(
-            signal=self.Signal.START_TRANSCRIPT_ROUND, ritual_id=ritual.id, nodes=nodes
+        self.emit_event(
+            signal=self.Events.START_TRANSCRIPT_ROUND, ritual_id=ritual.id, nodes=nodes
         )
+        return self.blockchain.FAKE_RECEIPT
 
-    def post_transcript(self, ritual_id: int, node_index: int, transcript: bytes, transacting_power: TransactingPower) -> None:
+    def post_transcript(
+            self,
+            ritual_id: int,
+            node_index: int,
+            transcript: bytes,
+            transacting_power: TransactingPower
+    ) -> TxReceipt:
         ritual = self.rituals[ritual_id]
         ritual.participants[node_index].transcript = transcript
         ritual.total_transcripts += 1
-        if ritual.total_transcripts == ritual.dkg_size:
+        if ritual.total_transcripts >= ritual.dkg_size:
             ritual.status = self.RitualStatus.AWAITING_AGGREGATIONS
-            self.emit_signal(
-                signal=self.Signal.START_AGGREGATION_ROUND,
+            self.emit_event(
+                signal=self.Events.START_AGGREGATION_ROUND,
                 ritual_id=ritual_id,
                 nodes=[p.node for p in ritual.participants],
             )
         return self.blockchain.FAKE_RECEIPT
 
-    def post_aggregation(self, ritual_id: int, node_index: int, aggregated_transcript: bytes, transacting_power: TransactingPower) -> None:
+    def post_aggregation(
+            self,
+            ritual_id: int,
+            node_index: int,
+            aggregated_transcript: bytes,
+            transacting_power: TransactingPower
+    ) -> TxReceipt:
         ritual = self.rituals[ritual_id]
         ritual.participants[node_index].aggregated_transcript = aggregated_transcript
         ritual.participants[node_index].aggregated_transcript_hash = keccak(aggregated_transcript)
         ritual.total_aggregations += 1
         return self.blockchain.FAKE_RECEIPT
+
+    def post_public_key(
+            self,
+            ritual_id: int,
+            public_key: PublicKey,
+            transacting_power: TransactingPower
+    ) -> TxReceipt:
+        ritual = self.rituals[ritual_id]
+        ritual.public_key = public_key
+        return self.blockchain.FAKE_RECEIPT
+
+    #
+    # Calls
+    #
+
+    def number_of_rituals(self) -> int:
+        return len(self.rituals)
 
     def get_ritual(self, ritual_id: int, with_participants: bool = False) -> CoordinatorAgent.Ritual:
         return self.rituals[ritual_id]
@@ -79,23 +118,18 @@ class MockCoordinatorV1:
     def get_node_index(self, ritual_id: int, node: ChecksumAddress) -> int:
         return self.get_participants(ritual_id).index(node)
 
-    def post_public_key(self, ritual_id: int, public_key: PublicKey, transacting_power: TransactingPower) -> None:
-        ritual = self.rituals[ritual_id]
-        ritual.public_key = bytes(public_key)
-        return self.blockchain.FAKE_RECEIPT
-
     def get_ritual_status(self, ritual_id: int) -> int:
         ritual = self.rituals[ritual_id]
         timestamp = int(ritual.init_timestamp)
         deadline = timestamp + self.timeout
         if timestamp == 0:
-            return self.RitualStatus.NOT_INITIATED
-        elif ritual.public_key is not None and len(ritual.public_key) == self.PUBLIC_KEY_SIZE:
+            return self.RitualStatus.NON_INITIATED
+        elif ritual.public_key:
             return self.RitualStatus.FINALIZED
         elif ritual.total_aggregations == ritual.dkg_size:
             return self.RitualStatus.FINALIZED
         elif ritual.aggregation_mismatch:
-            return self.RitualStatus.AGGREGATION_MISMATCH
+            return self.RitualStatus.INVALID
         elif timestamp > deadline:
             return self.RitualStatus.TIMEOUT
         elif ritual.total_transcripts < ritual.dkg_size:
@@ -104,7 +138,3 @@ class MockCoordinatorV1:
             return self.RitualStatus.AWAITING_AGGREGATIONS
         else:
             raise RuntimeError(f"Ritual {ritual_id} is in an unknown state")  # :-(
-
-    def reset(self) -> None:
-        self.rituals = {}
-        self.SIGNALS = {}
