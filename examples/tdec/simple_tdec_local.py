c0991e0b62583c42ca37b00b30719e94298d3523
jMyles <justin@justinholmes.com>
2023-06-21 13:46:49 +0200

    Casting to bytes for comparison.

--- examples/tdec/simple_tdec_local.py
+++ examples/tdec/simple_tdec_local.py
@@ -17,7 +17,7 @@ ciphertext, tdr = enrico.encrypt_for_dkg_and_produce_decryption_request(
     ritual_id=ANYTHING_CAN_BE_PASSED_AS_RITUAL_DATA,
 )
 print(f"After encryption: {enrico.policy_pubkey}")
-decrypted_cleartext_from_ciphertext = bob.threshold_decrypt(
+decrypted_cleartext_from_ciphertext_list = bob.threshold_decrypt(
     ciphertext=ciphertext,
     ritual_id=ANYTHING_CAN_BE_PASSED_AS_RITUAL_DATA,
     conditions=[ten_oclock_florida_time],
@@ -29,6 +29,8 @@ decrypted_cleartext_from_ciphertext = bob.threshold_decrypt(
 #                                                                                            participant_public_keys=THESE_CAN_BE_FAKE_FOR_THE_PURPOSES_OF_THIS_DEMO,
 #                                                                                            cohort=THESE_CAN_BE_FAKE_FOR_THE_PURPOSES_OF_THIS_DEMO,
 #                                                                                            threshold=ANYTHING_CAN_BE_PASSED_AS_RITUAL_DATA)
+
+decrypted_cleartext_from_ciphertext = bytes(decrypted_cleartext_from_ciphertext_list)
 
 assert decrypted_cleartext_from_ciphertext == plaintext
 assert plaintext == decrypted_cleartext_from_ciphertext
