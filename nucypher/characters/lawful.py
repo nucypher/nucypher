nucypher/nucypher

commit a4de8a882edffdad33e5168251606255ae498c93

Make the use of string literal type hints compatible with PEP 484
Because:
- Not doing so causes F821 linting errors

https://peps.python.org/pep-0484/#forward-references
https://www.flake8rules.com/rules/F821.html

nucypher/characters/lawful.py
@@ -68,7 +68,7 @@ from web3.types import TxReceipt
 import nucypher
 from nucypher.acumen.nicknames import Nickname
 from nucypher.acumen.perception import ArchivedFleetState, RemoteUrsulaStatus
-from nucypher.blockchain.eth.actors import Operator, PolicyAuthor, Ritualist
+from nucypher.blockchain.eth import actors
 from nucypher.blockchain.eth.agents import (
     ContractAgency,
     CoordinatorAgent,
@@ -100,6 +100,7 @@ from nucypher.crypto.powers import (
     TLSHostingPower,
     TransactingPower,
 )
+from nucypher.network import trackers
 from nucypher.network.decryption import ThresholdDecryptionClient
 from nucypher.network.exceptions import NodeSeemsToBeDown
 from nucypher.network.middleware import RestMiddleware
@@ -107,7 +108,6 @@ from nucypher.network.nodes import TEACHER_NODES, NodeSprout, Teacher
 from nucypher.network.protocols import parse_node_uri
 from nucypher.network.retrieval import PRERetrievalClient
 from nucypher.network.server import ProxyRESTServer, make_rest_app
-from nucypher.network.trackers import AvailabilityTracker
 from nucypher.policy.conditions.types import Lingo
 from nucypher.policy.conditions.utils import validate_condition_lingo
 from nucypher.policy.kits import PolicyMessageKit
@@ -116,9 +116,10 @@ from nucypher.policy.policies import Policy
 from nucypher.utilities.emitters import StdoutEmitter
 from nucypher.utilities.logging import Logger
 from nucypher.utilities.networking import validate_operator_ip
+from nucypher.utilities.prometheus.metrics import PrometheusMetricsConfig
 
 
-class Alice(Character, PolicyAuthor):
+class Alice(Character, actors.PolicyAuthor):
     banner = ALICE_BANNER
     _default_crypto_powerups = [SigningPower, DecryptingPower, DelegatingPower]
 
@@ -183,7 +184,7 @@ class Alice(Character, PolicyAuthor):
                 account=checksum_address, signer=signer
             )
             self._crypto_power.consume_power_up(self.transacting_power)
-            PolicyAuthor.__init__(
+            actors.PolicyAuthor.__init__(
                 self,
                 domain=self.domain,
                 transacting_power=self.transacting_power,
@@ -369,7 +370,7 @@ class Alice(Character, PolicyAuthor):
 
     def revoke(
         self, policy: Policy, onchain: bool = True, offchain: bool = True
-    ) -> Tuple[TxReceipt, Dict[ChecksumAddress, Tuple["Revocation", Exception]]]:
+    ) -> Tuple[TxReceipt, Dict[ChecksumAddress, Tuple["actors.Revocation", Exception]]]:
         if not (offchain or onchain):
             raise ValueError("offchain or onchain must be True to issue revocation")
 
@@ -798,7 +799,7 @@ class Bob(Character):
         return cleartext
 
 
-class Ursula(Teacher, Character, Operator, Ritualist):
+class Ursula(Teacher, Character, actors.Operator, actors.Ritualist):
     banner = URSULA_BANNER
     _alice_class = Alice
 
@@ -864,11 +865,11 @@ class Ursula(Teacher, Character, Operator, Ritualist):
 
             # Health Checks
             self._availability_check = availability_check
-            self._availability_tracker = AvailabilityTracker(ursula=self)
+            self._availability_tracker = trackers.AvailabilityTracker(ursula=self)
 
             try:
                 payment_method: ContractPayment
-                Operator.__init__(
+                actors.Operator.__init__(
                     self,
                     is_me=is_me,
                     domain=self.domain,
@@ -882,7 +883,7 @@ class Ursula(Teacher, Character, Operator, Ritualist):
                 )
 
                 # DKG Ritualist
-                Ritualist.__init__(
+                actors.Ritualist.__init__(
                     self,
                     domain=domain,
                     provider_uri=payment_method.provider,
@@ -1010,7 +1011,7 @@ class Ursula(Teacher, Character, Operator, Ritualist):
         ritualist: bool = True,
         hendrix: bool = True,
         start_reactor: bool = True,
-        prometheus_config: "PrometheusMetricsConfig" = None,
+        prometheus_config: PrometheusMetricsConfig = None,
         preflight: bool = True,
         block_until_ready: bool = True,
         eager: bool = False,

nucypher/acumen/perception.py
@@ -11,6 +11,7 @@ import maya
 from eth_typing import ChecksumAddress
 from nucypher_core import FleetStateChecksum, NodeMetadata
 
+from nucypher import characters
 from nucypher.utilities.logging import Logger
 
 from .nicknames import Nickname
@@ -41,7 +42,7 @@ class StateDiff(NamedTuple):
 
 class FleetState:
     """
-    Fleet state as perceived by a local Ursula.
+    Fleet state as perceived by a local "lawful.Ursula".
 
     Assumptions we're based on:
 
@@ -57,7 +58,9 @@ class FleetState:
     """
 
     @classmethod
-    def new(cls, this_node: Optional['Ursula'] = None) -> 'FleetState':
+    def new(
+        cls, this_node: Optional["characters.lawful.Ursula"] = None
+    ) -> "FleetState":
         this_node_ref = weakref.ref(this_node) if this_node else None
         # `this_node` might not have its metadata available yet.
         this_node_metadata = None
@@ -66,12 +69,16 @@ class FleetState:
                    this_node_ref=this_node_ref,
                    this_node_metadata=this_node_metadata)
 
-    def __init__(self,
-                 nodes: Dict[ChecksumAddress, 'Ursula'],
-                 this_node_ref: Optional[weakref.ReferenceType],
-                 this_node_metadata: Optional[NodeMetadata]):
-        self.checksum = FleetStateChecksum(this_node=this_node_metadata,
-                                           other_nodes=[node.metadata() for node in nodes.values()])
+    def __init__(
+        self,
+        nodes: Dict[ChecksumAddress, "characters.lawful.Ursula"],
+        this_node_ref: Optional[weakref.ReferenceType],
+        this_node_metadata: Optional[NodeMetadata],
+    ):
+        self.checksum = FleetStateChecksum(
+            this_node=this_node_metadata,
+            other_nodes=[node.metadata() for node in nodes.values()],
+        )
         self.nickname = Nickname.from_seed(bytes(self.checksum), length=1)
         self._nodes = nodes
         self.timestamp = maya.now()
@@ -84,12 +91,12 @@ class FleetState:
                                   timestamp=self.timestamp,
                                   population=self.population)
 
-    def _calculate_diff(self,
-                        this_node_updated: bool,
-                        nodes_to_add: Iterable['Ursula'],
-                        nodes_to_remove: Iterable[ChecksumAddress]
-                        ) -> StateDiff:
-
+    def _calculate_diff(
+        self,
+        this_node_updated: bool,
+        nodes_to_add: Iterable["characters.lawful.Ursula"],
+        nodes_to_remove: Iterable[ChecksumAddress],
+    ) -> StateDiff:
         nodes_updated = []
         for node in nodes_to_add:
             if node.checksum_address in nodes_to_remove:
@@ -107,12 +114,12 @@ class FleetState:
                          nodes_updated=nodes_updated,
                          nodes_removed=nodes_removed)
 
-    def with_updated_nodes(self,
-                           nodes_to_add: Iterable['Ursula'],
-                           nodes_to_remove: Iterable[ChecksumAddress],
-                           skip_this_node: bool = False,
-                           ) -> 'FleetState':
-
+    def with_updated_nodes(
+        self,
+        nodes_to_add: Iterable["characters.lawful.Ursula"],
+        nodes_to_remove: Iterable[ChecksumAddress],
+        skip_this_node: bool = False,
+    ) -> "FleetState":
         if self._this_node_ref is not None and not skip_this_node:
             this_node = self._this_node_ref()
             this_node_metadata = this_node.metadata()
@@ -169,7 +176,7 @@ class FleetState:
     def __len__(self):
         return len(self._nodes)
 
-    def shuffled(self) -> List['Ursula']:
+    def shuffled(self) -> List["characters.lawful.Ursula"]:
         nodes_we_know_about = list(self._nodes.values())
         random.shuffle(nodes_we_know_about)
         return nodes_we_know_about
@@ -206,8 +213,9 @@ class FleetSensor:
     """
     log = Logger("Learning")
 
-    def __init__(self, domain: str, this_node: Optional['Ursula'] = None):
-
+    def __init__(
+        self, domain: str, this_node: Optional["characters.lawful.Ursula"] = None
+    ):
         self._domain = domain
 
         self._current_state = FleetState.new(this_node)
@@ -221,7 +229,7 @@ class FleetSensor:
 
         self._auto_update_state = False
 
-    def record_node(self, node: 'Ursula'):
+    def record_node(self, node: "characters.lawful.Ursula"):
 
         if node.domain == self._domain:
             # Replace the existing object with a newer object, even if they're equal
@@ -330,7 +338,7 @@ class FleetSensor:
     def shuffled(self):
         return self._current_state.shuffled()
 
-    def mark_as(self, label: Exception, node: 'Ursula'):
+    def mark_as(self, label: Exception, node: "characters.lawful.Ursula"):
         # TODO: for now we're not using `label` in any way, so we're just ignoring it
         self._nodes_to_remove.add(node.checksum_address)
 
@@ -352,8 +360,10 @@ class FleetSensor:
         self._remote_last_seen[checksum_address] = maya.now()
         self._remote_states[checksum_address] = state
 
-    def status_info(self, checksum_address_or_node: Union[ChecksumAddress, 'Ursula']) -> 'RemoteUrsulaStatus':
-
+    def status_info(
+        self,
+        checksum_address_or_node: Union[ChecksumAddress, "characters.lawful.Ursula"],
+    ) -> "RemoteUrsulaStatus":
         if isinstance(checksum_address_or_node, str):
             node = self[checksum_address_or_node]
         else:

nucypher/blockchain/eth/actors.py
@@ -39,7 +39,7 @@ from nucypher.blockchain.eth.registry import (
 )
 from nucypher.blockchain.eth.signers import Signer
 from nucypher.blockchain.eth.token import NU
-from nucypher.blockchain.eth.trackers.dkg import ActiveRitualTracker
+from nucypher.blockchain.eth.trackers import dkg
 from nucypher.blockchain.eth.trackers.pre import WorkTracker
 from nucypher.crypto.ferveo.dkg import DecryptionShareSimple, FerveoVariant, Transcript
 from nucypher.crypto.powers import (
@@ -319,7 +319,7 @@ class Ritualist(BaseActor):
         )
 
         # track active onchain rituals
-        self.ritual_tracker = ActiveRitualTracker(
+        self.ritual_tracker = dkg.ActiveRitualTracker(
             ritualist=self,
         )
 

nucypher/blockchain/eth/agents.py
@@ -15,6 +15,8 @@ from nucypher_core.ferveo import AggregatedTranscript, DkgPublicKey, Transcript
 from web3.contract.contract import Contract, ContractFunction
 from web3.types import Timestamp, TxParams, TxReceipt, Wei
 
+from nucypher import types
+from nucypher.blockchain.eth import events
 from nucypher.blockchain.eth.constants import (
     ADJUDICATOR_CONTRACT_NAME,
     DISPATCHER_CONTRACT_NAME,
@@ -25,7 +27,6 @@ from nucypher.blockchain.eth.constants import (
     SUBSCRIPTION_MANAGER_CONTRACT_NAME,
 )
 from nucypher.blockchain.eth.decorators import contract_api
-from nucypher.blockchain.eth.events import ContractEvents
 from nucypher.blockchain.eth.interfaces import BlockchainInterfaceFactory
 from nucypher.blockchain.eth.registry import BaseContractRegistry
 from nucypher.config.constants import (
@@ -34,7 +35,6 @@ from nucypher.config.constants import (
 )
 from nucypher.crypto.powers import TransactingPower
 from nucypher.crypto.utils import sha256_digest
-from nucypher.types import Agent, NuNits, StakingProviderInfo, TuNits
 from nucypher.utilities.logging import Logger  # type: ignore
 
 
@@ -87,7 +87,7 @@ class EthereumContractAgent:
             )
 
         self.__contract = contract
-        self.events = ContractEvents(contract)
+        self.events = events.ContractEvents(contract)
         if not transaction_gas:
             transaction_gas = EthereumContractAgent.DEFAULT_TRANSACTION_GAS_LIMITS['default']
         self.transaction_gas = transaction_gas
@@ -129,23 +129,26 @@ class NucypherTokenAgent(EthereumContractAgent):
     contract_name: str = NUCYPHER_TOKEN_CONTRACT_NAME
 
     @contract_api(CONTRACT_CALL)
-    def get_balance(self, address: ChecksumAddress) -> NuNits:
+    def get_balance(self, address: ChecksumAddress) -> types.NuNits:
         """Get the NU balance (in NuNits) of a token holder address, or of this contract address"""
         balance: int = self.contract.functions.balanceOf(address).call()
-        return NuNits(balance)
+        return types.NuNits(balance)
 
     @contract_api(CONTRACT_CALL)
-    def get_allowance(self, owner: ChecksumAddress, spender: ChecksumAddress) -> NuNits:
+    def get_allowance(
+        self, owner: ChecksumAddress, spender: ChecksumAddress
+    ) -> types.NuNits:
         """Check the amount of tokens that an owner allowed to a spender"""
         allowance: int = self.contract.functions.allowance(owner, spender).call()
-        return NuNits(allowance)
+        return types.NuNits(allowance)
 
     @contract_api(TRANSACTION)
-    def increase_allowance(self,
-                           transacting_power: TransactingPower,
-                           spender_address: ChecksumAddress,
-                           increase: NuNits
-                           ) -> TxReceipt:
+    def increase_allowance(
+        self,
+        transacting_power: TransactingPower,
+        spender_address: ChecksumAddress,
+        increase: types.NuNits,
+    ) -> TxReceipt:
         """Increase the allowance of a spender address funded by a sender address"""
         contract_function: ContractFunction = self.contract.functions.increaseAllowance(spender_address, increase)
         receipt: TxReceipt = self.blockchain.send_transaction(contract_function=contract_function,
@@ -153,11 +156,12 @@ class NucypherTokenAgent(EthereumContractAgent):
         return receipt
 
     @contract_api(TRANSACTION)
-    def decrease_allowance(self,
-                           transacting_power: TransactingPower,
-                           spender_address: ChecksumAddress,
-                           decrease: NuNits
-                           ) -> TxReceipt:
+    def decrease_allowance(
+        self,
+        transacting_power: TransactingPower,
+        spender_address: ChecksumAddress,
+        decrease: types.NuNits,
+    ) -> TxReceipt:
         """Decrease the allowance of a spender address funded by a sender address"""
         contract_function: ContractFunction = self.contract.functions.decreaseAllowance(spender_address, decrease)
         receipt: TxReceipt = self.blockchain.send_transaction(contract_function=contract_function,
@@ -165,11 +169,12 @@ class NucypherTokenAgent(EthereumContractAgent):
         return receipt
 
     @contract_api(TRANSACTION)
-    def approve_transfer(self,
-                         amount: NuNits,
-                         spender_address: ChecksumAddress,
-                         transacting_power: TransactingPower
-                         ) -> TxReceipt:
+    def approve_transfer(
+        self,
+        amount: types.NuNits,
+        spender_address: ChecksumAddress,
+        transacting_power: TransactingPower,
+    ) -> TxReceipt:
         """Approve the spender address to transfer an amount of tokens on behalf of the sender address"""
         self._validate_zero_allowance(amount, spender_address, transacting_power)
 
@@ -181,7 +186,12 @@ class NucypherTokenAgent(EthereumContractAgent):
         return receipt
 
     @contract_api(TRANSACTION)
-    def transfer(self, amount: NuNits, target_address: ChecksumAddress, transacting_power: TransactingPower) -> TxReceipt:
+    def transfer(
+        self,
+        amount: types.NuNits,
+        target_address: ChecksumAddress,
+        transacting_power: TransactingPower,
+    ) -> TxReceipt:
         """Transfer an amount of tokens from the sender address to the target address."""
         contract_function: ContractFunction = self.contract.functions.transfer(target_address, amount)
         receipt: TxReceipt = self.blockchain.send_transaction(contract_function=contract_function,
@@ -189,13 +199,14 @@ class NucypherTokenAgent(EthereumContractAgent):
         return receipt
 
     @contract_api(TRANSACTION)
-    def approve_and_call(self,
-                         amount: NuNits,
-                         target_address: ChecksumAddress,
-                         transacting_power: TransactingPower,
-                         call_data: bytes = b'',
-                         gas_limit: Optional[Wei] = None
-                         ) -> TxReceipt:
+    def approve_and_call(
+        self,
+        amount: types.NuNits,
+        target_address: ChecksumAddress,
+        transacting_power: TransactingPower,
+        call_data: bytes = b"",
+        gas_limit: Optional[Wei] = None,
+    ) -> TxReceipt:
         self._validate_zero_allowance(amount, target_address, transacting_power)
 
         payload = None
@@ -402,10 +413,12 @@ class PREApplicationAgent(EthereumContractAgent):
         return result
 
     @contract_api(CONTRACT_CALL)
-    def get_staking_provider_info(self, staking_provider: ChecksumAddress) -> StakingProviderInfo:
+    def get_staking_provider_info(
+        self, staking_provider: ChecksumAddress
+    ) -> types.StakingProviderInfo:
         # remove reserved fields
         info: list = self.contract.functions.stakingProviderInfo(staking_provider).call()
-        return StakingProviderInfo(*info[0:3])
+        return types.StakingProviderInfo(*info[0:3])
 
     @contract_api(CONTRACT_CALL)
     def get_authorized_stake(self, staking_provider: ChecksumAddress) -> int:
@@ -441,8 +454,9 @@ class PREApplicationAgent(EthereumContractAgent):
             yield address
 
     @contract_api(CONTRACT_CALL)
-    def get_all_active_staking_providers(self, pagination_size: Optional[int] = None) -> Tuple[TuNits, Dict[ChecksumAddress, TuNits]]:
-
+    def get_all_active_staking_providers(
+        self, pagination_size: Optional[int] = None
+    ) -> Tuple[types.TuNits, Dict[ChecksumAddress, types.TuNits]]:
         if pagination_size is None:
             pagination_size = self.DEFAULT_PROVIDERS_PAGINATION_SIZE_LIGHT_NODE if self.blockchain.is_light else self.DEFAULT_PROVIDERS_PAGINATION_SIZE
             self.log.debug(f"Defaulting to pagination size {pagination_size}")
@@ -488,10 +502,12 @@ class PREApplicationAgent(EthereumContractAgent):
         def checksum_address(address: int) -> ChecksumAddress:
             return ChecksumAddress(to_checksum_address(address.to_bytes(ETH_ADDRESS_BYTE_LENGTH, 'big')))
 
-        typed_staking_providers = {checksum_address(address): TuNits(authorized_tokens)
-                                   for address, authorized_tokens in staking_providers.items()}
+        typed_staking_providers = {
+            checksum_address(address): types.TuNits(authorized_tokens)
+            for address, authorized_tokens in staking_providers.items()
+        }
 
-        return TuNits(n_tokens), typed_staking_providers
+        return types.TuNits(n_tokens), typed_staking_providers
 
     def get_staking_provider_reservoir(self,
                                        without: Iterable[ChecksumAddress] = None,
@@ -760,11 +776,11 @@ class ContractAgency:
     @classmethod
     def get_agent(
         cls,
-        agent_class: Type[Agent],
+        agent_class: Type[types.Agent],
         registry: Optional[BaseContractRegistry],
         provider_uri: Optional[str],
         contract_version: Optional[str] = None,
-    ) -> Agent:
+    ) -> types.Agent:
         if not issubclass(agent_class, EthereumContractAgent):
             raise TypeError("Only agent subclasses can be used from the agency.")
 
@@ -780,10 +796,10 @@ class ContractAgency:
         registry_id = registry.id
 
         try:
-            return cast(Agent, cls.__agents[registry_id][agent_class])
+            return cast(types.Agent, cls.__agents[registry_id][agent_class])
         except KeyError:
             agent = cast(
-                Agent,
+                types.Agent,
                 agent_class(
                     registry=registry,
                     provider_uri=provider_uri,

nucypher/blockchain/eth/events.py
@@ -3,6 +3,7 @@ import os
 
 from web3.contract.contract import Contract
 
+from nucypher.blockchain.eth import agents
 from nucypher.blockchain.eth.interfaces import BlockchainInterfaceFactory
 from nucypher.config.constants import NUCYPHER_EVENTS_THROTTLE_MAX_BLOCKS
 
@@ -71,13 +72,15 @@ class ContractEventsThrottler:
     # default to 1000 - smallest default heard about so far (alchemy)
     DEFAULT_MAX_BLOCKS_PER_CALL = int(os.environ.get(NUCYPHER_EVENTS_THROTTLE_MAX_BLOCKS, 1000))
 
-    def __init__(self,
-                 agent: 'EthereumContractAgent',
-                 event_name: str,
-                 from_block: int,
-                 to_block: int = None,  # defaults to latest block
-                 max_blocks_per_call: int = DEFAULT_MAX_BLOCKS_PER_CALL,
-                 **argument_filters):
+    def __init__(
+        self,
+        agent: "agents.EthereumContractAgent",
+        event_name: str,
+        from_block: int,
+        to_block: int = None,  # defaults to latest block
+        max_blocks_per_call: int = DEFAULT_MAX_BLOCKS_PER_CALL,
+        **argument_filters,
+    ):
         self.event_filter = agent.events[event_name]
         self.from_block = from_block
         self.to_block = to_block if to_block is not None else agent.blockchain.client.block_number

nucypher/blockchain/eth/trackers/dkg.py
@@ -6,6 +6,7 @@ from twisted.internet import threads
 from web3.contract.contract import ContractEvent
 from web3.datastructures import AttributeDict
 
+from nucypher.blockchain.eth.actors import Ritualist
 from nucypher.policy.conditions.utils import camel_case_to_snake
 from nucypher.utilities.events import EventScanner, JSONifiedState
 from nucypher.utilities.logging import Logger
@@ -66,7 +67,7 @@ class ActiveRitualTracker:
 
     def __init__(
         self,
-        ritualist: "Ritualist",
+        ritualist: Ritualist,
         persistent: bool = False,  # TODO: use persistent storage?
     ):
         self.log = Logger("RitualTracker")

nucypher/network/middleware.py
@@ -12,8 +12,9 @@ from cryptography.hazmat.backends import default_backend
 from nucypher_core import FleetStateChecksum, MetadataRequest, NodeMetadata
 from requests.exceptions import SSLError
 
+from nucypher import characters
 from nucypher.blockchain.eth.registry import BaseContractRegistry
-from nucypher.config.storages import ForgetfulNodeStorage
+from nucypher.config.storages import ForgetfulNodeStorage, NodeStorage
 from nucypher.network.exceptions import NodeSeemsToBeDown
 from nucypher.utilities.logging import Logger
 
@@ -29,7 +30,7 @@ class NucypherMiddlewareClient:
         self,
         eth_provider_uri: Optional[str],
         registry: Optional["BaseContractRegistry"] = None,
-        storage: Optional["NodeStorage"] = None,
+        storage: Optional[NodeStorage] = None,
         *args,
         **kwargs,
     ):
@@ -260,7 +261,12 @@ class RestMiddleware:
         )
         return response
 
-    def reencrypt(self, ursula: "Ursula", reencryption_request_bytes: bytes, timeout=8):
+    def reencrypt(
+        self,
+        ursula: "characters.lawful.Ursula",
+        reencryption_request_bytes: bytes,
+        timeout=8,
+    ):
         response = self.client.post(
             node_or_sprout=ursula,
             path="reencrypt",
@@ -270,7 +276,10 @@ class RestMiddleware:
         return response
 
     def get_encrypted_decryption_share(
-        self, ursula: "Ursula", decryption_request_bytes: bytes, timeout=8
+        self,
+        ursula: "characters.lawful.Ursula",
+        decryption_request_bytes: bytes,
+        timeout=8,
     ):
         response = self.client.post(
             node_or_sprout=ursula,

nucypher/network/nodes.py
@@ -23,6 +23,7 @@ from requests.exceptions import SSLError
 from twisted.internet import reactor, task
 from twisted.internet.defer import Deferred
 
+from nucypher import characters
 from nucypher.acumen.nicknames import Nickname
 from nucypher.acumen.perception import FleetSensor
 from nucypher.blockchain.eth.agents import ContractAgency, PREApplicationAgent
@@ -150,7 +151,6 @@ class NodeSprout:
         return self._metadata_payload.domain
 
     def finish(self):
-        from nucypher.characters.lawful import Ursula
 
         # Remote node cryptographic material
         crypto_power = CryptoPower()
@@ -167,16 +167,18 @@ class NodeSprout:
             self._metadata_payload.certificate_der, backend=default_backend()
         )
 
-        return Ursula(is_me=False,
-                      crypto_power=crypto_power,
-                      rest_host=self._metadata_payload.host,
-                      rest_port=self._metadata_payload.port,
-                      checksum_address=self.checksum_address,
-                      domain=self._metadata_payload.domain,
-                      timestamp=self.timestamp,
-                      operator_signature_from_metadata=self.operator_signature_from_metadata,
-                      certificate=tls_certificate,
-                      metadata=self._metadata)
+        return characters.lawful.Ursula(
+            is_me=False,
+            crypto_power=crypto_power,
+            rest_host=self._metadata_payload.host,
+            rest_port=self._metadata_payload.port,
+            checksum_address=self.checksum_address,
+            domain=self._metadata_payload.domain,
+            timestamp=self.timestamp,
+            operator_signature_from_metadata=self.operator_signature_from_metadata,
+            certificate=tls_certificate,
+            metadata=self._metadata,
+        )
 
     def mature(self):
         if self._is_finishing:
@@ -293,8 +295,7 @@ class Learner:
         if save_metadata and node_storage is NO_STORAGE_AVAILABLE:
             raise ValueError("Cannot save nodes without a configured node storage")
 
-        from nucypher.characters.lawful import Ursula
-        self.node_class = node_class or Ursula
+        self.node_class = node_class or characters.lawful.Ursula
         self.node_class.set_cert_storage_function(
             node_storage.store_node_certificate)  # TODO: Fix this temporary workaround for on-disk cert storage.  #1481
 
@@ -745,13 +746,15 @@ class Learner:
     def write_node_metadata(self, node, serializer=bytes) -> str:
         return self.node_storage.store_node_metadata(node=node)
 
-    def verify_from(self,
-                    stranger: 'Character',
-                    message: bytes,
-                    signature: Signature
-                    ):
-
-        if not signature.verify(verifying_pk=stranger.stamp.as_umbral_pubkey(), message=message):
+    def verify_from(
+        self,
+        stranger: "characters.base.Character",
+        message: bytes,
+        signature: Signature,
+    ):
+        if not signature.verify(
+            verifying_pk=stranger.stamp.as_umbral_pubkey(), message=message
+        ):
             try:
                 node_on_the_other_end = self.node_class.from_seednode_metadata(
                     stranger.seed_node_metadata(),

commit 545fe3b03f2c071066293698582985fb00fc98c4

Fix Prometheus import error when running Porter
Commit a4de8a88 introduced amazing changes and solved linting errors
like F821. To achive it, this change introduced the import of
PrometheusMetricsConfig in lawful.py, which is dependant of
prometheus-client package.

Ursula imports prometheus-client, so no problem with this. But Porter
does not. So a import error is raised when Porter is run.

Also, a remaining TODO comment has been cleaned. This TODO is part of a
larger comment and was introduced in commit 424e373a and solved
afterwards.

nucypher/characters/lawful.py
@@ -4,6 +4,7 @@ import time
 from pathlib import Path
 from queue import Queue
 from typing import (
+    TYPE_CHECKING,
     Any,
     Dict,
     Iterable,
@@ -116,7 +117,9 @@ from nucypher.policy.policies import Policy
 from nucypher.utilities.emitters import StdoutEmitter
 from nucypher.utilities.logging import Logger
 from nucypher.utilities.networking import validate_operator_ip
-from nucypher.utilities.prometheus.metrics import PrometheusMetricsConfig
+
+if TYPE_CHECKING:
+    from nucypher.utilities.prometheus.metrics import PrometheusMetricsConfig
 
 
 class Alice(Character, actors.PolicyAuthor):
@@ -990,13 +993,13 @@ class Ursula(Teacher, Character, actors.Operator, actors.Ritualist):
     def run(
         self,
         emitter: StdoutEmitter = None,
-        discovery: bool = True,  # TODO: see below
+        discovery: bool = True,
         availability: bool = False,
         worker: bool = True,
         ritualist: bool = True,
         hendrix: bool = True,
         start_reactor: bool = True,
-        prometheus_config: PrometheusMetricsConfig = None,
+        prometheus_config: "PrometheusMetricsConfig" = None,
         preflight: bool = True,
         block_until_ready: bool = True,
         eager: bool = False,