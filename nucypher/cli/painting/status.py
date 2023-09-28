875bc34f6ef25bcb0300f78148e9b4e610fd7fff
derekpierre <derek.pierre@gmail.com>
2023-09-28 09:13:02 -0400

    Update information printed by application status helper.

--- nucypher/cli/painting/status.py
+++ nucypher/cli/painting/status.py
@@ -1,5 +1,3 @@
-from web3.main import Web3
-
 from nucypher.blockchain.eth.agents import (
     ContractAgency,
     TACoApplicationAgent,
@@ -13,27 +11,17 @@ def paint_application_contract_status(emitter, registry, provider_uri):
     blockchain = application_agent.blockchain
 
     contracts = f"""
-| Contract Deployment |
-{application_agent.contract_name} .............. {application_agent.contract_address}
-    """
-
-    blockchain = f"""
-| '{blockchain.client.chain_name}' Blockchain Network |
-Gas Price ................ {Web3.from_wei(blockchain.client.gas_price, 'gwei')} Gwei
-ETH Provider URI ......... {blockchain.eth_provider_uri}
-Registry ................. {registry.filepath}
+Contract Deployment
+===================
+Blockchain ........................ {blockchain.client.chain_name}
+TACoApplication Contract........... {application_agent.contract_address}
     """
 
     staking = f"""
-| TACoApplication |
+TACoApplication
+================
 Staking Provider Population ....... {application_agent.get_staking_providers_population()}
     """
 
-    sep = '-' * 45
-    emitter.echo(sep)
     emitter.echo(contracts)
-    emitter.echo(sep)
-    emitter.echo(blockchain)
-    emitter.echo(sep)
     emitter.echo(staking)
-    emitter.echo(sep)
