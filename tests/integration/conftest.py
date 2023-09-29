bfa4d0bc79c216e12a9a638d76eebd6413e29c31
derekpierre <derek.pierre@gmail.com>
2023-09-29 13:12:54 -0400

    Integration tests mock modifications for network resolution.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -18,7 +18,12 @@ from nucypher.blockchain.eth.interfaces import (
     BlockchainInterface,
     BlockchainInterfaceFactory,
 )
-from nucypher.blockchain.eth.networks import NetworksInventory
+from nucypher.blockchain.eth.networks import (
+    EthNetwork,
+    NetworksInventory,
+    PolyNetwork,
+    TACoNetwork,
+)
 from nucypher.blockchain.eth.registry import (
     ContractRegistry,
 )
@@ -286,12 +291,16 @@ def mock_condition_blockchains(session_mocker):
         {TESTERCHAIN_CHAIN_ID: "eth-tester/pyevm"},
     )
 
-    session_mocker.patch.object(
-        NetworksInventory, "get_polygon_chain_id", return_value=TESTERCHAIN_CHAIN_ID
+    testing_network = TACoNetwork(
+        TEMPORARY_DOMAIN, EthNetwork.TESTERCHAIN, PolyNetwork.TESTERCHAIN
     )
 
     session_mocker.patch.object(
-        NetworksInventory, "get_ethereum_chain_id", return_value=TESTERCHAIN_CHAIN_ID
+        NetworksInventory, "get_network_names", return_value=[TEMPORARY_DOMAIN]
+    )
+
+    session_mocker.patch.object(
+        NetworksInventory, "get_network", return_value=testing_network
     )
 
 
