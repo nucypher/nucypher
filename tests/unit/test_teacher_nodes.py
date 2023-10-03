206ebb4d8e26c4bfc824ae674d461736ddee0670
derekpierre <derek.pierre@gmail.com>
2023-10-03 14:09:39 -0400

    IBEX no longer a supported domain name.

--- tests/unit/test_teacher_nodes.py
+++ tests/unit/test_teacher_nodes.py
@@ -12,8 +12,5 @@ def mock_teacher_nodes(mocker):
 
 def test_default_teacher_seednodes_defined():
     for domain in NetworksInventory.SUPPORTED_DOMAIN_NAMES:
-        if domain == NetworksInventory.IBEX.name:
-            # skip
-            continue
         teacher_nodes = TEACHER_NODES[domain]
         assert len(teacher_nodes) > 0
