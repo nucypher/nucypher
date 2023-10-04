cbb776f68b898166ce0fa47b188d5ee82a2a8348
derekpierre <derek.pierre@gmail.com>
2023-10-04 11:20:41 -0400

    Fix failing test (after rebase).

--- tests/unit/test_teacher_nodes.py
+++ tests/unit/test_teacher_nodes.py
@@ -6,7 +6,13 @@ from nucypher.network.nodes import TEACHER_NODES
 
 @pytest.fixture(autouse=True)
 def mock_teacher_nodes(mocker):
-    # override fixture
+    # override fixture which mocks TEACHER_NODES
+    yield
+
+
+@pytest.fixture(scope="module")
+def test_registry(module_mocker):
+    # override fixture which mocks SUPPORTED_DOMAIN_NAMES
     yield
 
 
