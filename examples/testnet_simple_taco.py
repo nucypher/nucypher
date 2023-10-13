aaa89c730edb70087b5ce98e81c6055885cf9b97
derekpierre <derek.pierre@gmail.com>
2023-10-13 11:44:35 -0400

    Add optional python profiling to script and use profiling functionality for lynx workflow job on CI.

--- examples/testnet_simple_taco.py
+++ examples/testnet_simple_taco.py
@@ -1,5 +1,7 @@
+import cProfile
 import os
-import time
+import pstats
+import sys
 
 from nucypher_core.ferveo import DkgPublicKey
 
@@ -15,6 +17,8 @@ from tests.constants import DEFAULT_TEST_ENRICO_PRIVATE_KEY
 ######################
 # Boring setup stuff #
 ######################
+
+collect_stats = True if len(sys.argv) == 2 and sys.argv[1] == "True" else False
 
 LOG_LEVEL = "info"
 GlobalLoggerSettings.set_log_level(log_level_name=LOG_LEVEL)
@@ -39,10 +43,10 @@ coordinator_agent = CoordinatorAgent(
     blockchain_endpoint=polygon_endpoint,
     registry=registry,
 )
-ritual_id = 1  # got this from a side channel
+ritual_id = 3  # got this from a side channel
 ritual = coordinator_agent.get_ritual(ritual_id)
 
-# known already authorized signer for ritual 1
+# known authorized encryptor for ritual 3
 signer = InMemorySigner(private_key=DEFAULT_TEST_ENRICO_PRIVATE_KEY)
 enrico = Enrico(
     encrypting_key=DkgPublicKey.from_bytes(bytes(ritual.public_key)), signer=signer
@@ -86,14 +90,24 @@ bob = Bob(
 
 bob.start_learning_loop(now=True)
 
-start = time.time()
-cleartext = bob.threshold_decrypt(
-    threshold_message_kit=threshold_message_kit,
-)
-end = time.time()
+pr = None
+try:
+    if collect_stats:
+        pr = cProfile.Profile()
+        pr.enable()
 
-cleartext = bytes(cleartext)
-print(f"\nCleartext: {cleartext.decode()}")
-assert message == cleartext
+    cleartext = bob.threshold_decrypt(
+        threshold_message_kit=threshold_message_kit,
+    )
 
-print(f"\nDecryption time: {end - start} seconds")
+    cleartext = bytes(cleartext)
+    print(f"\nCleartext: {cleartext.decode()}")
+    assert message == cleartext
+finally:
+    if pr:
+        pr.disable()
+        ps = pstats.Stats(pr).sort_stats(pstats.SortKey.TIME)
+        print("\n------ Profile Stats -------")
+        ps.print_stats(10)
+        print("\n- Caller Info -")
+        ps.print_callers(10)
