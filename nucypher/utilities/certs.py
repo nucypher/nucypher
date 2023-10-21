456fe8b05645bd0ee091f32592f4f55f4b91ab94
Kieran Prasch <kieranprasch@gmail.com>
2023-10-22 00:47:54 +0200

    Makes network middleware loally certificate agnostic

--- nucypher/utilities/certs.py
+++ nucypher/utilities/certs.py
@@ -1,53 +1,112 @@
 import socket
 import ssl
+from typing import Tuple
 from urllib.parse import urlparse
 
-from requests import Session
+import time
+from requests import Session, Response
 from requests.adapters import HTTPAdapter
+from requests.exceptions import RequestException
 from urllib3.poolmanager import PoolManager
 
 
+def parse_url(url) -> Tuple[str, int]:
+    parsed = urlparse(url)
+    hostname = parsed.hostname or ''
+    port = parsed.port or 443
+    return hostname, port
+
+
 class InMemoryCertAdapter(HTTPAdapter):
-    def __init__(self, *args, **kwargs):
+    """Transport adapter that uses a cached certificate for HTTPS requests"""
+
+    def __init__(
+            self,
+            cache_duration: int = 3600,
+            refresh_interval: int = 600,
+            *args, **kwargs
+    ):
         self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        self.cert_cache = {}
+        self.cache_expiry = {}
+        self.cache_duration = cache_duration
+        self.cache_refresh_interval = refresh_interval
         super().__init__(*args, **kwargs)
 
-    def init_poolmanager(self, *args, **kwargs):
+    def init_poolmanager(self, *args, **kwargs) -> None:
+        """Override the default poolmanager to use the local SSL context"""
         self.poolmanager = PoolManager(*args, ssl_context=self.ssl_context, **kwargs)
 
-    def update_cert(self, cert_pem):
+    def set_active_cert(self, cert_pem: str) -> None:
+        """Set the active certificate for the SSL context"""
         self.ssl_context.load_verify_locations(cadata=cert_pem)
+
+    def get_cached_cert(self, hostname: str, port: int) -> str:
+        return self.cert_cache.get((hostname, port))
+
+    def set_cached_cert(self, hostname: str, port: int, cert_pem: str) -> None:
+        self.cert_cache[(hostname, port)] = cert_pem
+        self.cache_expiry[(hostname, port)] = time.time() + self.cache_duration
+
+    def is_cert_expired(self, hostname: str, port: int) -> bool:
+        return ((hostname, port) in self.cache_expiry
+                and time.time() > self.cache_expiry[(hostname, port)])
+
+    def should_cache_now(self, hostname: str, port: int) -> bool:
+        return (
+                (hostname, port) not in self.cache_expiry
+                or time.time()
+                > self.cache_expiry[(hostname, port)]
+                - self.cache_refresh_interval
+        )
 
 
 class InMemoryCertSession(Session):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+    def __init__(self):
+        super().__init__()
         self.adapter = InMemoryCertAdapter()
         self.mount("https://", self.adapter)
 
-    def send(self, *args, **kwargs):
+    def send(self, *args, **kwargs) -> Response:
+        """
+        Override the send to check if the certificate should be refreshed
+        and to refresh it if needed before sending the request.
+        """
+
         # Parse the URL to extract hostname and port
         url = kwargs.get('url', args[0].url)
-        parsed = urlparse(url)
-        hostname = parsed.hostname or ''
-        port = parsed.port or 443
+        hostname, port = parse_url(url=url)
 
-        # Fetch and update the certificate
-        cert_pem = self.fetch_server_cert(hostname, port)
-        self.adapter.update_cert(cert_pem)
+        if self.adapter.should_cache_now(hostname=hostname, port=port):
+            self.refresh_certificate(hostname=hostname, port=port)
 
-        # Perform the actual request
-        return super().send(*args, **kwargs)
+        try:
+            # Perform the actual request
+            response = super().send(*args, **kwargs)
+        except RequestException:
+            # reconnect and retry once
+            self.refresh_certificate(hostname=hostname, port=port)
+            response = super().send(*args, **kwargs)
+
+        return response
+
+    def refresh_certificate(self, hostname: str, port: int) -> None:
+        cert_pem = self.__fetch_server_cert(hostname, port)
+        self.adapter.set_cached_cert(
+            hostname=hostname,
+            port=port,
+            cert_pem=cert_pem
+        )
 
     @staticmethod
-    def fetch_server_cert(hostname, port):
+    def __fetch_server_cert(hostname: str, port: int) -> str:
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
 
         with socket.create_connection((hostname, port)) as sock:
-            with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-                cert_bin = ssock.getpeercert(True)
+            with context.wrap_socket(sock, server_hostname=hostname) as wrapped_sock:
+                cert_bin = wrapped_sock.getpeercert(binary_form=True)
         cert_pem = ssl.DER_cert_to_PEM_cert(cert_bin)
         return cert_pem
-
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -102,14 +102,6 @@ def temp_dir_path():
     yield Path(temp_dir.name)
     temp_dir.cleanup()
 
-
-@pytest.fixture(scope='function')
-def certificates_tempdir():
-    custom_filepath = '/tmp/nucypher-test-certificates-'
-    cert_tmpdir = tempfile.TemporaryDirectory(prefix=custom_filepath)
-    yield Path(cert_tmpdir.name)
-    cert_tmpdir.cleanup()
-
 #
 # Accounts
 #
@@ -388,7 +380,6 @@ def get_random_checksum_address():
 @pytest.fixture(scope="module")
 def fleet_of_highperf_mocked_ursulas(ursula_test_config, request, testerchain):
     mocks = (
-        mock_cert_storage,
         mock_cert_loading,
         mock_rest_app_creation,
         mock_cert_generation,
@@ -451,7 +442,7 @@ def highperf_mocked_alice(
         reload_metadata=False,
     )
 
-    with mock_cert_storage, mock_verify_node, mock_message_verification, mock_keep_learning:
+    with mock_verify_node, mock_message_verification, mock_keep_learning:
         alice = config.produce(known_nodes=list(fleet_of_highperf_mocked_ursulas)[:1])
     yield alice
     # TODO: Where does this really, truly belong?
@@ -472,7 +463,7 @@ def highperf_mocked_bob(fleet_of_highperf_mocked_ursulas):
         reload_metadata=False,
     )
 
-    with mock_cert_storage, mock_verify_node, mock_record_fleet_state, mock_keep_learning:
+    with mock_verify_node, mock_record_fleet_state, mock_keep_learning:
         bob = config.produce(known_nodes=list(fleet_of_highperf_mocked_ursulas)[:1])
     yield bob
     bob._learning_task.stop()
