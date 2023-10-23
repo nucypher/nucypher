e4c743fecbf034c7698945319593668008673d92
Kieran Prasch <kieranprasch@gmail.com>
2023-10-23 10:33:43 +0200

    isolate cert caching from memory adapter

--- nucypher/utilities/certs.py
+++ nucypher/utilities/certs.py
@@ -1,112 +1,175 @@
 import socket
 import ssl
-from typing import Tuple
-from urllib.parse import urlparse
+from _socket import gethostbyname
+from typing import NamedTuple, Dict
+from urllib.parse import urlparse, urlunparse
 
 import time
-from requests import Session, Response
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from requests import Session, Response, PreparedRequest
 from requests.adapters import HTTPAdapter
 from requests.exceptions import RequestException
-from urllib3.poolmanager import PoolManager
+from urllib3 import PoolManager
+
+from nucypher.crypto.tls import _read_tls_certificate
+from nucypher.utilities import logging
+
+Certificate = str
 
 
-def parse_url(url) -> Tuple[str, int]:
-    parsed = urlparse(url)
-    hostname = parsed.hostname or ''
-    port = parsed.port or 443
-    return hostname, port
+class Address(NamedTuple):
+    hostname: str
+    port: int
 
 
-class InMemoryCertAdapter(HTTPAdapter):
-    """Transport adapter that uses a cached certificate for HTTPS requests"""
+class CertificateCache:
+
+    DEFAULT_DURATION = 3600
+    DEFAULT_REFRESH_INTERVAL = 600
 
     def __init__(
             self,
-            cache_duration: int = 3600,
-            refresh_interval: int = 600,
-            *args, **kwargs
+            cache_duration: int = DEFAULT_DURATION,
+            refresh_interval: int = DEFAULT_REFRESH_INTERVAL
     ):
-        self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        self.cert_cache = {}
-        self.cache_expiry = {}
+        self._certificates: Dict[Address, Certificate] = dict()
+        self._expirations: Dict[Address, float] = dict()
         self.cache_duration = cache_duration
         self.cache_refresh_interval = refresh_interval
-        super().__init__(*args, **kwargs)
 
-    def init_poolmanager(self, *args, **kwargs) -> None:
-        """Override the default poolmanager to use the local SSL context"""
-        self.poolmanager = PoolManager(*args, ssl_context=self.ssl_context, **kwargs)
+    def get(self, address: Address) -> str:
+        return self._certificates.get(address)
 
-    def set_active_cert(self, cert_pem: str) -> None:
-        """Set the active certificate for the SSL context"""
-        self.ssl_context.load_verify_locations(cadata=cert_pem)
+    def set(self, address: Address, certificate: Certificate) -> None:
+        self._certificates[address] = certificate
+        self._expirations[address] = time.time() + self.cache_duration
 
-    def get_cached_cert(self, hostname: str, port: int) -> str:
-        return self.cert_cache.get((hostname, port))
+    def is_expired(self, address: Address) -> bool:
+        return (address in self._expirations
+                and time.time() > self._expirations[address])
 
-    def set_cached_cert(self, hostname: str, port: int, cert_pem: str) -> None:
-        self.cert_cache[(hostname, port)] = cert_pem
-        self.cache_expiry[(hostname, port)] = time.time() + self.cache_duration
-
-    def is_cert_expired(self, hostname: str, port: int) -> bool:
-        return ((hostname, port) in self.cache_expiry
-                and time.time() > self.cache_expiry[(hostname, port)])
-
-    def should_cache_now(self, hostname: str, port: int) -> bool:
+    def should_cache_now(self, address: Address) -> bool:
         return (
-                (hostname, port) not in self.cache_expiry
+                address not in self._expirations
                 or time.time()
-                > self.cache_expiry[(hostname, port)]
+                > self._expirations[address]
                 - self.cache_refresh_interval
         )
 
 
+class InMemoryCertAdapter(HTTPAdapter):
+    log = logging.Logger(__name__)
+
+    def __init__(self, *args, **kwargs):
+        self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        self.ssl_context.verify_mode = ssl.CERT_REQUIRED  # Enforce certificate verification
+        self.ssl_context.check_hostname = False  # Disable hostname checking
+        super().__init__(*args, **kwargs)
+
+    def init_poolmanager(self, *args, **kwargs) -> None:
+        self.poolmanager = PoolManager(*args, ssl_context=self.ssl_context, **kwargs)
+
+    def accept_certificate(self, certificate: Certificate) -> None:
+        try:
+            self.ssl_context.load_verify_locations(cadata=certificate)
+        except ssl.SSLError as e:
+            self.log.debug(f"Failed to load certificate {e}.")
+
+
 class InMemoryCertSession(Session):
+
+    _DEFAULT_HOSTNAME = ''
+    _DEFAULT_PORT = 443
 
     def __init__(self):
         super().__init__()
         self.adapter = InMemoryCertAdapter()
+        self.cache = CertificateCache()
         self.mount("https://", self.adapter)
 
-    def send(self, *args, **kwargs) -> Response:
-        """
-        Override the send to check if the certificate should be refreshed
-        and to refresh it if needed before sending the request.
-        """
-
-        # Parse the URL to extract hostname and port
-        url = kwargs.get('url', args[0].url)
-        hostname, port = parse_url(url=url)
-
-        if self.adapter.should_cache_now(hostname=hostname, port=port):
-            self.refresh_certificate(hostname=hostname, port=port)
-
-        try:
-            # Perform the actual request
-            response = super().send(*args, **kwargs)
-        except RequestException:
-            # reconnect and retry once
-            self.refresh_certificate(hostname=hostname, port=port)
-            response = super().send(*args, **kwargs)
-
-        return response
-
-    def refresh_certificate(self, hostname: str, port: int) -> None:
-        cert_pem = self.__fetch_server_cert(hostname, port)
-        self.adapter.set_cached_cert(
-            hostname=hostname,
-            port=port,
-            cert_pem=cert_pem
+    @classmethod
+    def _parse_url(cls, url) -> Address:
+        parsed = urlparse(url)
+        return Address(
+            parsed.hostname or cls._DEFAULT_HOSTNAME,
+            parsed.port or cls._DEFAULT_PORT
         )
 
+    def __retry_send(self, address, request, *args, **kwargs) -> Response:
+        certificate = self._refresh_certificate(address)
+        self.adapter.accept_certificate(certificate=certificate)
+        try:
+            return super().send(request, *args, **kwargs)
+        except RequestException as e:
+            self.adapter.log.debug(f"Request failed due to {e}, giving up.")
+            raise
+
+    def extract_ip_from_certificate(self, certificate_pem: str) -> str:
+        """
+        Extract IP address from the Subject Alternative Name (SAN) field of the certificate.
+        """
+        certificate = x509.load_pem_x509_certificate(certificate_pem.encode(), default_backend())
+        try:
+            san = certificate.extensions.get_extension_for_class(x509.SubjectAlternativeName)
+        except x509.ExtensionNotFound:
+            raise ValueError("No SAN extension found in certificate")
+
+        # Check if an IP address is listed in SAN and return the first one
+        for ip in san.value.get_values_for_type(x509.IPAddress):
+            return str(ip)
+
+    def replace_hostname_with_ip(self, url: str, ip_address: str) -> str:
+        """
+        Replace the hostname in the URL with the provided IP address.
+        """
+        parsed_url = urlparse(url)
+        # Reconstruct the URL with the IP address instead of the hostname
+        return urlunparse((
+            parsed_url.scheme,
+            f"{ip_address}:{parsed_url.port}",
+            parsed_url.path,
+            parsed_url.params,
+            parsed_url.query,
+            parsed_url.fragment
+        ))
+
+    def send(self, request: PreparedRequest, *args, **kwargs) -> Response:
+        address = self._parse_url(url=request.url)
+        certificate = self.get_or_refresh_certificate(address)
+        self.adapter.accept_certificate(certificate=certificate)
+        url = self.replace_hostname_with_ip(
+            url=request.url,
+            ip_address=gethostbyname(address.hostname)
+        )
+        request.url = url
+        try:
+            return super().send(request, *args, **kwargs)
+        except RequestException as e:
+            self.adapter.log.debug(f"Request failed due to {e}, retrying...")
+            return self.__retry_send(address, request, *args, **kwargs)
+
+    def get_or_refresh_certificate(self, address: Address) -> Certificate:
+        if self.cache.should_cache_now(address):
+            return self._refresh_certificate(address)
+        certificate = self.cache.get(address)
+        return certificate
+
+    def _refresh_certificate(self, address: Address) -> Certificate:
+        certificate = self.__fetch_server_cert(address)
+        self.cache.set(address, certificate)
+        return certificate
+
     @staticmethod
-    def __fetch_server_cert(hostname: str, port: int) -> str:
+    def __fetch_server_cert(address: Address) -> Certificate:
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
 
-        with socket.create_connection((hostname, port)) as sock:
-            with context.wrap_socket(sock, server_hostname=hostname) as wrapped_sock:
-                cert_bin = wrapped_sock.getpeercert(binary_form=True)
-        cert_pem = ssl.DER_cert_to_PEM_cert(cert_bin)
-        return cert_pem
+        with socket.create_connection(address) as sock:
+            with context.wrap_socket(sock, server_hostname=address.hostname) as ssock:
+                sock.close()  # close the insecure socket
+                certificate_bin = ssock.getpeercert(binary_form=True)
+
+        certificate = Certificate(ssl.DER_cert_to_PEM_cert(certificate_bin))
+        return certificate
