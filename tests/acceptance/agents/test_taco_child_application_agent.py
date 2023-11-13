dfa1dc17e662f621b0e4bb34a577cd129b77525d
derekpierre <derek.pierre@gmail.com>
2023-11-13 15:06:41 -0500

    Add sampling and other tests for updated TACoChildApplicationAgent functionality.

--- tests/acceptance/agents/test_taco_child_application_agent.py
+++ tests/acceptance/agents/test_taco_child_application_agent.py
@@ -1,7 +1,29 @@
+import pytest
+
 from nucypher.blockchain.eth.constants import NULL_ADDRESS
 
 
-def test_staking_provider_from_operator(taco_child_application_agent, ursulas):
+def test_get_min_authorization(
+    taco_child_application_agent, taco_child_application, taco_application
+):
+    result = taco_child_application_agent.get_min_authorization()
+    assert result == taco_child_application.minimumAuthorization()
+    assert result == taco_application.minimumAuthorization()
+
+
+def test_authorized_tokens(
+    testerchain, taco_application, taco_child_application_agent, staking_providers
+):
+    provider_account = staking_providers[0]
+    authorized_amount = taco_child_application_agent.get_authorized_stake(
+        staking_provider=provider_account
+    )
+    assert authorized_amount >= taco_application.minimumAuthorization()
+
+
+def test_staking_provider_from_operator(
+    taco_child_application_agent, ursulas, get_random_checksum_address
+):
     for ursula in ursulas:
         assert (
             ursula.checksum_address
@@ -10,24 +32,32 @@ def test_staking_provider_from_operator(taco_child_application_agent, ursulas):
             )
         )
 
+    assert NULL_ADDRESS == taco_child_application_agent.staking_provider_from_operator(
+        operator_address=get_random_checksum_address()
+    )
+
 
 def test_staking_provider_info(
     taco_child_application_agent,
     ursulas,
-    taco_application,
     get_random_checksum_address,
 ):
+    staking_providers = taco_child_application_agent.get_staking_providers()
+
     for ursula in ursulas:
         provider_info = taco_child_application_agent.staking_provider_info(
             ursula.checksum_address
         )
         assert provider_info.operator_confirmed is True
         assert provider_info.operator == ursula.operator_address
-        assert provider_info.authorized >= taco_application.minimumAuthorization()
+        assert (
+            provider_info.authorized
+            >= taco_child_application_agent.get_min_authorization()
+        )
+        assert (
+            provider_info.index == staking_providers.index(ursula.checksum_address) + 1
+        )
 
-    # non-existing staking provider
-    # TODO is this right? Technically this is what the contract returns
-    #  Should returned info be "None"; although we don't really expect this situation
     provider_info = taco_child_application_agent.staking_provider_info(
         get_random_checksum_address()
     )
@@ -59,3 +89,39 @@ def test_is_operator_confirmed(
         )
         is False
     )
+
+
+def test_get_staker_population(taco_child_application_agent, staking_providers):
+    # Apart from all the providers in the fixture, we also added a new provider above
+    assert taco_child_application_agent.get_staking_providers_population() == len(
+        staking_providers
+    )
+
+
+@pytest.mark.usefixtures("staking_providers", "ursulas")
+def test_sample_staking_providers(taco_child_application_agent):
+    providers_population = (
+        taco_child_application_agent.get_staking_providers_population()
+    )
+
+    with pytest.raises(taco_child_application_agent.NotEnoughStakingProviders):
+        taco_child_application_agent.get_staking_provider_reservoir().draw(
+            providers_population + 1
+        )  # One more than we have deployed
+
+    providers = taco_child_application_agent.get_staking_provider_reservoir().draw(3)
+    assert len(providers) == 3  # Three...
+    assert len(set(providers)) == 3  # ...unique addresses
+
+    # Same but with pagination
+    providers = taco_child_application_agent.get_staking_provider_reservoir(
+        pagination_size=1
+    ).draw(3)
+    assert len(providers) == 3
+    assert len(set(providers)) == 3
+    light = taco_child_application_agent.blockchain.is_light
+    taco_child_application_agent.blockchain.is_light = not light
+    providers = taco_child_application_agent.get_staking_provider_reservoir().draw(3)
+    assert len(providers) == 3
+    assert len(set(providers)) == 3
+    taco_child_application_agent.blockchain.is_light = light
