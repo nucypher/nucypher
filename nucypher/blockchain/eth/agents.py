fb6a3ae800032df6833a918aae5aca207bf3e77e
derekpierre <derek.pierre@gmail.com>
2023-11-15 09:44:45 -0500

    Check whether ritual is active or expired before attempting to get ritual public key.

--- nucypher/blockchain/eth/agents.py
+++ nucypher/blockchain/eth/agents.py
@@ -931,7 +931,8 @@ class CoordinatorAgent(EthereumContractAgent):
         return result
 
     def get_ritual_public_key(self, ritual_id: int) -> DkgPublicKey:
-        if self.get_ritual_status(ritual_id=ritual_id) != self.Ritual.Status.ACTIVE:
+        status = self.get_ritual_status(ritual_id=ritual_id)
+        if status != self.Ritual.Status.ACTIVE and status != self.Ritual.Status.EXPIRED:
             # TODO should we raise here instead?
             return None
 
