c3d126f522cdf19e8badf76ed20af034897e2b4b
Kieran Prasch <kieranprasch@gmail.com>
2023-12-01 11:29:11 +0100

    Bump version: 7.0.0-rc.10 → 7.0.0

--- nucypher/__about__.py
+++ nucypher/__about__.py
@@ -16,7 +16,7 @@ __url__ = "https://github.com/nucypher/nucypher"
 
 __summary__ = "A threshold access control application to empower privacy in decentralized systems."
 
-__version__ = "7.0.0-rc.10"
+__version__ = "7.0.0"
 
 __author__ = "NuCypher"
 
