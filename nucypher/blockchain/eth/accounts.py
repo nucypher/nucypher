265d6a5863923af3c94dadf43a9c807aeef254e2
KPrasch <kieranprasch@gmail.com>
2024-01-12 10:40:22 +0100

    docstrings for LocalAccount

--- nucypher/blockchain/eth/accounts.py
+++ nucypher/blockchain/eth/accounts.py
@@ -17,13 +17,20 @@ EthAccount.enable_unaudited_hdwallet_features()
 
 
 class InvalidKeystore(Exception):
-    pass
+    """Raised when the keystore file is invalid."""
 
 
 class LocalAccount(EthLocalAccount):
+    """
+    Subclass of eth_account.signers.local.LocalAccount with additional
+    functionality for managing keystore files, defaulting to the HD path
+    and standardizing signatures.
+    """
+
     __HD_PATH = "m/44'/60'/0'/0/0"
 
     def sign_message(self, message: bytes, standardize: bool = True) -> HexBytes:
+        """Sign a message with the private key of this account."""
         signature = super().sign_message(signable_message=encode_defunct(primitive=message)).signature
         if standardize:
             # This signature will need to be passed to Rust, so we are cleaning the chain identifier
@@ -32,6 +39,7 @@ class LocalAccount(EthLocalAccount):
         return HexBytes(signature)
 
     def sign_transaction(self, transaction_dict: dict) -> HexBytes:
+        """Sign a transaction with the private key of this account."""
         if not transaction_dict['to']:
             # Edge case: do not include a 'to' field when deploying a contract.
             transaction_dict = dissoc(transaction_dict, 'to')
@@ -39,7 +47,8 @@ class LocalAccount(EthLocalAccount):
         return HexBytes(signed_raw_transaction)
 
     @classmethod
-    def from_mnemonic(cls, mnemonic: str, password: str, filepath: Path,) -> Tuple['LocalAccount', Path]:
+    def from_mnemonic(cls, mnemonic: str, password: str, filepath: Path) -> Tuple['LocalAccount', Path]:
+        """Derive an account from a mnemonic phrase and save the resulting keystore to the disk."""
         account = EthAccount.from_mnemonic(mnemonic=mnemonic, account_path=cls.__HD_PATH)
         account = cls(key=PrivateKey(account.key), account=EthAccount)
         filepath = account.to_keystore(path=filepath, password=password)
@@ -47,29 +56,46 @@ class LocalAccount(EthLocalAccount):
 
     @classmethod
     def from_keystore(cls, path: Path, password: str) -> 'LocalAccount':
+        """
+        Decrypt a keystore file using its password and return the resulting account.
+        Keystore files must be in web3 secret storage format.
+        """
         metadata = cls._read_wallet(filepath=path)
         private_key = EthAccount.decrypt(metadata, password=password)
         account = EthAccount.from_key(private_key=private_key)
         return cls(key=PrivateKey(account.key), account=EthAccount)
 
     def to_keystore(self, path: Path, password: str) -> Path:
+        """
+        Encrypt this account with a password and save the resulting keystore to the disk.
+        Keystore files are produced in web3 secret storage format.
+        """
         keyfile_json = self.encrypt(password=password)
         self._write_wallet(filepath=path, data=keyfile_json)
         return Path(path)
 
     @staticmethod
     def _read(filepath: Path) -> str:
+        """
+        Read a file and return its contents.
+        This method is discrete from for testing & mocking purposes
+        ."""
         with open(filepath, 'r') as f:
             data = f.read()
         return data
 
     @staticmethod
     def _write(filepath: Path, data: str) -> None:
+        """
+        Write data to a file.
+        This method is discrete from for testing & mocking purposes.
+        """
         with open(filepath, 'w') as f:
             f.write(data)
 
     @classmethod
     def _read_wallet(cls, filepath: Path) -> Dict:
+        """Read a keystore file and return its contents."""
         data = cls._read(filepath=filepath)
         try:
             metadata = json.loads(data)
@@ -79,6 +105,7 @@ class LocalAccount(EthLocalAccount):
 
     @classmethod
     def _write_wallet(cls, filepath: Path, data: Dict) -> None:
+        """Write keystore data to a file."""
         if filepath.exists():
             raise FileExistsError(f'File {filepath} already exists.')
         filepath.parent.mkdir(parents=True, exist_ok=True)
