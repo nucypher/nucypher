29db0aed258b6cf61753a11c1987621ad61d8849
derekpierre <derek.pierre@gmail.com>
2024-01-15 16:09:53 -0500

    Obtain node ferveo public key from Coordinator contract instead of learning.
    Co-authored-by: Kieran Prasch <kieranprasch@gmail.com>

--- nucypher/blockchain/eth/actors.py
+++ nucypher/blockchain/eth/actors.py
@@ -296,19 +296,10 @@ class Operator(BaseActor):
         return ritual
 
     def _resolve_validators(
-            self,
-            ritual: CoordinatorAgent.Ritual,
-            timeout: int = 60
+        self,
+        ritual: CoordinatorAgent.Ritual,
+        ritual_id: int,
     ) -> List[Tuple[Validator, Transcript]]:
-
-        validators = [n[0] for n in ritual.transcripts]
-        if timeout > 0:
-            nodes_to_discover = list(set(validators) - {self.checksum_address})
-            self.block_until_specific_nodes_are_known(
-                addresses=nodes_to_discover,
-                timeout=timeout,
-                allow_missing=0
-            )
 
         result = list()
         for staking_provider_address, transcript_bytes in ritual.transcripts:
@@ -320,12 +311,9 @@ class Operator(BaseActor):
                 )
             else:
                 # Remote
-                try:
-                    remote_operator = self.known_nodes[staking_provider_address]
-                except KeyError:
-                    raise self.ActorError(f"Unknown node {staking_provider_address}")
-                remote_operator.mature()
-                public_key = remote_operator.public_keys(RitualisticPower)
+                public_key = self.coordinator_agent.get_provider_public_key(
+                    provider=staking_provider_address, ritual_id=ritual_id
+                )
                 self.log.debug(
                     f"Ferveo public key for {staking_provider_address} is {bytes(public_key).hex()[:-8:-1]}"
                 )
@@ -423,7 +411,7 @@ class Operator(BaseActor):
 
         # gather the cohort
         ritual = self.coordinator_agent.get_ritual(ritual_id, with_participants=True)
-        nodes, transcripts = list(zip(*self._resolve_validators(ritual)))
+        nodes, transcripts = list(zip(*self._resolve_validators(ritual, ritual_id)))
         nodes = sorted(nodes, key=lambda n: n.address)
         if any(transcripts):
             self.log.debug(
@@ -498,7 +486,7 @@ class Operator(BaseActor):
         )
 
         ritual = self.coordinator_agent.get_ritual(ritual_id, with_participants=True)
-        transcripts = self._resolve_validators(ritual)
+        transcripts = self._resolve_validators(ritual, ritual_id)
         if not all([t for _, t in transcripts]):
             raise self.ActorError(
                 f"ritual #{ritual_id} is missing transcripts from {len([t for t in transcripts if not t])} nodes."
@@ -555,7 +543,7 @@ class Operator(BaseActor):
         if not self.coordinator_agent.is_ritual_active(ritual_id=ritual_id):
             raise self.ActorError(f"Ritual #{ritual_id} is not active.")
 
-        nodes, transcripts = list(zip(*self._resolve_validators(ritual)))
+        nodes, transcripts = list(zip(*self._resolve_validators(ritual, ritual_id)))
         if not all(transcripts):
             raise self.ActorError(f"Ritual #{ritual_id} is missing transcripts")
 
