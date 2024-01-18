3a191041de0a4bf087f9d48638d0ffd4c09881d4
derekpierre <derek.pierre@gmail.com>
2024-01-18 16:17:28 -0500

    Increase robustness of prometheus metrics task by implementing a PrometheusMetricsTracker.

---
+++ nucypher/blockchain/eth/trackers/prometheus.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from nucypher.utilities.prometheus.collector import MetricsCollector
+from nucypher.utilities.task import SimpleTask
+
+
+class PrometheusMetricsTracker(SimpleTask):
+    def __init__(self, collectors: List[MetricsCollector], interval: float):
+        self.metrics_collectors = collectors
+        super().__init__(interval=interval)
+
+    def run(self):
+        for collector in self.metrics_collectors:
+            collector.collect()
+
+    def handle_errors(self, *args, **kwargs):
+        self.log.warn(
+            "Error during prometheus metrics collection: {}".format(
+                args[0].getTraceback()
+            )
+        )
+        if not self._task.running:
+            self.log.warn("Restarting prometheus metrics task!")
+            self.start(now=False)  # take a breather
