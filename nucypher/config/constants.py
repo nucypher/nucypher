04b3202877bd2c5055bb45f010cbe4912a4f3617
Piotr Roslaniec <p.roslaniec@gmail.com>
2024-01-18 11:53:19 +0100

    set MAX_UPLOAD_CONTENT_LENGTH to 250kb

--- nucypher/config/constants.py
+++ nucypher/config/constants.py
@@ -38,7 +38,7 @@ NUCYPHER_SENTRY_ENDPOINT = f"https://{NUCYPHER_SENTRY_PUBLIC_KEY}@sentry.io/{NUC
 
 # Web
 CLI_ROOT = NUCYPHER_PACKAGE / 'network' / 'templates'
-MAX_UPLOAD_CONTENT_LENGTH = 1024 * 50
+MAX_UPLOAD_CONTENT_LENGTH = 1024 * 250 # 250kb
 
 # Dev Mode
 TEMPORARY_DOMAIN_NAME = ":temporary-domain:"  # for use with `--dev` node runtimes
