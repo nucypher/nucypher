55f8b2102b986f1640c915e4e154ca0c3ada294a
KPrasch <kieranprasch@gmail.com>
2024-01-23 16:19:18 +0100

    respond to RFCs in PR #3398

--- tests/unit/test_ping_utils.py
+++ tests/unit/test_ping_utils.py
@@ -1,12 +1,13 @@
+from ipaddress import IPv4Address
+
 import pytest
 from flask import Request
 
 from nucypher.utilities.networking import (
     LOOPBACK_ADDRESS,
     _ip_sources,
-    _is_global_ipv4,
     _resolve_ipv4,
-    get_request_global_ipv4,
+    get_global_source_ipv4,
 )
 
 
@@ -21,38 +22,12 @@ def mock_request_factory(mocker):
     return _mock_request_factory
 
 
-@pytest.mark.parametrize(
-    "ip, expected",
-    [
-        ("8.8.8.8", True),  # public IPv4
-        ("192.168.1.1", False),  # private IPv4
-        ("2001:0db8:85a3:0000:0000:8a2e:0370:7334", False),  # IPv6
-    ],
-)
-def test_is_global_ipv4(ip, expected):
-    assert _is_global_ipv4(ip) == expected
-
-
-def test_is_global_ipv4_with_invalid_address():
-    with pytest.raises(
-        ValueError, match="'not_an_ip' does not appear to be an IPv4 or IPv6 address"
-    ):
-        _is_global_ipv4("not_an_ip")
-
-
 def test_resolve_ipv4_with_valid_ipv4():
-    assert _resolve_ipv4("8.8.8.8") == "8.8.8.8"
+    assert _resolve_ipv4("8.8.8.8") == IPv4Address("8.8.8.8")
 
 
 def test_resolve_ipv4_with_valid_mapped_ipv6():
-    assert _resolve_ipv4("::ffff:8.8.8.8") == "8.8.8.8"
-
-
-def test_resolve_ipv4_with_invalid_ip():
-    with pytest.raises(
-        ValueError, match="'not_an_ip' does not appear to be an IPv4 or IPv6 address"
-    ):
-        _is_global_ipv4("not_an_ip")
+    assert _resolve_ipv4("::ffff:8.8.8.8") == IPv4Address("8.8.8.8")
 
 
 def test_resolve_ipv4_with_non_mapped_ipv6():
@@ -81,19 +56,21 @@ def test_ip_sources_with_no_headers_but_remote_addr(mock_request_factory):
 
 def test_get_request_global_ipv4_with_forwarded_ip(mock_request_factory):
     request = mock_request_factory(headers={"X-Forwarded-For": "8.8.8.8, 192.168.1.1"})
-    assert get_request_global_ipv4(request) == "8.8.8.8"
+    assert get_global_source_ipv4(request) == "8.8.8.8"
+    assert isinstance(get_global_source_ipv4(request), str)
 
 
 def test_get_request_global_ipv4_with_private_ip_only(mock_request_factory):
     request = mock_request_factory(headers={"X-Forwarded-For": "192.168.1.1"})
-    assert get_request_global_ipv4(request) is None
+    assert get_global_source_ipv4(request) is None
 
 
 def test_get_request_global_ipv4_with_no_headers_but_valid_remote_addr(
     mock_request_factory,
 ):
     request = mock_request_factory(remote_addr="8.8.8.8")
-    assert get_request_global_ipv4(request) == "8.8.8.8"
+    assert get_global_source_ipv4(request) == "8.8.8.8"
+    assert isinstance(get_global_source_ipv4(request), str)
 
 
 def test_get_request_global_ipv4_with_invalid_remote_addr(mock_request_factory):
@@ -101,4 +78,4 @@ def test_get_request_global_ipv4_with_invalid_remote_addr(mock_request_factory):
     with pytest.raises(
         ValueError, match="'not_an_ip' does not appear to be an IPv4 or IPv6 address"
     ):
-        get_request_global_ipv4(request)
+        get_global_source_ipv4(request)
