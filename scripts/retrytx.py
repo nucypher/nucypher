6203f6d2d57414f12a24be46c99ec512f782141f
KPrasch <kieranprasch@gmail.com>
2024-02-04 19:21:34 +0100

    updates retrytx.py example script for automated transaction queue

--- scripts/retrytx.py
+++ scripts/retrytx.py
@@ -1,8 +1,10 @@
+import os
+
 from twisted.internet import reactor
-from web3.types import TxParams
 
 from nucypher.blockchain.eth.agents import CoordinatorAgent
 from nucypher.blockchain.eth.domains import LYNX
+from nucypher.blockchain.eth.interfaces import BlockchainInterface, BlockchainInterfaceFactory
 from nucypher.blockchain.eth.registry import ContractRegistry
 from nucypher.blockchain.eth.signers import InMemorySigner
 from nucypher.blockchain.eth.trackers.transactions import TransactionTracker
@@ -10,52 +12,63 @@ from nucypher.crypto.powers import TransactingPower
 from nucypher.utilities.logging import GlobalLoggerSettings
 from tests.constants import DEFAULT_TEST_ENRICO_PRIVATE_KEY
 
+
+#
+# Configuration
+#
+
 LOG_LEVEL = "debug"
+ENDPOINT = os.environ["WEB3_PROVIDER_URI"]
+PRIVATE_KEY = DEFAULT_TEST_ENRICO_PRIVATE_KEY
+DOMAIN = LYNX
+
+
+#
+# Setup
+#
+
 GlobalLoggerSettings.set_log_level(log_level_name=LOG_LEVEL)
 GlobalLoggerSettings.start_console_logging()
 
-signer = InMemorySigner(private_key=DEFAULT_TEST_ENRICO_PRIVATE_KEY)
+signer = InMemorySigner(private_key=PRIVATE_KEY)
 address = signer.accounts[0]
 transacting_power = TransactingPower(signer=signer, account=address)
-print(address)
 
-endpoint = "https://polygon-mumbai.infura.io/v3/YOUR_INFURA_KEY_HERE"
-registry = ContractRegistry.from_latest_publication(
-    domain=LYNX,
+blockchain = BlockchainInterfaceFactory.get_or_create_interface(endpoint=ENDPOINT)
+
+
+#
+# Prepare Tx
+#
+
+
+base_fee = blockchain.w3.eth.get_block("latest")["baseFeePerGas"]
+tip = blockchain.w3.eth.max_priority_fee
+nonce = blockchain.w3.eth.get_transaction_count(address, 'pending')
+
+tx = {
+    'type': '0x2',
+    'chainId': 80001,
+    'nonce': nonce,
+    'from': address,
+    'to': address,
+    'value': 0,
+    'gas': 21000,
+    'maxPriorityFeePerGas': tip,
+    'maxFeePerGas': base_fee + tip,
+    'data': b'',
+}
+
+#
+# Queue Tx
+#
+
+tracker = TransactionTracker(w3=blockchain.w3)
+_future_tx = tracker.queue_transaction(
+    tx=tx,
+    signer=transacting_power.sign_transaction,  # callable
+    info={"message": f"This is transaction {nonce}"},  # optional
 )
-coordinator_agent = CoordinatorAgent(
-    blockchain_endpoint=endpoint,
-    registry=registry
-)
-w3 = coordinator_agent.blockchain.w3
-
-tracker = TransactionTracker(
-    w3=coordinator_agent.blockchain.w3,
-)
-
-txs = []
-for i in range(3):
-    nonce = w3.eth.get_transaction_count(address, 'pending')
-    base_fee = w3.eth.get_block("latest")["baseFeePerGas"]
-    tip = w3.eth.max_priority_fee
-    tx = TxParams({
-        'nonce': nonce + i,
-        'to': address,
-        'value': 0,
-        'gas': 21000,
-        'maxPriorityFeePerGas': tip,
-        'maxFeePerGas': base_fee + tip,
-        'chainId': 80001,
-        'type': '0x2',
-        'from': address
-    })
-    future_tx = tracker.queue_transaction(
-        tx=tx,
-        signer=transacting_power.sign_transaction,
-        info={"message": f"This is transaction {i}"},
-    )
-    txs.append(future_tx)
-
 
 tracker.start(now=True)
 reactor.run()
