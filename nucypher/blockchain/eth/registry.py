ddc1b454e5478a942d95ec820d56947e81985c9d
derekpierre <derek.pierre@gmail.com>
2024-02-06 10:30:27 -0500

    Use `nucypher-contracts:main` as the canonical source for contract registries. Add test to ensure github raw path is actually correct.

--- nucypher/blockchain/eth/registry.py
+++ nucypher/blockchain/eth/registry.py
@@ -60,7 +60,7 @@ class RegistrySource(ABC):
 
 class GithubRegistrySource(RegistrySource):
 
-    _PUBLICATION_REPO = "nucypher/nucypher"
+    _PUBLICATION_REPO = "nucypher/nucypher-contracts"
     _BASE_URL = f'https://raw.githubusercontent.com/{_PUBLICATION_REPO}'
 
     name = "GitHub Registry Source"
@@ -74,7 +74,7 @@ class GithubRegistrySource(RegistrySource):
 
     def get_publication_endpoint(self) -> str:
         """Get the GitHub endpoint for the registry publication."""
-        url = f"{self._BASE_URL}/main/nucypher/blockchain/eth/contract_registry/{self.registry_name}"
+        url = f"{self._BASE_URL}/main/deployment/artifacts/{self.registry_name}"
         return url
 
     def decode(self, response: Response, endpoint: str) -> RegistryData:
