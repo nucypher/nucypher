db1ff0eeeed8b2d3f983882d9dd2e5d7ba6cb863
derekpierre <derek.pierre@gmail.com>
2024-04-27 10:28:10 -0400

    Add simple test for ReservedTestAccountManager to ensure that accounts are appropriately organized.

---
+++ tests/unit/test_reserved_test_account_manager.py
@@ -0,0 +1,31 @@
+from tests.constants import NUMBER_OF_ETH_TEST_ACCOUNTS
+from tests.utils.blockchain import ReservedTestAccountManager
+
+
+def test_account_organization():
+    account_manager = ReservedTestAccountManager()
+
+    account_addresses = account_manager.accounts_addresses
+    assert (
+        len(set(account_addresses)) == NUMBER_OF_ETH_TEST_ACCOUNTS
+    ), "all unique addresses"
+
+    # special accounts
+    assert account_manager.etherbase_account == account_addresses[0]
+    assert account_manager.alice_account == account_addresses[1]
+    assert account_manager.bob_account == account_addresses[2]
+
+    # staking provider addresses
+    staking_providers = account_manager.stake_providers_accounts
+    assert staking_providers == account_addresses[3:13]
+    for i in range(len(staking_providers)):
+        assert account_manager.stake_provider_account(i) == staking_providers[i]
+
+    # ursula addresses
+    ursulas = account_manager.ursulas_accounts
+    assert ursulas == account_addresses[13:23]
+    for i in range(len(ursulas)):
+        assert account_manager.ursula_account(i) == ursulas[i]
+
+    # unassigned addresses
+    assert account_manager.unassigned_accounts == account_addresses[23:30]
