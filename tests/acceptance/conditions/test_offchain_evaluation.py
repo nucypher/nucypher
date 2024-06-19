c23618a54128ef7c8436c0a099ea380a906ac89d
KPrasch <kieranprasch@gmail.com>
2024-06-19 19:12:07 +0800

    mocked ETH price condition from 3rd party party API

--- tests/acceptance/conditions/test_offchain_evaluation.py
+++ tests/acceptance/conditions/test_offchain_evaluation.py
@@ -1,12 +1,23 @@
-from nucypher.policy.conditions.evm import OffchainCondition
 from nucypher.policy.conditions.lingo import ReturnValueTest
+from nucypher.policy.conditions.offchain import OffchainCondition
 
 
 def test_basic_offchain_condition_evaluation(accounts, condition_providers, mocker):
+    mocker.patch(
+        "nucypher.policy.conditions.offchain.OffchainCondition.fetch",  # Path to the method you want to patch
+        return_value=mocker.Mock(
+            status_code=200, json=lambda: {"ethereum": {"usd": 0.0}}
+        ),
+    )
 
     condition = OffchainCondition(
-        endpoint="https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd",
-        return_value_test=ReturnValueTest(">", 0),
+        endpoint="https://api.coingecko.com/api/v3/simple/price",
+        parameters={
+            "ids": "ethereum",
+            "vs_currencies": "usd",
+        },
+        query="ethereum.usd",
+        return_value_test=ReturnValueTest("==", 0.0),
     )
 
     assert condition.verify() == (True, 0.0)
