27b29a668a9284fb3d03c9e8c338b81f177ce9e5
KPrasch <kieranprasch@gmail.com>
2024-06-20 16:08:06 +0800

    exposes request headers for offchain condition GET requests

--- tests/acceptance/conditions/test_offchain_evaluation.py
+++ tests/acceptance/conditions/test_offchain_evaluation.py
@@ -2,9 +2,11 @@ from nucypher.policy.conditions.lingo import ReturnValueTest
 from nucypher.policy.conditions.offchain import OffchainCondition
 
 
-def test_basic_offchain_condition_evaluation(accounts, condition_providers, mocker):
-    mocker.patch(
-        "nucypher.policy.conditions.offchain.OffchainCondition.fetch",  # Path to the method you want to patch
+def test_basic_offchain_condition_evaluation_with_parameters(
+    accounts, condition_providers, mocker
+):
+    mocked_get = mocker.patch(
+        "requests.get",
         return_value=mocker.Mock(
             status_code=200, json=lambda: {"ethereum": {"usd": 0.0}}
         ),
@@ -21,3 +23,30 @@ def test_basic_offchain_condition_evaluation(accounts, condition_providers, mock
     )
 
     assert condition.verify() == (True, 0.0)
+    assert mocked_get.call_count == 1
+
+
+def test_basic_offchain_condition_evaluation_with_headers(
+    accounts, condition_providers, mocker
+):
+    mocked_get = mocker.patch(
+        "requests.get",
+        return_value=mocker.Mock(
+            status_code=200, json=lambda: {"ethereum": {"usd": 0.0}}
+        ),
+    )
+
+    condition = OffchainCondition(
+        endpoint="https://api.coingecko.com/api/v3/simple/price",
+        parameters={
+            "ids": "ethereum",
+            "vs_currencies": "usd",
+        },
+        headers={"Authorization": "Bearer 1234567890"},
+        query="ethereum.usd",
+        return_value_test=ReturnValueTest("==", 0.0),
+    )
+
+    assert condition.verify() == (True, 0.0)
+    assert mocked_get.call_count == 1
+    assert mocked_get.call_args[1]["headers"]["Authorization"] == "Bearer 1234567890"
