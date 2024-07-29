7bf5de22dde32b13d8f84b5000d489313adb2361
KPrasch <kieranprasch@gmail.com>
2024-07-29 15:32:24 +0700

    revises ferveo key mismatch warning text

--- nucypher/utilities/warnings.py
+++ nucypher/utilities/warnings.py
@@ -1,19 +1,22 @@
 def render_ferveo_key_mismatch_warning(local_key, onchain_key):
     message = f"""
-Local Ferveo public key {bytes(local_key).hex()[:8]} does not match on-chain public key {bytes(onchain_key).hex()[:8]}!
 
-This is a critical error.  Without private keys your node will not be able to provide service for existing DKGs.
+ERROR: The local Ferveo public key {bytes(local_key).hex()[:8]} does not match the on-chain public key {bytes(onchain_key).hex()[:8]}!
 
-If you are relocating your node to a new host you must copy the keystore directory to the new host.
+This is a critical error. Without the original private keys, your node cannot service existing DKGs.
 
-If you do not have a backup of the keystore or have lost your password, you will need to recover your node using the 
-recovery phrase by running:
+IMPORTANT: Running `nucypher ursula init` will generate new private keys, which is not the correct procedure
+for relocating or restoring a TACo node.
+
+To relocate your node to a new host copy the keystore directory (~/.local/share/nucypher) to the new host.
+If you do not have a backup of the original keystore or have lost your password, you will need to recover your 
+node using the recovery phrase assigned during the initial setup by running:
 
 nucypher ursula recover
 
-If you have lost your recovery phrase please open up a support ticket in the Threshold discord server (#taco). 
-Your stake may be slashed but the punishment will be significantly smaller if you disclose the loss and a key material
-handover is completed quickly, which will restore your node and ensure the service is not disrupted.
+If you have lost your recovery phrase: Open a support ticket in the Threshold Discord server (#taco).
+Disclose the loss immediately to minimize penalties. Your stake may be slashed, but the punishment will be significantly
+reduced if a key material handover is completed quickly, ensuring the node's service is not disrupted.
 
 """
     return message
