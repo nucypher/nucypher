b5940f0bf10093363f58e39a6bd84fb8069043da
derekpierre <derek.pierre@gmail.com>
2024-08-15 08:13:37 -0400

    Remove license text and not applicable comment.

--- scripts/dkg/differing_ferveo_keys.py
+++ scripts/dkg/differing_ferveo_keys.py
@@ -1,20 +1,3 @@
-"""
- This file is part of nucypher.
-
- nucypher is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- nucypher is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 from typing import List, Optional
 
 import click
@@ -32,11 +15,7 @@ from nucypher.blockchain.eth.registry import ContractRegistry
 from nucypher.utilities.emitters import StdoutEmitter
 from nucypher.utilities.logging import GlobalLoggerSettings
 
-# Get an interactive Python session with all the NuCypher agents loaded by running:
-#    python scripts/hooks/nucypher_agents.py [OPTIONS]
-
 GlobalLoggerSettings.start_console_logging()
-
 emitter = StdoutEmitter(verbosity=2)
 
 
