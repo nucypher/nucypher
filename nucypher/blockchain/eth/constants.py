d3af0637d094c5869fc5c9745bd30a801313d68c
derekpierre <derek.pierre@gmail.com>
2024-12-12 09:42:14 -0500

    Fix linting issue.

--- nucypher/blockchain/eth/constants.py
+++ nucypher/blockchain/eth/constants.py
@@ -54,4 +54,6 @@ POA_CHAINS = {
     80002,  # "Polygon/Amoy"
 }
 
-CHAINLIST_URL = "https://raw.githubusercontent.com/nucypher/chainlist/main/{domain}.json"
+CHAINLIST_URL = (
+    "https://raw.githubusercontent.com/nucypher/chainlist/main/{domain}.json"
+)
