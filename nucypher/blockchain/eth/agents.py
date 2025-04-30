95455d38a00b48576357be2314500cabd1e87da8
derekpierre <derek.pierre@gmail.com>
2025-04-29 22:16:49 -0400

    Increase gas estimation when posting signature.

--- nucypher/blockchain/eth/agents.py
+++ nucypher/blockchain/eth/agents.py
@@ -1091,6 +1091,7 @@ class SigningCoordinatorAgent(EthereumContractAgent):
         )
         async_tx = self.blockchain.send_async_transaction(
             contract_function=contract_function,
+            gas_estimation_multiplier=1.4,
             transacting_power=transacting_power,
             async_tx_hooks=async_tx_hooks,
         )
