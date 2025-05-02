992c95e45494f038fd0e88b368a30c4065a5f76e
derekpierre <derek.pierre@gmail.com>
2025-05-02 13:44:27 -0400

    Fix deprecation warnings for use_fixtures.

--- tests/acceptance/agents/test_signing_coordinator_agent.py
+++ tests/acceptance/agents/test_signing_coordinator_agent.py
@@ -16,7 +16,6 @@ def agent(signing_coordinator_agent) -> SigningCoordinatorAgent:
     return signing_coordinator_agent
 
 
-@pytest.mark.usefixtures("ursulas")
 @pytest.fixture(scope="module")
 def cohort_providers(staking_providers):
     # "ursulas" fixture is needed to set provider public key
