221b14e949daf6a69b0e6ef06f40f7b638d9afd6
derekpierre <derek.pierre@gmail.com>
2025-05-01 20:47:40 -0400

    Update example script for testing signing requests on testnet based on latest changes to `nucypher` and to `nucypher-porter`.

--- examples/testnet_simple_signing.py
+++ examples/testnet_simple_signing.py
@@ -1,41 +1,44 @@
+import base64
 import os
 
 import requests
-from nucypher_core.ferveo import DkgPublicKey
 
 from nucypher.blockchain.eth import domains
-from nucypher.blockchain.eth.agents import CoordinatorAgent, SigningCoordinatorAgent
-from nucypher.blockchain.eth.domains import PolygonChain
+from nucypher.blockchain.eth.agents import SigningCoordinatorAgent
 from nucypher.blockchain.eth.registry import ContractRegistry
-from nucypher.blockchain.eth.signers import InMemorySigner
-from nucypher.characters.lawful import Bob, Enrico, Ursula
-from nucypher.policy.conditions.lingo import ConditionLingo, ConditionType
+from nucypher.characters.lawful import Bob
+from nucypher.types import ThresholdSignatureRequest
 from nucypher.utilities.logging import GlobalLoggerSettings
-from nucypher.utilities.profiler import Profiler
-from tests.constants import DEFAULT_TEST_ENRICO_PRIVATE_KEY
 
 LOG_LEVEL = "debug"
 GlobalLoggerSettings.set_log_level(log_level_name=LOG_LEVEL)
 GlobalLoggerSettings.start_console_logging()
 
-eth_endpoint = os.environ["DEMO_L1_PROVIDER_URI"]
 domain = domains.LYNX
 
+cohort_id = 0  # got this from a side channel
+threshold = 2
+
+data_to_sign = b"paz al amanecer"
+signing_request = ThresholdSignatureRequest(
+    cohort_id=cohort_id,
+    data_to_sign=data_to_sign,
+    context=None,
+)
+
+print("--------- Threshold Signing Bob ---------")
+
+eth_endpoint = os.environ["DEMO_L1_PROVIDER_URI"]
 polygon_endpoint = os.environ["DEMO_L2_PROVIDER_URI"]
-
-
-print("--------- Threshold Signing ---------")
 
 registry = ContractRegistry.from_latest_publication(
     domain=domain,
 )
 
-coordinator_agent = SigningCoordinatorAgent(
+signing_coordinator_agent = SigningCoordinatorAgent(
     blockchain_endpoint=polygon_endpoint,
     registry=registry,
 )
-cohort_id = 1  # got this from a side channel
-ritual = coordinator_agent.get_signing_cohort(cohort_id)
 
 # known authorized encryptor for ritual 3
 bob = Bob(
@@ -46,38 +49,42 @@ bob = Bob(
 )
 
 print(f"BOB: {bob}")
-
-
-time_condition = {
-    "version": ConditionLingo.VERSION,
-    "condition": {
-        "conditionType": ConditionType.TIME.value,
-        "chain": 80002,
-        "method": "blocktime",
-        "returnValueTest": {"comparator": ">", "value": 0},
-    },
-}
-
 bob.start_learning_loop(now=True)
 
-porter_url = "http://127.0.0.1:9155/get_ursulas"
-result = requests.get(porter_url, params={"quantity": 3})
-ursula_metadata = result.json()["result"]["ursulas"]
-
-ursulas = []
-for u in ursula_metadata:
-    ursula = Ursula.from_teacher_uri(u["uri"], eth_endpoint=eth_endpoint, min_stake=0)
-    ursulas.append(ursula)
-
-data_to_sign = b"paz al amanecer"
-
 signatures = bob.request_threshold_signatures(
-    data_to_sign=data_to_sign,
-    cohort_id=cohort_id,
-    threshold=2,
-    conditions=time_condition,
-    timeout=60,
-    ursulas=ursulas,
+    signing_request=signing_request,
 )
 
 print(f"\nSignatures:\n{signatures}")
+
+print("--------- Threshold Signing Porter ---------")
+
+porter_base_url = "http://127.0.0.1:9155"
+response = requests.get(f"{porter_base_url}/get_ursulas", params={"quantity": 3})
+response.raise_for_status()
+
+data = response.json()
+ursula_metadata = data["result"]["ursulas"]
+
+signing_request_b64 = base64.b64encode(bytes(signing_request)).decode()
+signing_requests = {}
+for u in ursula_metadata:
+    signing_requests[u["checksum_address"]] = signing_request_b64
+
+params = {
+    "signing_requests": signing_requests,
+    "threshold": threshold,
+}
+
+response = requests.post(f"{porter_base_url}/sign", json=params)
+response.raise_for_status()
+data = response.json()
+signing_results = data["result"]["signing_results"]
+errors = signing_results["errors"]
+assert len(errors) == 0, f"{errors}"  # no errors
+
+assert len(signing_results["signatures"]) >= threshold
+
+print(
+    f"\nSignatures:\n{[base64.b64decode(s[1]) for s in signing_results['signatures'].values()]}"
+)
