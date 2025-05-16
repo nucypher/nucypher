5f985f11d9421c897c5ea65ecc8a8a75a42691c8
Kieran Prasch <kieranprasch@gmail.com>
2025-05-16 10:21:33 +0200

    exposes message hash from signed messages

--- nucypher/blockchain/eth/actors.py
+++ nucypher/blockchain/eth/actors.py
@@ -1357,6 +1357,7 @@ class Operator(BaseActor):
     def handle_threshold_signing_request(
         self, signing_request: ThresholdSignatureRequest
     ) -> ThresholdSignatureResponse:
+
         if not self.signing_coordinator_agent.is_cohort_active(
             signing_request.cohort_id
         ):
