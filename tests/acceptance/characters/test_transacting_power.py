bc20ecf6f50e65fdf0f3bf06ce8ed63739f82a80
KPrasch <kieranprasch@gmail.com>
2025-05-16 12:45:44 +0200

    handles tuple unpacking of message hash from signing in select tests (can be squashed)

--- tests/acceptance/characters/test_transacting_power.py
+++ tests/acceptance/characters/test_transacting_power.py
@@ -41,7 +41,7 @@ def test_character_transacting_power_signing(testerchain, test_registry, account
 
     # Sign Message
     data_to_sign = b"Premium Select Luxury Pencil Holder"
-    message, signature = power.sign_message(message=data_to_sign)
+    _message_hash, signature = power.sign_message(message=data_to_sign)
     is_verified = verify_eip_191(
         address=eth_address, message=data_to_sign, signature=signature
     )
@@ -80,7 +80,7 @@ def test_transacting_power_sign_message(testerchain, accounts):
 
     # Sign
     data_to_sign = b"Premium Select Luxury Pencil Holder"
-    signature = power.sign_message(message=data_to_sign)
+    _message_hash, signature = power.sign_message(message=data_to_sign)
 
     # Verify
     is_verified = verify_eip_191(
