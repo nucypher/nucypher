718fea61fcf1aac678a3ed7aea011e356937215a
derekpierre <derek.pierre@gmail.com>
2025-05-29 20:07:19 -0400

    Use a test contract to verify message hash calculations for PackedUserOperation. Create associated test.

---
+++ tests/acceptance/contracts/EntryPoint.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+
+contract EntryPoint is EIP712 {
+
+    string constant internal DOMAIN_NAME = "ERC4337";
+    string constant internal DOMAIN_VERSION = "1";
+
+    bytes32 internal constant PACKED_USEROP_TYPEHASH = keccak256(
+        "PackedUserOperation(address sender,uint256 nonce,bytes initCode,bytes callData,bytes32 accountGasLimits,uint256 preVerificationGas,bytes32 gasFees,bytes paymasterAndData)"
+    );
+
+    struct PackedUserOperation {
+        address sender;
+        uint256 nonce;
+        bytes initCode;
+        bytes callData;
+        bytes32 accountGasLimits;
+        uint256 preVerificationGas;
+        bytes32 gasFees;
+        bytes paymasterAndData;
+        bytes signature;
+    }
+
+    constructor() EIP712(DOMAIN_NAME, DOMAIN_VERSION) {
+    }
+
+    function encode(
+        PackedUserOperation calldata userOp
+    ) internal pure returns (bytes memory ret) {
+        address sender = userOp.sender;
+        uint256 nonce = userOp.nonce;
+        bytes32 hashInitCode = keccak256(userOp.initCode);
+        bytes32 hashCallData = keccak256(userOp.callData);
+        bytes32 accountGasLimits = userOp.accountGasLimits;
+        uint256 preVerificationGas = userOp.preVerificationGas;
+        bytes32 gasFees = userOp.gasFees;
+        bytes32 hashPaymasterAndData = keccak256(userOp.paymasterAndData);
+
+        return abi.encode(
+            PACKED_USEROP_TYPEHASH,
+            sender, nonce,
+            hashInitCode, hashCallData,
+            accountGasLimits, preVerificationGas, gasFees,
+            hashPaymasterAndData
+        );
+    }
+
+    function getUserOpHashV8(
+        PackedUserOperation calldata userOp
+    ) public view returns (bytes32) {
+        bytes memory encodedOp = encode(userOp);
+        return _hashTypedDataV4(keccak256(encode(userOp)));
+    }
+}
