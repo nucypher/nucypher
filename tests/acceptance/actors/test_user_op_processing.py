ace603d27441e3fb2ac3ee2e795691a8927a2dcb
derekpierre <derek.pierre@gmail.com>
2025-06-09 21:04:59 -0400

    Add tests for checking packing of factory and factory data into initCode.

--- tests/acceptance/actors/test_user_op_processing.py
+++ tests/acceptance/actors/test_user_op_processing.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 from eth_account import Account
 
+from nucypher.blockchain.eth.constants import NULL_ADDRESS
 from nucypher.utilities.erc4337_utils import (
     AAVersion,
     PackedUserOperation,
@@ -14,11 +17,13 @@ def transactor(initiator):
 
 
 @pytest.fixture(scope="module")
-def user_op(accounts):
+def user_op(accounts, get_random_checksum_address):
     user_op = UserOperation(
         sender=accounts[0].address,
         nonce=0,
-        call_data=b"deadbeef",
+        factory=get_random_checksum_address(),
+        factory_data=os.urandom(23),
+        call_data=os.urandom(32),
         verification_gas_limit=100000,
         # these should all be unique values (for proper testing)
         call_gas_limit=100000,
@@ -133,3 +138,37 @@ def test_packed_user_operation_paymaster_and_data_packing(
         == user_op.paymaster_post_op_gas_limit
     )
     assert aa_entry_point.paymasterData(packed_user_op_dict) == user_op.paymaster_data
+
+
+def test_packed_user_operation_init_code_packing(
+    accounts, chain, user_op, aa_entry_point
+):
+    # retrieved individual factory values packed into factory should match
+    packed_user_op = PackedUserOperation.from_user_operation(user_op)
+    packed_user_op_dict = packed_user_op.to_eip712_struct(
+        AAVersion.V08, chain.chain_id
+    )["message"]
+    packed_user_op_dict["signature"] = b""
+    assert aa_entry_point.factory(packed_user_op_dict) == user_op.factory
+    assert aa_entry_point.factoryData(packed_user_op_dict) == user_op.factory_data
+
+    # factory with no data
+    user_op.factory_data = b""
+    packed_user_op = PackedUserOperation.from_user_operation(user_op)
+    packed_user_op_dict = packed_user_op.to_eip712_struct(
+        AAVersion.V08, chain.chain_id
+    )["message"]
+    packed_user_op_dict["signature"] = b""
+    assert aa_entry_point.factory(packed_user_op_dict) == user_op.factory
+    assert aa_entry_point.factoryData(packed_user_op_dict) == b""
+
+    # retry with empty values
+    user_op.factory = None
+    user_op.factory_data = b""
+    packed_user_op = PackedUserOperation.from_user_operation(user_op)
+    packed_user_op_dict = packed_user_op.to_eip712_struct(
+        AAVersion.V08, chain.chain_id
+    )["message"]
+    packed_user_op_dict["signature"] = b""
+    assert aa_entry_point.factory(packed_user_op_dict) == NULL_ADDRESS
+    assert aa_entry_point.factoryData(packed_user_op_dict) == b""
