a568ef3c157b66b6f3f199843eb302412abd5349
derekpierre <derek.pierre@gmail.com>
2025-06-13 16:51:00 -0400

    Update tests to prevent having competing settings - mainly the certs file - now that tests are run in parallel.

--- tests/integration/network/test_static_files.py
+++ tests/integration/network/test_static_files.py
@@ -1,6 +1,4 @@
 import os
-import tempfile
-from pathlib import Path
 
 import pytest_twisted
 import requests
@@ -11,51 +9,61 @@ from tests.utils.ursula import make_reserved_ursulas
 
 
 @pytest_twisted.inlineCallbacks
-def test_ursula_serves_statics(ursula_test_config, accounts):
-    with tempfile.TemporaryDirectory() as STATICS_DIR:
-        os.environ['NUCYPHER_STATIC_FILES_ROOT'] = str(STATICS_DIR)
+def test_ursula_serves_statics(ursula_test_config, accounts, mocker, temp_dir_path):
+    original_getenv = os.getenv
 
-        node = make_reserved_ursulas(
-            accounts=accounts,
-            ursula_config=ursula_test_config,
-            quantity=1,
-        ).pop()
-        node_deployer = node.get_deployer()
+    # mock the environment variable to point to the temporary directory
+    def mock_getenv(key, default=None):
+        if key == "NUCYPHER_STATIC_FILES_ROOT":
+            return str(temp_dir_path)
+        return original_getenv(key, default)
 
-        node_deployer.addServices()
-        node_deployer.catalogServers(node_deployer.hendrix)
-        node_deployer.start()
+    mocker.patch("os.getenv", side_effect=mock_getenv)
 
-        cert = node_deployer.cert.to_cryptography()
-        cert_bytes = cert.public_bytes(serialization.Encoding.PEM)
+    node = make_reserved_ursulas(
+        accounts=accounts,
+        ursula_config=ursula_test_config,
+        quantity=1,
+    ).pop()
+    node_deployer = node.get_deployer()
 
-        def check_static_service(node, cert_file):
+    node_deployer.addServices()
+    node_deployer.catalogServers(node_deployer.hendrix)
+    node_deployer.start()
 
-            response = requests.get(
-                "https://{}/statics/test-never-make-a-file-with-this-name.js".format(node.rest_url()),
-                verify=cert_file
-            )
-            assert response.status_code == 200
-            assert "I am Javascript" in response.text
-            return node
+    cert = node_deployer.cert.to_cryptography()
+    cert_bytes = cert.public_bytes(serialization.Encoding.PEM)
 
-        def check_static_file_not_there(node, cert_file):
+    def check_static_service(node, cert_file):
 
-            response = requests.get(
-                "https://{}/statics/no-file-by-this-name.js".format(node.rest_url()),
-                verify=cert_file
-            )
-            assert response.status_code == 404
-            return node
+        response = requests.get(
+            "https://{}/statics/test-never-make-a-file-with-this-name.js".format(
+                node.rest_url()
+            ),
+            verify=cert_file,
+        )
+        assert response.status_code == 200
+        assert "I am Javascript" in response.text
+        return node
 
-        try:
-            with open("test-cert", "wb") as f:
-                f.write(cert_bytes)
-            Path(STATICS_DIR).mkdir(exist_ok=True)
-            with open(Path(STATICS_DIR, 'test-never-make-a-file-with-this-name.js'), 'w+') as fout:
-                fout.write("console.log('I am Javascript')\n")
-                fout.close()
-            yield threads.deferToThread(check_static_service, node, "test-cert")
-            yield threads.deferToThread(check_static_file_not_there, node, "test-cert")
-        finally:
-            Path("test-cert").unlink()
+    def check_static_file_not_there(node, cert_file):
+
+        response = requests.get(
+            "https://{}/statics/no-file-by-this-name.js".format(node.rest_url()),
+            verify=cert_file,
+        )
+        assert response.status_code == 404
+        return node
+
+    cert_file = temp_dir_path / "test-cert"
+
+    with open(cert_file, "wb") as f:
+        f.write(cert_bytes)
+
+    temp_dir_path.mkdir(exist_ok=True)
+    with open(temp_dir_path / "test-never-make-a-file-with-this-name.js", "w+") as fout:
+        fout.write("console.log('I am Javascript')\n")
+        fout.close()
+
+    yield threads.deferToThread(check_static_service, node, cert_file)
+    yield threads.deferToThread(check_static_file_not_there, node, cert_file)
