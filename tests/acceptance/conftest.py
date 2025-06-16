c62cfbaa4ff5be3c793c287e5f8082747e0cf265
derekpierre <derek.pierre@gmail.com>
2025-06-16 10:42:51 -0400

    Handle ape fixtures better - allow them to be session scoped, but set to autouse so they get loaded earlier.

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -85,8 +85,7 @@ def initiator(testerchain, alice, ritual_token, deployer_account):
 # Contracts Dependencies
 #
 
-# module-scoped on purpose for parallelized tests
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session", autouse=True)
 def nucypher_dependency(project):
     nucypher_contracts_dependency_api = project.dependencies["nucypher-contracts"]
     # simply use first entry - could be from github ('main') or local ('local')
@@ -94,8 +93,7 @@ def nucypher_dependency(project):
     return nucypher_dependency
 
 
-# module-scoped on purpose for parallelized tests
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session", autouse=True)
 def oz_dependency(project):
     _oz_dependency = project.dependencies["openzeppelin"]["5.0.0"]
     return _oz_dependency
